91122a835feeb396c01e82b81b471c303f5a0682
Yifeng Wang <doodlewind@qq.com>
2022-09-11 06:38:01 +0800

    feat: add lint setup

--- packages/blocks/src/index.ts
+++ packages/blocks/src/index.ts
@@ -1,3 +1,9 @@
-export function hello() {
-  return 0;
+import { LitElement, html } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+@customElement('block-counter')
+export class CounterBlock extends LitElement {
+  render() {
+    return html` <div>0</div> `;
+  }
 }
