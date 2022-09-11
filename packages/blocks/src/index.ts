1384785936a269fab7c93967b83a1058bceea709
Yifeng Wang <doodlewind@qq.com>
2022-09-11 08:10:59 +0800

    feat: add lit-react wrapper setup

--- packages/blocks/src/index.ts
+++ packages/blocks/src/index.ts
@@ -1,9 +1,30 @@
 import { LitElement, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 
-@customElement('block-counter')
+@customElement('counter-block')
 export class CounterBlock extends LitElement {
+  @property()
+  count: number;
+
+  constructor() {
+    super();
+    this.count = 0;
+  }
+
+  private async _notify() {
+    this.count++;
+
+    // https://lit.dev/docs/components/events/#dispatching-events-after-an-element-updates
+    await this.updateComplete;
+    const options: CustomEventInit<number> = {
+      detail: this.count,
+      bubbles: true,
+      composed: true,
+    };
+    this.dispatchEvent(new CustomEvent('block-count-update', options));
+  }
+
   render() {
-    return html` <div>0</div> `;
+    return html` <div @click=${this._notify}>${this.count}</div> `;
   }
 }
--- packages/playground/src/playground-app.tsx
+++ packages/playground/src/playground-app.tsx
@@ -1,8 +1,24 @@
-import { useState } from 'react';
-import { hello } from '@building-blocks/blocks';
+import React, { useCallback } from 'react';
+import { CounterBlock } from '@building-blocks/blocks';
+import { createComponent, type EventName } from '@lit-labs/react';
+
+const CounterBlockComponent = createComponent(
+  React,
+  'counter-block',
+  CounterBlock,
+  {
+    onUpdate: 'block-count-update' as EventName<CustomEvent<number>>,
+  }
+);
 
 export function App() {
-  const [count, setCount] = useState(hello());
+  const onUpdate = useCallback((e: CustomEvent<number>) => {
+    console.log('new count', e.detail);
+  }, []);
 
-  return <div onClick={() => setCount(count + 1)}>{count}</div>;
+  return (
+    <div>
+      <CounterBlockComponent onUpdate={onUpdate} />
+    </div>
+  );
 }
