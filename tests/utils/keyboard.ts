12859757e5fa9b633b4a10a3aa1be43ba7ca157f
Yifeng Wang <doodlewind@qq.com>
2022-09-15 21:05:43 +0800

    fix: redo hotkey test

--- tests/utils/keyboard.ts
+++ tests/utils/keyboard.ts
@@ -27,15 +27,9 @@ export async function keyboardUndo(page: Page) {
 }
 
 export async function keyboardRedo(page: Page) {
-  if (IS_MAC) {
-    await page.keyboard.down('Meta');
-    await page.keyboard.down('Shift');
-    await page.keyboard.press('z');
-    await page.keyboard.up('Shift');
-    await page.keyboard.up('Meta');
-  } else {
-    await page.keyboard.down('Control');
-    await page.keyboard.press('y');
-    await page.keyboard.up('Control');
-  }
+  await keyDownCtrlOrMeta(page);
+  await page.keyboard.down('Shift');
+  await page.keyboard.press('z');
+  await page.keyboard.up('Shift');
+  await keyDownCtrlOrMeta(page);
 }
