7c0d937dea1266b8550e945d92cd37060c17e27c
Yifeng Wang <doodlewind@qq.com>
2022-09-18 16:33:40 +0800

    refactor: lift store observer up to container

--- packages/blocks/src/page-block/page-block.ts
+++ packages/blocks/src/page-block/page-block.ts
@@ -1,8 +1,8 @@
 import { LitElement, html } from 'lit';
-import { customElement, property, state, query } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 import { repeat } from 'lit/directives/repeat.js';
 import { Store } from '@building-blocks/framework';
-import { TextBlockModel, ITextBlockModel } from '../text-block/text-block';
+import { TextBlockModel } from '../text-block/text-block';
 import { BaseBlockModel } from '@building-blocks/framework/src/model/base';
 export * from '@building-blocks/framework/src/managers/selection';
 
@@ -20,134 +20,22 @@ export class PageBlockElement extends LitElement {
   @property()
   store!: Store;
 
-  @state()
-  model = new PageBlockModel(this.store);
-
-  @state()
-  connectionBtnText = 'Disconnect';
-
-  @state()
-  isEmptyPage = true;
-
-  @state()
-  canUndo = false;
-
-  @state()
-  canRedo = false;
-
-  @query('.block-placeholder-input')
-  private _placeholderInput!: HTMLInputElement;
+  @property({
+    hasChanged() {
+      return true;
+    },
+  })
+  model!: PageBlockModel;
 
   // disable shadow DOM to workaround quill
   createRenderRoot() {
     return this;
   }
 
-  constructor() {
-    super();
-  }
-
-  update(changedProperties: Map<string | number | symbol, unknown>) {
-    super.update(changedProperties);
-
-    if (changedProperties.has('store')) {
-      this._subscribeStore();
-    }
-  }
-
-  private _subscribeStore() {
-    // if undo to empty page, reset to empty placeholder
-    this.store.slots.update.on(() => {
-      this.isEmptyPage = this.store.isEmpty;
-    });
-
-    this.store.slots.historyUpdate.on(() => {
-      this.canUndo = this.store.canUndo;
-      this.canRedo = this.store.canRedo;
-    });
-
-    this.store.slots.addBlock.on(blockProps => {
-      const block = new TextBlockModel(
-        this.store,
-        blockProps as ITextBlockModel
-      );
-      if (!this.model.children.find(child => child.id === block.id)) {
-        this.model.children.push(block);
-      }
-
-      this.requestUpdate();
-    });
-
-    this.store.slots.deleteBlock.on(id => {
-      const index = this.model.children.findIndex(child => child.id === id);
-      if (index !== -1) {
-        this.model.children.splice(index, 1);
-      }
-
-      this.isEmptyPage = this.model.children.length === 0;
-      this.requestUpdate();
-    });
-  }
-
-  private _onVoidStateUpdate(e: MouseEvent | KeyboardEvent) {
-    e.preventDefault();
-
-    if (this.isEmptyPage) {
-      this.isEmptyPage = false;
-
-      const blockProps: ITextBlockModel = {
-        flavour: 'text',
-        id: this.store.createId(),
-        text: '',
-      };
-      this.store.addBlock(blockProps);
-    }
-  }
-
-  firstUpdated() {
-    this._placeholderInput.focus();
-  }
-
-  private _onToggleConnection() {
-    if (this.connectionBtnText === 'Disconnect') {
-      this.store.provider.disconnect();
-      this.connectionBtnText = 'Connect';
-    } else {
-      this.store.provider.connect();
-      this.connectionBtnText = 'Disconnect';
-    }
-  }
-
-  protected render() {
+  render() {
     this.setAttribute('data-block-id', this.model.id);
 
-    const emptyPagePlaceholder = html`
-      <style>
-        .block-placeholder {
-          box-sizing: border-box;
-        }
-        .block-placeholder-input {
-          display: block;
-          box-sizing: border-box;
-          margin-bottom: 12px;
-          padding: 6px;
-          width: 100%;
-          line-height: 1.4;
-          border: 1px solid rgb(204, 204, 204);
-          border-radius: 0;
-          outline: none;
-        }
-      </style>
-      <div
-        @click=${this._onVoidStateUpdate}
-        @keydown=${this._onVoidStateUpdate}
-        class="block-placeholder"
-      >
-        <input class="block-placeholder-input" />
-      </div>
-    `;
-
-    const blockContent = html`
+    const childBlocks = html`
       ${repeat(
         this.model.children,
         child => child.id,
@@ -162,25 +50,7 @@ export class PageBlockElement extends LitElement {
       )}
     `;
 
-    const buttons = html`
-      <div style="margin-bottom: 10px">
-        <button .disabled=${!this.canUndo} @click=${() => this.store.undo()}>
-          Undo
-        </button>
-        <button .disabled=${!this.canRedo} @click=${() => this.store.redo()}>
-          Redo
-        </button>
-        <button @click=${this._onToggleConnection}>
-          ${this.connectionBtnText}
-        </button>
-      </div>
-    `;
-
-    return html`
-      <div class="page-container" data-block-id=${this.model.id}>
-        ${[buttons, this.isEmptyPage ? emptyPagePlaceholder : blockContent]}
-      </div>
-    `;
+    return html` <div class="page-container">${childBlocks}</div> `;
   }
 }
 
--- packages/framework/src/components/containers/paper-container.ts
+++ packages/framework/src/components/containers/paper-container.ts
@@ -1,8 +1,14 @@
 import { LitElement, html } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement, state, query } from 'lit/decorators.js';
 import { Store, SelectionManager, MouseManager } from '../..';
 import { noop } from '../../model/utils/utils';
 import { RichText } from '../rich-text/rich-text';
+// FIXME circular deps
+import {
+  ITextBlockModel,
+  TextBlockModel,
+  PageBlockModel,
+} from '../../../../blocks';
 
 // avoid being tree-shaked
 noop(RichText);
@@ -12,27 +18,157 @@ const room =
 
 @customElement('paper-container')
 export class PaperContainer extends LitElement {
-  @property()
+  @state()
   store = new Store(room);
 
-  @property()
+  @state()
+  model = new PageBlockModel(this.store);
+
+  @state()
   mouse = new MouseManager(this.addEventListener.bind(this));
 
-  @property()
-  selectionManager = new SelectionManager(this);
+  @state()
+  selection = new SelectionManager(this);
 
-  // disable shadow DOM
-  createRenderRoot() {
-    return this;
-  }
+  @state()
+  connectionBtnText = 'Disconnect';
+
+  @state()
+  isEmptyPage = true;
+
+  @state()
+  canUndo = false;
+
+  @state()
+  canRedo = false;
+
+  @query('.block-placeholder-input')
+  private _placeholderInput!: HTMLInputElement;
 
   constructor() {
     super();
+    this._subscribeStore();
+
     // @ts-ignore
     window.store = this.store;
   }
 
-  protected render() {
+  private _subscribeStore() {
+    // if undo to empty page, reset to empty placeholder
+    this.store.slots.update.on(() => {
+      this.isEmptyPage = this.store.isEmpty;
+    });
+
+    this.store.slots.historyUpdate.on(() => {
+      this.canUndo = this.store.canUndo;
+      this.canRedo = this.store.canRedo;
+    });
+
+    this.store.slots.addBlock.on(blockProps => {
+      const block = new TextBlockModel(
+        this.store,
+        blockProps as ITextBlockModel
+      );
+      if (!this.model.children.find(child => child.id === block.id)) {
+        this.model.children.push(block);
+      }
+
+      this.requestUpdate();
+    });
+
+    this.store.slots.deleteBlock.on(id => {
+      const index = this.model.children.findIndex(child => child.id === id);
+      if (index !== -1) {
+        this.model.children.splice(index, 1);
+      }
+
+      this.isEmptyPage = this.model.children.length === 0;
+      this.requestUpdate();
+    });
+  }
+
+  private _onVoidStateUpdate(e: MouseEvent | KeyboardEvent) {
+    e.preventDefault();
+
+    if (this.isEmptyPage) {
+      this.isEmptyPage = false;
+
+      const blockProps: ITextBlockModel = {
+        flavour: 'text',
+        id: this.store.createId(),
+        text: '',
+      };
+      this.store.addBlock(blockProps);
+    }
+  }
+
+  private _onToggleConnection() {
+    if (this.connectionBtnText === 'Disconnect') {
+      this.store.provider.disconnect();
+      this.connectionBtnText = 'Connect';
+    } else {
+      this.store.provider.connect();
+      this.connectionBtnText = 'Disconnect';
+    }
+  }
+
+  // disable shadow DOM to workaround quill
+  createRenderRoot() {
+    return this;
+  }
+
+  firstUpdated() {
+    this._placeholderInput.focus();
+  }
+
+  render() {
+    const emptyPagePlaceholder = html`
+      <style>
+        .block-placeholder {
+          box-sizing: border-box;
+        }
+        .block-placeholder-input {
+          display: block;
+          box-sizing: border-box;
+          margin-bottom: 12px;
+          padding: 6px;
+          width: 100%;
+          line-height: 1.4;
+          border: 1px solid rgb(204, 204, 204);
+          border-radius: 0;
+          outline: none;
+        }
+      </style>
+      <div
+        @click=${this._onVoidStateUpdate}
+        @keydown=${this._onVoidStateUpdate}
+        class="block-placeholder"
+      >
+        <input class="block-placeholder-input" />
+      </div>
+    `;
+
+    const buttons = html`
+      <div style="margin-bottom: 10px">
+        <button .disabled=${!this.canUndo} @click=${() => this.store.undo()}>
+          Undo
+        </button>
+        <button .disabled=${!this.canRedo} @click=${() => this.store.redo()}>
+          Redo
+        </button>
+        <button @click=${this._onToggleConnection}>
+          ${this.connectionBtnText}
+        </button>
+      </div>
+    `;
+
+    const blockRoot = html`
+      <page-block-element
+        .model=${this.model}
+        .store=${this.store}
+      ></page-block-element>
+    `;
+
     return html`
       <style>
         .paper-container {
@@ -41,7 +177,7 @@ export class PaperContainer extends LitElement {
       </style>
       <div class="paper-container">
         <selection-rect .mouse=${this.mouse}></selection-rect>
-        <page-block-element .store=${this.store}></page-block-element>
+        ${[buttons, this.isEmptyPage ? emptyPagePlaceholder : blockRoot]}
       </div>
     `;
   }
