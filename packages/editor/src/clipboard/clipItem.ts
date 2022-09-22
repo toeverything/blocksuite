2c47dd018f51287632cfef49a8687fc666af54af
xiaodong zuo <zuoxd02@gmail.com>
2022-09-22 17:59:23 +0800

    clipboard feature

---
+++ packages/editor/src/clipboard/clipItem.ts
@@ -0,0 +1,11 @@
+export class ClipItem {
+    constructor(
+        public readonly mimeType: string, 
+        public readonly data: string
+    ) {
+    }
+
+    public hasData() {
+        return this.data !== null && this.data !== undefined;
+    }
+}
