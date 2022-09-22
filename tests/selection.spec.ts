90205be10856f9381bb149bc270fdc26240f9f57
MingLiang Wang <mingliangwang0o0@gmail.com>
2022-09-22 15:38:57 +0800

    feat: remove console

--- tests/selection.spec.ts
+++ tests/selection.spec.ts
@@ -18,7 +18,6 @@ test(' select blocks ', async ({ page }) => {
     const lastTextBox = textBoxes[textBoxes.length - 1];
     const { left: x1, top: y1 } = firstTextBox.getBoundingClientRect();
     const { left: x2, bottom: y2 } = lastTextBox.getBoundingClientRect();
-    console.log(lastTextBox, textBoxes, lastTextBox.getBoundingClientRect());
     return [
       { x: Math.floor(x1) - 5, y: Math.floor(y1) - 5 },
       { x: Math.floor(x2) + 40, y: Math.floor(y2) - 10 },
