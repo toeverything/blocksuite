8c8e73f6e1d29d28c8a636785fb5055303cfd8a4
lawvs <18554747+lawvs@users.noreply.github.com>
2022-10-08 17:51:18 +0800

    test: update indent snapshot

--- tests/indent.spec.ts
+++ tests/indent.spec.ts
@@ -6,7 +6,7 @@ import {
   pressEnter,
   shiftTab,
 } from './utils/actions';
-import { assertBlockChildrenIds, assertRichTexts } from './utils/asserts';
+import { assertStoreMatchSnapshot } from './utils/asserts';
 
 const indent = (page: Page) => page.keyboard.press('Tab');
 const unindent = shiftTab;
@@ -19,17 +19,60 @@ test('basic indent and unindent', async ({ page }) => {
   await pressEnter(page);
   await page.keyboard.type('text2');
 
-  indent(page);
-  await assertRichTexts(page, ['text1', 'text2']);
-  await assertBlockChildrenIds(page, '0', ['1']);
-  await assertBlockChildrenIds(page, '1', ['2']);
-  await assertBlockChildrenIds(page, '2', []);
-
+  await assertStoreMatchSnapshot(
+    page,
+    `<page>
+  <group
+    prop:xywh="[0,0,0,0]"
+  >
+    <paragraph
+      prop:text="text1"
+      prop:type="text"
+    />
+    <paragraph
+      prop:text="text2"
+      prop:type="text"
+    />
+  </group>
+</page>`
+  );
+  await indent(page);
+  await assertStoreMatchSnapshot(
+    page,
+    `<page>
+  <group
+    prop:xywh="[0,0,0,0]"
+  >
+    <paragraph
+      prop:text="text1"
+      prop:type="text"
+    >
+      <paragraph
+        prop:text="text2"
+        prop:type="text"
+      />
+    </paragraph>
+  </group>
+</page>`
+  );
   await unindent(page);
-  await assertRichTexts(page, ['text1', 'text2']);
-  await assertBlockChildrenIds(page, '0', ['1', '2']);
-  await assertBlockChildrenIds(page, '1', []);
-  await assertBlockChildrenIds(page, '2', []);
+  await assertStoreMatchSnapshot(
+    page,
+    `<page>
+  <group
+    prop:xywh="[0,0,0,0]"
+  >
+    <paragraph
+      prop:text="text1"
+      prop:type="text"
+    />
+    <paragraph
+      prop:text="text2"
+      prop:type="text"
+    />
+  </group>
+</page>`
+  );
 });
 
 test('indent with multiple children', async ({ page }) => {
@@ -41,13 +84,50 @@ test('indent with multiple children', async ({ page }) => {
   await page.keyboard.type('text2');
   await pressEnter(page);
   await page.keyboard.type('text3');
-  await assertRichTexts(page, ['text1', 'text2', 'text3']);
-
+  await assertStoreMatchSnapshot(
+    page,
+    `<page>
+  <group
+    prop:xywh="[0,0,0,0]"
+  >
+    <paragraph
+      prop:text="text1"
+      prop:type="text"
+    />
+    <paragraph
+      prop:text="text2"
+      prop:type="text"
+    />
+    <paragraph
+      prop:text="text3"
+      prop:type="text"
+    />
+  </group>
+</page>`
+  );
   indent(page);
   await focusLine(page, 2);
   indent(page);
-
-  await assertRichTexts(page, ['text1', 'text2', 'text3']);
-  await assertBlockChildrenIds(page, '0', ['1']);
-  await assertBlockChildrenIds(page, '1', ['2', '3']);
+  await assertStoreMatchSnapshot(
+    page,
+    `<page>
+  <group
+    prop:xywh="[0,0,0,0]"
+  >
+    <paragraph
+      prop:text="text1"
+      prop:type="text"
+    >
+      <paragraph
+        prop:text="text2"
+        prop:type="text"
+      >
+    </paragraph>
+    <paragraph
+      prop:text="text3"
+      prop:type="text"
+    />
+  </group>
+</page>`
+  );
 });
