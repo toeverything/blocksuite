20e517de947cc0206f913d15a86151c7cfc47e23
Whitewater <me@waterwater.moe>
2022-10-11 19:43:41 +0800

    Fix/import path incorrect (#40)
    * fix: import path
    
    * chore: update deps
    
    * fix: vitest env
    
    * Revert "fix: vitest env"
    
    This reverts commit 91528e85b036f92e8faf3d733757912d7d3965d7.
    
    * fix: vitest without dom env
    
    Co-authored-by: Yifeng Wang <doodlewind@qq.com>

--- packages/editor/src/block-loader.ts
+++ packages/editor/src/block-loader.ts
@@ -1,10 +1,10 @@
 // Use manual per-module import/export to support vitest environment on Node.js
-import { PageBlockModel } from '../../blocks/src/page-block/page-model';
-import { ParagraphBlockModel } from '../../blocks/src/paragraph-block/paragraph-model';
-import { ListBlockModel } from '../../blocks/src/list-block/list-model';
-import { GroupBlockModel } from '../../blocks/src/group-block/group-model';
+import { PageBlockModel } from '@blocksuite/blocks/src/page-block/page-model';
+import { ParagraphBlockModel } from '@blocksuite/blocks/src/paragraph-block/paragraph-model';
+import { ListBlockModel } from '@blocksuite/blocks/src/list-block/list-model';
+import { GroupBlockModel } from '@blocksuite/blocks/src/group-block/group-model';
 
-export type { ParagraphBlockProps as TextBlockProps } from '../../blocks';
+export type { ParagraphBlockProps as TextBlockProps } from '@blocksuite/blocks/src/paragraph-block/paragraph-model';
 
 // TODO support dynamic register
 export const BlockSchema = {
