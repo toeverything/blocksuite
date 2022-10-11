a2335ed81ea62a53d55984c83d467578a80f65df
lawvs <18554747+lawvs@users.noreply.github.com>
2022-10-11 12:46:10 +0800

    chore: update import path

--- packages/editor/src/block-loader.ts
+++ packages/editor/src/block-loader.ts
@@ -1,10 +1,10 @@
 // Use manual per-module import/export to support vitest environment on Node.js
-import { PageBlockModel } from '../../blocks/src/page-block/page-model';
-import { ParagraphBlockModel } from '../../blocks/src/paragraph-block/paragraph-model';
-import { ListBlockModel } from '../../blocks/src/list-block/list-model';
-import { GroupBlockModel } from '../../blocks/src/group-block/group-model';
+import { PageBlockModel } from '@blocksuite/blocks';
+import { ParagraphBlockModel } from '@blocksuite/blocks';
+import { ListBlockModel } from '@blocksuite/blocks';
+import { GroupBlockModel } from '@blocksuite/blocks';
 
-export type { ParagraphBlockProps as TextBlockProps } from '../../blocks';
+export type { ParagraphBlockProps as TextBlockProps } from '@blocksuite/blocks';
 
 // TODO support dynamic register
 export const BlockSchema = {
