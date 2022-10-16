3632df15ee9f75dabdaeb2b4c531179d3a3d9ecf
Yifeng Wang <doodlewind@qq.com>
2022-10-17 04:01:45 +0800

    feat: test single block select all & delete

--- tests/hotkey.spec.ts
+++ tests/hotkey.spec.ts
@@ -9,15 +9,27 @@ import {
   strikethrough,
   undoByKeyboard,
   redoByKeyboard,
+  pressEnter,
 } from './utils/actions';
-import { assertSelection, assertTextFormat } from './utils/asserts';
+import {
+  assertRichTexts,
+  assertSelection,
+  assertTextFormat,
+} from './utils/asserts';
 
 test('rich-text hotkey scope on single press', async ({ page }) => {
   await enterPlaygroundRoom(page);
   await focusRichText(page);
   await page.keyboard.type('hello');
+  await pressEnter(page);
+  await page.keyboard.type('world');
+  await assertRichTexts(page, ['hello', 'world']);
+
   await selectAllByKeyboard(page); // first select all in rich text
-  await assertSelection(page, 0, 0, 5);
+  await assertSelection(page, 1, 0, 5);
+
+  await page.keyboard.press('Backspace');
+  await assertRichTexts(page, ['hello', '\n']);
 });
 
 test('rich-text code-inline hotkey', async ({ page }) => {
