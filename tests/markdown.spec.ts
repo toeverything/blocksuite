6f113db0c4c6feab935b3fa2bd54fe53036ec2ff
Yifeng Wang <doodlewind@qq.com>
2022-10-17 04:07:19 +0800

    fix: flaky test

--- tests/markdown.spec.ts
+++ tests/markdown.spec.ts
@@ -17,7 +17,7 @@ test('markdown shortcut', async ({ page }) => {
   await assertBlockType(page, id, 'todo');
 
   await undoByClick(page);
-  await page.keyboard.type('[ ] ');
+  await page.keyboard.type('[ ] ', { delay: 50 });
   [id] = await getCursorBlockIdAndHeight(page);
   await assertBlockType(page, id, 'todo');
 
