52db81910e6b14032bc4cef510af8250c7ddfe2c
lawvs <18554747+lawvs@users.noreply.github.com>
2022-10-24 10:04:49 +0800

    fix: type check

--- packages/playground/src/main.ts
+++ packages/playground/src/main.ts
@@ -1,7 +1,7 @@
 import '@blocksuite/blocks';
 import '@blocksuite/editor';
 import { createEditor } from '@blocksuite/editor';
-import { DebugProvider } from '@blocksuite/store/src/providers';
+import { DebugProvider } from '@blocksuite/store';
 import './style.css';
 
 const params = new URLSearchParams(location.search);
