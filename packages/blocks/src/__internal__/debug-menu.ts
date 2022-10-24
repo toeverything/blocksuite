a7c2f8a4fa3fb3a43d8d724384b54d33b23ba3d2
Yifeng Wang <doodlewind@qq.com>
2022-10-24 15:55:55 +0800

    docs: optimize README desc & debug menu ui

--- packages/blocks/src/__internal__/debug-menu.ts
+++ packages/blocks/src/__internal__/debug-menu.ts
@@ -29,7 +29,7 @@ const icons = {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 448 512"
-      style="width: 12px;"
+      style="width: 11px;"
     >
       <path
         d="M0 216C0 149.7 53.7 96 120 96h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V320 288 216zm256 0c0-66.3 53.7-120 120-120h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H320c-35.3 0-64-28.7-64-64V320 288 216z"
@@ -333,6 +333,14 @@ export class DebugMenu extends LitElement {
           𝐓
         </button>
         <button
+          aria-label="quote"
+          title="quote"
+          tabindex="-1"
+          @click=${() => this._onSetParagraphType('quote')}
+        >
+          ${icons.quote}
+        </button>
+        <button
           aria-label="convert to bulleted list"
           title="convert to bulleted list"
           tabindex="-1"
@@ -355,14 +363,6 @@ export class DebugMenu extends LitElement {
           @click=${() => this._convertToList('todo')}
         >
           ${icons.checklist}
-        </button>
-        <button
-          aria-label="quote"
-          title="quote"
-          tabindex="-1"
-          @click=${() => this._onSetParagraphType('quote')}
-        >
-          ${icons.quote}
         </button>
         <!--
         <button
