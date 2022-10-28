107ef32742cee94de67344aa2e171e0fb793adcb
MingLiang Wang <mingliangwang0o0@gmail.com>
2022-10-28 12:25:06 +0800

    feat: add double click choose word (#139)
    * feat: repair context menu change range
    
    * feat: add double click choose word

--- packages/blocks/src/page-block/edgeless/selection-manager.ts
+++ packages/blocks/src/page-block/edgeless/selection-manager.ts
@@ -127,7 +127,8 @@ export class EdgelessSelectionManager {
       this._onContainerClick,
       this._onContainerDblClick,
       this._onContainerMouseMove,
-      this._onContainerMouseOut
+      this._onContainerMouseOut,
+      this._onContainerContextMenu
     );
     this._wheelDisposeCallback = initWheelEventHandlers(container);
   }
@@ -233,6 +234,25 @@ export class EdgelessSelectionManager {
 
   private _onContainerMouseOut = (e: SelectionEvent) => {
     noop();
+  };
+
+  private _onContainerContextMenu = (e: SelectionEvent) => {
+    const currentRange = window.getSelection()?.getRangeAt(0);
+    const pointRange = caretRangeFromPoint(e.x, e.y);
+    // repair browser context menu change selection can not go through blocks
+    if (
+      currentRange &&
+      pointRange &&
+      currentRange.isPointInRange(
+        pointRange.startContainer,
+        pointRange.startOffset
+      ) &&
+      currentRange.isPointInRange(pointRange.endContainer, pointRange.endOffset)
+    ) {
+      requestAnimationFrame(() => {
+        resetNativeSelection(currentRange);
+      });
+    }
   };
 
   dispose() {
