6c5fdde686c7bf47a3dda72d4c413f6c1cba4e76
lawvs <18554747+lawvs@users.noreply.github.com>
2022-11-01 11:52:31 +0800

    chore: update unit test

--- packages/store/src/utils/jsx.spec.ts
+++ packages/store/src/utils/jsx.spec.ts
@@ -1,12 +1,12 @@
 // checkout https://vitest.dev/guide/debugging.html for debugging tests
 
 import { describe, expect, it } from 'vitest';
-import { blockRecordToJSXNode } from './jsx';
+import { yDocToJSXNode } from './jsx';
 
 describe('basic', () => {
-  it('doc record match snapshot', () => {
+  it('serialized doc match snapshot', () => {
     expect(
-      blockRecordToJSXNode(
+      yDocToJSXNode(
         {
           '0': {
             'sys:id': '0',
@@ -17,7 +17,7 @@ describe('basic', () => {
             'sys:id': '1',
             'sys:children': [],
             'sys:flavour': 'paragraph',
-            'prop:text': '',
+            'prop:text': [],
             'prop:type': 'text',
           },
         },
@@ -26,9 +26,84 @@ describe('basic', () => {
     ).toMatchInlineSnapshot(`
       <page>
         <paragraph
-          prop:text=""
           prop:type="text"
         />
+      </page>
+    `);
+  });
+
+  it('doc record match snapshot', () => {
+    expect(
+      yDocToJSXNode(
+        {
+          '0': {
+            'sys:id': '0',
+            'sys:flavour': 'page',
+            'sys:children': ['1'],
+            'prop:title': 'this is title',
+          },
+          '1': {
+            'sys:id': '1',
+            'sys:flavour': 'group',
+            'sys:children': ['2'],
+            'prop:xywh': '[0,0,720,32]',
+          },
+          '2': {
+            'sys:id': '2',
+            'sys:flavour': 'paragraph',
+            'sys:children': [],
+            'prop:type': 'text',
+            'prop:text': [
+              { insert: 'this is ' },
+              {
+                insert: 'a ',
+                attributes: { link: 'http://www.example.com' },
+              },
+              {
+                insert: 'link',
+                attributes: { link: 'http://www.example.com', bold: true },
+              },
+              { insert: ' with', attributes: { bold: true } },
+              { insert: ' bold' },
+            ],
+          },
+        },
+        '0'
+      )
+    ).toMatchInlineSnapshot(`
+      <page
+        prop:title="this is title"
+      >
+        <group
+          prop:xywh="[0,0,720,32]"
+        >
+          <paragraph
+            prop:text={
+              <>
+                <text
+                  insert="this is "
+                />
+                <text
+                  insert="a "
+                  link="http://www.example.com"
+                />
+                <text
+                  bold={true}
+                  insert="link"
+                  link="http://www.example.com"
+                />
+                <text
+                  bold={true}
+                  insert=" with"
+                />
+                <text
+                  insert=" bold"
+                />
+              </>
+            }
+            prop:type="text"
+          />
+        </group>
       </page>
     `);
   });
