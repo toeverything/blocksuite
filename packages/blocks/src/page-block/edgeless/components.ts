a357c70c732d187978b6b22aa7299d8d7a1bf73d
MingLiang Wang <mingliangwang0o0@gmail.com>
2022-11-09 17:06:35 +0800

    feat: drag resize handler in edgeless

--- packages/blocks/src/page-block/edgeless/components.ts
+++ packages/blocks/src/page-block/edgeless/components.ts
@@ -1,17 +1,18 @@
-import { html, type TemplateResult } from 'lit';
+import { html, LitElement, PropertyValueMap, type TemplateResult } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
 import { styleMap } from 'lit/directives/style-map.js';
 import { repeat } from 'lit/directives/repeat.js';
 import type { BaseBlockModel } from '@blocksuite/store';
 
 import type { GroupBlockModel } from '../..';
 import type {
-  EdgelessSelectionManager,
+  BlockSelectionState,
   ViewportState,
   XYWH,
 } from './selection-manager';
-import { BlockElement, BlockHost } from '../../__internal__';
+import { BlockElement, BlockHost, getBlockById } from '../../__internal__';
 import '../../__internal__';
-import { PADDING_X, PADDING_Y } from './utils';
+import { PADDING_X, PADDING_Y, GROUP_MIN_LENGTH } from './utils';
 
 function getCommonRectStyle(rect: DOMRect, zoom: number) {
   return {
@@ -39,55 +40,32 @@ export function EdgelessHoverRect(rect: DOMRect | null, zoom: number) {
   `;
 }
 
-function Handle(centerX: number, centerY: number) {
+function Handle(
+  centerX: number,
+  centerY: number,
+  handleType: 'left' | 'right',
+  onMouseDown?: (e: MouseEvent, type: 'left' | 'right') => void
+) {
   const style = {
     position: 'absolute',
     left: centerX - 6 + 'px',
     top: centerY - 6 + 'px',
     width: '12px',
     height: '12px',
+    boxSizing: 'border-box',
     borderRadius: '6px',
     zIndex: '10',
     border: '2px var(--affine-primary-color) solid',
     background: 'white',
-    cursor: 'not-allowed',
+    cursor: 'ew-resize',
   };
 
-  return html` <div style=${styleMap(style)}></div> `;
-}
-
-export function EdgelessSelectedRect(
-  selection: EdgelessSelectionManager,
-  zoom: number
-) {
-  const state = selection.blockSelectionState;
-  const { type } = state;
-  if (type === 'none') return html``;
-
-  const { rect } = state;
-  const style = {
-    border: `${state.active ? 2 : 1}px solid var(--affine-primary-color)`,
-    ...getCommonRectStyle(rect, zoom),
+  const handlerMouseDown = (e: MouseEvent) => {
+    onMouseDown && onMouseDown(e, handleType);
   };
-
-  let handles: TemplateResult | null = null;
-  if (!state.active) {
-    const leftCenter = [
-      rect.x,
-      rect.y + rect.height / 2 + (PADDING_Y * zoom) / 2,
-    ];
-    const rightCenter = [
-      rect.x + rect.width + PADDING_X * zoom,
-      rect.y + rect.height / 2 + (PADDING_Y * zoom) / 2,
-    ];
-    const handleLeft = Handle(leftCenter[0], leftCenter[1]);
-    const handleRight = Handle(rightCenter[0], rightCenter[1]);
-    handles = html` ${handleLeft}${handleRight} `;
-  }
 
   return html`
-    ${handles}
-    <div class="affine-edgeless-selected-rect" style=${styleMap(style)}></div>
+    <div style=${styleMap(style)} @mousedown=${handlerMouseDown}></div>
   `;
 }
 
@@ -187,4 +165,145 @@ export function EdgelessBlockChildrenContainer(
       )}
     </div>
   `;
+}
+
+@customElement('edgeless-selected-rect')
+export class EdgelessSelectedRect extends LitElement {
+  @property({ type: Number })
+  zoom!: number;
+
+  @property({ type: Object })
+  state!: BlockSelectionState;
+
+  @property({ type: Object })
+  rect!: DOMRect;
+
+  private _dragStartInfo = {
+    startMouseLeft: 0,
+    absoluteLeft: 0,
+    width: 0,
+    direction: 'left',
+  };
+
+  private getHandles(rect: DOMRect) {
+    let handles: TemplateResult | null = null;
+    if (this.state.type === 'none') return handles;
+    if (!this.state.active) {
+      const leftCenter = [
+        rect.x,
+        rect.y + rect.height / 2 + (PADDING_Y * this.zoom) / 2,
+      ];
+      const rightCenter = [
+        rect.x + rect.width + PADDING_X * this.zoom,
+        rect.y + rect.height / 2 + (PADDING_Y * this.zoom) / 2,
+      ];
+      const handleLeft = Handle(
+        leftCenter[0],
+        leftCenter[1],
+        'left',
+        this._onHandleMouseDown
+      );
+      const handleRight = Handle(
+        rightCenter[0],
+        rightCenter[1],
+        'right',
+        this._onHandleMouseDown
+      );
+      handles = html` ${handleLeft}${handleRight} `;
+    }
+    return handles;
+  }
+
+  private _onHandleMouseDown = (e: MouseEvent, type: 'left' | 'right') => {
+    // prevent selection action being fired
+    e.stopPropagation();
+    if (this.state?.type !== 'none') {
+      const {
+        rect,
+        selected: { xywh },
+      } = this.state;
+      const [x] = JSON.parse(xywh) as XYWH;
+      this._dragStartInfo = {
+        startMouseLeft: e.clientX,
+        absoluteLeft: x,
+        // the width of the selected group may 0 after init use rect.width instead
+        width: rect.width,
+        direction: type,
+      };
+      // parent ele is the edgeless block container
+      this.parentElement?.addEventListener('mousemove', this._onDrag);
+      this.parentElement?.addEventListener('mouseup', this._onDragEnd);
+    }
+  };
+
+  private _onDrag = (e: MouseEvent) => {
+    let newX = 0;
+    let newW = 0;
+    if (this.state.type !== 'none') {
+      const {
+        selected,
+        selected: { space, xywh },
+      } = this.state;
+      const [x, y, w, h] = JSON.parse(xywh) as XYWH;
+      const minus = this._dragStartInfo.startMouseLeft - e.clientX;
+      if (this._dragStartInfo.direction === 'left') {
+        newX = this._dragStartInfo.absoluteLeft - minus / this.zoom;
+        newW = (this._dragStartInfo.width + minus) / this.zoom;
+      } else {
+        newX = x;
+        newW = (this._dragStartInfo.width - minus) / this.zoom;
+      }
+      // limit the width of the selected group
+      if (newW < GROUP_MIN_LENGTH) {
+        newW = GROUP_MIN_LENGTH;
+        newX = x;
+      }
+      // if xywh do not change, no need to update
+      if (newW === w && newX === x) {
+        return;
+      }
+      const groupBlock = getBlockById<'div'>(selected.id);
+      const groupContainer = groupBlock?.parentElement;
+      // first change container`s x/w directly for get groups real height
+      if (groupContainer) {
+        groupContainer.style.width = newW + 'px';
+        groupContainer.style.translate = `translate(${newX}px, ${y}px) scale(${this.zoom})`;
+      }
+      // refresh xywh by model
+      space.updateBlock(selected, {
+        xywh: JSON.stringify([
+          newX,
+          y,
+          newW,
+          (groupBlock?.getBoundingClientRect().height || 0) / this.zoom,
+        ]),
+      });
+    }
+  };
+
+  private _onDragEnd = (e: MouseEvent) => {
+    this.parentElement?.removeEventListener('mousemove', this._onDrag);
+    this.parentElement?.removeEventListener('mouseup', this._onDragEnd);
+  };
+
+  render() {
+    if (this.state.type === 'none') return html``;
+    const style = {
+      border: `${
+        this.state.active ? 2 : 1
+      }px solid var(--affine-primary-color)`,
+      ...getCommonRectStyle(this.rect, this.zoom),
+    };
+
+    return html`
+      ${this.getHandles(this.rect)}
+      <div class="affine-edgeless-selected-rect" style=${styleMap(style)}></div>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'edgeless-selected-rect': EdgelessSelectedRect;
+  }
 }
