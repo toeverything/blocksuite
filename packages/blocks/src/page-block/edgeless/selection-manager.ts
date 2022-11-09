bf220ea4e580b9de9c380a8bf267009f4fbd2058
lawvs <18554747+lawvs@users.noreply.github.com>
2022-11-09 18:29:51 +0800

    chore: integrate selection

--- packages/blocks/src/page-block/edgeless/selection-manager.ts
+++ packages/blocks/src/page-block/edgeless/selection-manager.ts
@@ -14,6 +14,7 @@ import {
   getNativeSelectionMouseDragInfo,
   repairContextMenuRange,
 } from '../utils/cursor';
+import { showFormatQuickBar } from '../../components/format-quick-bar';
 
 interface NoneBlockSelectionState {
   type: 'none';
@@ -301,8 +302,14 @@ export class EdgelessSelectionManager {
   };
 
   private _onContainerDragEnd = (e: SelectionEvent) => {
-    if ( this.blockSelectionState.type === 'single' && this.blockSelectionState.active ) {
-      getNativeSelectionMouseDragInfo(e);
+    if (
+      this.blockSelectionState.type === 'single' &&
+      this.blockSelectionState.active
+    ) {
+      const { anchor } = getNativeSelectionMouseDragInfo(e);
+      if (anchor) {
+        showFormatQuickBar({ anchorEl: anchor });
+      }
     }
     this._frameSelectionState = null;
   };
