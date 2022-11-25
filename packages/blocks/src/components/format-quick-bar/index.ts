989a8c63f8c7c843fac7881f40d0020feddf0869
lawvs <18554747+lawvs@users.noreply.github.com>
2022-11-25 19:56:01 +0800

    feat: support convert type

--- packages/blocks/src/components/format-quick-bar/index.ts
+++ packages/blocks/src/components/format-quick-bar/index.ts
@@ -3,7 +3,11 @@ import { html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { handleFormat } from '../../page-block/utils';
 import { createLink } from '../../__internal__/rich-text/link-node';
-import { getCurrentRange, getModelsByRange } from '../../__internal__/utils';
+import {
+  convertToList,
+  getCurrentRange,
+  getModelsByRange,
+} from '../../__internal__/utils';
 import { toast } from '../toast';
 import './button';
 import {
@@ -24,6 +28,7 @@ import {
   NumberedIcon,
   QuoteIcon,
   StrikethroughIcon,
+  TextIcon,
   TodoIcon,
   UnderlineIcon,
 } from './icons';
@@ -50,19 +55,27 @@ const restoreSelection = (range: Range) => {
 };
 
 const paragraphButtons = [
-  { key: 'H1', name: 'Heading 1', icon: H1Icon },
-  { key: 'H2', name: 'Heading 2', icon: H2Icon },
-  { key: 'H3', name: 'Heading 3', icon: H3Icon },
-  { key: 'H4', name: 'Heading 4', icon: H4Icon },
-  { key: 'H5', name: 'Heading 5', icon: H5Icon },
-  { key: 'H6', name: 'Heading 6', icon: H6Icon },
-  { key: 'BulletedList', name: 'Bulleted List', icon: BulletedListIcon },
-  { key: 'Numbered', name: 'Numbered List', icon: NumberedIcon },
-  { key: 'Todo', name: 'To-do List', icon: TodoIcon },
-  { key: 'Code', name: 'Code Block', icon: CodeIcon },
-  { key: 'Quote', name: 'Quote', icon: QuoteIcon },
-  { key: 'Callout', name: 'Callout', icon: CalloutIcon },
+  {
+    key: 'text',
+    name: 'Text',
+    icon: TextIcon,
+  },
+  { key: 'h1', name: 'Heading 1', icon: H1Icon },
+  { key: 'h2', name: 'Heading 2', icon: H2Icon },
+  { key: 'h3', name: 'Heading 3', icon: H3Icon },
+  { key: 'h4', name: 'Heading 4', icon: H4Icon },
+  { key: 'h5', name: 'Heading 5', icon: H5Icon },
+  { key: 'h6', name: 'Heading 6', icon: H6Icon },
+  { key: 'bulleted', name: 'Bulleted List', icon: BulletedListIcon },
+  { key: 'numbered', name: 'Numbered List', icon: NumberedIcon },
+  { key: 'todo', name: 'To-do List', icon: TodoIcon },
+  { key: 'code', name: 'Code Block', icon: CodeIcon },
+  { key: 'quote', name: 'Quote', icon: QuoteIcon },
+  { key: 'callout', name: 'Callout', icon: CalloutIcon },
 ];
+
+const isListType = (type: string): type is 'bulleted' | 'numbered' | 'todo' =>
+  ['bulleted', 'numbered', 'todo'].includes(type);
 
 const formatButtons = [
   {
@@ -133,6 +146,9 @@ export class FormatQuickBar extends LitElement {
   @property()
   space: Space | null = null;
 
+  @property()
+  paragraphType = 'text';
+
   override connectedCallback(): void {
     super.connectedCallback();
     // TODO handle multiple selection
@@ -142,23 +158,39 @@ export class FormatQuickBar extends LitElement {
       return;
     }
     const startModel = models[0];
+    this.paragraphType = startModel.type;
     this.space = startModel.space;
     if (models.length > 1) {
       // Select multiple models
     }
+
+    console.log(models);
   }
 
   private paragraphPanelTemplate() {
-    const PARAGRAPH_PANEL_OFFSET = /* format bar height */ 40 + /* margin */ 4;
     return html`<div
       class="paragraph-panel"
       style="left: 0; top: calc(100% + 4px)"
     >
       ${paragraphButtons.map(
-        ({ name, icon }) => html`<format-bar-button
+        ({ key, name, icon }) => html`<format-bar-button
           width="100%"
           style="padding-left: 12px; justify-content: flex-start;"
           text="${name}"
+          @click=${() => {
+            this.models
+              .filter(i => i.type !== key)
+              .forEach(model => {
+                if (!this.space) {
+                  throw new Error('Space is not defined');
+                }
+                if (isListType(key)) {
+                  convertToList(this.space, model, key, '');
+                } else {
+                  this.space.updateBlock(model, { type: key });
+                }
+              });
+          }}
         >
           ${icon}
         </format-bar-button>`
