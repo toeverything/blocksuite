2e81e7259e0a22614af5310f5039aa6a44a82274
himself65 <himself65@outlook.com>
2022-12-06 01:48:25 -0600

    feat: add d

--- packages/blocks/src/pencil-block/pencil-model.ts
+++ packages/blocks/src/pencil-block/pencil-model.ts
@@ -5,6 +5,10 @@ export type PencilColor = 'black';
 export interface PencilBlockProps extends IBaseBlockProps {
   flavor: 'affine:pencil';
   color: PencilColor;
+
+  // Refs: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d
+  d: string;
+  xywh: string;
 }
 
 export class PencilBlockModel extends BaseBlockModel {}
