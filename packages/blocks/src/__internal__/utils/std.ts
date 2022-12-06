ab741835a394f614ee40176ae5286ebc26951dd1
Whitewater <me@waterwater.moe>
2022-12-06 09:26:49 -0800

    fix: format quick bar position (#267)
    * feat: add throttle
    
    * feat: add resize observer to prevent format bar from going outside the window

--- packages/blocks/src/__internal__/utils/std.ts
+++ packages/blocks/src/__internal__/utils/std.ts
@@ -43,3 +43,50 @@ export function noop() {
  */
 export const sleep = (ms = 0) =>
   new Promise(resolve => setTimeout(resolve, ms));
+
+/**
+ * @example
+ * ```ts
+ * const log = (message: string) => console.log(`[${new Date().toISOString()}] ${message}`);
+ *
+ * const throttledLog = throttle(log, 1000);
+ *
+ * throttledLog("Hello, world!");
+ * throttledLog("Hello, world!");
+ * throttledLog("Hello, world!");
+ * throttledLog("Hello, world!");
+ * throttledLog("Hello, world!");
+ * ```
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any -- The any is safe, because it in the generic type T
+export const throttle = <T extends (...args: any[]) => void>(
+  fn: T,
+  limit: number,
+  { leading = true, trailing = true } = {}
+): T => {
+  let timer: ReturnType<typeof setTimeout> | null = null;
+  let lastArgs: Parameters<T> | null = null;
+
+  const setTimer = () => {
+    if (lastArgs && trailing) {
+      fn.apply(this, lastArgs);
+      lastArgs = null;
+      timer = setTimeout(setTimer, limit);
+    } else {
+      timer = null;
+    }
+  };
+
+  return function (this: unknown, ...args: Parameters<T>) {
+    if (timer) {
+      // in throttle
+      lastArgs = args;
+      return;
+    }
+    // Execute the function on the leading edge
+    if (leading) {
+      fn.apply(this, args);
+    }
+    timer = setTimeout(setTimer, limit);
+  } as T;
+};
