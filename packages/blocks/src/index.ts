0f91fffeab40f0dcdbf274a8e0534f71a1b2833d
lawvs <18554747+lawvs@users.noreply.github.com>
2022-12-13 11:29:00 +0800

    chore: reduce `@ts-ignore`
    See https://github.com/toeverything/blocksuite/pull/277
    
    Co-authored-by: Himself65 <himself65@outlook.com>

--- packages/blocks/src/index.ts
+++ packages/blocks/src/index.ts
@@ -23,7 +23,7 @@ export * from './__internal__/utils/common-operations';
 export * from './__internal__/utils/std';
 export * from './__internal__/utils/query';
 
-const env =
+const env: Record<string, unknown> =
   typeof globalThis !== 'undefined'
     ? globalThis
     : typeof window !== 'undefined'
@@ -33,12 +33,11 @@ const env =
     : {};
 const importIdentifier = '__ $BLOCKSUITE_BLOCKS$ __';
 
-// @ts-ignore
 if (env[importIdentifier] === true) {
   // https://github.com/yjs/yjs/issues/438
   console.error(
     '@blocksuite/blocks was already imported. This breaks constructor checks and will lead to issues!'
   );
 }
-// @ts-ignore
+
 env[importIdentifier] = true;
