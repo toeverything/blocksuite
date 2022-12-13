68048063c06cdd09fdd3385340752d0bb6d9e351
himself65 <himself65@outlook.com>
2022-12-12 19:55:51 -0600

    fix: move mouse mode to top

--- packages/editor/src/components/editor-container/editor-container.ts
+++ packages/editor/src/components/editor-container/editor-container.ts
@@ -4,7 +4,7 @@ import { choose } from 'lit/directives/choose.js';
 
 import type { Page, Disposable } from '@blocksuite/store';
 import { ClipboardManager, ContentParser } from '../..';
-import type { PageBlockModel } from '@blocksuite/blocks';
+import type { MouseMode, PageBlockModel } from '@blocksuite/blocks';
 
 @customElement('editor-container')
 export class EditorContainer extends LitElement {
@@ -13,6 +13,9 @@ export class EditorContainer extends LitElement {
 
   @property()
   mode?: 'page' | 'edgeless' = 'page';
+
+  @property()
+  mouseMode: MouseMode = 'default';
 
   // TODO only select block
   @state()
@@ -42,12 +45,21 @@ export class EditorContainer extends LitElement {
     return this;
   }
 
+  private _handleSwitchMouseMode = ({ detail }: CustomEvent<MouseMode>) => {
+    this.mouseMode = detail;
+  };
+
   override connectedCallback() {
     super.connectedCallback();
 
     if (!this.page) {
       throw new Error('Missing page for EditorContainer!');
     }
+
+    window.addEventListener(
+      'affine.switch-mouse-mode',
+      this._handleSwitchMouseMode
+    );
 
     this._subscribeStore();
 
@@ -57,6 +69,10 @@ export class EditorContainer extends LitElement {
   override disconnectedCallback() {
     super.disconnectedCallback();
 
+    window.removeEventListener(
+      'affine.switch-mouse-mode',
+      this._handleSwitchMouseMode
+    );
     this._disposables.forEach(disposable => disposable.dispose());
   }
 
@@ -76,6 +92,7 @@ export class EditorContainer extends LitElement {
         .mouseRoot=${this as HTMLElement}
         .page=${this.page}
         .model=${this.model}
+        .mouseMode=${this.mouseMode}
       ></edgeless-page-block>
     `;
 
