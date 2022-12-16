89893e2336d1cd8788529c3bd74e799601ef8fcd
Whitewater <me@waterwater.moe>
2022-12-16 15:24:27 +0800

    fix: Inefficient regular expression

--- packages/blocks/src/components/link-popover/link-popover.ts
+++ packages/blocks/src/components/link-popover/link-popover.ts
@@ -20,8 +20,50 @@ const MAIL_REGEX =
   /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 // For more detail see https://stackoverflow.com/questions/8667070/javascript-regular-expression-to-validate-url
-const URL_REGEX =
-  /^(?:(?:(?:https?|s?ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+
+// For more detail see https://stackoverflow.com/questions/8667070/javascript-regular-expression-to-validate-url
+const URL_REGEX = new RegExp(
+  '^' +
+    // protocol identifier (optional)
+    // short syntax // still required
+    '(?:(?:(?:https?|ftp):)?\\/\\/)' +
+    // user:pass BasicAuth (optional)
+    '(?:\\S+(?::\\S*)?@)?' +
+    '(?:' +
+    // IP address exclusion
+    // private & local networks
+    '(?!(?:10|127)(?:\\.\\d{1,3}){3})' +
+    '(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})' +
+    '(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})' +
+    // IP address dotted notation octets
+    // excludes loopback network 0.0.0.0
+    // excludes reserved space >= 224.0.0.0
+    // excludes network & broadcast addresses
+    // (first & last IP address of each class)
+    '(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])' +
+    '(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}' +
+    '(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))' +
+    '|' +
+    // host & domain names, may end with dot
+    // can be replaced by a shortest alternative
+    // (?![-_])(?:[-\\w\\u00a1-\\uffff]{0,63}[^-_]\\.)+
+    '(?:' +
+    '(?:' +
+    '[a-z0-9\\u00a1-\\uffff]' +
+    '[a-z0-9\\u00a1-\\uffff_-]{0,62}' +
+    ')?' +
+    '[a-z0-9\\u00a1-\\uffff]\\.' +
+    ')+' +
+    // TLD identifier name, may end with dot
+    '(?:[a-z\\u00a1-\\uffff]{2,}\\.?)' +
+    ')' +
+    // port number (optional)
+    '(?::\\d{2,5})?' +
+    // resource path (optional)
+    '(?:[/?#]\\S*)?' +
+    '$',
+  'i'
+);
 
 function normalizeUrl(url: string) {
   const hasScheme = ALLOWED_SCHEMES.some(scheme =>
