8a0e6917a78e7d256350c4e9a8a3dc188f6cef1d
Yifeng Wang <doodlewind@toeverything.info>
2022-12-17 14:39:14 +0800

    fix: online playground signaling server config

--- packages/store/src/doc-providers.ts
+++ packages/store/src/doc-providers.ts
@@ -27,19 +27,25 @@ export interface DocProviderConstructor {
   ): DocProvider;
 }
 
+// When using playground from blocksuite repo, this comes from "serve" script in "@blocksuite/store" package.
+// We use our own sync server because a local service for sync makes everything much faster for dev.
+const LOCAL_SIGNALING = ['ws://localhost:4444'];
+
+const DEFAULT_SIGNALING = [
+  // Default config from yjs (but these are kinda slow by comparison to self host sync ~100ms-+1000ms latency observed).
+  // This slowness is also avoided in order to improve test reliability in CI.
+  'wss://y-webrtc-signaling-us.herokuapp.com',
+  'wss://y-webrtc-signaling-eu.herokuapp.com',
+];
+
+const isLocalhost = window.location.hostname === 'localhost';
+const signaling = isLocalhost ? LOCAL_SIGNALING : DEFAULT_SIGNALING;
+
 export class DebugDocProvider extends WebrtcProvider implements DocProvider {
   constructor(room: string, doc: Y.Doc, options?: { awareness?: Awareness }) {
     super(room, doc, {
       awareness: options?.awareness ?? null,
-      signaling: [
-        // When using playground from blocksuite repo, this comes from "serve" script in "@blocksuite/store" package.
-        // We use our own sync server because a local service for sync makes everything much faster for dev.
-        'ws://localhost:4444',
-        // // Default config from yjs (but these are kinda slow by comparison to self host sync ~100ms-+1000ms latency observed).
-        // // This slowness is also avoided in order to improve test reliability in CI.
-        // 'wss://y-webrtc-signaling-us.herokuapp.com',
-        // 'wss://y-webrtc-signaling-eu.herokuapp.com',
-      ],
+      signaling,
       // YJS has broken default types. 🥲
       ...({} as {
         // calling each one of these null to make TypeScript happy, because YJS is wrong.
