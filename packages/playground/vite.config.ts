4dec2c030301645a05ade9385b3a41a5e33cd845
himself65 <himself65@outlook.com>
2022-12-18 13:38:57 -0600

    fix: build

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@blocksuite/shared': resolve(__dirname, '..', 'shared', 'src'),
+    },
+  },
   plugins: [react()],
   // enable if want debug with jwst backend
   // server: {
