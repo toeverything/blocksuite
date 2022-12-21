f458ff90afddbff8b341da53ee239030d97324e9
Austaras <austaras@outlook.com>
2022-12-21 14:41:35 +0800

    fix: do not use base64url (#403)

--- packages/store/src/blob/utils.ts
+++ packages/store/src/blob/utils.ts
@@ -6,7 +6,7 @@ export async function sha(input: ArrayBuffer): Promise<string> {
   const hash = await crypto.subtle.digest('SHA-256', input);
   const buffer = Buffer.from(hash);
 
-  return buffer.toString('base64url');
+  return buffer.toString('base64').replace(/\+/g, '-').replace(/\//g, '_');
 }
 
 export function getDatabase<T = ArrayBufferLike>(
