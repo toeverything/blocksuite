eacd63d62e1f7caa7bee43795e18f6899d10b5c4
Whitewater <me@waterwater.moe>
2022-12-21 19:47:45 -0800

    test: upload trace for failed test (#409)

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,6 +7,12 @@ const config: PlaywrightTestConfig = {
     browserName: 'chromium',
     viewport: { width: 900, height: 600 },
     actionTimeout: 5 * 1000,
+    // Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer
+    // You can open traces locally(`npx playwright show-trace trace.zip`)
+    // or in your browser on [Playwright Trace Viewer](https://trace.playwright.dev/).
+    trace: 'on-first-retry',
+    // Record video only when retrying a test for the first time.
+    video: 'on-first-retry',
   },
   workers: '100%',
   retries: 2,
