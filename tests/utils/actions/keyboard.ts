bc6229ef57df3f079d2f1a78ca5e52c27598e3df
himself65 <himself65@outlook.com>
2022-12-23 09:36:48 +0800

    fix: test

--- tests/utils/actions/keyboard.ts
+++ tests/utils/actions/keyboard.ts
@@ -151,14 +151,24 @@ export async function copyByKeyboard(page: Page) {
 }
 
 export async function cutByKeyboard(page: Page) {
-  await page.click('editor-container');
+  const doesEditorActive = page.evaluate(() =>
+    document.activeElement?.closest('editor-container')
+  );
+  if (!doesEditorActive) {
+    await page.click('editor-container');
+  }
   await keyDownCtrlOrMeta(page);
   await page.keyboard.press('x', { delay: 50 });
   await keyUpCtrlOrMeta(page);
 }
 
 export async function pasteByKeyboard(page: Page) {
-  await page.click('editor-container');
+  const doesEditorActive = page.evaluate(() =>
+    document.activeElement?.closest('editor-container')
+  );
+  if (!doesEditorActive) {
+    await page.click('editor-container');
+  }
   await keyDownCtrlOrMeta(page);
   await page.keyboard.press('v', { delay: 50 });
   await keyUpCtrlOrMeta(page);
