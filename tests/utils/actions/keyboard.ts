dc2a37c9480871c2ef1031598c0cd5ff944fbe01
himself65 <himself65@outlook.com>
2022-12-23 09:17:37 +0800

    test: click editor before copy

--- tests/utils/actions/keyboard.ts
+++ tests/utils/actions/keyboard.ts
@@ -145,6 +145,7 @@ export async function fillLine(page: Page, toNext = false) {
 }
 
 export async function copyByKeyboard(page: Page) {
+  await page.click('editor-container');
   await keyDownCtrlOrMeta(page);
   await page.keyboard.press('c', { delay: 50 });
   await keyUpCtrlOrMeta(page);
