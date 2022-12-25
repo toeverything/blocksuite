7dbce10acfd2d5a9c109264a8e083c5a601295e8
himself65 <himself65@outlook.com>
2022-12-25 12:04:27 +0800

    fix: type

--- packages/blocks/src/models.ts
+++ packages/blocks/src/models.ts
@@ -7,7 +7,7 @@ import { ListBlockModel } from './list-block/list-model.js';
 import { PageBlockModel } from './page-block/page-model.js';
 import { ParagraphBlockModel } from './paragraph-block/paragraph-model.js';
 import { ShapeBlockModel } from './shape-block/shape-model.js';
-import { BaseBlockModel } from '@blocksuite/store';
+import type { BaseBlockModel } from '@blocksuite/store';
 
 export {
   CodeBlockModel,
