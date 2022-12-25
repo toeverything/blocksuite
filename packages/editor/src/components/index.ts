6e3fc08e8cb2bc1a36f3e3b42db9510ae439c740
himself65 <himself65@outlook.com>
2022-12-25 08:35:48 +0800

    fix: import js

--- packages/editor/src/components/index.ts
+++ packages/editor/src/components/index.ts
@@ -1,5 +1,5 @@
-import './editor-container/editor-container';
-import './debug-menu/debug-menu';
+import './editor-container/editor-container.js';
+import './debug-menu/debug-menu.js';
 
 export * from './editor-container/editor-container.js';
 export * from './debug-menu/debug-menu.js';
