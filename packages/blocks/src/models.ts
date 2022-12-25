485489229dc6acab9db944b4ccf4bc2a54b5ea9e
himself65 <himself65@outlook.com>
2022-12-25 11:06:24 +0800

    fix: export

--- packages/blocks/src/models.ts
+++ packages/blocks/src/models.ts
@@ -1,9 +1,20 @@
 // Import models only, the bundled file should not include anything else.
-export { CodeBlockModel } from './code-block/code-model.js';
-export { DividerBlockModel } from './divider-block/divider-model.js';
-export { EmbedBlockModel } from './embed-block/embed-model.js';
-export { GroupBlockModel } from './group-block/group-model.js';
-export { ListBlockModel } from './list-block/list-model.js';
-export { PageBlockModel } from './page-block/page-model.js';
-export { ParagraphBlockModel } from './paragraph-block/paragraph-model.js';
-export { ShapeBlockModel } from './shape-block/shape-model.js';
+import { CodeBlockModel } from './code-block/code-model.js';
+import { DividerBlockModel } from './divider-block/divider-model.js';
+import { EmbedBlockModel } from './embed-block/embed-model.js';
+import { GroupBlockModel } from './group-block/group-model.js';
+import { ListBlockModel } from './list-block/list-model.js';
+import { PageBlockModel } from './page-block/page-model.js';
+import { ParagraphBlockModel } from './paragraph-block/paragraph-model.js';
+import { ShapeBlockModel } from './shape-block/shape-model.js';
+
+export {
+  CodeBlockModel,
+  DividerBlockModel,
+  EmbedBlockModel,
+  GroupBlockModel,
+  ListBlockModel,
+  PageBlockModel,
+  ParagraphBlockModel,
+  ShapeBlockModel,
+};
