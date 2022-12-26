00001e94803491d1642e74d73f8eedd4f8514f03
himself65 <himself65@outlook.com>
2022-12-26 21:36:14 +0800

    feat: init yMap proxy

---
+++ packages/store/src/utils/proxy.ts
@@ -0,0 +1,59 @@
+import { Map as YMap } from 'yjs';
+
+export interface EnhancedYMap<
+  Data extends Record<string, unknown>,
+  Keys extends keyof Data & string = keyof Data & string
+> extends YMap<any> {
+  clone(): EnhancedYMap<Data, Keys>;
+
+  delete(key: Keys & string): void;
+
+  set<Key extends Keys>(key: Key, value: Data[Key]): Data[Key];
+
+  get<Key extends Keys>(key: Key): Data[Key];
+
+  has<Key extends Keys>(key: Key): boolean;
+
+  clear(): void;
+}
+
+export type ProxyConfig = {
+  allowSet?: boolean;
+};
+
+export function createYMapProxy<
+  Data extends Record<string, unknown>,
+  Keys extends keyof Data & string = keyof Data & string
+>(yMap: EnhancedYMap<Data, Keys>, config?: ProxyConfig): Data;
+export function createYMapProxy<
+  Data extends Record<string, unknown>,
+  Keys extends keyof Data & string = keyof Data & string
+>(
+  yMap: EnhancedYMap<Data, Keys>,
+  config: ProxyConfig = {
+    allowSet: false,
+  }
+): any {
+  const { allowSet = false } = config;
+  if (!(yMap instanceof YMap)) {
+    throw new TypeError();
+  }
+  return new Proxy(yMap, {
+    has: (target, p) => {
+      const has = Reflect.get(target, 'has');
+      return Reflect.apply(has, target, [p]);
+    },
+    // set: (target, p, value, receiver) => {
+    set: () => {
+      if (!allowSet) {
+        throw new Error('Modify data is not allowed');
+      } else {
+        throw new Error('Not Implemented');
+      }
+    },
+    get: (target, p, receiver) => {
+      const get = Reflect.get(target, 'get', receiver);
+      return Reflect.apply(get, target, [p]);
+    },
+  });
+}
