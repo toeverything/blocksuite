f11fd16791cede90230aeaedcc0025615b3353ca
x1a0t <thorseraq@gmail.com>
2022-12-29 15:43:57 +0800

    feat: exporting html, markdown support of code block

--- packages/blocks/src/code-block/code-model.ts
+++ packages/blocks/src/code-block/code-model.ts
@@ -1,5 +1,6 @@
 import { BaseBlockModel, IBaseBlockProps, Page } from '@blocksuite/store';
 import { literal } from 'lit/static-html.js';
+import { assertExists, getBlockById } from '../__internal__/index.js';
 
 export class CodeBlockModel extends BaseBlockModel implements IBaseBlockProps {
   static version = [1, 0] as [number, number];
@@ -20,4 +21,19 @@ export class CodeBlockModel extends BaseBlockModel implements IBaseBlockProps {
   }
 
   // TODO block2html
+
+  block2html(
+    childText: string,
+    _previousSiblingId: string,
+    _nextSiblingId: string,
+    begin?: number,
+    end?: number
+  ): string {
+    const codeBlockElement = getBlockById(this.id);
+    assertExists(codeBlockElement);
+    const codeElement = codeBlockElement.querySelector('pre');
+    assertExists(codeElement);
+    codeElement.setAttribute('code-lang', this.language);
+    return codeElement.outerHTML;
+  }
 }
