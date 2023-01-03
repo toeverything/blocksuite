f274021e1608960797491e3e70531be3638cbffe
lawvs <18554747+lawvs@users.noreply.github.com>
2023-01-04 01:34:46 +0800

    refactor: simplify format hotkey

--- packages/blocks/src/page-block/utils/bind-hotkey.ts
+++ packages/blocks/src/page-block/utils/bind-hotkey.ts
@@ -1,28 +1,33 @@
+import {
+  formatConfig,
+  paragraphConfig,
+} from '../../components/format-quick-bar/config.js';
 import type { Page } from '@blocksuite/store';
 import { hotkey, HOTKEYS } from '../../__internal__/index.js';
-import { createLink } from '../../__internal__/rich-text/link-node/index.js';
-import { handleFormat } from './container-operations.js';
-const { UNDO, REDO, INLINE_CODE, STRIKE, LINK } = HOTKEYS;
+import { updateSelectedTextType } from './container-operations.js';
+
+const { UNDO, REDO } = HOTKEYS;
 
 export function bindCommonHotkey(page: Page) {
-  hotkey.addListener(INLINE_CODE, e => {
-    // workaround page title
-    e.preventDefault();
-    if (e.target instanceof HTMLTextAreaElement) return;
-    handleFormat(page, 'code');
+  formatConfig.forEach(({ hotkey: hotkeyStr, action }) => {
+    hotkey.addListener(hotkeyStr, e => {
+      // workaround page title
+      e.preventDefault();
+      // TODO also disable hotkey when focus on other input
+      if (e.target instanceof HTMLTextAreaElement) return;
+      action({ page });
+    });
   });
-  hotkey.addListener(STRIKE, e => {
-    // workaround page title
-    e.preventDefault();
-    if (e.target instanceof HTMLTextAreaElement) return;
-    handleFormat(page, 'strike');
+
+  paragraphConfig.forEach(({ flavour, type, hotkey: hotkeyStr }) => {
+    if (!hotkeyStr) {
+      return;
+    }
+    hotkey.addListener(hotkeyStr, () => {
+      updateSelectedTextType(flavour, type, page);
+    });
   });
-  hotkey.addListener(LINK, e => {
-    // Prevent conflict with browser's search hotkey at windows
-    e.preventDefault();
-    if (e.target instanceof HTMLTextAreaElement) return;
-    createLink(page);
-  });
+
   hotkey.addListener(UNDO, () => page.undo());
   hotkey.addListener(REDO, () => page.redo());
   // !!!
@@ -30,5 +35,12 @@ export function bindCommonHotkey(page: Page) {
 }
 
 export function removeCommonHotKey() {
-  hotkey.removeListener([UNDO, REDO, INLINE_CODE, STRIKE, LINK]);
+  hotkey.removeListener([
+    ...formatConfig.map(({ hotkey: hotkeyStr }) => hotkeyStr),
+    ...paragraphConfig
+      .map(({ hotkey: hotkeyStr }) => hotkeyStr)
+      .filter((i): i is string => !!i),
+    UNDO,
+    REDO,
+  ]);
 }
