148f9931ce47ff69d8db2302a747e7348305a1ed
Himself65 <himself65@outlook.com>
2023-01-06 14:31:22 +0800

    test: move drag handle (#560)

--- tests/drag.spec.ts
+++ tests/drag.spec.ts
@@ -4,6 +4,7 @@ import {
   initEmptyParagraphState,
   initThreeParagraphs,
 } from './utils/actions/index.js';
+import { assertRichTexts } from './utils/asserts.js';
 
 test('only have one drag handle in screen', async ({ page }) => {
   await enterPlaygroundRoom(page);
@@ -39,4 +40,35 @@ test('only have one drag handle in screen', async ({ page }) => {
     return handles.length;
   }, []);
   expect(length2).toBe(1);
+});
+
+test('move drag handle', async ({ page }) => {
+  await enterPlaygroundRoom(page);
+  await initEmptyParagraphState(page);
+  await initThreeParagraphs(page);
+  await assertRichTexts(page, ['123', '456', '789']);
+  const handle = await page.locator('affine-drag-handle').boundingBox();
+  const paragraph = await page.locator('[data-block-id="4"]').boundingBox();
+  if (!handle || !paragraph) {
+    throw new Error();
+  }
+  await page.mouse.click(
+    handle.x + handle.width / 2,
+    handle.y + handle.height / 2
+  );
+  await page.mouse.move(
+    handle.x + handle.width / 2,
+    handle.y + handle.height / 2
+  );
+  await page.mouse.down();
+  await page.mouse.move(
+    paragraph.x + paragraph.width / 2,
+    paragraph.y + paragraph.height - 1,
+    {
+      steps: 50,
+    }
+  );
+  await page.mouse.up();
+
+  await assertRichTexts(page, ['456', '789', '123']);
 });
