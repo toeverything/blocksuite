93a727e30862b45e60009b3b48285d3cd407d8f3
Himself65 <himself65@outlook.com>
2023-01-06 16:41:17 +0800

    fix(playground): split room in different version (#568)

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,5 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { execSync } from 'node:child_process';
+let DEFAULT_ROOM = '';
+// Refs: https://vercel.com/docs/concepts/projects/environment-variables#system-environment-variables
+if (process.env.VERCEL === '1') {
+  DEFAULT_ROOM = `${process.env.VERCEL_GIT_COMMIT_REF}-${process.env.VERCEL_GIT_COMMIT_SHA}`;
+} else if (!process.env.CI) {
+  try {
+    DEFAULT_ROOM = execSync('git rev-parse --short HEAD').toString();
+  } catch (_) {
+    // ignore error
+  }
+}
+
+process.env.VITE_DEFAULT_ROOM = DEFAULT_ROOM;
 
 // https://vitejs.dev/config/
 export default defineConfig({
