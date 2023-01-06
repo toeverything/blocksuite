deb4d70650436fcb96c75347a85903ee21780676
Yifeng Wang <doodlewind@toeverything.info>
2023-01-06 13:48:21 +0800

    refactor: migration sequence

--- packages/store/src/workspace/migrations.ts
+++ packages/store/src/workspace/migrations.ts
@@ -9,7 +9,43 @@ interface Migration {
   migrate: (doc: Y.Doc) => void;
 }
 
+// New migration should be added to the end of this list
 const migrations: Migration[] = [
+  {
+    desc: 'convert affine:group to affine:frame',
+    condition: doc => {
+      const yVersions = doc
+        .getMap('space:meta')
+        .get('versions') as Y.Map<number>;
+      if (!yVersions) return false;
+
+      return yVersions.get('affine:group') === 1;
+    },
+    migrate: doc => {
+      // @ts-ignore
+      const pageIds = doc
+        .getMap('space:meta')
+        .get('pages')
+        .map((a: Y.Map<unknown>) => a.get('id')) as string[];
+
+      for (const pageId of pageIds) {
+        const spaceId = `space:${pageId}`;
+        const yBlocks = doc.getMap(spaceId);
+        // @ts-ignore
+        yBlocks.forEach((yBlock: Y.Map<unknown>) => {
+          if (yBlock.get('sys:flavour') === 'affine:group') {
+            yBlock.set('sys:flavour', 'affine:frame');
+          }
+        });
+      }
+
+      const yVersions = doc
+        .getMap('space:meta')
+        .get('versions') as Y.Map<number>;
+      yVersions.delete('affine:group');
+      yVersions.set('affine:frame', 1);
+    },
+  },
   {
     desc: 'add affine:surface',
     condition: doc => {
@@ -48,41 +84,6 @@ const migrations: Migration[] = [
           }
         });
       }
-    },
-  },
-  {
-    desc: 'convert affine:group to affine:frame',
-    condition: doc => {
-      const yVersions = doc
-        .getMap('space:meta')
-        .get('versions') as Y.Map<number>;
-      if (!yVersions) return false;
-
-      return yVersions.get('affine:group') === 1;
-    },
-    migrate: doc => {
-      // @ts-ignore
-      const pageIds = doc
-        .getMap('space:meta')
-        .get('pages')
-        .map((a: Y.Map<unknown>) => a.get('id')) as string[];
-
-      for (const pageId of pageIds) {
-        const spaceId = `space:${pageId}`;
-        const yBlocks = doc.getMap(spaceId);
-        // @ts-ignore
-        yBlocks.forEach((yBlock: Y.Map<unknown>) => {
-          if (yBlock.get('sys:flavour') === 'affine:group') {
-            yBlock.set('sys:flavour', 'affine:frame');
-          }
-        });
-      }
-
-      const yVersions = doc
-        .getMap('space:meta')
-        .get('versions') as Y.Map<number>;
-      yVersions.delete('affine:group');
-      yVersions.set('affine:frame', 1);
     },
   },
 ];
