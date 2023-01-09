57835602463a3bf33e46bddd68686bbce52f8789
fi3ework <fi3ework@gmail.com>
2023-01-09 14:10:54 +0800

    feat(playground): add HMR for playground

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -2,10 +2,30 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import { fileURLToPath } from 'url';
+import { hmrPlugin, presets } from 'vite-plugin-web-components-hmr';
+
+const customLitPath = path.resolve(
+  __dirname,
+  '../blocks/src/__internal__/index.js'
+);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    hmrPlugin({
+      include: ['../blocks/src/**/*.ts'],
+      exclude: ['**/*/node_modules/**/*'],
+      presets: [presets.lit],
+      decorators: [{ name: 'customElement', import: 'lit/decorators.js' }],
+      baseClasses: [
+        {
+          name: 'NonShadowLitElement',
+          import: customLitPath,
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@blocksuite/blocks': path.resolve(
