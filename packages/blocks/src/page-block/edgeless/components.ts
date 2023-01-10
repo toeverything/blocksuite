22335c6396eed5a59d49512664e19c0e81d1ca45
x1a0t <thorseraq@gmail.com>
2023-01-10 11:28:09 +0800

    feat: extracting css style to separate file

--- packages/blocks/src/page-block/edgeless/components.ts
+++ packages/blocks/src/page-block/edgeless/components.ts
@@ -1,4 +1,4 @@
-import { css, html, LitElement, type TemplateResult } from 'lit';
+import { html, LitElement, type TemplateResult } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { styleMap } from 'lit/directives/style-map.js';
 import { repeat } from 'lit/directives/repeat.js';
@@ -26,7 +26,7 @@ import {
 } from './utils.js';
 import { SHAPE_PADDING } from '../../index.js';
 import { toolbarDrawConfig, toolbarActionConfig } from '../utils/const.js';
-import { toolTipStyle } from '../../components/tooltip.js';
+import { toolbarStyle } from './styles.js';
 
 function getCommonRectStyle(
   rect: DOMRect,
@@ -192,60 +192,7 @@ export function EdgelessBlockChildrenContainer(
 
 @customElement('edgeless-toolbar')
 export class EdgelessToolBar extends LitElement {
-  static styles = css`
-    .edgeless-toolbar-container {
-      height: 320px;
-      position: absolute;
-      left: 12px;
-      top: 0;
-      bottom: 100px;
-      margin: auto;
-      z-index: 999;
-    }
-
-    .styled-toolbar-wrapper {
-      width: 44px;
-      border-radius: 10px;
-      box-shadow: 4px 4px 7px rgba(58, 76, 92, 0.04),
-        -4px -4px 13px rgba(58, 76, 92, 0.02),
-        6px 6px 36px rgba(58, 76, 92, 0.06);
-      padding: 4px;
-      background: #fff;
-      transition: background 0.5s;
-      margin-bottom: 12px;
-    }
-
-    .toolbar-basic {
-      width: 36px;
-      height: 36px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      align-content: unset;
-      border-radius: 5px;
-    }
-
-    .toolbar {
-      color: #888a9e;
-      cursor: pointer;
-    }
-
-    .toolbar-disabled {
-      color: var(--affine-disable-color);
-      cursor: not-allowed;
-    }
-
-    .toolbar:hover {
-      color: var(--affine-primary-color);
-      background: var(--affine-hover-background);
-    }
-
-    ${toolTipStyle}
-
-    tool-tip {
-      padding: 0 12px;
-    }
-  `;
+  static styles = toolbarStyle;
 
   @property()
   page!: Page;
