9f5b40620fffe968a1dbdca53380644e3b8dc6bc
himself65 <himself65@outlook.com>
2023-01-11 01:30:32 +0800

    fix: move type into global

--- packages/global/src/index.d.ts
+++ packages/global/src/index.d.ts
@@ -1,4 +1,20 @@
-/// <reference types="./shims.d.ts" />
+// type shims for https://github.com/toeverything/blocksuite/issues/398
+// remove this until hotkeys-js and quill turned to `type: "module"`
+
+declare module 'hotkeys-js' {
+  import hotkeys = require('hotkeys-js/index');
+  export type * from 'hotkeys-js/index' assert { 'resolution-mode': 'require' };
+  declare const hotkeysDefault: typeof hotkeys.default;
+  export default hotkeysDefault;
+}
+
+declare module 'quill' {
+  import quill = require('quill/index');
+  export type * from 'quill/index' assert { 'resolution-mode': 'require' };
+  declare const quillDefault: typeof quill.default;
+  export default quillDefault;
+}
+
 // eslint-disable-next-line @typescript-eslint/prefer-namespace-keyword
 declare module NodeJS {
   interface ProcessEnv {
