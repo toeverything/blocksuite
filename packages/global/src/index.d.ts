dcbf0530c946becf08e6d0cbdb5735ee9d381aff
himself65 <himself65@outlook.com>
2023-01-11 01:58:03 +0800

    fix: type

--- packages/global/src/index.d.ts
+++ packages/global/src/index.d.ts
@@ -1,17 +1,5 @@
 // type shims for https://github.com/toeverything/blocksuite/issues/398
 // remove this until hotkeys-js and quill turned to `type: "module"`
-
-declare module 'hotkeys-js' {
-  import hotkeys = require('hotkeys-js/index');
-  export type {
-    KeyHandler,
-    HotkeysEvent,
-    Hotkeys,
-  } from 'hotkeys-js/index' assert { 'resolution-mode': 'require' };
-  declare const hotkeysDefault: typeof hotkeys.default;
-  export default hotkeysDefault;
-}
-
 declare module 'quill' {
   import quill = require('quill/index');
   export type * from 'quill/index' assert { 'resolution-mode': 'require' };
