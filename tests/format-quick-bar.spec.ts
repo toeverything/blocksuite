92b794761d52a042cc74f588b05d1386c30d0c71
lawvs <18554747+lawvs@users.noreply.github.com>
2023-01-14 03:58:38 +0800

    test: add keyboard select text test

--- tests/format-quick-bar.spec.ts
+++ tests/format-quick-bar.spec.ts
@@ -3,12 +3,15 @@ import {
   clickBlockTypeMenuItem,
   dragBetweenIndices,
   enterPlaygroundRoom,
+  focusRichText,
   initEmptyParagraphState,
   initThreeParagraphs,
   pressEnter,
   switchReadonly,
+  withPressKey,
 } from './utils/actions/index.js';
 import {
+  assertAlmostEqual,
   assertClipItems,
   assertLocatorVisible,
   assertSelection,
@@ -34,6 +37,50 @@ test('should format quick bar show when select text', async ({ page }) => {
 
   await page.mouse.click(0, 0);
   await expect(formatQuickBar).not.toBeVisible();
+});
+
+test('should format quick bar show when select text by keyboard', async ({
+  page,
+}) => {
+  await enterPlaygroundRoom(page);
+  await initEmptyParagraphState(page);
+  await focusRichText(page);
+  await page.keyboard.type('hello world');
+  await withPressKey(page, 'Shift', async () => {
+    let i = 10;
+    while (i--) {
+      await page.keyboard.press('ArrowLeft');
+    }
+  });
+
+  const formatQuickBar = page.locator(`.format-quick-bar`);
+  await expect(formatQuickBar).toBeVisible();
+
+  const leftBox = await formatQuickBar.boundingBox();
+  if (!leftBox) {
+    throw new Error("formatQuickBar doesn't exist");
+  }
+  assertAlmostEqual(leftBox.x, 20, 1);
+  assertAlmostEqual(leftBox.y, 106, 1);
+
+  await page.keyboard.press('ArrowLeft');
+  await expect(formatQuickBar).not.toBeVisible();
+
+  await withPressKey(page, 'Shift', async () => {
+    let i = 10;
+    while (i--) {
+      await page.keyboard.press('ArrowRight');
+    }
+  });
+
+  await expect(formatQuickBar).toBeVisible();
+
+  const rightBox = await formatQuickBar.boundingBox();
+  if (!rightBox) {
+    throw new Error("formatQuickBar doesn't exist");
+  }
+  assertAlmostEqual(rightBox.x, 43, 1);
+  assertAlmostEqual(rightBox.y, 180, 1);
 });
 
 test('should format quick bar can only display one at a time', async ({
