849768fcbe20cdd455e13e665ec1e300b02e9414
fi3ework <fi3ework@gmail.com>
2023-01-17 01:42:14 +0800

    feat(playground): add HMR for playground (#621)

---
+++ packages/playground/scripts/hmr-plugin/index.ts
@@ -0,0 +1,37 @@
+import path from 'node:path';
+import {
+  hmrPlugin as wcHmrPlugin,
+  presets,
+} from 'vite-plugin-web-components-hmr';
+
+import { fineTuneHmr } from './fine-tune';
+
+const customLitPath = path.resolve(
+  __dirname,
+  '../../../blocks/src/__internal__/index.js'
+);
+
+const include = ['../blocks/src/**/*'];
+const exclude = ['**/*/node_modules/**/*'];
+
+// https://vitejs.dev/config/
+export const hmrPlugin = process.env.WC_HMR
+  ? [
+      wcHmrPlugin({
+        include,
+        exclude,
+        presets: [presets.lit],
+        decorators: [{ name: 'customElement', import: 'lit/decorators.js' }],
+        baseClasses: [
+          {
+            name: 'NonShadowLitElement',
+            import: customLitPath,
+          },
+        ],
+      }),
+      fineTuneHmr({
+        include,
+        exclude,
+      }),
+    ]
+  : [];
--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import { fileURLToPath } from 'url';
+import { hmrPlugin } from './scripts/hmr-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), hmrPlugin],
   resolve: {
     alias: {
       '@blocksuite/blocks': path.resolve(
