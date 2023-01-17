f579ffe8eff2c9250a05ade527cfac9e21de5267
lawvs <18554747+lawvs@users.noreply.github.com>
2023-01-17 17:23:30 +0800

    feat: slash menu support keyboard

--- packages/blocks/src/components/slash-menu/slash-menu-node.ts
+++ packages/blocks/src/components/slash-menu/slash-menu-node.ts
@@ -1,6 +1,8 @@
-import type { Page } from '@blocksuite/store';
-import { css, html, LitElement } from 'lit';
-import { customElement, property, query } from 'lit/decorators.js';
+import { getRichTextByModel } from '@blocksuite/blocks/std.js';
+import type { RichText } from '@blocksuite/blocks/__internal__/rich-text/rich-text.js';
+import type { BaseBlockModel } from '@blocksuite/store';
+import { css, html, LitElement, PropertyValueMap } from 'lit';
+import { customElement, property, query, state } from 'lit/decorators.js';
 import { styleMap } from 'lit/directives/style-map.js';
 import { paragraphConfig } from '../../page-block/utils/const.js';
 import { updateSelectedTextType } from '../../page-block/utils/index.js';
@@ -37,6 +39,14 @@ export class SlashMenu extends LitElement {
       box-shadow: var(--affine-popover-shadow);
       z-index: var(--affine-z-index-popover);
     }
+    .overlay-mask {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100vw;
+      height: 100vh;
+      z-index: var(--affine-z-index-popover);
+    }
     ${slashMenuStyle}
   `;
 
@@ -47,7 +57,7 @@ export class SlashMenu extends LitElement {
   top: string | null = null;
 
   @property()
-  page: Page | null = null;
+  model!: BaseBlockModel;
 
   @property()
   abortController = new AbortController();
@@ -55,30 +65,83 @@ export class SlashMenu extends LitElement {
   @property()
   searchString = '';
 
+  @state()
+  activeItemIndex = 0;
+
   @query('.slash-menu')
   slashMenuElement!: HTMLElement;
 
+  // Just a temp variable
+  richText?: RichText;
+
   override connectedCallback() {
     super.connectedCallback();
-    window.addEventListener('mouseup', this._clickAwayListener);
-    window.addEventListener('keyup', this._escapeListener);
+    window.addEventListener('keydown', this._escapeListener);
+
+    const richText = getRichTextByModel(this.model);
+    if (!richText) {
+      console.warn(
+        'Slash Menu may not work properly! No rich text found for model',
+        this.model
+      );
+      return;
+    }
+    richText.addEventListener('keydown', this._keyDownListener, {
+      // Workaround Use capture to prevent the event from triggering the keyboard bindings action
+      capture: true,
+    });
+    this.richText = richText;
   }
 
   override disconnectedCallback() {
     super.disconnectedCallback();
-    window.removeEventListener('mouseup', this._clickAwayListener);
-    window.removeEventListener('keyup', this._escapeListener);
+    window.removeEventListener('keydown', this._escapeListener);
+    this.richText?.removeEventListener('keydown', this._keyDownListener, {
+      capture: true,
+    });
   }
 
   /**
-   * Handle click outside
+   * Handle arrow key
    */
-  private _clickAwayListener = (e: MouseEvent | KeyboardEvent) => {
-    if (e.target === this) {
+  private _keyDownListener = (e: KeyboardEvent) => {
+    if (
+      !['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown', 'Enter'].includes(
+        e.key
+      )
+    ) {
       return;
     }
-    this.abortController.abort('ABORT');
-    window.removeEventListener('mouseup', this._clickAwayListener);
+    if (e.key === 'ArrowLeft') {
+      this.abortController.abort('ABORT');
+      return;
+    }
+    const configLen = this._filterConfig().length;
+    switch (e.key) {
+      case 'Enter': {
+        this._handleItemClick(this.activeItemIndex);
+        break;
+      }
+      case 'ArrowUp': {
+        this.activeItemIndex =
+          (this.activeItemIndex - 1 + configLen) % configLen;
+        break;
+      }
+      case 'ArrowDown': {
+        this.activeItemIndex = (this.activeItemIndex + 1) % configLen;
+        break;
+      }
+      case 'ArrowRight':
+      case 'ArrowLeft':
+        // do nothing temporarily
+        return;
+      default:
+        throw new Error(`Unknown key: ${e.key}`);
+    }
+    // prevent arrow key from moving cursor
+    e.preventDefault();
+    // prevent the event from triggering the keyboard bindings action
+    e.stopPropagation();
   };
 
   /**
@@ -91,6 +154,12 @@ export class SlashMenu extends LitElement {
     this.abortController.abort('ABORT');
     window.removeEventListener('keyup', this._escapeListener);
   };
+
+  private _handleItemClick(index: number) {
+    this.abortController.abort();
+    const { flavour, type } = this._filterConfig()[index];
+    updateSelectedTextType(flavour, type, this.model.page);
+  }
 
   private _filterConfig() {
     const normalizeString = this.searchString.slice(1).trim().toLowerCase();
@@ -131,19 +200,20 @@ export class SlashMenu extends LitElement {
     }
 
     return html`<div class="slash-menu-container" style="${containerStyles}">
+      <div
+        class="overlay-mask"
+        @click="${() => this.abortController.abort()}"
+      ></div>
       <div class="slash-menu" style="${slashMenuStyles}">
         ${filterConfig.map(
-          ({ flavour, type, name, icon }) => html`<format-bar-button
+          ({ flavour, type, name, icon }, index) => html`<format-bar-button
             width="100%"
             style="padding-left: 12px; justify-content: flex-start;"
+            ?hover=${this.activeItemIndex === index}
             text="${name}"
             data-testid="${type}"
             @click=${() => {
-              if (!this.page) {
-                throw new Error('Failed to format paragraph! Page not found.');
-              }
-              this.abortController.abort();
-              updateSelectedTextType(flavour, type, this.page);
+              this._handleItemClick(index);
             }}
           >
             ${icon}
