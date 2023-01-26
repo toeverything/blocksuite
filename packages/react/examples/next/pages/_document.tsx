4e98a6054fca5514c04026fa31f8b7136aecff1d
himself65 <himself65@outlook.com>
2023-01-26 16:22:23 -0600

    fix: lint

--- packages/react/examples/next/pages/_document.tsx
+++ packages/react/examples/next/pages/_document.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-} from 'next/document';
+import type { DocumentContext } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { CssBaseline } from '@nextui-org/react';
 
 class BlockSuiteDocument extends Document {
