369a9e3df2ae8c515d5078e49b97e93db31c33a2
Yifeng Wang <doodlewind@toeverything.info>
2023-01-27 16:54:09 +0800

    refactor: remove `IndexedDBPersistence`

--- packages/store/src/persistence/doc/idb-provider.ts
+++ packages/store/src/persistence/doc/idb-provider.ts
@@ -9,64 +9,67 @@ const updatesStoreName = 'updates';
 const PREFERRED_TRIM_SIZE = 500;
 
 async function fetchUpdates(
-  idbPersistence: IndexedDBPersistence,
+  idbProvider: IndexedDBDocProvider,
   beforeApplyUpdatesCallback: (store: IDBObjectStore) => void = () => {
     void 0;
   }
 ) {
-  if (!idbPersistence.db) {
+  if (!idbProvider.db) {
     throw new Error('idbPersistence.db is null');
   }
 
-  const [updatesStore] = idb.transact(idbPersistence.db, [updatesStoreName]);
+  const [updatesStore] = idb.transact(idbProvider.db, [updatesStoreName]);
   const updates = await idb.getAll(
     updatesStore,
-    idb.createIDBKeyRangeLowerBound(idbPersistence._dbref, false)
+    idb.createIDBKeyRangeLowerBound(idbProvider._dbref, false)
   );
 
   beforeApplyUpdatesCallback(updatesStore);
   Y.transact(
-    idbPersistence.doc,
+    idbProvider.doc,
     () => {
-      updates.forEach(val => Y.applyUpdate(idbPersistence.doc, val));
+      updates.forEach(val => Y.applyUpdate(idbProvider.doc, val));
     },
-    idbPersistence,
+    idbProvider,
     false
   );
 
   const lastKey = await idb.getLastKey(updatesStore);
-  idbPersistence._dbref = lastKey + 1;
+  idbProvider._dbref = lastKey + 1;
 
   const cnt = await idb.count(updatesStore);
-  idbPersistence._dbsize = cnt;
+  idbProvider._dbsize = cnt;
 
   return updatesStore;
 }
 
 async function storeState(
-  idbPersistence: IndexedDBPersistence,
+  idbProvider: IndexedDBDocProvider,
   forceStore = true
 ) {
-  const updatesStore = await fetchUpdates(idbPersistence);
+  const updatesStore = await fetchUpdates(idbProvider);
 
-  if (forceStore || idbPersistence._dbsize >= PREFERRED_TRIM_SIZE) {
+  if (forceStore || idbProvider._dbsize >= PREFERRED_TRIM_SIZE) {
     idb
-      .addAutoKey(updatesStore, Y.encodeStateAsUpdate(idbPersistence.doc))
+      .addAutoKey(updatesStore, Y.encodeStateAsUpdate(idbProvider.doc))
       .then(() =>
         idb.del(
           updatesStore,
-          idb.createIDBKeyRangeUpperBound(idbPersistence._dbref, true)
+          idb.createIDBKeyRangeUpperBound(idbProvider._dbref, true)
         )
       )
       .then(() =>
         idb.count(updatesStore).then(cnt => {
-          idbPersistence._dbsize = cnt;
+          idbProvider._dbsize = cnt;
         })
       );
   }
 }
 
-class IndexedDBPersistence extends Observable<string> {
+export class IndexedDBDocProvider
+  extends Observable<string>
+  implements DocProvider
+{
   doc: Y.Doc;
   name: string;
   _dbref = 0;
@@ -75,7 +78,7 @@ class IndexedDBPersistence extends Observable<string> {
   db: IDBDatabase | null;
   synced = false;
   _db: Promise<IDBDatabase>;
-  whenSynced: Promise<IndexedDBPersistence>;
+  whenSynced: Promise<IndexedDBDocProvider>;
   /**
    * Timeout in ms untill data is merged and persisted in idb.
    */
@@ -83,19 +86,20 @@ class IndexedDBPersistence extends Observable<string> {
 
   _storeTimeoutId: ReturnType<typeof setTimeout> | null;
   _storeUpdate: (update: Uint8Array, origin: this) => void;
-  constructor(name: string, doc: Y.Doc) {
+
+  constructor(room: string, doc: Y.Doc) {
     super();
     this.doc = doc;
-    this.name = name;
+    this.name = room;
     /**
      * @type {IDBDatabase|null}
      */
     this.db = null;
-    this._db = idb.openDB(name, db =>
+    this._db = idb.openDB(room, db =>
       idb.createStores(db, [['updates', { autoIncrement: true }], ['custom']])
     );
     /**
-     * @type {Promise<IndexedDBPersistence>}
+     * @type {Promise<IndexedDBDocProvider>}
      */
     this.whenSynced = this._db.then(db => {
       this.db = db;
@@ -173,28 +177,5 @@ class IndexedDBPersistence extends Observable<string> {
     const db = await this._db;
     const [custom] = idb.transact(db, [customStoreName]);
     return idb.del(custom, key);
-  }
-}
-
-export class IndexedDBDocProvider
-  extends IndexedDBPersistence
-  implements DocProvider
-{
-  constructor(room: string, doc: Y.Doc) {
-    super(room, doc);
-  }
-
-  // Consider whether "connect" and "disconnect" are good to put on the SyncProvider
-  connect() {
-    // not necessary as it will be set up in indexeddb persistence
-  }
-
-  disconnect() {
-    // not necessary as it will be set up in indexeddb persistence
-  }
-
-  public clearData() {
-    // Do nothing for now
-    return Promise.resolve();
   }
 }
