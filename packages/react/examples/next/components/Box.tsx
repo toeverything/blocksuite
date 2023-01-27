296b17e4c3c68db2f636fe2f63346dd3d2fa3de9
Himself65 <himself65@outlook.com>
2023-01-27 13:01:54 -0600

    fix(next-example): remove ts-ignore (#871)

--- packages/react/examples/next/components/Box.tsx
+++
@@ -1,6 +0,0 @@
-import { styled } from '@nextui-org/react';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const Box: React.FC<any> = styled('div', {
-  boxSizing: 'border-box',
-});
