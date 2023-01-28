4a6e664c18fda63cf83f88cda8da22e2e35a867d
himself65 <himself65@outlook.com>
2023-01-28 11:02:33 -0600

    fix: update code

--- packages/playground/src/data/index.ts
+++ packages/playground/src/data/index.ts
@@ -55,9 +55,22 @@ export function heavy(workspace: Workspace) {
   });
 }
 
-const presetMarkdown = `This example is designed to:
+const presetMarkdown = `This playground is designed to:
 
-* ⚛️ Test react binding with BlockSuite.
+* 📝 Test basic editing experience.
+* ⚙️ Serve as E2E test entry.
+* 🔗 Demonstrate how BlockSuite reconciles real-time collaboration with [local-first](https://martin.kleppmann.com/papers/local-first.pdf) data ownership.
+
+## Controlling Playground Data Source
+You might initially enter this page with the \`?init\` URL param. This is the default (opt-in) setup that automatically loads this built-in article. Meanwhile, you'll connect to a random single-user room via a WebRTC provider by default. This is the "single-user mode" for local testing.
+
+To test real-time collaboration, you can specify the room to join by adding the \`?room=foo\` config - Try opening this page with \`?room=foo\` in two different tabs and see what happens!
+
+> Note that the second and subsequent users should not open the page with the \`?init\` param in this case. Also, due to the P2P nature of WebRTC, as long as there is at least one user connected to the room, the content inside the room will **always** exist.
+
+If you are the only user in the room, your content will be lost after refresh. This is great for local debugging. But if you want local persistence, you can open this page with the \`?providers=indexeddb&room=foo\` config, then click the init button in the bottom-left corner to initialize this default content.
+
+As a pro tip, you can combine multiple providers! For example, feel free to open this page with \`?providers=indexeddb,webrtc&room=hello\` params, and see if everything works as expected. Have fun!
 
 For any feedback, please visit [BlockSuite issues](https://github.com/toeverything/blocksuite/issues) 📍`;
 
@@ -68,7 +81,7 @@ export function preset(workspace: Workspace) {
 
       // Add page block and surface block at root level
       const pageBlockId = page.addBlockByFlavour('affine:page', {
-        title: 'Welcome to BlockSuite React Example',
+        title: 'Welcome to BlockSuite playground',
       });
       page.addBlockByFlavour('affine:surface', {}, null);
 
--- packages/react/examples/next/src/components/PageManger.tsx
+++ packages/react/examples/next/src/components/PageManger.tsx
@@ -10,22 +10,9 @@ const Editor: React.ComponentType<EditorProps> = dynamic(
   }
 );
 
-const presetMarkdown = `This playground is designed to:
+const presetMarkdown = `This example is designed to:
 
-* 📝 Test basic editing experience.
-* ⚙️ Serve as E2E test entry.
-* 🔗 Demonstrate how BlockSuite reconciles real-time collaboration with [local-first](https://martin.kleppmann.com/papers/local-first.pdf) data ownership.
-
-## Controlling Playground Data Source
-You might initially enter this page with the \`?init\` URL param. This is the default (opt-in) setup that automatically loads this built-in article. Meanwhile, you'll connect to a random single-user room via a WebRTC provider by default. This is the "single-user mode" for local testing.
-
-To test real-time collaboration, you can specify the room to join by adding the \`?room=foo\` config - Try opening this page with \`?room=foo\` in two different tabs and see what happens!
-
-> Note that the second and subsequent users should not open the page with the \`?init\` param in this case. Also, due to the P2P nature of WebRTC, as long as there is at least one user connected to the room, the content inside the room will **always** exist.
-
-If you are the only user in the room, your content will be lost after refresh. This is great for local debugging. But if you want local persistence, you can open this page with the \`?providers=indexeddb&room=foo\` config, then click the init button in the bottom-left corner to initialize this default content.
-
-As a pro tip, you can combine multiple providers! For example, feel free to open this page with \`?providers=indexeddb,webrtc&room=hello\` params, and see if everything works as expected. Have fun!
+* ⚛️ Test react binding with BlockSuite.
 
 For any feedback, please visit [BlockSuite issues](https://github.com/toeverything/blocksuite/issues) 📍`;
 
@@ -110,7 +97,7 @@ export const PageManger = () => {
           page={currentPage}
           onInit={async (page, editor) => {
             const pageBlockId = page.addBlockByFlavour('affine:page', {
-              title: 'Welcome to BlockSuite playground',
+              title: 'Welcome to BlockSuite React example',
             });
             page.addBlockByFlavour('affine:surface', {}, null);
             const frameId = page.addBlockByFlavour(
