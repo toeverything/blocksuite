71fe178b0b532c0a1ae85175018ba0e032ba20a2
himself65 <himself65@outlook.com>
2023-01-28 10:58:11 -0600

    feat(react,next-example): add `onInit` props

--- packages/react/examples/next/src/components/PageManger.tsx
+++ packages/react/examples/next/src/components/PageManger.tsx
@@ -1,14 +1,33 @@
 import dynamic from 'next/dynamic';
-import { useBlockSuiteStore } from '@blocksuite/react';
-import type { Page } from '@blocksuite/store';
+import { type EditorProps, useBlockSuiteStore } from '@blocksuite/react';
 import { Button, Card, Grid, Text } from '@nextui-org/react';
 import { Box } from './Box';
 
-const Editor: React.ComponentType<{
-  page: Page;
-}> = dynamic(async () => (await import('@blocksuite/react/editor')).Editor, {
-  ssr: false,
-});
+const Editor: React.ComponentType<EditorProps> = dynamic(
+  async () => (await import('@blocksuite/react/editor')).Editor,
+  {
+    ssr: false,
+  }
+);
+
+const presetMarkdown = `This playground is designed to:
+
+* 📝 Test basic editing experience.
+* ⚙️ Serve as E2E test entry.
+* 🔗 Demonstrate how BlockSuite reconciles real-time collaboration with [local-first](https://martin.kleppmann.com/papers/local-first.pdf) data ownership.
+
+## Controlling Playground Data Source
+You might initially enter this page with the \`?init\` URL param. This is the default (opt-in) setup that automatically loads this built-in article. Meanwhile, you'll connect to a random single-user room via a WebRTC provider by default. This is the "single-user mode" for local testing.
+
+To test real-time collaboration, you can specify the room to join by adding the \`?room=foo\` config - Try opening this page with \`?room=foo\` in two different tabs and see what happens!
+
+> Note that the second and subsequent users should not open the page with the \`?init\` param in this case. Also, due to the P2P nature of WebRTC, as long as there is at least one user connected to the room, the content inside the room will **always** exist.
+
+If you are the only user in the room, your content will be lost after refresh. This is great for local debugging. But if you want local persistence, you can open this page with the \`?providers=indexeddb&room=foo\` config, then click the init button in the bottom-left corner to initialize this default content.
+
+As a pro tip, you can combine multiple providers! For example, feel free to open this page with \`?providers=indexeddb,webrtc&room=hello\` params, and see if everything works as expected. Have fun!
+
+For any feedback, please visit [BlockSuite issues](https://github.com/toeverything/blocksuite/issues) 📍`;
 
 export const PageManger = () => {
   const pages = useBlockSuiteStore(store => store.pages);
@@ -87,7 +106,23 @@ export const PageManger = () => {
         }}
       />
       <Box css={{ bg: 'white' }}>
-        <Editor page={currentPage} />
+        <Editor
+          page={currentPage}
+          onInit={async (page, editor) => {
+            const pageBlockId = page.addBlockByFlavour('affine:page', {
+              title: 'Welcome to BlockSuite playground',
+            });
+            page.addBlockByFlavour('affine:surface', {}, null);
+            const frameId = page.addBlockByFlavour(
+              'affine:frame',
+              {},
+              pageBlockId
+            );
+            // Import preset markdown content inside frame block
+            await editor.clipboard.importMarkdown(presetMarkdown, frameId);
+            page.resetHistory();
+          }}
+        />
       </Box>
     </Box>
   );
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,1 +1,2 @@
 export * from './store/index.js';
+export type { EditorProps } from './components/Editor/index.js';
