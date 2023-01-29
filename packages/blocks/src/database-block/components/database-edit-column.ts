893fa8ef1bbb02f6f5807d356f1e99e07ad7a240
himself65 <himself65@outlook.com>
2023-01-29 17:23:07 -0600

    feat: support edit form

--- packages/blocks/src/database-block/components/database-edit-column.ts
+++ packages/blocks/src/database-block/components/database-edit-column.ts
@@ -1,9 +1,48 @@
-import { html, LitElement } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { css, html, LitElement } from 'lit';
+import { customElement, property, query } from 'lit/decorators.js';
+import type { DatabaseBlockModel } from '../database-model.js';
+import TagSchema = BlockSuiteInternal.TagSchema;
 
 @customElement('affine-database-edit-column')
 export class DatabaseEditColumn extends LitElement {
+  static styles = css`
+    :host {
+      background: red;
+      padding: 1rem;
+    }
+  `;
+
+  @property()
+  targetModel!: DatabaseBlockModel;
+
+  @property()
+  targetTagSchema!: TagSchema;
+
+  @query('input')
+  titleInput!: HTMLInputElement;
+
   protected render(): unknown {
-    return html` <div>Edit Column</div> `;
+    console.log(this.targetTagSchema, this.targetModel);
+    return html`
+      <div>
+        <input value=${this.targetTagSchema.name} />
+        <button
+          @click=${(event: MouseEvent) => {
+            const newName = this.titleInput.value;
+            this.targetModel.page.captureSync();
+            this.targetModel.page.setTagSchema({
+              ...this.targetTagSchema,
+              name: newName,
+            });
+            this.targetModel.propsUpdated.emit();
+            requestAnimationFrame(() => {
+              this.remove();
+            });
+          }}
+        >
+          Save Title Name
+        </button>
+      </div>
+    `;
   }
 }
