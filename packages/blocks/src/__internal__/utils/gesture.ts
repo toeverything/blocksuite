84f7ff3c1e3205576f3e51ad918cce01b89f97ce
Pionxzh <spigbbbbb@gmail.com>
2023-01-31 17:32:13 +0800

    fix: keep blank-area-click cursor in edgeless mode (#878)
    Co-authored-by: Yifeng Wang <doodlewind@toeverything.info>

--- packages/blocks/src/__internal__/utils/gesture.ts
+++ packages/blocks/src/__internal__/utils/gesture.ts
@@ -1,16 +1,6 @@
-import {
-  getClosestHorizontalEditor,
-  resetNativeSelection,
-  setEndRange,
-  setStartRange,
-} from './selection.js';
 import { debounce } from './std.js';
 import { MOVE_DETECT_THRESHOLD } from '@blocksuite/global/config';
-import {
-  isDatabaseInput,
-  isInsideBlockContainer,
-  isTitleElement,
-} from './query.js';
+import { isDatabaseInput, isTitleElement } from './query.js';
 
 export interface IPoint {
   x: number;
@@ -162,38 +152,14 @@ export function initMouseEventHandlers(
       e.preventDefault();
     }
 
-    if (!isDragging)
-      onContainerClick(
-        toSelectionEvent(e, getBoundingClientRect, startX, startY)
-      );
-    else
+    if (isDragging) {
       onContainerDragEnd(
         toSelectionEvent(e, getBoundingClientRect, startX, startY, last)
       );
-
-    const horizontalElement = getClosestHorizontalEditor(e.clientY);
-
-    if (!isInsideBlockContainer(e.target) && horizontalElement) {
-      let containerDom = document.querySelector(
-        '.affine-default-page-block-container'
+    } else {
+      onContainerClick(
+        toSelectionEvent(e, getBoundingClientRect, startX, startY)
       );
-      // TODO adapt edgeless block
-      if (!containerDom) {
-        containerDom = document.querySelector(
-          '.affine-edgeless-page-block-container'
-        );
-      }
-
-      if (containerDom) {
-        const react = containerDom.getBoundingClientRect();
-        if (e.clientX < react.left) {
-          const range = setStartRange(horizontalElement);
-          resetNativeSelection(range);
-        } else {
-          const range = setEndRange(horizontalElement);
-          resetNativeSelection(range);
-        }
-      }
     }
 
     startX = startY = -Infinity;
