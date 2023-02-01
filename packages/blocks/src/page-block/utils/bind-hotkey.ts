2cef04d7b6ac119e91abd6cd301bbd593ce8ce1e
Whitewater <me@waterwater.moe>
2023-02-01 09:12:21 -0800

    refactor: extract hotkey bindings from utils (#931)

--- packages/blocks/src/page-block/utils/bind-hotkey.ts
+++ packages/blocks/src/page-block/utils/bind-hotkey.ts
@@ -1,8 +1,34 @@
-import { formatConfig } from './const.js';
-import type { Page } from '@blocksuite/store';
-import { hotkey } from '../../__internal__/index.js';
-import { updateSelectedTextType } from './container-operations.js';
 import { HOTKEYS, paragraphConfig } from '@blocksuite/global/config';
+import {
+  assertExists,
+  caretRangeFromPoint,
+  matchFlavours,
+} from '@blocksuite/global/utils';
+import type { BaseBlockModel, Page } from '@blocksuite/store';
+import { hotkey } from '../../__internal__/index.js';
+import { isAtLineEdge } from '../../__internal__/rich-text/rich-text-operations.js';
+import {
+  asyncFocusRichText,
+  focusNextBlock,
+  focusPreviousBlock,
+  focusTitle,
+  getBlockElementByModel,
+  getDefaultPageBlock,
+  getModelByElement,
+  getPreviousBlock,
+  getStartModelBySelection,
+  isCaptionElement,
+  Point,
+} from '../../__internal__/utils/index.js';
+import type { DefaultPageSignals } from '../default/default-page-block.js';
+import type { DefaultSelectionManager } from '../default/selection-manager.js';
+import {
+  handleBlockSelectionBatchDelete,
+  handleMultiBlockBackspace,
+  handleSelectAll,
+} from '../utils/index.js';
+import { formatConfig } from './const.js';
+import { updateSelectedTextType } from './container-operations.js';
 
 const { UNDO, REDO } = HOTKEYS;
 
@@ -46,5 +72,271 @@ export function removeCommonHotKey() {
       .filter((i): i is string => !!i),
     UNDO,
     REDO,
+  ]);
+}
+
+export function handleUp(
+  selection: DefaultSelectionManager,
+  signals: DefaultPageSignals,
+  e: KeyboardEvent
+) {
+  const nativeSelection = window.getSelection();
+  if (nativeSelection?.anchorNode) {
+    const model = getStartModelBySelection();
+    const activePreNodeModel = getPreviousBlock(model);
+    const editableContainer = getBlockElementByModel(model)?.querySelector(
+      '.ql-editor'
+    ) as HTMLElement;
+    const range = nativeSelection.getRangeAt(0);
+    const { height, left, top } = range.getBoundingClientRect();
+    const newRange = caretRangeFromPoint(left, top - height / 2);
+    if (
+      (!newRange || !editableContainer.contains(newRange.startContainer)) &&
+      !isAtLineEdge(range)
+    ) {
+      if (!activePreNodeModel) {
+        focusTitle();
+      } else {
+        focusPreviousBlock(model, new Point(left, top));
+      }
+    }
+    return;
+  } else {
+    signals.updateSelectedRects.emit([]);
+    const { state } = selection;
+    const selectedModel = getModelByElement(state.selectedBlocks[0]);
+    const page = getDefaultPageBlock(selectedModel);
+    e.preventDefault();
+    focusPreviousBlock(
+      selectedModel,
+      page.lastSelectionPosition instanceof Point
+        ? page.lastSelectionPosition
+        : 'end'
+    );
+  }
+}
+
+export function handleDown(
+  selection: DefaultSelectionManager,
+  signals: DefaultPageSignals,
+  e: KeyboardEvent
+) {
+  if (!selection.state.selectedBlocks.length) {
+    const nativeSelection = window.getSelection();
+    const model = getStartModelBySelection();
+    if (matchFlavours(model, ['affine:code'])) {
+      return;
+    }
+    assertExists(nativeSelection);
+    const range = nativeSelection.getRangeAt(0);
+    const { left, bottom } = range.getBoundingClientRect();
+    focusNextBlock(model, new Point(left, bottom));
+  } else {
+    signals.updateSelectedRects.emit([]);
+    const { state } = selection;
+    const selectedModel = getModelByElement(state.selectedBlocks[0]);
+    const page = getDefaultPageBlock(selectedModel);
+    e.preventDefault();
+    focusNextBlock(
+      selectedModel,
+      page.lastSelectionPosition instanceof Point
+        ? page.lastSelectionPosition
+        : 'start'
+    );
+    return;
+  }
+}
+
+function isDispatchFromCodeBlock(e: KeyboardEvent) {
+  if (!e.target || !(e.target instanceof Element)) {
+    return false;
+  }
+  try {
+    // if the target is `body`, it will throw an error
+    const model = getModelByElement(e.target);
+    return matchFlavours(model, ['affine:code']);
+  } catch (error) {
+    // just check failed, no need to handle
+    return false;
+  }
+}
+
+export function bindHotkeys(
+  page: Page,
+  selection: DefaultSelectionManager,
+  signals: DefaultPageSignals
+) {
+  const {
+    BACKSPACE,
+    SELECT_ALL,
+
+    SHIFT_UP,
+    SHIFT_DOWN,
+
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    ENTER,
+  } = HOTKEYS;
+
+  bindCommonHotkey(page);
+
+  hotkey.addListener(ENTER, e => {
+    const { type, selectedBlocks } = selection.state;
+    const targetInput = e.target;
+    // TODO caption ad-hoc should be moved to the caption input for processing
+    const isCaption = isCaptionElement(targetInput);
+    // select blocks or focus caption input, then enter will create a new block.
+    if ((type === 'block' && selectedBlocks.length) || isCaption) {
+      e.stopPropagation();
+      e.preventDefault();
+      const element = isCaption
+        ? targetInput
+        : selectedBlocks[selectedBlocks.length - 1];
+      const model = getModelByElement(element);
+      const parentModel = page.getParent(model);
+      const index = parentModel?.children.indexOf(model);
+      assertExists(index);
+      assertExists(parentModel);
+      const id = page.addBlockByFlavour(
+        'affine:paragraph',
+        { type: 'text' },
+        parentModel,
+        index + 1
+      );
+      asyncFocusRichText(page, id);
+      selection.state.clear();
+      selection.clearRects();
+      return;
+    }
+  });
+
+  hotkey.addListener(BACKSPACE, e => {
+    const { state } = selection;
+    if (state.type === 'none') {
+      // Will be handled in the `keyboard.onBackspace` function
+      return;
+    }
+    if (state.type === 'native') {
+      handleMultiBlockBackspace(page, e);
+      return;
+    }
+
+    if (state.type === 'block') {
+      // XXX Ad-hoc for code block
+      // At the beginning of the code block,
+      // the backspace will selected the block first.
+      // The select logic already processed in the `handleLineStartBackspace` function.
+      // So we need to prevent the default delete behavior.
+      if (isDispatchFromCodeBlock(e)) {
+        return;
+      }
+      const { selectedBlocks } = state;
+
+      // delete selected blocks
+      handleBlockSelectionBatchDelete(
+        page,
+        selectedBlocks.map(block => getModelByElement(block))
+      );
+      e.preventDefault();
+      state.clear();
+      signals.updateSelectedRects.emit([]);
+      signals.updateEmbedRects.emit([]);
+      signals.updateEmbedEditingState.emit(null);
+      return;
+    }
+  });
+
+  hotkey.addListener(SELECT_ALL, e => {
+    e.preventDefault();
+    handleSelectAll(selection);
+    selection.state.type = 'block';
+  });
+
+  hotkey.addListener(UP, e => {
+    handleUp(selection, signals, e);
+  });
+  hotkey.addListener(DOWN, e => {
+    handleDown(selection, signals, e);
+  });
+  hotkey.addListener(LEFT, e => {
+    let model: BaseBlockModel | null = null;
+    const {
+      state: { selectedBlocks, type },
+    } = selection;
+    if (
+      selectedBlocks.length &&
+      !(type === 'native' && window.getSelection()?.rangeCount)
+    ) {
+      model = getModelByElement(selection.state.selectedBlocks[0]);
+      signals.updateSelectedRects.emit([]);
+      selection.state.clear();
+      e.preventDefault();
+    } else {
+      const range = window.getSelection()?.getRangeAt(0);
+      if (range && range.collapsed && range.startOffset === 0) {
+        model = getStartModelBySelection();
+      }
+    }
+    model && focusPreviousBlock(model, 'end');
+  });
+  hotkey.addListener(RIGHT, e => {
+    let model: BaseBlockModel | null = null;
+    const {
+      state: { selectedBlocks, type },
+    } = selection;
+    if (
+      selectedBlocks.length &&
+      !(type === 'native' && window.getSelection()?.rangeCount)
+    ) {
+      model = getModelByElement(
+        selection.state.selectedBlocks[
+          selection.state.selectedBlocks.length - 1
+        ]
+      );
+      signals.updateSelectedRects.emit([]);
+      selection.state.clear();
+      e.preventDefault();
+    } else {
+      const range = window.getSelection()?.getRangeAt(0);
+      const textModel = getStartModelBySelection();
+      if (
+        range &&
+        range.collapsed &&
+        range.startOffset === textModel.text?.length
+      ) {
+        // handleUp(this.selection, this.signals);
+        model = getStartModelBySelection();
+      }
+    }
+    model && focusNextBlock(model, 'start');
+  });
+
+  hotkey.addListener(SHIFT_UP, e => {
+    // TODO expand selection up
+  });
+  hotkey.addListener(SHIFT_DOWN, e => {
+    // TODO expand selection down
+  });
+
+  // !!!
+  // Don't forget to remove hotkeys at `_removeHotkeys`
+}
+
+export function removeHotkeys() {
+  removeCommonHotKey();
+  hotkey.removeListener([
+    HOTKEYS.BACKSPACE,
+    HOTKEYS.SELECT_ALL,
+
+    HOTKEYS.SHIFT_UP,
+    HOTKEYS.SHIFT_DOWN,
+
+    HOTKEYS.UP,
+    HOTKEYS.DOWN,
+    HOTKEYS.LEFT,
+    HOTKEYS.RIGHT,
+    HOTKEYS.ENTER,
   ]);
 }
