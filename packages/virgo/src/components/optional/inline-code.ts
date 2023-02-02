591b3d01c2446c19b9185429084e921855615efb
Flrande <1978616327@qq.com>
2023-02-02 18:41:45 +0800

    feat(virgo): add optional inline code element

---
+++ packages/virgo/src/components/optional/inline-code.ts
@@ -0,0 +1,49 @@
+import { LitElement, html } from 'lit';
+import { property, customElement } from 'lit/decorators.js';
+import type { BaseArrtiubtes, DeltaInsert } from '../../types.js';
+import { VirgoUnitText } from '../virgo-unit-text.js';
+import { ZERO_WIDTH_SPACE } from '../../constant.js';
+
+export interface InlineCodeAttributes {
+  type: 'inline-code';
+}
+
+@customElement('v-inline-code')
+export class InlineCode extends LitElement {
+  @property({ type: Object })
+  delta: DeltaInsert<InlineCodeAttributes> = {
+    insert: ZERO_WIDTH_SPACE,
+    attributes: {
+      type: 'inline-code',
+    },
+  };
+
+  render() {
+    const unitText = new VirgoUnitText();
+    const textDelta: DeltaInsert<BaseArrtiubtes> = {
+      insert: this.delta.insert,
+      attributes: {
+        type: 'base',
+      },
+    };
+
+    unitText.delta = textDelta;
+
+    return html`<code data-virgo-element="true"
+      ><span contentEditable="false">${ZERO_WIDTH_SPACE}</span>${unitText}<span
+        contentEditable="false"
+        >${ZERO_WIDTH_SPACE}</span
+      ></code
+    >`;
+  }
+
+  createRenderRoot() {
+    return this;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'v-inline-code': InlineCode;
+  }
+}
