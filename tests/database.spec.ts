90ee236c5947ec47f7ad4c609ed79d20559765b3
himself65 <himself65@outlook.com>
2023-02-02 15:07:49 -0600

    test: fix

--- tests/database.spec.ts
+++ tests/database.spec.ts
@@ -3,6 +3,7 @@ import {
   enterPlaygroundRoom,
   initEmptyDatabaseState,
 } from './utils/actions/index.js';
+import { assertStoreMatchJSX } from './utils/asserts.js';
 
 test('init database block', async ({ page }) => {
   await enterPlaygroundRoom(page, {
@@ -12,4 +13,25 @@ test('init database block', async ({ page }) => {
 
   const locator = page.locator('affine-database');
   await expect(locator).toBeVisible();
+  const databaseTitle = page.locator('.affine-database-block-title');
+  await databaseTitle.clear();
+  const expected = 'hello';
+  await databaseTitle.type(expected);
+  await assertStoreMatchJSX(
+    page,
+    `
+<affine:page
+  prop:title=""
+>
+  <affine:frame
+    prop:xywh="[0,0,720,117]"
+  >
+    <affine:database
+      prop:columns={Array []}
+      prop:mode={2}
+      prop:title="${expected}"
+    />
+  </affine:frame>
+</affine:page>`
+  );
 });
