5fdd6d7beedf0a6f9637d9e25b16c8f255a84c07
himself65 <himself65@outlook.com>
2023-02-06 15:59:12 -0600

    fix(react): connect

--- packages/store/src/store.ts
+++ packages/store/src/store.ts
@@ -84,6 +84,7 @@ export class Store {
   readonly spaces = new Map<string, Space>();
   readonly awarenessStore: AwarenessStore;
   readonly idGenerator: IdGenerator;
+  connected = false;
 
   // TODO: The user cursor should be spread by the spaceId in awareness
   constructor({
@@ -132,10 +133,12 @@ export class Store {
 
   connect = () => {
     this.providers.forEach(provider => provider.connect?.());
+    this.connected = true;
   };
 
   disconnect = () => {
     this.providers.forEach(provider => provider.disconnect?.());
+    this.connected = false;
   };
 
   addSpace(space: Space) {
--- packages/store/src/workspace/workspace.ts
+++ packages/store/src/workspace/workspace.ts
@@ -286,6 +286,10 @@ export class Workspace {
     this._handlePageEvent();
   }
 
+  get connected(): boolean {
+    return this._store.connected;
+  }
+
   connect = () => {
     this._store.connect();
   };
