1427b3a92c332f76e837911b48a2399a7c2d9729
himself65 <himself65@outlook.com>
2023-02-05 23:54:22 -0600

    ci: enable virgo test

--- packages/virgo/playwright.config.ts
+++ packages/virgo/playwright.config.ts
@@ -7,6 +7,14 @@ const config: PlaywrightTestConfig = {
   testDir: 'src/',
   testIgnore: ['**.unit.spec.ts'],
   workers: 1,
+  webServer: {
+    command: 'pnpm dev',
+    port: 5173,
+    reuseExistingServer: !process.env.CI,
+    env: {
+      COVERAGE: process.env.COVERAGE ?? '',
+    },
+  },
   use: {
     browserName:
       (process.env.BROWSER as PlaywrightWorkerOptions['browserName']) ??
