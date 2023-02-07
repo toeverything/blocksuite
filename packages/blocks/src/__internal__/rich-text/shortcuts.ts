e26d486532eff0dc24de2e2c03d4a96b6652052e
Himself65 <himself65@outlook.com>
2023-02-07 09:46:53 -0600

    Update packages/blocks/src/__internal__/rich-text/shortcuts.ts

--- packages/blocks/src/__internal__/rich-text/shortcuts.ts
+++ packages/blocks/src/__internal__/rich-text/shortcuts.ts
@@ -24,7 +24,7 @@ export class Shortcuts {
   ): boolean {
     const selection = quill.getSelection();
     if (!selection) {
-      return false;
+      return PREVENT_DEFAULT;
     }
     const [line] = quill.getLine(selection.index);
     if (Shortcuts._isValid(prefix, line.domNode.tagName)) {
