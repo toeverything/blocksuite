dd2466faf3fa3e8d005e6afcac36614b576bb091
x1a0t <thorseraq@gmail.com>
2023-02-10 20:32:15 +0800

    chore: update overflow style

--- packages/blocks/src/code-block/code-block.ts
+++ packages/blocks/src/code-block/code-block.ts
@@ -136,7 +136,7 @@ export class CodeBlockComponent extends NonShadowLitElement {
     .affine-code-block-container .ql-syntax {
       width: 620px;
       margin: 0;
-      overflow: scroll;
+      overflow-x: scroll;
       /*scrollbar-color: #fff0 #fff0;*/
     }
 
