943ee421a3fcd659122b3179d727d5986ca00d1a
himself65 <himself65@outlook.com>
2023-02-13 23:46:56 -0600

    fix: extension

--- packages/mock/src/index.ts
+++ packages/mock/src/index.ts
@@ -1,6 +1,7 @@
-import express from 'express';
-import bodyParser from 'body-parser';
 import { createHash } from 'node:crypto';
+
+import bodyParser from 'body-parser';
+import express from 'express';
 
 const PORT: number = +(process.env.PORT ?? 3000);
 
