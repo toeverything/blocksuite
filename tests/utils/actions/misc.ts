bdecdfde7c885aea94caa1dbaf71de52be1b5e98
Himself65 <himself65@outlook.com>
2023-02-13 20:18:06 -0600

    test: fix spec name (#1160)

--- tests/utils/actions/misc.ts
+++ tests/utils/actions/misc.ts
@@ -33,7 +33,7 @@ function shamefullyIgnoreConsoleMessage(message: ConsoleMessage): boolean {
     // See https://github.com/quilljs/quill/issues/2030
     '[JavaScript Warning: "Use of Mutation Events is deprecated. Use MutationObserver instead."',
     // Fixme: https://github.com/toeverything/blocksuite/issues/1126
-    'Error: Unexpected console message: Failed to clean slash search text!',
+    'Failed to clean slash search text!',
   ];
   return ignoredMessages.some(msg => message.text().startsWith(msg));
 }
