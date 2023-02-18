ee331cf6885fb87aea0ba978672cdf1aeba7bb1f
lawvs <18554747+lawvs@users.noreply.github.com>
2023-02-18 12:02:14 +0800

    fix: slash with shift key

--- packages/blocks/src/__internal__/rich-text/keyboard.ts
+++ packages/blocks/src/__internal__/rich-text/keyboard.ts
@@ -342,6 +342,7 @@ export function createKeyboardBindings(page: Page, model: BaseBlockModel) {
         // Compatible with CJK IME
         '、',
       ],
+      shiftKey: null,
       // prefix non digit or empty string
       // see https://stackoverflow.com/questions/19127384/what-is-a-regex-to-match-only-an-empty-string
       // prefix: /[^\d]$|^(?![\s\S])/,
