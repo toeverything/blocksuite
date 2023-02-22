dc63245701347e0b6abb232584d64c2d0a3613d0
alt0 <wxlxinglong@outlook.com>
2023-02-22 20:56:22 +0800

    feat(playground): phasor supports hover event

--- packages/playground/examples/canvas/main.ts
+++ packages/playground/examples/canvas/main.ts
@@ -7,6 +7,9 @@ import {
 } from '@blocksuite/phasor';
 import { Workspace } from '@blocksuite/store';
 
+import { ViewportState } from './viewport-state.js';
+import { initWheelEventHandlers } from './wheel-event-handlers';
+
 const { Y } = Workspace;
 
 function testClick(surface: SurfaceManager, e: MouseEvent) {
@@ -21,14 +24,51 @@ function testClick(surface: SurfaceManager, e: MouseEvent) {
   );
 }
 
+const hoverRect = document.querySelector('#hover-rect') as HTMLDivElement;
+function mouseMove(
+  surface: SurfaceManager,
+  viewportState: ViewportState,
+  e: MouseEvent
+) {
+  const [modelX, modelY] = surface.toModelCoord(e.offsetX, e.offsetY);
+  const topElement = surface.pickTop(modelX, modelY);
+
+  if (topElement) {
+    const [x, y] = surface.toViewCoord(topElement.x, topElement.y);
+
+    hoverRect.style.visibility = 'visible';
+    hoverRect.style.left = `${x}px`;
+    hoverRect.style.top = `${y}px`;
+    hoverRect.style.width = `${topElement.w * viewportState.zoom}px`;
+    hoverRect.style.height = `${topElement.h * viewportState.zoom}px`;
+  } else {
+    hoverRect.style.visibility = 'hidden';
+  }
+}
+
 function main() {
   const doc = new Y.Doc();
-  const canvas = document.querySelector('canvas') as HTMLCanvasElement;
   const yContainer = doc.getMap('container');
+
+  const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+
+  const viewportState = new ViewportState();
+  const bound = canvas.getBoundingClientRect();
+  viewportState.setSize(bound.width, bound.height);
+  viewportState.setCenter(0, 0);
+
   const surface = new SurfaceManager(canvas, yContainer);
+
+  initWheelEventHandlers(surface, viewportState, canvas);
+  surface.setViewport(
+    viewportState.centerX,
+    viewportState.centerY,
+    viewportState.zoom
+  );
 
   surface.addDebugElement(new Bound(0, 0, 100, 100), 'red');
   surface.addDebugElement(new Bound(50, 50, 100, 100), 'black');
+  surface.addDebugElement(new Bound(298, 0, 2, 300), 'gray');
 
   surface.addBrushElement([300, 120], 'green', [
     [0, 0],
@@ -52,6 +92,10 @@ function main() {
 
   surface.initDefaultGestureHandler();
   canvas.addEventListener('click', e => testClick(surface, e));
+  canvas.addEventListener('mousemove', e =>
+    mouseMove(surface, viewportState, e)
+  );
+  canvas.addEventListener('wheel', e => mouseMove(surface, viewportState, e));
 
   // @ts-ignore
   window.surface = surface;
