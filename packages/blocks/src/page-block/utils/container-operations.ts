f2adadaa183f8b85f519978da3f1354f85c333fe
Flrande <50035259+Flrande@users.noreply.github.com>
2023-02-23 18:26:09 +0800

    refactor: add block update side effect in store (#1340)

--- packages/blocks/src/page-block/utils/container-operations.ts
+++ packages/blocks/src/page-block/utils/container-operations.ts
@@ -1,4 +1,3 @@
-import { getServiceOrRegister } from '@blocksuite/blocks';
 import { assertExists, assertFlavours } from '@blocksuite/global/utils';
 import { BaseBlockModel, Page, Text } from '@blocksuite/store';
 import type { TextAttributes } from '@blocksuite/virgo';
@@ -30,7 +29,7 @@ import {
   restoreSelection,
   saveBlockRange,
 } from '../../__internal__/utils/selection.js';
-import type { BlockSchema, ParagraphBlockModel } from '../../models.js';
+import type { BlockSchema } from '../../models.js';
 import type { DefaultSelectionManager } from '../default/selection-manager.js';
 import { DEFAULT_SPACING } from '../edgeless/utils.js';
 
@@ -172,12 +171,6 @@ export async function updateBlockType(
       }
       updateBlockRange(savedBlockRange, model, newModel);
       lastNewId = newId;
-    }
-  });
-  models.forEach(async model => {
-    if (model.flavour === 'affine:paragraph' && type) {
-      const service = await getServiceOrRegister(model.flavour);
-      service.updateTypeEffect(model as ParagraphBlockModel, type);
     }
   });
 
