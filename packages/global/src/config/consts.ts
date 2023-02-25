8bb866141e9dbfce26a26a9cbde7bf53fa501b8d
zqran <uuxnet@gmail.com>
2023-02-25 23:47:27 +0800

    feat: remove redundant `as const`

--- packages/global/src/config/consts.ts
+++ packages/global/src/config/consts.ts
@@ -1,8 +1,8 @@
 export * from './consts/affine-style-consts.js';
 export * from './consts/blockhub.js';
 
-export const BLOCK_ID_ATTR = 'data-block-id' as const;
-export const BLOCK_SERVICE_LOADING_ATTR = 'data-service-loading' as const;
+export const BLOCK_ID_ATTR = 'data-block-id';
+export const BLOCK_SERVICE_LOADING_ATTR = 'data-service-loading';
 export const PREVENT_DEFAULT = false;
 export const ALLOW_DEFAULT = true;
 
