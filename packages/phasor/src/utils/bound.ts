93049ae56d07dfb1bbe95314c99315ff7ebab65f
alt0 <wxlxinglong@outlook.com>
2023-03-02 11:30:09 +0800

    fix(phasor): using surface.getElementsBound replace surface.elements

---
+++ packages/phasor/src/utils/bound.ts
@@ -0,0 +1,43 @@
+import type { IBound } from '../consts.js';
+
+export class Bound implements IBound {
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+
+  constructor(x: number, y: number, w: number, h: number) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+}
+
+function getExpandedBound(a: IBound, b: IBound): IBound {
+  const minX = Math.min(a.x, b.x);
+  const minY = Math.min(a.y, b.y);
+  const maxX = Math.max(a.x + a.w, b.x + b.w);
+  const maxY = Math.max(a.y + a.h, b.y + b.h);
+  const width = Math.abs(maxX - minX);
+  const height = Math.abs(maxY - minY);
+
+  return {
+    x: minX,
+    y: minY,
+    w: width,
+    h: height,
+  };
+}
+
+export function getCommonBound(bounds: IBound[]): IBound | null {
+  if (bounds.length < 2) return bounds[0] ?? null;
+
+  let result = bounds[0];
+
+  for (let i = 1; i < bounds.length; i++) {
+    result = getExpandedBound(result, bounds[i]);
+  }
+
+  return result;
+}
