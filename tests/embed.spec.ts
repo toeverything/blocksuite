520c00c87cfea167a55cf77afed6ae5f5d2e527b
lawvs <18554747+lawvs@users.noreply.github.com>
2023-03-03 01:02:40 +0800

    test: add test

--- tests/embed.spec.ts
+++ tests/embed.spec.ts
@@ -4,6 +4,7 @@ import { expect, Page } from '@playwright/test';
 
 import {
   activeEmbed,
+  dragBetweenCoords,
   dragEmbedResizeByTopLeft,
   dragEmbedResizeByTopRight,
   enterPlaygroundRoom,
@@ -352,4 +353,26 @@ test('popup menu should follow position of image when scrolling', async ({
 
   // < 1.059180567624251
   expect(imageRect.top).toBeCloseTo(menuRect.top, -0.325);
+});
+
+test('select image should not show format bar', async ({ page }) => {
+  await enterPlaygroundRoom(page);
+  await initImageState(page);
+  await assertRichImage(page, 1);
+
+  const image = page.locator('affine-image');
+  const rect = await image.boundingBox();
+  if (!rect) {
+    throw new Error('image not found');
+  }
+  await dragBetweenCoords(
+    page,
+    { x: rect.x + 20, y: rect.y + 20 },
+    { x: rect.x - 20, y: rect.y - 20 }
+  );
+  const formatQuickBar = page.locator(`.format-quick-bar`);
+  await expect(formatQuickBar).not.toBeVisible();
+  await page.mouse.wheel(0, rect.y + rect.height);
+  await expect(formatQuickBar).not.toBeVisible();
+  await page.mouse.click(0, 0);
 });
