da4ea152403f374c2afe391f5114bc73b7dd59c0
Fangdun Tsai <cfddream@gmail.com>
2023-03-05 11:35:36 +0800

    chore: remove dom utils

--- packages/global/src/utils.ts
+++ packages/global/src/utils.ts
@@ -4,7 +4,6 @@ import type { BlockModels } from './types.js';
 
 export type { Disposable } from './utils/disposable.js';
 export { DisposableGroup, flattenDisposable } from './utils/disposable.js';
-export { Dom } from './utils/dom.js';
 export { Slot } from './utils/slot.js';
 export { caretRangeFromPoint, isFirefox, isWeb } from './utils/web.js';
 export const SYS_KEYS = new Set(['id', 'flavour', 'children']);
