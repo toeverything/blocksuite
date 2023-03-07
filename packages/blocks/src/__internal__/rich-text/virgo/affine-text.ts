13ecc380eca934d94adc462383334f8a00d5967a
Flrande <1978616327@qq.com>
2023-03-07 19:12:59 +0800

    chore: update css

--- packages/blocks/src/__internal__/rich-text/virgo/affine-text.ts
+++ packages/blocks/src/__internal__/rich-text/virgo/affine-text.ts
@@ -20,14 +20,13 @@ function affineTextStyles(
   let inlineCodeStyle = {};
   if (props.code) {
     inlineCodeStyle = {
-      'font-family':
-        '"SFMono-Regular", Menlo, Consolas, "PT Mono", "Liberation Mono", Courier, monospace',
-      'line-height': 'normal',
-      background: 'rgba(135,131,120,0.15)',
-      color: '#EB5757',
-      'border-radius': '3px',
-      'font-size': '85%',
-      padding: '0.2em 0.4em',
+      'font-family': 'var(--affine-font-code-family)',
+      background: 'var(--affine-code-background))',
+      color: 'var(--affine-code-color))',
+      'border-radius': '5px',
+      padding: '0 5px',
+      'font-size': 'calc(var(--affine-font-base) - 4px)',
+      'font-variant-ligatures': 'none',
     };
   }
 
