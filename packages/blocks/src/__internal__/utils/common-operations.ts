36d9bb6fc1d8129fd2bd0501ea5ba7f66c0c815f
donaldkicksyourass <mzhlovefama@gmail.com>
2023-03-09 00:07:04 +0800

    fix(blocks): update paragraph type with new vRange (#1583)

--- packages/blocks/src/__internal__/utils/common-operations.ts
+++ packages/blocks/src/__internal__/utils/common-operations.ts
@@ -135,6 +135,13 @@ export function convertToParagraph(
     page.captureSync();
 
     model.text?.delete(0, prefix.length + 1);
+    const vEditor = getRichTextByModel(model)?.vEditor;
+    if (vEditor) {
+      vEditor.setVRange({
+        index: 0,
+        length: 0,
+      });
+    }
     page.updateBlock(model, { type: type });
   }
   return true;
