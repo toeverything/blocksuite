2ebbde21db9f57c202f9f3ef235ce293f727082d
Whitewater <me@waterwater.moe>
2023-03-10 01:03:29 -0800

    fix: compatible with old data (#1646)

--- packages/virgo/src/utils/base-attributes.ts
+++ packages/virgo/src/utils/base-attributes.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod';
 
 export const baseTextAttributes = z.object({
-  bold: z.literal(true).optional(),
-  italic: z.literal(true).optional(),
-  underline: z.literal(true).optional(),
-  strike: z.literal(true).optional(),
-  code: z.literal(true).optional(),
+  bold: z.boolean().optional(),
+  italic: z.boolean().optional(),
+  underline: z.boolean().optional(),
+  strike: z.boolean().optional(),
+  code: z.boolean().optional(),
 });
 
 export type BaseTextAttributes = z.infer<typeof baseTextAttributes>;
