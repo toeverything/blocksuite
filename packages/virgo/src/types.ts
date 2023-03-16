66ac2ab293c6e46ade4bc9c24482a623f5dba404
Mirone <Saul-Mirone@outlook.com>
2023-03-16 16:54:12 +0800

    refactor(virgo): extract events into event service

--- packages/virgo/src/types.ts
+++ packages/virgo/src/types.ts
@@ -13,3 +13,27 @@ export type DeltaInsert<
 export type AttributesRenderer<
   TextAttributes extends BaseTextAttributes = BaseTextAttributes
 > = (vText: VText, attributes?: TextAttributes) => TemplateResult<1>;
+
+export interface VRange {
+  index: number;
+  length: number;
+}
+
+export type VRangeUpdatedProp = [
+  range: VRange | null,
+  type: 'native' | 'input' | 'other'
+];
+
+export type DeltaEntry = [delta: DeltaInsert, range: VRange];
+
+// corresponding to [anchorNode/focusNode, anchorOffset/focusOffset]
+export type NativePoint = readonly [node: Node, offset: number];
+// the number here is relative to the text node
+export type TextPoint = readonly [text: Text, offset: number];
+
+export interface DomPoint {
+  // which text node this point is in
+  text: Text;
+  // the index here is relative to the Editor, not text node
+  index: number;
+}
