4678e854a9db6b3391d8d9b662c8cacb6ae4d70b
lawvs <18554747+lawvs@users.noreply.github.com>
2023-03-17 17:48:52 +0800

    refactor: migrate nativeKeydown to hotkey

--- packages/blocks/src/__internal__/utils/std.ts
+++ packages/blocks/src/__internal__/utils/std.ts
@@ -206,3 +206,11 @@ export function maxBy<T>(items: T[], value: (item: T) => number): T | null {
 
   return maxItem;
 }
+
+export function isControlledKeyboardEvent(e: KeyboardEvent) {
+  return e.ctrlKey || e.metaKey || e.altKey;
+}
+
+export function isPrintableKeyEvent(event: KeyboardEvent): boolean {
+  return event.key.length === 1 && !isControlledKeyboardEvent(event);
+}
