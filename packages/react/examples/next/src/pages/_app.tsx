39b4679134fae18dd36b102b9c45eee9f84df661
himself65 <himself65@outlook.com>
2023-03-16 20:04:35 -0400

    chore: bump version

--- packages/react/examples/next/src/pages/_app.tsx
+++ packages/react/examples/next/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import '@blocksuite/editor/themes/affine.css';
+import '@blocksuite/editor';
 
 import { NextUIProvider } from '@nextui-org/react';
 import type { AppProps } from 'next/app';
