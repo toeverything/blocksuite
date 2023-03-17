46a012623d5ef180c55f18932a1eb80c98421dbc
himself65 <himself65@outlook.com>
2023-03-16 20:05:19 -0400

    fix: remove unused code

--- packages/react/examples/next/src/pages/_app.tsx
+++ packages/react/examples/next/src/pages/_app.tsx
@@ -1,6 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import '@blocksuite/editor/themes/affine.css';
-import '@blocksuite/editor';
 
 import { NextUIProvider } from '@nextui-org/react';
 import type { AppProps } from 'next/app';
