d6f5dbd3d6b2677158e6412958710c9ad367937f
zuomeng wang <wxlxinglong@outlook.com>
2023-03-23 10:27:05 +0800

    fix(edgeless): pinch event compatible (#1821)

--- packages/blocks/src/__internal__/utils/shortcut.ts
+++
@@ -1,7 +0,0 @@
-const IS_SAFARI = /Apple Computer/.test(navigator.vendor);
-const IS_IOS =
-  IS_SAFARI &&
-  (/Mobile\/\w+/.test(navigator.userAgent) || navigator.maxTouchPoints > 2);
-const IS_MAC = /Mac/i.test(navigator.platform);
-
-export const SHORTKEY = IS_IOS || IS_MAC ? 'metaKey' : 'ctrlKey';
