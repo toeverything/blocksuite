ed3d052e5f8997bf1362007981a84c8b9ba54f9b
Fangdun Tsai <cfddream@gmail.com>
2023-03-23 22:21:35 +0800

    fix(drag-handle): display only in the currently activated frame

--- packages/blocks/src/components/block-hub.ts
+++ packages/blocks/src/components/block-hub.ts
@@ -455,7 +455,8 @@ export class BlockHub extends NonShadowLitElement {
 
   @property()
   public getHoveringFrameState: (point: Point) => {
-    rect: Rect | null;
+    container?: Element;
+    rect?: Rect;
     scale: number;
   };
 
@@ -515,7 +516,8 @@ export class BlockHub extends NonShadowLitElement {
     enableDatabase: boolean;
     getAllowedBlocks: () => BaseBlockModel[];
     getHoveringFrameState: (point: Point) => {
-      rect: Rect | null;
+      container?: Element;
+      rect?: Rect;
       scale: number;
     };
     onDragStarted: () => void;
@@ -714,27 +716,31 @@ export class BlockHub extends NonShadowLitElement {
     };
 
     const point = new Point(x, y);
-    const { rect: frameRect, scale } = this.getHoveringFrameState(
+    const { container, rect, scale } = this.getHoveringFrameState(
       point.clone()
     );
     let element = null;
-    if (frameRect) {
-      element = getClosestBlockElementByPoint(point, frameRect, scale);
+    if (rect) {
+      element = getClosestBlockElementByPoint(
+        point,
+        { container, rect },
+        scale
+      );
     }
 
-    let rect = null;
+    let targetRect = null;
     let lastModelState = null;
     if (element) {
-      rect = getRectByBlockElement(element);
+      targetRect = getRectByBlockElement(element);
       lastModelState = {
-        rect,
+        rect: targetRect,
         element: element as BlockComponentElement,
         model: getModelByBlockElement(element),
       };
     }
 
     this._lastModelState = lastModelState;
-    this._indicator.targetRect = rect;
+    this._indicator.targetRect = targetRect;
   };
 
   private _onDragOver = (e: DragEvent) => {
