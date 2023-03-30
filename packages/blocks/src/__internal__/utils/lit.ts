c579c998f5415a6cc50c6fa5ace05d6e91e956de
lawvs <18554747+lawvs@users.noreply.github.com>
2023-03-31 05:32:53 +0800

    feat: add DisposableMixin

--- packages/blocks/src/__internal__/utils/lit.ts
+++ packages/blocks/src/__internal__/utils/lit.ts
@@ -1,9 +1,72 @@
+import { DisposableGroup } from '@blocksuite/global/utils';
 import {
   CSSResult,
   type CSSResultGroup,
   type CSSResultOrNative,
   LitElement,
 } from 'lit';
+
+export class DisposableLitElement extends LitElement {
+  protected _disposables = new DisposableGroup();
+
+  connectedCallback(): void {
+    super.connectedCallback();
+    if (this._disposables.disposed) {
+      this._disposables = new DisposableGroup();
+    }
+  }
+
+  disconnectedCallback(): void {
+    super.disconnectedCallback();
+    this._disposables.dispose();
+  }
+}
+
+// see https://lit.dev/docs/composition/mixins/#mixins-in-typescript
+declare class DisposableMixinInterface {
+  protected _disposables: DisposableGroup;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Constructor<T = object> = new (...args: any[]) => T;
+
+/**
+ * Mixin that adds a `_disposables: DisposableGroup` property to the class.
+ *
+ * The `_disposables` property is initialized in `connectedCallback` and disposed in `disconnectedCallback`.
+ *
+ * see https://lit.dev/docs/composition/mixins/
+ *
+ * @example
+ * ```ts
+ * class MyElement extends DisposableMixin(NonShadowLitElement) {
+ *   onClick() {
+ *     this.disposableGroup.add(...);
+ *   }
+ * }
+ * ```
+ */
+export const DisposableMixin = <T extends Constructor<LitElement>>(
+  superClass: T
+) => {
+  class DisposableMixinClass extends superClass {
+    protected _disposables = new DisposableGroup();
+
+    connectedCallback(): void {
+      super.connectedCallback();
+      if (this._disposables.disposed) {
+        this._disposables = new DisposableGroup();
+      }
+    }
+
+    disconnectedCallback(): void {
+      super.disconnectedCallback();
+      this._disposables.dispose();
+    }
+  }
+  return DisposableMixinClass as unknown as T &
+    Constructor<DisposableMixinInterface>;
+};
 
 export class NonShadowLitElement extends LitElement {
   static disableShadowRoot = true;
