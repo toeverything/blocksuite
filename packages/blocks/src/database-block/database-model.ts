0268dd03a8919f03805130a5194b13c671f3d382
Yifeng Wang <doodlewind@toeverything.info>
2023-04-02 08:25:27 +0800

    refactor(database): remove `mode` in crdt model

--- packages/blocks/src/database-block/database-model.ts
+++ packages/blocks/src/database-block/database-model.ts
@@ -2,18 +2,11 @@ import type { ColumnSchema } from '@blocksuite/global/database';
 import { defineBlockSchema, type SchemaToModel } from '@blocksuite/store';
 import { literal } from 'lit/static-html.js';
 
-export enum DatabaseBlockDisplayMode {
-  Text,
-  Grid,
-  Database,
-}
-
 export const DatabaseBlockSchema = defineBlockSchema(
   'affine:database',
   internal => ({
     title: internal.Text(),
     columns: [] as ColumnSchema['id'][],
-    mode: DatabaseBlockDisplayMode.Database,
     titleColumn: '',
   }),
   {
