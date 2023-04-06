2951dbdf77b23c8b32e528b0cf533fd01ca3825d
Yifeng Wang <doodlewind@toeverything.info>
2023-04-06 10:10:14 +0800

    chore: add nightly version suffix

--- scripts/replace-nightly-version.sh
+++ scripts/replace-nightly-version.sh
@@ -18,7 +18,7 @@ replace() {
 
   VERSION=0.0.0
 
-  pnpm version "$VERSION-$BUILD_VERSION" --no-git-tag-version  --no-commit-hooks
+  pnpm version "$VERSION-$BUILD_VERSION-nightly" --no-git-tag-version  --no-commit-hooks
 }
 
 for package in "${packages[@]}"
