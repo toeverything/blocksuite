fbaea7a27174271eb585e2b141ee2055dad641dd
Fangdun Tsai <cfddream@gmail.com>
2023-04-07 08:52:18 +0800

    fix: lint

--- packages/blocks/src/page-block/default/utils.ts
+++ packages/blocks/src/page-block/default/utils.ts
@@ -11,7 +11,6 @@ import {
   getBlockElementsExcludeSubtrees,
   getClosestBlockElementByPoint,
   getModelByBlockElement,
-  getRectByBlockElement,
   isInEmptyDatabaseByPoint,
 } from '@blocksuite/blocks/std';
 import {
