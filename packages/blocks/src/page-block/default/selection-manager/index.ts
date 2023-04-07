b0c90df0cccfc0dca4679fb3d4e9112694493779
Fangdun Tsai <cfddream@gmail.com>
2023-04-07 09:52:38 +0800

    fix: export affine-page-selected-rects component

--- packages/blocks/src/page-block/default/selection-manager/index.ts
+++ packages/blocks/src/page-block/default/selection-manager/index.ts
@@ -1,5 +1,4 @@
 export { DefaultSelectionManager } from './default-selection-manager.js';
-export { type AffinePageSelectedRects } from './selected-rects.js';
 export {
   PageSelectionState,
   type PageSelectionType,
