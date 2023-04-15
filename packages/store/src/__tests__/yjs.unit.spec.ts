cb3e907ead13dec0423a7523613f254bf31c30d9
Mirone <Saul-Mirone@outlook.com>
2023-04-15 21:11:59 +0800

    test: add unit test

--- packages/store/src/__tests__/yjs.unit.spec.ts
+++ packages/store/src/__tests__/yjs.unit.spec.ts
@@ -1,6 +1,7 @@
 import { describe, expect, test } from 'vitest';
+import * as Y from 'yjs';
 
-import { BlockSuiteDoc } from '../yjs/index.js';
+import { BlockSuiteDoc, createYArrayProxy } from '../yjs/index.js';
 
 describe('blocksuite yjs', () => {
   test('doc', () => {
@@ -10,5 +11,23 @@ describe('blocksuite yjs', () => {
     expect(proxy.x).toBeUndefined();
     map.set('x', 1);
     expect(proxy.x).toBe(1);
+  });
+
+  describe('array', () => {
+    test('proxy', () => {
+      const ydoc = new Y.Doc();
+      const arr = ydoc.getArray('arr');
+      arr.push([0]);
+
+      const proxy = createYArrayProxy(arr);
+      expect(arr.get(0)).toBe(0);
+
+      proxy.push(1);
+      expect(arr.get(1)).toBe(1);
+      expect(arr.length).toBe(2);
+
+      proxy.splice(1, 1);
+      expect(arr.length).toBe(1);
+    });
   });
 });
