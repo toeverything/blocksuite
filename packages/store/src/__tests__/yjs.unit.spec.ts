29f21ef9abb8bb5168bf8592b34391846e5e1df4
Mirone <Saul-Mirone@outlook.com>
2023-04-17 00:42:01 +0800

    feat(store): add Y.Map deep proxy (#2111)

--- packages/store/src/__tests__/yjs.unit.spec.ts
+++ packages/store/src/__tests__/yjs.unit.spec.ts
@@ -1,7 +1,11 @@
 import { describe, expect, test } from 'vitest';
 import * as Y from 'yjs';
 
-import { BlockSuiteDoc, createYArrayProxy } from '../yjs/index.js';
+import {
+  BlockSuiteDoc,
+  createYArrayProxy,
+  createYMapProxy,
+} from '../yjs/index.js';
 
 describe('blocksuite yjs', () => {
   test('doc', () => {
@@ -28,6 +32,48 @@ describe('blocksuite yjs', () => {
 
       proxy.splice(1, 1);
       expect(arr.length).toBe(1);
+    });
+
+    test('readonly', () => {
+      const ydoc = new Y.Doc();
+      const arr = ydoc.getArray('arr');
+      arr.push([0]);
+
+      const proxy = createYArrayProxy(arr, { readonly: true });
+      expect(arr.get(0)).toBe(0);
+
+      expect(() => proxy.push(1)).toThrowError('Modify data is not allowed');
+    });
+  });
+
+  describe('object', () => {
+    test('deep', () => {
+      const ydoc = new Y.Doc();
+      const map = ydoc.getMap('map');
+      const obj = new Y.Map();
+      obj.set('foo', 1);
+      map.set('obj', obj);
+      map.set('num', 0);
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const proxy = createYMapProxy<Record<string, any>>(map, { deep: true });
+
+      expect(proxy.num).toBe(0);
+      expect(proxy.obj.foo).toBe(1);
+
+      proxy.obj.bar = 100;
+      expect(obj.get('bar')).toBe(100);
+
+      proxy.obj2 = { foo: 2, bar: { num: 3 } };
+      expect(map.get('obj2')).toBeInstanceOf(Y.Map);
+      expect(
+        (map.get('obj2') as Y.Map<Y.Map<number>>).get('bar').get('num')
+      ).toBe(3);
+
+      proxy.obj2.bar.str = 'hello';
+      expect(
+        (map.get('obj2') as Y.Map<Y.Map<string>>).get('bar').get('str')
+      ).toBe('hello');
     });
   });
 });
