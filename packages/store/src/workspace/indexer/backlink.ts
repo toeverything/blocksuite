3140b083fdda35f4cf047c5bca0fd42502f69084
lawvs <18554747+lawvs@users.noreply.github.com>
2023-04-19 18:05:42 +0800

    feat: add `SubpageProofreader`

--- packages/store/src/workspace/indexer/backlink.ts
+++ packages/store/src/workspace/indexer/backlink.ts
@@ -1,8 +1,9 @@
-import { DisposableGroup, Slot } from '@blocksuite/global/utils';
+import { assertExists, DisposableGroup, Slot } from '@blocksuite/global/utils';
 import type { BaseTextAttributes } from '@blocksuite/virgo/index';
 import type { DeltaInsert } from '@blocksuite/virgo/types';
 import { Text } from 'yjs';
 
+import type { Workspace } from '../workspace.js';
 import type { BlockIndexer, IndexBlockEvent } from './base.js';
 
 type PageId = string;
@@ -13,14 +14,32 @@ type LinkedNode = { type: 'LinkedPage' | 'Subpage'; pageId: PageId };
  */
 type TextDelta = DeltaInsert<BaseTextAttributes & { reference: LinkedNode }>;
 
-export class BacklinkIndexer {
+type IndexUpdatedEvent =
+  | {
+      action: 'delete';
+      pageId: PageId;
+      blockId?: BlockId;
+    }
+  | {
+      action: 'add' | 'update';
+      pageId: PageId;
+      blockId: BlockId;
+    };
+
+type TextIndexer = {
+  onPageRemoved: (pageId: PageId) => void;
+  onBlockUpdated: ({ action, pageId, block, blockId }: IndexBlockEvent) => void;
+  onRefreshIndex: () => void;
+};
+
+export class BacklinkIndexer implements TextIndexer {
   private _linkIndexMap: Record<PageId, Record<BlockId, LinkedNode[]>> = {};
   private _disposables = new DisposableGroup();
   public slots = {
     /**
      * Note: sys:children update will not trigger event
      */
-    indexUpdated: new Slot<{ pageId: PageId; blockId?: BlockId }>(),
+    indexUpdated: new Slot<IndexUpdatedEvent>(),
   };
 
   constructor(blockIndexer: BlockIndexer) {
@@ -55,8 +74,21 @@ export class BacklinkIndexer {
     return backlinkList;
   }
 
-  // private _reviseDoc() {
-  // }
+  getParentPageNodes(subpageId: PageId) {
+    const backlinks = this.getBacklink(subpageId);
+    return backlinks.filter(
+      link => link.type === 'Subpage' && link.pageId === subpageId
+    );
+  }
+
+  getSubpageNodes(pageId: PageId) {
+    if (!(pageId in this._linkIndexMap)) {
+      return [];
+    }
+    return Object.values(this._linkIndexMap[pageId])
+      .flat()
+      .filter(link => link.type === 'Subpage');
+  }
 
   onRefreshIndex() {
     this._linkIndexMap = {};
@@ -67,7 +99,7 @@ export class BacklinkIndexer {
       return;
     }
     this._linkIndexMap[pageId] = {};
-    this.slots.indexUpdated.emit({ pageId });
+    this.slots.indexUpdated.emit({ action: 'delete', pageId });
   }
 
   onBlockUpdated({ action, pageId, block, blockId }: IndexBlockEvent) {
@@ -82,7 +114,7 @@ export class BacklinkIndexer {
           return;
         }
         const deltas: TextDelta[] = text.toDelta();
-        this._indexDelta(pageId, blockId, deltas);
+        this._indexDelta({ action, pageId, blockId, deltas });
         return;
       }
       case 'delete': {
@@ -92,7 +124,17 @@ export class BacklinkIndexer {
     }
   }
 
-  private _indexDelta(pageId: PageId, blockId: BlockId, deltas: TextDelta[]) {
+  private _indexDelta({
+    action,
+    pageId,
+    blockId,
+    deltas,
+  }: {
+    action: IndexBlockEvent['action'];
+    pageId: PageId;
+    blockId: BlockId;
+    deltas: TextDelta[];
+  }) {
     if (!deltas.length) return;
     const links = deltas
       .filter(delta => delta.attributes && delta.attributes.reference)
@@ -104,25 +146,115 @@ export class BacklinkIndexer {
     )
       return;
 
+    const before = this._linkIndexMap[pageId]?.[blockId] ?? [];
+    const diff = diffArray(before, links);
+    if (!diff.changed) return;
+
     this._linkIndexMap[pageId] = {
       ...this._linkIndexMap[pageId],
       [blockId]: links,
     };
-    this.slots.indexUpdated.emit({ pageId, blockId });
+    this.slots.indexUpdated.emit({ action: action, pageId, blockId });
   }
 
   private _removeIndex(pageId: PageId, blockId: BlockId) {
     if (!this._linkIndexMap[pageId] || !this._linkIndexMap[pageId][blockId]) {
       return;
     }
-    this._linkIndexMap[pageId] = {
-      ...this._linkIndexMap[pageId],
-      [blockId]: [],
-    };
-    this.slots.indexUpdated.emit({ pageId, blockId });
+    const previousLink = this._linkIndexMap[pageId][blockId];
+    delete this._linkIndexMap[pageId][blockId];
+    if (previousLink.length) {
+      this.slots.indexUpdated.emit({
+        action: 'delete',
+        pageId,
+        blockId,
+      });
+    }
   }
 
   dispose() {
     this._disposables.dispose();
   }
+}
+
+export class SubpageProofreader {
+  constructor(
+    private backlinkIndexer: BacklinkIndexer,
+    private workspace: Workspace
+  ) {
+    backlinkIndexer.slots.indexUpdated.on(e => {
+      this._reviseSubpage(e);
+    });
+  }
+
+  private _reviseSubpage({ action, pageId, blockId }: IndexUpdatedEvent) {
+    if (action === 'delete') {
+      if (!blockId) {
+        // delete page
+        // removed subpageId from the parent page when remove page
+        const pageMetas = this.workspace.meta.pageMetas;
+        pageMetas
+          .filter(({ subpageIds }) => subpageIds.includes(pageId))
+          .forEach(meta => {
+            console.warn('Unexpected subpageId in parent page', meta);
+            this.workspace.setPageMeta(meta.id, {
+              subpageIds: meta.subpageIds.filter(id => id !== pageId),
+            });
+          });
+        return;
+      }
+      // delete block
+    }
+    const page = this.workspace.getPage(pageId);
+    if (!page) {
+      console.warn('Failed to revise doc! Page not found', pageId);
+      return;
+    }
+    const pageMetas = this.workspace.meta.pageMetas;
+    const curPageMeta = pageMetas.find(meta => meta.id === pageId);
+    assertExists(curPageMeta);
+    const curSubpageIds = curPageMeta.subpageIds;
+    const subpageNodes = this.backlinkIndexer.getSubpageNodes(pageId);
+    // check change
+    const diff = diffArray(
+      curSubpageIds,
+      subpageNodes.map(node => node.pageId)
+    );
+    if (!diff.changed) {
+      return;
+    }
+    this.workspace.setPageMeta(pageId, {
+      subpageIds: subpageNodes.map(node => node.pageId),
+    });
+  }
+}
+
+/**
+ * Returns an object with four arrays: add, remove and unchanged.
+ *
+ * add: elements in after that are not in before
+ * remove: elements in before that are not in after
+ * unchanged: elements in both before and after
+ */
+function diffArray<T>(before: T[], after: T[]) {
+  const add: T[] = [];
+  const remove: T[] = [];
+  const unchanged: T[] = [];
+
+  // Find elements in before that are not in after
+  for (const elem of before) {
+    if (!after.includes(elem)) {
+      remove.push(elem);
+    } else {
+      unchanged.push(elem);
+    }
+  }
+  // Find elements in after that are not in before
+  for (const elem of after) {
+    if (!before.includes(elem)) {
+      add.push(elem);
+    }
+  }
+
+  return { changed: add.length || remove.length, add, remove, unchanged };
 }
