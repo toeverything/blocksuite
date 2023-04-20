f1fe8bb506ed0ba1632b0a2d22a9a24ac0573ede
himself65 <himself65@outlook.com>
2023-04-20 01:22:54 -0500

    test: import from node environment

---
+++ scripts/node-import-test.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-restricted-imports
+import '../packages/store/src/index';
+// eslint-disable-next-line @typescript-eslint/no-restricted-imports
+import '../packages/blocks/src/models';
+// eslint-disable-next-line @typescript-eslint/no-restricted-imports
+import '../packages/blocks/src/content-parser';
