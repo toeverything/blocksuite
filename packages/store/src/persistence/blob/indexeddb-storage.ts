17d0b170fc3944753514ed38a568494b10c45174
himself65 <himself65@outlook.com>
2023-04-19 23:16:41 -0500

    fix: async import

--- packages/store/src/persistence/blob/indexeddb-storage.ts
+++ packages/store/src/persistence/blob/indexeddb-storage.ts
@@ -1,23 +1,36 @@
-import { createStore, del, get, keys, set } from 'idb-keyval';
+import type { createStore } from 'idb-keyval';
 
 import type { BlobStorage } from './types.js';
 
 export const createIndexeddbStorage = (database: string): BlobStorage => {
-  // don't change the db name, it's for backward compatibility
-  const db = createStore(`${database}_blob`, 'blob');
+  let dbPromise: ReturnType<typeof createStore>;
+  // async import `idb-keyval` to avoid side effect
+  const idbPromise = import('idb-keyval').then(({ createStore, ...idb }) => {
+    // don't change the db name, it's for backward compatibility
+    dbPromise = createStore(`${database}_blob`, 'blob');
+    return idb;
+  });
   return {
     crud: {
       get: async (key: string) => {
+        const get = (await idbPromise).get;
+        const db = await dbPromise;
         return (await get<Blob>(key, db)) ?? null;
       },
       set: async (key: string, value: Blob) => {
+        const set = (await idbPromise).set;
+        const db = await dbPromise;
         await set(key, value, db);
         return key;
       },
       delete: async (key: string) => {
+        const del = (await idbPromise).del;
+        const db = await dbPromise;
         await del(key, db);
       },
       list: async () => {
+        const keys = (await idbPromise).keys;
+        const db = await dbPromise;
         return keys<string>(db);
       },
     },
