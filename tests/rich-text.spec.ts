13714063cdebd3a1b1956d45acba12dde1f2b9cd
zuozijian3720 <zuozijian1994@gmail.com>
2023-04-23 17:09:18 +0800

    fix: works locally, but fails in GitHub. why?

--- tests/rich-text.spec.ts
+++ tests/rich-text.spec.ts
@@ -18,12 +18,17 @@ test('ZERO_WIDTH_SPACE should be counted by one cursor position', async ({
   await page.keyboard.down('Shift');
   await press(page, 'Enter');
   await page.keyboard.up('Shift');
+  await page.waitForTimeout(100);
   await type(page, 'asdfg');
+  await page.waitForTimeout(100);
   await press(page, 'Enter');
+  await page.waitForTimeout(100);
   await page.keyboard.down('Meta');
   await press(page, 'z');
   await page.keyboard.up('Meta');
+  await page.waitForTimeout(100);
   await press(page, 'Backspace');
+  await page.waitForTimeout(100);
   const line = await page.locator('v-line').last();
   expect(await line.innerText()).toBe('asdf');
 });
