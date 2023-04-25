eddc11734cf592b09a8d4a2e7b3bc1f620d8d55c
lawvs <18554747+lawvs@users.noreply.github.com>
2023-04-25 15:38:34 +0800

    chore: add api migration guide

--- packages/store/src/workspace/workspace.ts
+++ packages/store/src/workspace/workspace.ts
@@ -212,10 +212,19 @@ export class Workspace {
     });
   }
 
-  createPage({
-    pageId = this.idGenerator(),
-    init,
-  }: { pageId?: string; init?: true | { title: string } } = {}) {
+  createPage(
+    options: { pageId?: string; init?: true | { title: string } } = {}
+  ) {
+    // Migration guide
+    if (typeof options === 'string') {
+      options = { pageId: options };
+      console.warn(
+        '`createPage(pageId)` is deprecated, use `createPage()` directly or `createPage({ pageId })` instead'
+      );
+    }
+    // End of migration guide. Remove this in the next major version
+
+    const { pageId = this.idGenerator(), init } = options;
     if (this._hasPage(pageId)) {
       throw new Error('page already exists');
     }
