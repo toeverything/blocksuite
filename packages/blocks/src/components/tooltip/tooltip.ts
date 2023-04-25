a5c4c44f37194dd9d85748a03555f04499a7384b
JimmFly <yangjinfei001@gmail.com>
2023-04-25 16:12:05 +0800

    chore: update block hub style

--- packages/blocks/src/components/tooltip/tooltip.ts
+++ packages/blocks/src/components/tooltip/tooltip.ts
@@ -21,7 +21,7 @@ export const tooltipStyle = css`
     padding: 4px 12px;
     color: var(--affine-white);
     /* TODO: use --affine-tooltip-background */
-    background: #33258a;
+    background: var(--affine-tooltip);
     box-shadow: var(--affine-tooltip-shadow);
     opacity: 0;
     transition: opacity 0.2s ease, transform 0.2s ease;
