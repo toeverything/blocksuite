1e902c2c2f7b0949d9053ce00f78939744bbf361
Fangdun Tsai <cfddream@gmail.com>
2023-04-27 18:01:46 +0800

    fix(selection): responsive image block (#2224)
    Co-authored-by: Yifeng Wang <doodlewind@toeverything.info>

--- packages/blocks/src/__internal__/utils/query.ts
+++ packages/blocks/src/__internal__/utils/query.ts
@@ -11,7 +11,7 @@ import type {
   EdgelessPageBlockComponent,
 } from '../../index.js';
 import type { RichText } from '../rich-text/rich-text.js';
-import type { Point, Rect } from './rect.js';
+import { type Point, Rect } from './rect.js';
 import { getCurrentNativeRange } from './selection.js';
 
 const ATTR_SELECTOR = `[${ATTR}]`;
@@ -810,10 +810,16 @@ export function getRectByBlockElement(
 export function getSelectedStateRectByBlockElement(
   element: Element | BlockComponentElement
 ) {
-  if (isImage(element))
-    return (
-      element.querySelector('.resizable-img') ?? element
-    ).getBoundingClientRect();
+  if (isImage(element)) {
+    const wrapper = element.querySelector('.affine-image-wrapper');
+    const resizable = element.querySelector('.resizable-img');
+    assertExists(wrapper);
+    assertExists(resizable);
+    const w = Rect.fromDOM(wrapper);
+    const r = Rect.fromDOM(resizable);
+    const d = w.intersect(r);
+    return d.toDOMRect();
+  }
   return getRectByBlockElement(element);
 }
 
