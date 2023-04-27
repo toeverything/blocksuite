0298e586e5a3c8df59447086856742355015bc90
himself65 <himself65@outlook.com>
2023-04-26 19:51:24 -0500

    fix: remove unused

--- packages/blocks/src/embed-block/image/image-block.ts
+++ packages/blocks/src/embed-block/image/image-block.ts
@@ -193,10 +193,6 @@ export class ImageBlockComponent extends WithDisposable(ShadowlessElement) {
       .catch(this._fetchError);
   };
 
-  private _onDoubleClick = () => {
-    console.log(1);
-  };
-
   override connectedCallback() {
     super.connectedCallback();
     this._imageState = 'loading';
