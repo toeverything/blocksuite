1ae13511f9835373d25f790a6b27f83a3da4a1b4
zqran <uuxnet@gmail.com>
2023-04-28 12:09:05 +0800

    fix(database): title column cannot be selected by block-level selection (#2346)

--- packages/blocks/src/database-block/database-block.ts
+++ packages/blocks/src/database-block/database-block.ts
@@ -17,7 +17,6 @@ import {
 } from '../__internal__/index.js';
 import { ShadowlessElement } from '../__internal__/utils/lit.js';
 import { tooltipStyle } from '../components/tooltip/tooltip.js';
-import { stopPropagation } from '../page-block/edgeless/utils.js';
 import type { DatabaseColumnHeader } from './components/column-header/column-header.js';
 import { registerInternalRenderer } from './components/column-type/index.js';
 import { DataBaseRowContainer } from './components/row-container.js';
@@ -183,16 +182,6 @@ export class DatabaseBlockComponent
       this.querySelector('affine-database-column-header')?.requestUpdate();
     });
 
-    // prevent block selection
-    const onStopPropagation = (event: Event) => event.stopPropagation();
-    const databaseRows = this.querySelectorAll<HTMLElement>(
-      '.affine-database-block-rows'
-    );
-    databaseRows.forEach(row => {
-      this._disposables.addFromEvent(row, 'pointerdown', onStopPropagation);
-      this._disposables.addFromEvent(row, 'pointermove', onStopPropagation);
-    });
-
     if (this.readonly) return;
     const tableContent = this._tableContainer.parentElement;
     assertExists(tableContent);
@@ -296,10 +285,7 @@ export class DatabaseBlockComponent
     );
 
     return html`
-      <div
-        class="affine-database-block-container"
-        @pointerdown=${stopPropagation}
-      >
+      <div class="affine-database-block-container">
         <div class="affine-database-block-title-container">
           <affine-database-title
             .addRow=${this._addRow}
