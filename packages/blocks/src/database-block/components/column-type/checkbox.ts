38d6fabcb0112eac5293426dc54b05a50724bde6
zqran <zqranx@gmail.com>
2023-05-04 09:35:30 +0800

    feat(database): support column type `checkbox` (#2376)
    Co-authored-by: Himself65 <himself65@outlook.com>

---
+++ packages/blocks/src/database-block/components/column-type/checkbox.ts
@@ -0,0 +1,65 @@
+import { css, html } from 'lit';
+import { customElement } from 'lit/decorators.js';
+import { literal } from 'lit/static-html.js';
+
+import { DatabaseCellElement, defineColumnRenderer } from '../../register.js';
+
+@customElement('affine-database-checkbox-cell')
+class CheckboxCell extends DatabaseCellElement<boolean> {
+  static override tag = literal`affine-database-checkbox-cell`;
+
+  static override styles = css`
+    affine-database-checkbox-cell {
+      display: block;
+      width: 100%;
+      height: 100%;
+    }
+
+    .affine-database-checkbox-container {
+      display: flex;
+      align-items: center;
+      height: 100%;
+    }
+  `;
+
+  protected override firstUpdated() {
+    this._disposables.addFromEvent(this, 'click', this._onChange);
+  }
+
+  private _onChange() {
+    const checked = !this.cell?.value;
+
+    this.rowHost.setValue(checked);
+  }
+
+  protected override render() {
+    const checked = this.cell?.value ?? false;
+
+    return html`<div class="affine-database-checkbox-container">
+      <input
+        class="affine-database-checkbox checkbox"
+        type="checkbox"
+        .checked=${checked}
+      />
+    </div>`;
+  }
+}
+
+@customElement('affine-database-checkbox-column-property-editing')
+class CheckboxColumnPropertyEditing extends DatabaseCellElement<boolean> {
+  static override tag = literal`affine-database-checkbox-column-property-editing`;
+}
+
+export const CheckboxColumnRenderer = defineColumnRenderer(
+  'checkbox',
+  () => ({}),
+  () => false,
+  {
+    Cell: CheckboxCell,
+    CellEditing: false,
+    ColumnPropertyEditing: CheckboxColumnPropertyEditing,
+  },
+  {
+    displayName: 'Checkbox',
+  }
+);
