44e59e486e36691de72bdc415cf485047e6a4424
xiaodong zuo <53252747+zuoxiaodong0815@users.noreply.github.com>
2023-05-12 15:05:37 +0800

    fix: part of what is copied in the list block is lost (#2538)

--- packages/blocks/src/__internal__/service/json2block.ts
+++ packages/blocks/src/__internal__/service/json2block.ts
@@ -43,10 +43,22 @@ export async function json2block(
         return sum + (data.insert?.length || 0);
       }, 0) ?? 0;
 
+    const shouldSplitBlock = focusedBlockModel.text?.length !== range.endOffset;
+
+    shouldSplitBlock &&
+      (await handleBlockSplit(page, focusedBlockModel, range.startOffset, 0));
+
     if (shouldMergeFirstBlock) {
       focusedBlockModel.text?.insertList(
         firstBlock.text || [],
         range?.startOffset || 0
+      );
+
+      await addSerializedBlocks(
+        page,
+        firstBlock.children || [],
+        focusedBlockModel,
+        0
       );
 
       await setRange(focusedBlockModel, {
@@ -54,12 +66,6 @@ export async function json2block(
         length: 0,
       });
     } else {
-      const shouldSplitBlock =
-        focusedBlockModel.text?.length !== range.endOffset;
-
-      shouldSplitBlock &&
-        (await handleBlockSplit(page, focusedBlockModel, range.startOffset, 0));
-
       const [id] = await addSerializedBlocks(
         page,
         pastedBlocks,
@@ -90,6 +96,12 @@ export async function json2block(
     focusedBlockModel.text?.insertList(
       firstBlock.text || [],
       range?.startOffset || 0
+    );
+    await addSerializedBlocks(
+      page,
+      firstBlock.children || [],
+      focusedBlockModel,
+      0
     );
   }
 
