e749be655d8e97659f89bcc090fe4ef2efe93d9b
Flrande <50035259+Flrande@users.noreply.github.com>
2023-05-15 19:03:46 +0800

    feat(phasor): support hand-drawn-like style (#2575)

--- packages/phasor/src/elements/shape/shapes/diamond.ts
+++ packages/phasor/src/elements/shape/shapes/diamond.ts
@@ -1,22 +1,17 @@
+import type { RoughCanvas } from 'roughjs/bin/canvas.js';
+
 import { type IBound, StrokeStyle } from '../../../consts.js';
-import { setLineDash } from '../../../utils/canvas.js';
 import { Utils } from '../../../utils/tl-utils.js';
 import type { HitTestOptions } from '../../surface-element.js';
 import type { ShapeElement } from '../shape-element.js';
 import type { ShapeMethods } from '../types.js';
 
-function createDiamondPath(width: number, height: number) {
-  const path = new Path2D();
-  path.moveTo(width / 2, 0);
-  path.lineTo(width, height / 2);
-  path.lineTo(width / 2, height);
-  path.lineTo(0, height / 2);
-  path.closePath();
-  return path;
-}
-
 export const DiamondMethods: ShapeMethods = {
-  render(ctx: CanvasRenderingContext2D, element: ShapeElement) {
+  render(
+    ctx: CanvasRenderingContext2D,
+    rc: RoughCanvas,
+    element: ShapeElement
+  ) {
     const {
       w,
       h,
@@ -33,18 +28,22 @@ export const DiamondMethods: ShapeMethods = {
 
     ctx.translate(renderOffset, renderOffset);
 
-    const path = createDiamondPath(renderWidth, renderHeight);
-    if (filled) {
-      ctx.fillStyle = realFillColor;
-      ctx.fill(path);
-    }
-
-    if (strokeWidth > 0 && strokeStyle !== StrokeStyle.None) {
-      ctx.strokeStyle = realStrokeColor;
-      setLineDash(ctx, strokeStyle);
-      ctx.lineWidth = strokeWidth;
-      ctx.stroke(path);
-    }
+    rc.polygon(
+      [
+        [renderWidth / 2, 0],
+        [renderWidth, renderHeight / 2],
+        [renderWidth / 2, renderHeight],
+        [0, renderHeight / 2],
+      ],
+      {
+        roughness: 2,
+        strokeLineDash:
+          strokeStyle === StrokeStyle.Dashed ? [12, 12] : undefined,
+        stroke: realStrokeColor,
+        strokeWidth,
+        fill: filled ? realFillColor : undefined,
+      }
+    );
   },
 
   hitTest(x: number, y: number, bound: IBound, options?: HitTestOptions) {
--- packages/phasor/src/elements/shape/shapes/rect.ts
+++ packages/phasor/src/elements/shape/shapes/rect.ts
@@ -1,64 +1,17 @@
+import type { RoughCanvas } from 'roughjs/bin/canvas.js';
+
 import { type IBound, StrokeStyle } from '../../../consts.js';
-import { setLineDash } from '../../../utils/canvas.js';
 import { isPointIn } from '../../../utils/hit-utils.js';
 import type { HitTestOptions } from '../../surface-element.js';
 import type { ShapeElement } from '../shape-element.js';
 import type { ShapeMethods } from '../types.js';
 
-/* "magic number" for bezier approximations of arcs (http://itc.ktu.lt/itc354/Riskus354.pdf) */
-const kRect = 1 - 0.5522847498;
-
-function createRectPath(
-  x: number,
-  y: number,
-  w: number,
-  h: number,
-  rx: number,
-  ry: number
-) {
-  const path = new Path2D();
-
-  path.moveTo(x + rx, y);
-
-  path.lineTo(x + w - rx, y);
-  path.bezierCurveTo(
-    x + w - kRect * rx,
-    y,
-    x + w,
-    y + kRect * ry,
-    x + w,
-    y + ry
-  );
-
-  path.lineTo(x + w, y + h - ry);
-  path.bezierCurveTo(
-    x + w,
-    y + h - kRect * ry,
-    x + w - kRect * rx,
-    y + h,
-    x + w - rx,
-    y + h
-  );
-
-  path.lineTo(x + rx, y + h);
-  path.bezierCurveTo(
-    x + kRect * rx,
-    y + h,
-    x,
-    y + h - kRect * ry,
-    x,
-    y + h - ry
-  );
-
-  path.lineTo(x, y + ry);
-  path.bezierCurveTo(x, y + kRect * ry, x + kRect * rx, y, x + rx, y);
-
-  path.closePath();
-  return path;
-}
-
 export const RectMethods: ShapeMethods = {
-  render(ctx: CanvasRenderingContext2D, element: ShapeElement) {
+  render(
+    ctx: CanvasRenderingContext2D,
+    rc: RoughCanvas,
+    element: ShapeElement
+  ) {
     const {
       w,
       h,
@@ -66,30 +19,22 @@ export const RectMethods: ShapeMethods = {
       filled,
       realFillColor,
       realStrokeColor,
-      radius,
       strokeStyle,
     } = element;
 
     const renderOffset = Math.max(strokeWidth, 0) / 2;
     const renderWidth = w - renderOffset * 2;
     const renderHeight = h - renderOffset * 2;
-    const r = Math.min(renderWidth * radius, renderHeight * radius);
 
     ctx.translate(renderOffset, renderOffset);
 
-    const path = createRectPath(0, 0, renderWidth, renderHeight, r, r);
-
-    if (filled) {
-      ctx.fillStyle = realFillColor;
-      ctx.fill(path);
-    }
-
-    if (strokeWidth > 0 && strokeStyle !== StrokeStyle.None) {
-      ctx.strokeStyle = realStrokeColor;
-      setLineDash(ctx, strokeStyle);
-      ctx.lineWidth = strokeWidth;
-      ctx.stroke(path);
-    }
+    rc.rectangle(0, 0, renderWidth, renderHeight, {
+      roughness: 2,
+      strokeLineDash: strokeStyle === StrokeStyle.Dashed ? [12, 12] : undefined,
+      stroke: realStrokeColor,
+      strokeWidth,
+      fill: filled ? realFillColor : undefined,
+    });
   },
 
   hitTest(x: number, y: number, bound: IBound, options?: HitTestOptions) {
