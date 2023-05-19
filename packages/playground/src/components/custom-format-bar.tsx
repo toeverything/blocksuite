c53524f0a4bafae0f4a0a3a905b5e722a15d54e6
himself65 <himself65@outlook.com>
2023-05-18 19:34:18 -0700

    fix: alert

--- packages/playground/src/components/custom-format-bar.tsx
+++ packages/playground/src/components/custom-format-bar.tsx
@@ -4,7 +4,23 @@ import { createRoot } from 'react-dom/client';
 FormatQuickBar.customElements.push(page => {
   const element = document.createElement('div');
 
-  createRoot(element).render(<div>Hello, world!</div>);
+  createRoot(element).render(
+    <div
+      style={{
+        height: '32px',
+        width: '32px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        cursor: 'pointer',
+      }}
+      onClick={() => {
+        window.alert('Hello World!');
+      }}
+    >
+      <div>X</div>
+    </div>
+  );
 
   return element;
 });
