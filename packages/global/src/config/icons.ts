015624906fb237abe887db7b4f43e8b740bdf294
Fangdun Tsai <cfddream@gmail.com>
2023-05-25 20:16:36 +0800

    fix: connector icons

--- packages/global/src/config/icons.ts
+++ packages/global/src/config/icons.ts
@@ -1502,7 +1502,6 @@ export const ConnectorXIcon = html`<svg
   width="24"
   height="24"
   viewBox="0 0 24 24"
-  fill="none"
   xmlns="http://www.w3.org/2000/svg"
 >
   <path
@@ -1516,7 +1515,6 @@ export const ConnectorLIcon = html`<svg
   width="24"
   height="24"
   viewBox="0 0 24 24"
-  fill="none"
   xmlns="http://www.w3.org/2000/svg"
 >
   <path
