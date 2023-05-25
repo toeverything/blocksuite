eae404fab3fefff6ecbb6dedbc2bd52644d2d24b
zqran <uuxnet@gmail.com>
2023-05-25 13:56:00 +0800

    feat: add keyup

--- packages/lit/src/event/keyboard.ts
+++ packages/lit/src/event/keyboard.ts
@@ -7,6 +7,7 @@ export class KeyboardControl {
 
   listen() {
     this._dispatcher.disposables.addFromEvent(document, 'keydown', this._down);
+    this._dispatcher.disposables.addFromEvent(document, 'keyup', this._up);
   }
 
   private _down = (event: KeyboardEvent) => {
@@ -18,5 +19,13 @@ export class KeyboardControl {
       'keyDown',
       UIEventStateContext.from(keyboardEventState)
     );
+  };
+
+  private _up = (event: KeyboardEvent) => {
+    const keyboardEventState = new KeyboardEventState({
+      event,
+    });
+
+    this._dispatcher.run('keyUp', UIEventStateContext.from(keyboardEventState));
   };
 }
