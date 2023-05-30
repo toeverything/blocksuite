2be59152ef48cbdcb8665f6159549094393f563d
Yifeng Wang <doodlewind@toeverything.info>
2023-05-30 17:10:52 +0800

    refactor: simplify test case

--- tests/edgeless/selection.spec.ts
+++ tests/edgeless/selection.spec.ts
@@ -1,19 +1,13 @@
-import {
-  decreaseZoomLevel,
-  increaseZoomLevel,
-  switchEditorEmbedMode,
-  switchEditorMode,
-} from '../utils/actions/edgeless.js';
+import * as actions from '../utils/actions/edgeless.js';
 import {
   addBasicRectShapeElement,
+  clickInCenter,
   dragBetweenCoords,
   enterPlaygroundRoom,
+  getBoundingRect,
   initEmptyEdgelessState,
 } from '../utils/actions/index.js';
-import {
-  assertEdgelessNonSelectedRect,
-  assertEdgelessSelectedRect,
-} from '../utils/asserts.js';
+import { assertEdgelessSelectedRect } from '../utils/asserts.js';
 import { test } from '../utils/playwright.js';
 
 test('should update rect of selection when resizing viewport', async ({
@@ -21,69 +15,50 @@ test('should update rect of selection when resizing viewport', async ({
 }) => {
   await enterPlaygroundRoom(page);
   await initEmptyEdgelessState(page);
-  await switchEditorMode(page);
+  await actions.switchEditorMode(page);
 
   await addBasicRectShapeElement(page, { x: 100, y: 100 }, { x: 200, y: 200 });
-
   await dragBetweenCoords(page, { x: 120, y: 90 }, { x: 220, y: 130 });
+
+  const selectedRectClass = '.affine-edgeless-selected-rect';
+
   await assertEdgelessSelectedRect(page, [100, 100, 100, 100]);
+  await actions.decreaseZoomLevel(page);
+  await actions.decreaseZoomLevel(page);
 
-  await decreaseZoomLevel(page);
-  await decreaseZoomLevel(page);
-
-  const div = page.locator('.affine-edgeless-selected-rect');
-
-  const selectedRect0 = await div.boundingBox();
-  if (!selectedRect0) {
-    throw new Error('Missing .affine-edgeless-selected-rect');
-  }
+  let selectedRect = await getBoundingRect(page, selectedRectClass);
 
   await page.mouse.click(0, 0);
-  await assertEdgelessNonSelectedRect(page);
-
-  await page.mouse.click(
-    selectedRect0.x + selectedRect0.width / 2,
-    selectedRect0.y + selectedRect0.height / 2
-  );
+  await clickInCenter(page, selectedRect);
   await assertEdgelessSelectedRect(page, [
-    selectedRect0.x,
-    selectedRect0.y,
+    selectedRect.x,
+    selectedRect.y,
     50,
     50,
   ]);
 
-  await switchEditorEmbedMode(page);
+  await actions.switchEditorEmbedMode(page);
 
-  const selectedRect1 = await div.boundingBox();
-  if (!selectedRect1) {
-    throw new Error('Missing .affine-edgeless-selected-rect');
-  }
+  selectedRect = await getBoundingRect(page, selectedRectClass);
 
   await page.mouse.click(0, 0);
-  await assertEdgelessNonSelectedRect(page);
-
-  await page.mouse.click(
-    selectedRect1.x + selectedRect1.width / 2,
-    selectedRect1.y + selectedRect1.height / 2
-  );
+  await clickInCenter(page, selectedRect);
   await assertEdgelessSelectedRect(page, [
-    selectedRect1.x,
-    selectedRect1.y,
+    selectedRect.x,
+    selectedRect.y,
     50,
     50,
   ]);
 
-  await switchEditorEmbedMode(page);
-
+  await actions.switchEditorEmbedMode(page);
   await assertEdgelessSelectedRect(page, [
-    selectedRect0.x,
-    selectedRect0.y,
+    selectedRect.x,
+    selectedRect.y,
     50,
     50,
   ]);
 
-  await increaseZoomLevel(page);
-  await increaseZoomLevel(page);
-
+  await actions.increaseZoomLevel(page);
+  await actions.increaseZoomLevel(page);
   await assertEdgelessSelectedRect(page, [100, 100, 100, 100]);
 });
