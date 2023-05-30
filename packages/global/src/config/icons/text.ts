28dc5bb75b5957810b0896b97391e981d8039c73
JimmFly <yangjinfei001@gmail.com>
2023-05-30 17:32:59 +0800

    chore: adjust block hub style (#2876)
    Co-authored-by: Yifeng Wang <doodlewind@toeverything.info>

--- packages/global/src/config/icons/text.ts
+++ packages/global/src/config/icons/text.ts
@@ -548,6 +548,20 @@ export const BlockHubIcon = html`<svg
   />
 </svg>`;
 
+export const BlockHubRoundedRectangleIcon = html`<svg
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M2.25 7C2.25 5.48122 3.48122 4.25 5 4.25H19C20.5188 4.25 21.75 5.48122 21.75 7V17C21.75 18.5188 20.5188 19.75 19 19.75H5C3.48122 19.75 2.25 18.5188 2.25 17V7ZM5 5.75C4.30964 5.75 3.75 6.30964 3.75 7V17C3.75 17.6904 4.30964 18.25 5 18.25H19C19.6904 18.25 20.25 17.6904 20.25 17V7C20.25 6.30964 19.6904 5.75 19 5.75H5Z"
+    fill="#77757D"
+  />
+</svg>`;
+
 export const SearchIcon = html`<svg
   width="20"
   height="20"
