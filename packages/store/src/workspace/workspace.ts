f5691c3b344dddc08566259b169e01191a375e34
Yifeng Wang <doodlewind@toeverything.info>
2023-06-01 11:08:32 +0800

    feat: add snapshot scaffold

--- packages/store/src/workspace/workspace.ts
+++ packages/store/src/workspace/workspace.ts
@@ -12,6 +12,7 @@ import {
   Store,
   type StoreOptions,
 } from '../store.js';
+import { serializeYDoc } from '../utils/jsx.js';
 import { BacklinkIndexer } from './indexer/backlink.js';
 import { BlockIndexer } from './indexer/base.js';
 import { type QueryContent, SearchIndexer } from './indexer/search.js';
@@ -331,11 +332,9 @@ export class Workspace {
     URL.revokeObjectURL(fileUrl);
   }
 
-  /**
-   * @internal Only for testing
-   */
-  importYDoc(): Promise<void> {
-    return new Promise((res, rej) => {
+  /** @internal Only for testing */
+  async importYDoc() {
+    return new Promise<void>((resolve, reject) => {
       const input = document.createElement('input');
       input.type = 'file';
       input.accept = '.ydoc';
@@ -343,20 +342,375 @@ export class Workspace {
       input.onchange = async () => {
         const file = input.files?.item(0);
         if (!file) {
-          return rej();
+          return reject();
         }
         const buffer = await file.arrayBuffer();
         Y.applyUpdate(this.doc, new Uint8Array(buffer));
-        res();
+        resolve();
       };
-      input.onerror = rej;
+      input.onerror = reject;
       input.click();
     });
   }
 
-  /**
-   * @internal Only for testing
-   */
+  /** @internal Only for testing */
+  importSnapshot() {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const json: any = {
+      'space:meta': {
+        pages: [
+          {
+            id: 'page0',
+            title: 'Welcome to BlockSuite Playground',
+            createDate: 1685558351082,
+            subpageIds: [],
+          },
+        ],
+        versions: {
+          'affine:code': 1,
+          'affine:paragraph': 1,
+          'affine:page': 2,
+          'affine:list': 1,
+          'affine:frame': 1,
+          'affine:divider': 1,
+          'affine:embed': 1,
+          'affine:surface': 3,
+          'affine:bookmark': 1,
+          'affine:database': 1,
+        },
+      },
+      'space:page0': {
+        '4266130717:0': {
+          'sys:id': '4266130717:0',
+          'sys:flavour': 'affine:page',
+          'sys:children': ['4266130717:1', '4266130717:2'],
+          'prop:title': [
+            {
+              insert: 'Welcome to BlockSuite Playground',
+            },
+          ],
+        },
+        '4266130717:1': {
+          'sys:id': '4266130717:1',
+          'sys:flavour': 'affine:surface',
+          'sys:children': [],
+          elements: {
+            '0': {
+              id: '0',
+              index: 'a0',
+              type: 'shape',
+              xywh: '[0,-100,100,100]',
+              seed: 1820170299,
+              shapeType: 'rect',
+              radius: 0,
+              filled: false,
+              fillColor: '--affine-palette-transparent',
+              strokeWidth: 4,
+              strokeColor: '--affine-palette-line-black',
+              strokeStyle: 'solid',
+              roughness: 2,
+            },
+          },
+        },
+        '4266130717:2': {
+          'sys:id': '4266130717:2',
+          'sys:flavour': 'affine:frame',
+          'sys:children': [
+            '4266130717:3',
+            '4266130717:4',
+            '4266130717:5',
+            '4266130717:6',
+            '4266130717:7',
+            '4266130717:8',
+            '4266130717:9',
+            '4266130717:10',
+            '4266130717:11',
+            '4266130717:12',
+            '4266130717:13',
+          ],
+          'prop:xywh': '[0,0,800,48]',
+          'prop:background': '--affine-background-secondary-color',
+          'prop:index': 'a0',
+        },
+        '4266130717:3': {
+          'sys:id': '4266130717:3',
+          'sys:flavour': 'affine:paragraph',
+          'sys:children': [],
+          'prop:type': 'text',
+          'prop:text': [
+            {
+              insert: 'This playground is designed to:',
+            },
+          ],
+        },
+        '4266130717:4': {
+          'sys:id': '4266130717:4',
+          'sys:flavour': 'affine:list',
+          'sys:children': [],
+          'prop:type': 'bulleted',
+          'prop:text': [
+            {
+              insert: '📝 Test basic editing experience.',
+            },
+          ],
+          'prop:checked': false,
+        },
+        '4266130717:5': {
+          'sys:id': '4266130717:5',
+          'sys:flavour': 'affine:list',
+          'sys:children': [],
+          'prop:type': 'bulleted',
+          'prop:text': [
+            {
+              insert: '⚙️ Serve as E2E test entry.',
+            },
+          ],
+          'prop:checked': false,
+        },
+        '4266130717:6': {
+          'sys:id': '4266130717:6',
+          'sys:flavour': 'affine:list',
+          'sys:children': [],
+          'prop:type': 'bulleted',
+          'prop:text': [
+            {
+              insert:
+                '🔗 Demonstrate how BlockSuite reconciles real-time collaboration with ',
+            },
+            {
+              insert: 'local-first',
+              attributes: {
+                link: 'https://martin.kleppmann.com/papers/local-first.pdf',
+              },
+            },
+            {
+              insert: ' data ownership.',
+            },
+          ],
+          'prop:checked': false,
+        },
+        '4266130717:7': {
+          'sys:id': '4266130717:7',
+          'sys:flavour': 'affine:paragraph',
+          'sys:children': [],
+          'prop:type': 'h2',
+          'prop:text': [
+            {
+              insert: 'Controlling Playground Data Source',
+            },
+          ],
+        },
+        '4266130717:8': {
+          'sys:id': '4266130717:8',
+          'sys:flavour': 'affine:paragraph',
+          'sys:children': [],
+          'prop:type': 'text',
+          'prop:text': [
+            {
+              insert: 'You might initially enter this page with the ',
+            },
+            {
+              insert: '?init',
+              attributes: {
+                code: true,
+              },
+            },
+            {
+              insert:
+                ' URL param. This is the default (opt-in) setup that automatically loads this built-in article. Meanwhile, you\'ll connect to a random single-user room via a WebRTC provider by default. This is the "single-user mode" for local testing.',
+            },
+          ],
+        },
+        '4266130717:9': {
+          'sys:id': '4266130717:9',
+          'sys:flavour': 'affine:paragraph',
+          'sys:children': [],
+          'prop:type': 'text',
+          'prop:text': [
+            {
+              insert:
+                'To test real-time collaboration, you can specify the room to join by adding the ',
+            },
+            {
+              insert: '?room=foo',
+              attributes: {
+                code: true,
+              },
+            },
+            {
+              insert: ' config - Try opening this page with ',
+            },
+            {
+              insert: '?room=foo',
+              attributes: {
+                code: true,
+              },
+            },
+            {
+              insert: ' in two different tabs and see what happens!',
+            },
+          ],
+        },
+        '4266130717:10': {
+          'sys:id': '4266130717:10',
+          'sys:flavour': 'affine:paragraph',
+          'sys:children': [],
+          'prop:type': 'quote',
+          'prop:text': [
+            {
+              insert:
+                'Note that the second and subsequent users should not open the page with the ',
+            },
+            {
+              insert: '?init',
+              attributes: {
+                code: true,
+              },
+            },
+            {
+              insert:
+                ' param in this case. Also, due to the P2P nature of WebRTC, as long as there is at least one user connected to the room, the content inside the room will ',
+            },
+            {
+              insert: 'always',
+              attributes: {
+                bold: true,
+              },
+            },
+            {
+              insert: ' exist.',
+            },
+          ],
+        },
+        '4266130717:11': {
+          'sys:id': '4266130717:11',
+          'sys:flavour': 'affine:paragraph',
+          'sys:children': [],
+          'prop:type': 'text',
+          'prop:text': [
+            {
+              insert:
+                'If you are the only user in the room, your content will be lost after refresh. This is great for local debugging. But if you want local persistence, you can open this page with the ',
+            },
+            {
+              insert: '?providers=indexeddb&room=foo',
+              attributes: {
+                code: true,
+              },
+            },
+            {
+              insert:
+                ' config, then click the init button in the bottom-left corner to initialize this default content.',
+            },
+          ],
+        },
+        '4266130717:12': {
+          'sys:id': '4266130717:12',
+          'sys:flavour': 'affine:paragraph',
+          'sys:children': [],
+          'prop:type': 'text',
+          'prop:text': [
+            {
+              insert:
+                'As a pro tip, you can combine multiple providers! For example, feel free to open this page with ',
+            },
+            {
+              insert: '?providers=indexeddb,webrtc&room=hello',
+              attributes: {
+                code: true,
+              },
+            },
+            {
+              insert:
+                ' params, and see if everything works as expected. Have fun!',
+            },
+          ],
+        },
+        '4266130717:13': {
+          'sys:id': '4266130717:13',
+          'sys:flavour': 'affine:paragraph',
+          'sys:children': [],
+          'prop:type': 'text',
+          'prop:text': [
+            {
+              insert: 'For any feedback, please visit ',
+            },
+            {
+              insert: 'BlockSuite issues',
+              attributes: {
+                link: 'https://github.com/toeverything/blocksuite/issues',
+              },
+            },
+            {
+              insert: ' 📍',
+            },
+          ],
+        },
+      },
+    };
+
+    if (!json['space:meta']) return;
+
+    const unprefix = (str: string) =>
+      str.replace('sys:', '').replace('prop:', '').replace('space:', '');
+
+    const visited = new Set();
+
+    const pageIds = Object.keys(json).filter(key => key !== 'space:meta');
+    const firstPageBlocks = json[pageIds[0]];
+
+    console.log(firstPageBlocks);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const sanitize = (props: any) => {
+      const result: Record<string, unknown> = {};
+      Object.keys(props).forEach(key => {
+        if (
+          key === 'sys:children' ||
+          // key === 'sys:id' ||
+          key === 'sys:flavour'
+        ) {
+          return;
+        }
+        result[unprefix(key)] = props[key];
+      });
+      return result;
+    };
+
+    const addBlockByProps = (
+      page: Page,
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      props: any,
+      parent: string | null
+    ) => {
+      const sanitizedProps = sanitize(props);
+      page.addBlock(props['sys:flavour'], sanitizedProps, parent);
+      for (const id of props['sys:children']) {
+        if (visited.has(id)) continue;
+        visited.add(id);
+        addBlockByProps(page, firstPageBlocks[id], props['sys:id']);
+      }
+    };
+
+    const importPage = (pageId: string) => {
+      const page = this.createPage({ id: unprefix(pageId) });
+
+      // const blockIds = Object.keys(firstPageBlocks);
+      Object.values(firstPageBlocks).forEach(prefixedProps => {
+        addBlockByProps(page, prefixedProps, null);
+      });
+    };
+
+    importPage(pageIds[0]);
+    // console.log(json);
+  }
+
+  /** @internal Only for testing */
+  exportSnapshot() {
+    return serializeYDoc(this.doc);
+  }
+
+  /** @internal Only for testing */
   exportJSX(blockId?: string, pageId = this.meta.pageMetas.at(0)?.id) {
     assertExists(pageId);
     return this._store.exportJSX(pageId, blockId);
