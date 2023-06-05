e6bf7fb29e4c37bff4fbe0b8bcbe058e67fae583
QiShaoXuan <qishaoxuan777@gmail.com>
2023-06-05 11:41:12 +0800

    fix: error toast content

--- packages/blocks/src/bookmark-block/components/bookmark-operation-popper.ts
+++ packages/blocks/src/bookmark-block/components/bookmark-operation-popper.ts
@@ -81,7 +81,7 @@ const operations: Operation[] = [
         startOffset: 0,
         endOffset: 0,
       });
-      toast('Copied Database to clipboard');
+      toast('Copied link to clipboard');
       callback?.('copy');
     },
   },
