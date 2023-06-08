9fbdf6ba1027f710b086c1ad3bfe99957facc5a6
zqran <zqranx@gmail.com>
2023-06-08 10:25:59 +0800

    feat(database): support select option to modify color (#3015)

--- packages/blocks/src/database-block/utils.ts
+++ packages/blocks/src/database-block/utils.ts
@@ -36,25 +36,62 @@ export function onClickOutside(
   return removeClickListener;
 }
 
-/** select tag color poll */
-const tagColorPoll: string[] = [
-  'var(--affine-tag-blue)',
-  'var(--affine-tag-green)',
-  'var(--affine-tag-teal)',
-  'var(--affine-tag-white)',
-  'var(--affine-tag-purple)',
-  'var(--affine-tag-red)',
-  'var(--affine-tag-pink)',
-  'var(--affine-tag-yellow)',
-  'var(--affine-tag-orange)',
-  'var(--affine-tag-gray)',
+export type SelectOptionColor = {
+  color: string;
+  name: string;
+};
+
+export const selectOptionColors: SelectOptionColor[] = [
+  {
+    color: 'var(--affine-tag-red)',
+    name: 'Red',
+  },
+  {
+    color: 'var(--affine-tag-pink)',
+    name: 'Pink',
+  },
+  {
+    color: 'var(--affine-tag-orange)',
+    name: 'Orange',
+  },
+  {
+    color: 'var(--affine-tag-yellow)',
+    name: 'Yellow',
+  },
+  {
+    color: 'var(--affine-tag-green)',
+    name: 'Green',
+  },
+  {
+    color: 'var(--affine-tag-teal)',
+    name: 'Teal',
+  },
+  {
+    color: 'var(--affine-tag-blue)',
+    name: 'Blue',
+  },
+  {
+    color: 'var(--affine-tag-purple)',
+    name: 'Purple',
+  },
+  {
+    color: 'var(--affine-tag-gray)',
+    name: 'Gray',
+  },
+  {
+    color: 'var(--affine-tag-white)',
+    name: 'White',
+  },
 ];
 
+/** select tag color poll */
+const selectTagColorPoll = selectOptionColors.map(color => color.color);
+
 function tagColorHelper() {
-  let colors = [...tagColorPoll];
+  let colors = [...selectTagColorPoll];
   return () => {
     if (colors.length === 0) {
-      colors = [...tagColorPoll];
+      colors = [...selectTagColorPoll];
     }
     const index = Math.floor(Math.random() * colors.length);
     const color = colors.splice(index, 1)[0];
