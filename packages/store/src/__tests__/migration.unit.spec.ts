a576182bf5186888e5ee5065ff6d49773408fe4a
zuozijian3720 <zuozijian1994@gmail.com>
2023-06-08 17:30:58 +0800

    test: add database block migration test

--- packages/store/src/__tests__/migration.unit.spec.ts
+++ packages/store/src/__tests__/migration.unit.spec.ts
@@ -84,7 +84,7 @@ describe('migration', () => {
     tryMigrate(doc);
     assert.deepEqual(doc.toJSON()['space:meta']['versions'], {
       'affine:paragraph': 1,
-      'affine:database': 1,
+      'affine:database': 2,
       'affine:page': 2,
       'affine:list': 1,
       'affine:frame': 1,
@@ -129,7 +129,7 @@ describe('migration', () => {
     tryMigrate(doc);
     assert.deepEqual(doc.toJSON()['space:meta']['versions'], {
       'affine:paragraph': 1,
-      'affine:database': 1,
+      'affine:database': 2,
       'affine:page': 2,
       'affine:list': 1,
       'affine:frame': 1,
@@ -149,5 +149,229 @@ describe('migration', () => {
       ...oldElement?.toJSON(),
       seed: newElement?.get('seed'),
     });
+  });
+  test('migrate to new database block (support multi-view)', async () => {
+    {
+      const doc = await loadBinary('legacy-database-single-view');
+
+      const oldDatabase = doc
+        .getMap('space:page0')
+        .get('451460207:15') as Y.Map<unknown>;
+
+      assert.deepEqual(oldDatabase?.toJSON(), {
+        'sys:id': '451460207:15',
+        'sys:flavour': 'affine:database',
+        'sys:children': ['451460207:16', '451460207:17', '451460207:18'],
+        'prop:mode': 'table',
+        'prop:title': 'Database',
+        'prop:cells': {
+          '451460207:16': {
+            '451460207:20': {
+              columnId: '451460207:20',
+              value: '123',
+            },
+            '451460207:19': {
+              columnId: '451460207:19',
+              value: [
+                {
+                  id: 'Lcq8G-XAgU',
+                  value: 'asd',
+                  color: 'var(--affine-tag-gray)',
+                },
+              ],
+            },
+          },
+          '451460207:17': {
+            '451460207:20': {
+              columnId: '451460207:20',
+              value: '321',
+            },
+            '451460207:19': {
+              columnId: '451460207:19',
+              value: [
+                {
+                  id: '8qoowbabLE',
+                  value: 'ddd',
+                  color: 'var(--affine-tag-green)',
+                },
+              ],
+            },
+          },
+          '451460207:18': {
+            '451460207:20': {
+              columnId: '451460207:20',
+              value: '',
+            },
+            '451460207:19': {
+              columnId: '451460207:19',
+              value: [
+                {
+                  id: '8qoowbabLE',
+                  value: 'ddd',
+                  color: 'var(--affine-tag-green)',
+                },
+                {
+                  id: '40Q64UK52L',
+                  value: 'dddd',
+                  color: 'var(--affine-tag-red)',
+                },
+              ],
+            },
+          },
+        },
+        'prop:columns': [
+          {
+            name: 'Tag',
+            type: 'multi-select',
+            width: 200,
+            hide: false,
+            selection: [
+              {
+                id: 'Lcq8G-XAgU',
+                value: 'asd',
+                color: 'var(--affine-tag-gray)',
+              },
+              {
+                id: '8qoowbabLE',
+                value: 'ddd',
+                color: 'var(--affine-tag-green)',
+              },
+              {
+                id: '40Q64UK52L',
+                value: 'dddd',
+                color: 'var(--affine-tag-red)',
+              },
+            ],
+            id: '451460207:19',
+          },
+          {
+            type: 'number',
+            name: 'Column 2',
+            width: 200,
+            hide: false,
+            selection: [],
+            id: '451460207:20',
+            decimal: 0,
+          },
+        ],
+        'prop:titleColumnName': 'Title',
+        'prop:titleColumnWidth': 432,
+      });
+
+      tryMigrate(doc);
+      assert.deepEqual(doc.toJSON()['space:meta']['versions'], {
+        'affine:bookmark': 1,
+        'affine:paragraph': 1,
+        'affine:database': 2,
+        'affine:page': 2,
+        'affine:list': 1,
+        'affine:frame': 1,
+        'affine:divider': 1,
+        'affine:embed': 1,
+        'affine:code': 1,
+        'affine:surface': 3,
+      });
+
+      const newDatabase = doc
+        .getMap('space:page0')
+        .get('451460207:15') as Y.Map<unknown>;
+
+      assert.deepEqual(newDatabase?.toJSON(), {
+        'sys:id': '451460207:15',
+        'sys:flavour': 'affine:database',
+        'sys:children': ['451460207:16', '451460207:17', '451460207:18'],
+        'prop:title': 'Database',
+        'prop:cells': {
+          '451460207:16': {
+            '451460207:20': {
+              columnId: '451460207:20',
+              value: '123',
+            },
+            '451460207:19': {
+              columnId: '451460207:19',
+              value: ['Lcq8G-XAgU'],
+            },
+          },
+          '451460207:17': {
+            '451460207:20': {
+              columnId: '451460207:20',
+              value: '321',
+            },
+            '451460207:19': {
+              columnId: '451460207:19',
+              value: ['8qoowbabLE'],
+            },
+          },
+          '451460207:18': {
+            '451460207:20': {
+              columnId: '451460207:20',
+              value: '',
+            },
+            '451460207:19': {
+              columnId: '451460207:19',
+              value: ['8qoowbabLE', '40Q64UK52L'],
+            },
+          },
+        },
+        'prop:columns': [
+          {
+            id: '451460207:19',
+            type: 'multi-select',
+            name: 'Tag',
+            data: {
+              options: [
+                {
+                  id: 'Lcq8G-XAgU',
+                  value: 'asd',
+                  color: 'var(--affine-tag-gray)',
+                },
+                {
+                  id: '8qoowbabLE',
+                  value: 'ddd',
+                  color: 'var(--affine-tag-green)',
+                },
+                {
+                  id: '40Q64UK52L',
+                  value: 'dddd',
+                  color: 'var(--affine-tag-red)',
+                },
+              ],
+            },
+          },
+          {
+            id: '451460207:20',
+            type: 'number',
+            name: 'Column 2',
+            data: {},
+          },
+        ],
+        'prop:titleColumnName': 'Title',
+        'prop:titleColumnWidth': 432,
+        'prop:views': [
+          {
+            id: 'default',
+            name: 'Table',
+            columns: [
+              {
+                id: '451460207:19',
+                width: 200,
+                hide: false,
+              },
+              {
+                id: '451460207:20',
+                width: 200,
+                hide: false,
+              },
+            ],
+            filter: {
+              type: 'group',
+              op: 'and',
+              conditions: [],
+            },
+            mode: 'table',
+          },
+        ],
+      });
+    }
   });
 });
