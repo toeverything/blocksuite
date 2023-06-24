3dd9485e170fb2f228ab40f60c3044df2597051e
himself65 <himself65@outlook.com>
2023-06-24 20:45:52 +0800

    fix: code

--- packages/store/src/providers/broadcast-channel.ts
+++ packages/store/src/providers/broadcast-channel.ts
@@ -1,17 +1,17 @@
-import { BroadcastMessageChannel } from 'async-call-rpc/utils/web/broadcast.channel.js';
-
 import { createAsyncCallRPCProviderCreator } from './async-call-rpc.js';
 import type { DocProviderCreator } from './type.js';
 
 export const createBroadcastChannelProvider: DocProviderCreator = (...args) => {
   const id: string = args[0];
-  const broadcastMessageChannel = new BroadcastMessageChannel(id);
+  const channelPromise = import(
+    'async-call-rpc/utils/web/broadcast.channel.js'
+  ).then(({ BroadcastMessageChannel }) => new BroadcastMessageChannel(id));
   return createAsyncCallRPCProviderCreator(
     'broadcast-channel',
-    broadcastMessageChannel,
+    channelPromise,
     {
       cleanup: () => {
-        broadcastMessageChannel.close();
+        channelPromise.then(channel => channel.close());
       },
     }
   )(...args);
