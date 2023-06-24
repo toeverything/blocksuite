10e3e0be24a2a7423442788f057dd758a93578d9
Alex Yang <himself65@outlook.com>
2023-06-24 23:17:14 +0800

    fix(store): broadcast channel (#3200)

--- packages/store/src/providers/broadcast-channel.ts
+++ packages/store/src/providers/broadcast-channel.ts
@@ -1,18 +1,28 @@
+import type { EventBasedChannel } from 'async-call-rpc';
+
 import { createAsyncCallRPCProviderCreator } from './async-call-rpc.js';
 import type { DocProviderCreator } from './type.js';
 
+class BroadcastMessageChannel
+  extends BroadcastChannel
+  implements EventBasedChannel
+{
+  on(eventListener: (data: unknown) => void) {
+    const f = (e: MessageEvent): void => eventListener(e.data);
+    this.addEventListener('message', f);
+    return () => this.removeEventListener('message', f);
+  }
+  send(data: unknown) {
+    super.postMessage(data);
+  }
+}
+
 export const createBroadcastChannelProvider: DocProviderCreator = (...args) => {
   const id: string = args[0];
-  const channelPromise = import(
-    'async-call-rpc/utils/web/broadcast.channel.js'
-  ).then(({ BroadcastMessageChannel }) => new BroadcastMessageChannel(id));
-  return createAsyncCallRPCProviderCreator(
-    'broadcast-channel',
-    channelPromise,
-    {
-      cleanup: () => {
-        channelPromise.then(channel => channel.close());
-      },
-    }
-  )(...args);
+  const channel = new BroadcastMessageChannel(id);
+  return createAsyncCallRPCProviderCreator('broadcast-channel', channel, {
+    cleanup: () => {
+      channel.close();
+    },
+  })(...args);
 };
