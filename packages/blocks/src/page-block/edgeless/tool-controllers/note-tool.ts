3cb4fd82fa823022b650790dbca7a130d3fece58
donteatfriedrice <huisheng.chen7788@outlook.com>
2023-07-04 22:10:28 +0800

    feat(edgeless): add overlay for note and shape tool

--- packages/blocks/src/page-block/edgeless/tool-controllers/note-tool.ts
+++ packages/blocks/src/page-block/edgeless/tool-controllers/note-tool.ts
@@ -1,11 +1,87 @@
 import type { PointerEventState } from '@blocksuite/block-std';
+import { Overlay } from '@blocksuite/phasor/renderer.js';
 
-import { type NoteTool } from '../../../__internal__/index.js';
+import {
+  type EdgelessTool,
+  type NoteTool,
+} from '../../../__internal__/index.js';
 import { noop } from '../../../__internal__/index.js';
 import { DEFAULT_NOTE_WIDTH } from '../utils/consts.js';
 import { addNote, type NoteOptions } from '../utils/note.js';
 import { EdgelessToolController } from './index.js';
 
+const OVERLAY_OFFSET_X = 6;
+const OVERLAY_OFFSET_Y = 6;
+const OVERLAY_WIDTH = 100;
+const OVERLAY_HEIGHT = 50;
+const OVERLAY_CORNER_RADIUS = 6;
+
+class NoteOverlay extends Overlay {
+  x = 0;
+  y = 0;
+  text = '';
+  globalAlpha = 0;
+  override render(ctx: CanvasRenderingContext2D): void {
+    ctx.globalAlpha = this.globalAlpha;
+    // TODO: use theme color (should consider dark mode)
+    // Draw the overlay rectangle
+    ctx.strokeStyle = '#E3E2E4';
+    ctx.fillStyle = 'white';
+    ctx.lineWidth = 4;
+    ctx.beginPath();
+    ctx.moveTo(this.x + OVERLAY_CORNER_RADIUS, this.y);
+    ctx.lineTo(this.x + OVERLAY_WIDTH - OVERLAY_CORNER_RADIUS, this.y);
+    ctx.quadraticCurveTo(
+      this.x + OVERLAY_WIDTH,
+      this.y,
+      this.x + OVERLAY_WIDTH,
+      this.y + OVERLAY_CORNER_RADIUS
+    );
+    ctx.lineTo(
+      this.x + OVERLAY_WIDTH,
+      this.y + OVERLAY_HEIGHT - OVERLAY_CORNER_RADIUS
+    );
+    ctx.quadraticCurveTo(
+      this.x + OVERLAY_WIDTH,
+      this.y + OVERLAY_HEIGHT,
+      this.x + OVERLAY_WIDTH - OVERLAY_CORNER_RADIUS,
+      this.y + OVERLAY_HEIGHT
+    );
+    ctx.lineTo(this.x + OVERLAY_CORNER_RADIUS, this.y + OVERLAY_HEIGHT);
+    ctx.quadraticCurveTo(
+      this.x,
+      this.y + OVERLAY_HEIGHT,
+      this.x,
+      this.y + OVERLAY_HEIGHT - OVERLAY_CORNER_RADIUS
+    );
+    ctx.lineTo(this.x, this.y + OVERLAY_CORNER_RADIUS);
+    ctx.quadraticCurveTo(
+      this.x,
+      this.y,
+      this.x + OVERLAY_CORNER_RADIUS,
+      this.y
+    );
+    ctx.closePath();
+    ctx.stroke();
+    ctx.fill();
+
+    // Draw the overlay text
+    ctx.fillStyle = '#77757D';
+    let fontSize = 20;
+    ctx.font = `${fontSize}px Arial`;
+    ctx.textAlign = 'left';
+    ctx.textBaseline = 'middle';
+
+    // measure the width of the text
+    // if the text is wider than the rectangle, reduce the maximum width of the text
+    while (ctx.measureText(this.text).width > OVERLAY_WIDTH - 10) {
+      fontSize -= 2;
+      ctx.font = `${fontSize}px Arial`;
+    }
+
+    ctx.fillText(this.text, this.x + 10, this.y + OVERLAY_HEIGHT / 2);
+  }
+}
 export class NoteToolController extends EdgelessToolController<NoteTool> {
   readonly tool = <NoteTool>{
     type: 'note',
@@ -15,6 +91,8 @@ export class NoteToolController extends EdgelessToolController<NoteTool> {
   };
 
   private _dragStartEvent: PointerEventState | null = null;
+
+  private _noteOverlay: NoteOverlay | null = new NoteOverlay();
 
   private _addNote(
     e: PointerEventState,
@@ -29,6 +107,8 @@ export class NoteToolController extends EdgelessToolController<NoteTool> {
   }
 
   onContainerClick(e: PointerEventState): void {
+    this._clearOverlay();
+
     const { childFlavour, childType } = this.tool;
     const options = {
       childFlavour,
@@ -38,6 +118,7 @@ export class NoteToolController extends EdgelessToolController<NoteTool> {
   }
 
   onContainerContextMenu(e: PointerEventState): void {
+    // this._hideOverlay();
     noop();
   }
 
@@ -50,6 +131,8 @@ export class NoteToolController extends EdgelessToolController<NoteTool> {
   }
 
   onContainerDragStart(e: PointerEventState) {
+    this._clearOverlay();
+
     this._dragStartEvent = e;
     this._draggingArea = {
       start: new DOMPoint(e.x, e.y),
@@ -83,12 +166,57 @@ export class NoteToolController extends EdgelessToolController<NoteTool> {
     this._draggingArea = null;
   }
 
+  private _getOverlayText() {
+    // TODO: find a better way, should confirm what the tip should be
+    let text = '';
+    if (this.tool.childType) {
+      text = this.tool.childType;
+    } else {
+      // if child type is null, return second part of child flavour
+      const idx = this.tool.childFlavour.indexOf(':');
+      text = this.tool.childFlavour.slice(idx + 1);
+    }
+
+    text = text[0].toUpperCase() + text.slice(1);
+    return text;
+  }
+
+  private _updateOverlayPosition(x: number, y: number) {
+    if (!this._noteOverlay) return;
+    this._noteOverlay.x = x + OVERLAY_OFFSET_X;
+    this._noteOverlay.y = y + OVERLAY_OFFSET_Y;
+    this._edgeless.surface.refresh();
+  }
+
+  // Ensure clear overlay before adding a new note
+  private _clearOverlay() {
+    if (!this._noteOverlay) return;
+
+    this._edgeless.surface.viewport.removeOverlay(this._noteOverlay);
+    this._noteOverlay = null;
+    this._edgeless.surface.refresh();
+  }
+
+  // Should hide overlay when mouse is out of viewport or on menu and toolbar
+  private _hideOverlay() {
+    if (!this._noteOverlay) return;
+
+    this._noteOverlay.globalAlpha = 0;
+    this._edgeless.surface.refresh();
+  }
+
   onContainerMouseMove(e: PointerEventState) {
-    noop();
+    if (!this._noteOverlay) return;
+
+    // if mouse is in viewport and move, update overlay pointion and show overlay
+    if (this._noteOverlay.globalAlpha === 0) this._noteOverlay.globalAlpha = 1;
+    this._noteOverlay.text = this._getOverlayText();
+    const [x, y] = this._surface.viewport.toModelCoord(e.x, e.y);
+    this._updateOverlayPosition(x, y);
   }
 
   onContainerMouseOut(e: PointerEventState) {
-    noop();
+    this._hideOverlay();
   }
 
   onPressShiftKey(_: boolean) {
@@ -96,10 +224,13 @@ export class NoteToolController extends EdgelessToolController<NoteTool> {
   }
 
   beforeModeSwitch() {
-    noop();
+    this._clearOverlay();
   }
 
-  afterModeSwitch() {
-    noop();
+  afterModeSwitch(newTool: EdgelessTool) {
+    if (newTool.type !== 'note') return;
+    this._noteOverlay = new NoteOverlay();
+    this._noteOverlay.text = this._getOverlayText();
+    this._edgeless.surface.viewport.addOverlay(this._noteOverlay);
   }
 }
--- packages/blocks/src/page-block/edgeless/tool-controllers/shape-tool.ts
+++ packages/blocks/src/page-block/edgeless/tool-controllers/shape-tool.ts
@@ -1,8 +1,8 @@
 import type { PointerEventState } from '@blocksuite/block-std';
 import { assertExists } from '@blocksuite/global/utils';
-import { Bound, StrokeStyle } from '@blocksuite/phasor';
+import { Bound, Overlay, StrokeStyle } from '@blocksuite/phasor';
 
-import type { ShapeTool } from '../../../__internal__/index.js';
+import type { EdgelessTool, ShapeTool } from '../../../__internal__/index.js';
 import { noop } from '../../../__internal__/index.js';
 import {
   DEFAULT_SHAPE_FILL_COLOR,
@@ -11,6 +11,180 @@ import {
 import { isTransparent } from '../components/panel/color-panel.js';
 import type { SelectionArea } from '../utils/selection-manager.js';
 import { EdgelessToolController } from './index.js';
+
+const DEFAULT_SHAPE_WIDTH = 120;
+const DEFAULT_SHAPE_HEIGHT = 120;
+abstract class Shape {
+  x: number;
+  y: number;
+  globalAlpha: number;
+  type: string;
+
+  constructor(x: number, y: number, globalAlpha: number, type: string) {
+    this.x = x;
+    this.y = y;
+    this.globalAlpha = globalAlpha;
+    this.type = type;
+  }
+
+  abstract draw(ctx: CanvasRenderingContext2D): void;
+}
+
+class RectShape extends Shape {
+  draw(ctx: CanvasRenderingContext2D): void {
+    ctx.strokeRect(this.x, this.y, DEFAULT_SHAPE_WIDTH, DEFAULT_SHAPE_HEIGHT);
+  }
+}
+
+class TriangleShape extends Shape {
+  draw(ctx: CanvasRenderingContext2D): void {
+    const height = (Math.sqrt(3) / 2) * DEFAULT_SHAPE_WIDTH; // height of equilateral triangle with side length 100
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y - height / 2); // top vertex
+    ctx.lineTo(this.x - DEFAULT_SHAPE_WIDTH / 2, this.y + height / 2); // bottom-left vertex
+    ctx.lineTo(this.x + DEFAULT_SHAPE_WIDTH / 2, this.y + height / 2); // bottom-right vertex
+    ctx.closePath();
+    ctx.stroke();
+  }
+}
+
+class DiamondShape extends Shape {
+  draw(ctx: CanvasRenderingContext2D): void {
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y - DEFAULT_SHAPE_WIDTH / 2); // top vertex
+    ctx.lineTo(this.x - DEFAULT_SHAPE_WIDTH / 2, this.y); // left vertex
+    ctx.lineTo(this.x, this.y + DEFAULT_SHAPE_WIDTH / 2); // bottom vertex
+    ctx.lineTo(this.x + DEFAULT_SHAPE_WIDTH / 2, this.y); // right vertex
+    ctx.closePath();
+    ctx.stroke();
+  }
+}
+
+class CircleShape extends Shape {
+  draw(ctx: CanvasRenderingContext2D): void {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
+    ctx.stroke();
+  }
+}
+
+class RoundedRectShape extends Shape {
+  draw(ctx: CanvasRenderingContext2D): void {
+    const cornerRadius = 10;
+    ctx.beginPath();
+    ctx.moveTo(this.x + cornerRadius, this.y);
+    ctx.lineTo(this.x + DEFAULT_SHAPE_WIDTH - cornerRadius, this.y);
+    ctx.quadraticCurveTo(
+      this.x + DEFAULT_SHAPE_WIDTH,
+      this.y,
+      this.x + DEFAULT_SHAPE_WIDTH,
+      this.y + cornerRadius
+    );
+    ctx.lineTo(
+      this.x + DEFAULT_SHAPE_WIDTH,
+      this.y + DEFAULT_SHAPE_WIDTH - cornerRadius
+    );
+    ctx.quadraticCurveTo(
+      this.x + DEFAULT_SHAPE_WIDTH,
+      this.y + DEFAULT_SHAPE_WIDTH,
+      this.x + DEFAULT_SHAPE_WIDTH - cornerRadius,
+      this.y + DEFAULT_SHAPE_WIDTH
+    );
+    ctx.lineTo(this.x + cornerRadius, this.y + DEFAULT_SHAPE_WIDTH);
+    ctx.quadraticCurveTo(
+      this.x,
+      this.y + DEFAULT_SHAPE_WIDTH,
+      this.x,
+      this.y + DEFAULT_SHAPE_WIDTH - cornerRadius
+    );
+    ctx.lineTo(this.x, this.y + cornerRadius);
+    ctx.quadraticCurveTo(this.x, this.y, this.x + cornerRadius, this.y);
+    ctx.closePath();
+    ctx.stroke();
+  }
+}
+
+class ShapeFactory {
+  static createShape(
+    x: number,
+    y: number,
+    globalAlpha: number,
+    type: string
+  ): Shape {
+    switch (type) {
+      case 'rect':
+        return new RectShape(x, y, globalAlpha, type);
+      case 'triangle':
+        return new TriangleShape(x, y, globalAlpha, type);
+      case 'diamond':
+        return new DiamondShape(x, y, globalAlpha, type);
+      case 'ellipse':
+        return new CircleShape(x, y, globalAlpha, type);
+      case 'roundedRect':
+        return new RoundedRectShape(x, y, globalAlpha, type);
+      default:
+        throw new Error(`Unknown shape type: ${type}`);
+    }
+  }
+}
+
+class ShapeOverlay extends Overlay {
+  private shape: Shape;
+  private _x: number;
+  private _y: number;
+  private _globalAlpha: number;
+
+  constructor(x = 0, y = 0, globalAlpha = 0, type = 'rect') {
+    super();
+    this.shape = ShapeFactory.createShape(x, y, globalAlpha, type);
+    this._x = x;
+    this._y = y;
+    this._globalAlpha = globalAlpha;
+  }
+
+  get x(): number {
+    return this._x;
+  }
+
+  set x(value: number) {
+    this._x = value;
+    this.shape.x = value;
+  }
+
+  get y(): number {
+    return this._y;
+  }
+
+  set y(value: number) {
+    this._y = value;
+    this.shape.y = value;
+  }
+
+  get globalAlpha(): number {
+    return this._globalAlpha;
+  }
+
+  set globalAlpha(value: number) {
+    this._globalAlpha = value;
+    this.shape.globalAlpha = value;
+  }
+
+  setShape(type: string): void {
+    this.shape = ShapeFactory.createShape(
+      this._x,
+      this._y,
+      this._globalAlpha,
+      type
+    );
+  }
+
+  override render(ctx: CanvasRenderingContext2D): void {
+    ctx.globalAlpha = this.shape.globalAlpha;
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = 'black';
+    this.shape.draw(ctx);
+  }
+}
 
 export class ShapeToolController extends EdgelessToolController<ShapeTool> {
   readonly tool = <ShapeTool>{
@@ -22,10 +196,59 @@ export class ShapeToolController extends EdgelessToolController<ShapeTool> {
 
   private _draggingElementId: string | null = null;
 
+  // shape overlay
+  private _shapeOverlay: ShapeOverlay | null = null;
+
   protected override _draggingArea: SelectionArea | null = null;
 
+  private _addNewShapeToSurface(
+    e: PointerEventState,
+    width: number,
+    height: number
+  ): string {
+    const { viewport } = this._edgeless.surface;
+
+    // create a shape block when drag start
+    const [modelX, modelY] = viewport.toModelCoord(e.point.x, e.point.y);
+    const bound = new Bound(modelX, modelY, width, height);
+    const { shape, fillColor, strokeColor } = this.tool;
+
+    const shapeType = shape === 'roundedRect' ? 'rect' : shape;
+
+    const id = this._surface.addElement('shape', {
+      shapeType,
+      xywh: bound.serialize(),
+      strokeColor,
+      fillColor,
+      filled: !isTransparent(fillColor),
+      radius: shape === 'roundedRect' ? 0.1 : 0,
+      strokeWidth: 4,
+      strokeStyle: StrokeStyle.Solid,
+    });
+
+    return id;
+  }
+
   onContainerClick(e: PointerEventState): void {
-    noop();
+    if (!this._page.awarenessStore.getFlag('enable_surface')) return;
+    this._clearOverlay();
+
+    this._page.captureSync();
+
+    const id = this._addNewShapeToSurface(
+      e,
+      DEFAULT_SHAPE_WIDTH,
+      DEFAULT_SHAPE_HEIGHT
+    );
+
+    const element = this._surface.pickById(id);
+    assertExists(element);
+    this._edgeless.slots.surfaceUpdated.emit();
+
+    this._edgeless.selection.switchToDefaultMode({
+      selected: [element],
+      active: false,
+    });
   }
 
   onContainerContextMenu(e: PointerEventState): void {
@@ -46,27 +269,11 @@ export class ShapeToolController extends EdgelessToolController<ShapeTool> {
 
   onContainerDragStart(e: PointerEventState) {
     if (!this._page.awarenessStore.getFlag('enable_surface')) return;
+    this._clearOverlay();
 
     this._page.captureSync();
-    const { viewport } = this._edgeless.surface;
 
-    // create a shape block when drag start
-    const [modelX, modelY] = viewport.toModelCoord(e.point.x, e.point.y);
-    const bound = new Bound(modelX, modelY, 0, 0);
-    const { shape, fillColor, strokeColor } = this.tool;
-
-    const shapeType = shape === 'roundedRect' ? 'rect' : shape;
-
-    const id = this._surface.addElement('shape', {
-      shapeType,
-      xywh: bound.serialize(),
-      strokeColor,
-      fillColor,
-      filled: !isTransparent(fillColor),
-      radius: shape === 'roundedRect' ? 0.1 : 0,
-      strokeWidth: 4,
-      strokeStyle: StrokeStyle.Solid,
-    });
+    const id = this._addNewShapeToSurface(e, 0, 0);
 
     this._draggingElementId = id;
 
@@ -146,19 +353,49 @@ export class ShapeToolController extends EdgelessToolController<ShapeTool> {
     slots.surfaceUpdated.emit();
   }
 
+  private _updateOverlayPosition(x: number, y: number) {
+    if (!this._shapeOverlay) return;
+    this._shapeOverlay.x = x;
+    this._shapeOverlay.y = y;
+    this._edgeless.surface.refresh();
+  }
+
+  private _clearOverlay() {
+    if (!this._shapeOverlay) return;
+
+    this._edgeless.surface.viewport.removeOverlay(this._shapeOverlay);
+    this._shapeOverlay = null;
+    this._edgeless.surface.refresh();
+  }
+
+  private _hideOverlay() {
+    if (!this._shapeOverlay) return;
+
+    this._shapeOverlay.globalAlpha = 0;
+    this._edgeless.surface.refresh();
+  }
+
   onContainerMouseMove(e: PointerEventState) {
-    noop();
+    if (!this._shapeOverlay) return;
+
+    this._shapeOverlay.setShape(this.tool.shape);
+    if (this._shapeOverlay.globalAlpha === 0)
+      this._shapeOverlay.globalAlpha = 0.66;
+    const [x, y] = this._surface.viewport.toModelCoord(e.x, e.y);
+    this._updateOverlayPosition(x, y);
   }
 
   onContainerMouseOut(e: PointerEventState) {
-    noop();
+    this._hideOverlay();
   }
 
   beforeModeSwitch() {
-    noop();
+    this._clearOverlay();
   }
 
-  afterModeSwitch() {
-    noop();
+  afterModeSwitch(newTool: EdgelessTool) {
+    if (newTool.type !== 'shape') return;
+    this._shapeOverlay = new ShapeOverlay();
+    this._edgeless.surface.viewport.addOverlay(this._shapeOverlay);
   }
 }
