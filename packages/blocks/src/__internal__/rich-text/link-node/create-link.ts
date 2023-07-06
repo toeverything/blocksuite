4ea6a6af943802a7f3955c2ef508be0b8d1edcf9
lawvs <18554747+lawvs@users.noreply.github.com>
2023-07-06 15:09:23 +0800

    fix: hide turn card btn when the link in database

--- packages/blocks/src/__internal__/rich-text/link-node/create-link.ts
+++ packages/blocks/src/__internal__/rich-text/link-node/create-link.ts
@@ -69,7 +69,7 @@ export function createLink(page: Page) {
   setTimeout(async () => {
     const linkState = await showLinkPopover({
       anchorEl: mockSelection.shadowRoot?.querySelector('div') as HTMLElement,
-      page,
+      model: startModel,
     });
 
     mockSelection.remove();
