1f9a40aa7bd3fc9e71354e7641908b6fc6e43a70
Flrande <flrande.ch@gmail.com>
2023-07-13 15:37:24 +0800

    fix(edgeless): text align in canvas text

--- packages/blocks/src/page-block/edgeless/components/text/edgeless-shape-text-editor.ts
+++ packages/blocks/src/page-block/edgeless/components/text/edgeless-shape-text-editor.ts
@@ -167,7 +167,7 @@ export class EdgelessShapeTextEditor extends WithDisposable(ShadowlessElement) {
           ? `var(${this._element.color})`
           : this._element.color,
         padding: SHAPE_TEXT_PADDING + 'px',
-        textAlign: this._element.textVerticalAlign,
+        textAlign: this._element.textAlign,
         display: 'grid',
         gridTemplateColumns: '100%',
         alignItems:
