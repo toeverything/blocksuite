91cce77781c9eb9081bf4b356b04c6198e27ef9c
regischen <regischen@toeverything.info>
2023-08-03 17:44:38 +0800

    fix(edgeless): fix alt drag element not work (#3819)

--- packages/blocks/src/page-block/edgeless/tool-controllers/default-tool.ts
+++ packages/blocks/src/page-block/edgeless/tool-controllers/default-tool.ts
@@ -436,6 +436,15 @@ export class DefaultToolController extends EdgelessToolController<DefaultTool> {
     // Determine the drag type based on the current state and event
     let dragType = this._determineDragType(e);
 
+    const toBeMoved = new Set(this.state.selected);
+    this.state.selected.forEach(element => {
+      if (element instanceof FrameElement) {
+        this._edgeless.frame
+          .getElementsInFrame(element)
+          .forEach(ele => toBeMoved.add(ele));
+      }
+    });
+    this._toBeMoved = Array.from(toBeMoved);
     // If alt key is pressed and content is moving, clone the content
     if (e.keys.alt && dragType === DefaultModeDragType.ContentMoving) {
       dragType = DefaultModeDragType.AltCloning;
@@ -454,16 +463,6 @@ export class DefaultToolController extends EdgelessToolController<DefaultTool> {
     this._dragStartPos = { x, y };
     this._dragLastPos = { x, y };
 
-    const toBeMoved = new Set(this.state.selected);
-    this.state.selected.forEach(element => {
-      if (element instanceof FrameElement) {
-        this._edgeless.frame
-          .getElementsInFrame(element)
-          .forEach(ele => toBeMoved.add(ele));
-      }
-    });
-
-    this._toBeMoved = Array.from(toBeMoved);
     this._alignBound = this._edgeless.snap.setupAlignables(this._toBeMoved);
 
     this._selectedBounds = this._toBeMoved.map(element =>
@@ -589,6 +588,7 @@ export class DefaultToolController extends EdgelessToolController<DefaultTool> {
       this._edgeless.frame.calculateFrameColor(frame);
     });
     this._frames.clear();
+    this._toBeMoved = [];
     this._forceUpdateSelection(this.dragType);
     this.dragType = DefaultModeDragType.None;
   }
