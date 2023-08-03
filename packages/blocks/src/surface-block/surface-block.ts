8bf315d468deb7cfc33a6f5e6b350365e3d17e78
Mirone <Saul-Mirone@outlook.com>
2023-08-03 14:35:05 +0800

    chore: fix comments

--- packages/blocks/src/surface-block/surface-block.ts
+++ packages/blocks/src/surface-block/surface-block.ts
@@ -1,10 +1,15 @@
 import { BlockElement } from '@blocksuite/lit';
+import { nothing } from 'lit';
 import { customElement } from 'lit/decorators.js';
 
 import type { SurfaceBlockModel } from './surface-model.js';
 
 @customElement('affine-surface')
-export class SurfaceBlockComponent extends BlockElement<SurfaceBlockModel> {}
+export class SurfaceBlockComponent extends BlockElement<SurfaceBlockModel> {
+  override render() {
+    return nothing;
+  }
+}
 
 declare global {
   interface HTMLElementTagNameMap {
