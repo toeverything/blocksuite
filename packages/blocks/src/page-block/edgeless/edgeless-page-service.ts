8b24aea4238b611e5f4c8b676389c407dbab9c04
Mirone <Saul-Mirone@outlook.com>
2023-08-06 19:57:54 +0800

    chore: fix e2e

--- packages/blocks/src/page-block/edgeless/edgeless-page-service.ts
+++ packages/blocks/src/page-block/edgeless/edgeless-page-service.ts
@@ -1,39 +1,10 @@
 import { BlockService } from '@blocksuite/block-std';
 
 import type { PageBlockModel } from '../page-model.js';
-import type { EdgelessPageBlockComponent } from './edgeless-page-block.js';
-import { EdgelessSelectionManager } from './services/selection-manager.js';
-import { EdgelessToolsManager } from './services/tools-manager.js';
 
 export class EdgelessPageService extends BlockService<PageBlockModel> {
-  selection: EdgelessSelectionManager | null = null;
-  tools: EdgelessToolsManager | null = null;
-
-  mountSelectionManager(container: EdgelessPageBlockComponent) {
-    if (this.tools) {
-      return;
-    }
-
-    this.selection = new EdgelessSelectionManager(container);
-    this.tools = new EdgelessToolsManager(container, this.uiEventDispatcher);
-  }
-
-  unmountSelectionManager() {
-    if (this.tools) {
-      this.tools.clear();
-      this.tools.dispose();
-      this.tools = null;
-    }
-
-    if (this.selection) {
-      this.selectionManager.set([]);
-      this.selection.dispose();
-      this.selection = null;
-    }
-  }
-
   override unmounted() {
     super.unmounted();
-    this.unmountSelectionManager();
+    this.selectionManager.set([]);
   }
 }
