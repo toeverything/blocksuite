a389c5a078d04fda27c1649bd5db4666caca07eb
lawvs <18554747+lawvs@users.noreply.github.com>
2023-08-08 16:14:21 +0800

    fix: lint

--- packages/blocks/src/widgets/format-bar/styles.ts
+++ packages/blocks/src/widgets/format-bar/styles.ts
@@ -1,7 +1,6 @@
 import { css } from 'lit';
 
 import { tooltipStyle } from '../../components/tooltip/tooltip.js';
-import { scrollbarStyle } from '../../components/utils.js';
 
 const paragraphButtonStyle = css`
   .paragraph-button-icon > svg:nth-child(2) {
