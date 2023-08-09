03d856dbbf46e8f7a3a29461ad10a79b3153b963
Yifeng Wang <doodlewind@toeverything.info>
2023-08-09 10:00:12 +0800

    style: indent

--- packages/blocks/src/page-block/doc/meta-data/meta-data.ts
+++ packages/blocks/src/page-block/doc/meta-data/meta-data.ts
@@ -140,7 +140,7 @@ export class PageMetaData extends WithDisposable(LitElement) {
 
     .meta-data-expanded-item .arrow-icon svg {
       fill: var(--affine-icon-color);
-       height: 25px;
+      height: 25px;
     }
 
     .meta-data-expanded-item .value {
