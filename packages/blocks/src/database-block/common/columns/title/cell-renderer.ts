4430fd9707b5778ef058befb647264bc8f19f781
zuozijian3720 <zuozijian1994@gmail.com>
2023-08-10 18:06:03 +0800

    feat: block icon

--- packages/blocks/src/database-block/common/columns/title/cell-renderer.ts
+++ packages/blocks/src/database-block/common/columns/title/cell-renderer.ts
@@ -1,191 +1,159 @@
-import { PathFinder } from '@blocksuite/block-std';
-import type { TemplateResult } from 'lit';
 import { css } from 'lit';
 import { customElement, query, state } from 'lit/decorators.js';
-import { classMap } from 'lit/directives/class-map.js';
 import { html } from 'lit/static-html.js';
 
-import type { ParagraphBlockComponent } from '../../../../paragraph-block/index.js';
 import { BaseCellRenderer } from '../base-cell.js';
 import { columnRenderer, createFromBaseCellRenderer } from '../renderer.js';
-import {
-  RichTextCell,
-  RichTextCellEditing,
-} from '../rich-text/cell-renderer.js';
 import { titleColumnTypeName, titlePureColumnConfig } from './define.js';
 
 @customElement('affine-database-title-cell')
-export class TitleCell extends BaseCellRenderer<TemplateResult> {
+export class TitleCell extends BaseCellRenderer<string> {
   static override styles = css`
     affine-database-title-cell {
-      position: relative;
-    }
-
-    affine-database-title-cell .mask {
-      position: absolute;
       width: 100%;
-      height: 100%;
-      left: 0;
-      top: 0;
+      height: max-content;
+      display: flex;
+      align-items: center;
+      white-space: pre-wrap;
     }
 
-    .affine-database-block-row-cell-content {
+    .affine-database-title {
       display: flex;
       align-items: center;
       height: 100%;
-      min-height: 44px;
-      transform: translateX(0);
-    }
-
-    .affine-database-block-row-cell-content > [data-block-id] {
       width: 100%;
-    }
-
-    .affine-database-block-row-cell-content > affine-paragraph {
-      display: flex;
-      align-items: center;
-      width: 100%;
-      height: 100%;
-    }
-
-    .affine-database-block-row-cell-content > affine-paragraph > .text {
-      width: 100%;
-      margin-top: unset;
-    }
-
-    .mock-focus {
-      box-shadow: 0px 0px 0px 2px rgba(30, 150, 235, 0.3);
-      border: 2px solid var(--affine-primary-color) !important;
-      pointer-events: none;
-      box-sizing: border-box;
+      padding: 0;
+      border: none;
+      font-family: var(--affine-font-family);
+      font-size: 12px;
+      line-height: 20px;
+      color: var(--affine-text-primary-color);
+      font-weight: 400;
+      background-color: transparent;
     }
   `;
 
-  @state()
-  realEditing = false;
-  @query('affine-paragraph')
-  paragraph?: ParagraphBlockComponent;
-
-  jumpOut() {
-    this.paragraph?.root.selectionManager?.clear();
-    getSelection()?.removeAllRanges();
-    this.querySelector('rich-text')?.vEditor?.syncVRange();
-    this.selectCurrentCell(false);
+  override render() {
+    return html` <div class="affine-database-title">${this.value ?? ''}</div>`;
   }
-  protected override firstUpdated() {
-    this._disposables.addFromEvent(
-      this,
-      'keydown',
-      e => {
-        const block = this.paragraph;
-        if (!block) {
-          return;
-        }
-        const selectionManager = block.root.selectionManager;
-        const selection = selectionManager?.value.find(v =>
-          PathFinder.equals(v.path, block.path)
-        );
-        if (e.key === 'Escape') {
-          if (!selection) {
-            return;
-          }
-          e.stopPropagation();
-          e.preventDefault();
-          this.jumpOut();
-        }
-        if (e.key === 'Enter' && !e.shiftKey) {
-          if (selection) {
-            e.stopPropagation();
-            e.preventDefault();
-            this.jumpOut();
-            return;
-          }
-        }
-      },
-      true
-    );
-    this._disposables.addFromEvent(
-      this,
-      'click',
-      e => {
-        if (this.realEditing) {
-          e.stopPropagation();
-        }
-      },
-      true
-    );
-    // if (this.paragraph) {
-    //   this.paragraph.bindHotKey({
-    //     'Escape': () => {
-    //       const selected = this.paragraph?.selected;
-    //       if (selected) {
-    //         return true;
-    //       }
-    //     },
-    //   }, { flavour: true });
-    // }
-    this._disposables.addFromEvent(
-      this,
-      'pointerdown',
-      e => {
-        if (this.realEditing) {
-          e.stopPropagation();
-        }
-      },
-      true
-    );
+}
+
+@customElement('affine-database-title-cell-editing')
+export class TitleCellEditing extends BaseCellRenderer<string> {
+  static override styles = css`
+    affine-database-title-cell-editing {
+      width: 100%;
+      height: max-content;
+      cursor: text;
+      display: flex;
+      align-items: center;
+      white-space: pre-wrap;
+      position: relative;
+    }
+
+    .affine-database-title {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      padding: 0;
+      border: none;
+      font-family: var(--affine-font-family);
+      font-size: 12px;
+      line-height: 20px;
+      color: var(--affine-text-primary-color);
+      font-weight: 400;
+      background-color: transparent;
+    }
+
+    .affine-database-title:focus {
+      outline: none;
+    }
+
+    textarea.affine-database-title {
+      resize: none;
+      position: absolute;
+      left: 0;
+      top: 0;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      padding: 4px;
+    }
+  `;
+
+  @query('textarea')
+  private _inputEle!: HTMLTextAreaElement;
+
+  focusEnd = () => {
+    const end = this._inputEle.value.length;
+    this._inputEle.focus();
+    this._inputEle.setSelectionRange(end, end);
+  };
+
+  override onExitEditMode() {
+    this._setValue();
+  }
+
+  private _setValue = (str: string = this._inputEle.value) => {
+    if (!str) {
+      this.onChange(undefined);
+      return;
+    }
+    this.onChange(str);
+  };
+
+  private _keydown = (e: KeyboardEvent) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      requestAnimationFrame(() => {
+        this.selectCurrentCell(false);
+      });
+    }
+  };
+
+  override firstUpdated() {
     requestAnimationFrame(() => {
-      const editor = this.querySelector('rich-text')?.vEditor;
-      if (editor) {
-        this._disposables.add(
-          editor.slots.vRangeUpdated.on(([range]) => {
-            this.realEditing = !!range;
-          })
-        );
-      }
+      this.focusEnd();
     });
   }
 
-  override focusCell() {
-    const block = this.querySelector('affine-paragraph');
-    const selectionManager = block?.root.selectionManager;
-    if (selectionManager) {
-      const length =
-        block?.querySelector('rich-text')?.vEditor?.yText.length ?? 0;
-      const selection = selectionManager.getInstance('text', {
-        from: {
-          path: block.path,
-          index: length,
-          length: 0,
-        },
-        to: null,
-      });
-      selectionManager.set([selection]);
-    }
-    return false;
+  _blur() {
+    this.selectCurrentCell(false);
   }
 
-  override blurCell() {
-    return false;
+  _focus() {
+    if (!this.isEditing) {
+      this.selectCurrentCell(true);
+    }
+  }
+
+  @state()
+  text?: string;
+
+  _input() {
+    this.text = this._inputEle.value;
   }
 
   override render() {
-    const className = classMap({
-      'mock-focus': this.realEditing,
-      mask: true,
-    });
-    return html`
-      <div class="affine-database-block-row-cell-content">${this.value}</div>
-      <div class="${className}"></div>
-    `;
+    return html` <div class="affine-database-title" style="opacity: 0">
+        ${this.text ?? this.value}
+      </div>
+      <textarea
+        @keydown="${this._keydown}"
+        @blur="${this._blur}"
+        @focus="${this._focus}"
+        @input="${this._input}"
+        class="affine-database-title"
+      >
+${this.value ?? ''}</textarea
+      >`;
   }
 }
 
 columnRenderer.register({
   type: titleColumnTypeName,
   cellRenderer: {
-    view: createFromBaseCellRenderer(RichTextCell),
-    edit: createFromBaseCellRenderer(RichTextCellEditing),
+    view: createFromBaseCellRenderer(TitleCell),
+    edit: createFromBaseCellRenderer(TitleCellEditing),
   },
 });
 
