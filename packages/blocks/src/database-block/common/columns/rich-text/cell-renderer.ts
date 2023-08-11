07e63a0489931d10cc924da3bff4145d152caacc
zqran <uuxnet@gmail.com>
2023-08-11 23:12:12 +0800

    feat: process the rich-text data of database in `importPageSnapshot`

--- packages/blocks/src/database-block/common/columns/rich-text/cell-renderer.ts
+++ packages/blocks/src/database-block/common/columns/rich-text/cell-renderer.ts
@@ -1,4 +1,4 @@
-import type { DeltaOperation, Y } from '@blocksuite/store';
+import type { Y } from '@blocksuite/store';
 import { assertExists, Text } from '@blocksuite/store';
 import { VEditor } from '@blocksuite/virgo';
 import { css } from 'lit';
@@ -119,12 +119,6 @@ export class RichTextCell extends BaseCellRenderer<Y.Text> {
     return yText;
   };
 
-  private _initYTextFromDelta = (delta: DeltaOperation[]) => {
-    const yText = Text.fromDelta(delta).yText;
-    this.onChange(yText);
-    return yText;
-  };
-
   private _onInitVEditor() {
     let value: Y.Text;
     if (!this.value) {
@@ -133,10 +127,6 @@ export class RichTextCell extends BaseCellRenderer<Y.Text> {
       // When copying the database, the type of the value is `string`.
       if (typeof this.value === 'string') {
         value = this._initYText(this.value);
-      } else if (Array.isArray(this.value)) {
-        // When `Import Snapshot`, the type of the value is `Array`.
-        // See https://github.com/toeverything/AFFiNE/blob/master/packages/templates/v1/okr-template.json#L135-L139
-        value = this._initYTextFromDelta(this.value);
       } else {
         value = this.value;
       }
