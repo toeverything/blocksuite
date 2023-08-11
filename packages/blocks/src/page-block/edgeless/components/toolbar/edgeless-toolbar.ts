8eb0fdddc50ae5d24ef6f1c7b630993ff8de11b9
regischen <regischen@toeverything.info>
2023-08-11 14:37:46 +0800

    fix(edgeless): fix toolbar style (#3979)

--- packages/blocks/src/page-block/edgeless/components/toolbar/edgeless-toolbar.ts
+++ packages/blocks/src/page-block/edgeless/components/toolbar/edgeless-toolbar.ts
@@ -50,6 +50,7 @@ export class EdgelessToolbar extends WithDisposable(LitElement) {
       box-shadow: var(--affine-shadow-2);
       border-radius: 40px;
       fill: currentcolor;
+      min-height: 52px;
     }
     .edgeless-toolbar-container[level='second'] {
       position: absolute;
@@ -120,7 +121,7 @@ export class EdgelessToolbar extends WithDisposable(LitElement) {
       overflow: hidden;
       white-space: nowrap;
       text-overflow: ellipsis;
-      margin-right: 4px;
+      margin-right: 8px;
     }
 
     .edgeless-frame-navigator-count {
