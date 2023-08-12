7d125e23a0646ca473f1db97aa76f2951441a020
Whitewater <me@waterwater.moe>
2023-08-12 21:57:46 +0800

    fix: embed icon size

--- packages/global/src/config/icons/text.ts
+++ packages/global/src/config/icons/text.ts
@@ -752,8 +752,8 @@ export const ViewIcon = html`<svg
 </svg>`;
 
 export const EmbedWebIcon = html`<svg
-  width="24"
-  height="24"
+  width="20"
+  height="20"
   viewBox="0 0 24 24"
   xmlns="http://www.w3.org/2000/svg"
 >
