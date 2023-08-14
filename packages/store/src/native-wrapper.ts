03fa22563c7139d58f257dbda0cd08d22fb5e9c1
Mirone <Saul-Mirone@outlook.com>
2023-08-14 10:07:48 +0800

    feat(store): add native wrapper prop type

---
+++ packages/store/src/native-wrapper.ts
@@ -0,0 +1,15 @@
+import * as Y from 'yjs';
+
+export class NativeWrapper<T = unknown> extends Y.Map<T> {
+  constructor(value: T) {
+    const entry: [string, T] = ['value', value];
+    super([entry]);
+  }
+  setValue(value: T) {
+    return super.set('value', value);
+  }
+
+  getValue() {
+    return super.get('value');
+  }
+}
