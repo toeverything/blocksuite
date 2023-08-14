7a754cf1e5c10a9945e4b79bd71e8232be7c5869
Rajeev Lochan Dash <96860040+RajeevDash007@users.noreply.github.com>
2023-08-14 19:00:58 +0530

    gap: 10px;

--- packages/blocks/src/page-block/edgeless/components/panel/shape-style-panel.ts
+++ packages/blocks/src/page-block/edgeless/components/panel/shape-style-panel.ts
@@ -18,7 +18,7 @@ export class EdgelessShapeStylePanel extends LitElement {
       align-items: center;
       justify-content: center;
       background: var(--affine-background-overlay-panel-color);
-      gap: 10px;
+      gap: 8px;
     }
 
     edgeless-tool-icon-button svg {
