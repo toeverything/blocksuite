de28701c1d49fe8c62c4e327ab5d35fca8633f77
Mirone <Saul-Mirone@outlook.com>
2023-08-15 10:19:19 +0800

    chore: udpate consts

--- packages/global/src/config/consts.ts
+++ packages/global/src/config/consts.ts
@@ -1,11 +1,19 @@
-const IS_SAFARI = /Apple Computer/.test(globalThis.navigator?.vendor);
-export const IS_IOS =
-  IS_SAFARI &&
-  (/Mobile\/\w+/.test(globalThis.navigator?.userAgent) ||
-    globalThis.navigator?.maxTouchPoints > 2);
-export const IS_MAC = /Mac/i.test(globalThis.navigator?.platform);
+const agent = globalThis.navigator?.userAgent ?? '';
+const platform = globalThis.navigator?.platform;
 
 export const IS_WEB = typeof window !== 'undefined';
 
+const IS_SAFARI = /Apple Computer/.test(globalThis.navigator?.vendor);
+
 export const IS_FIREFOX =
   IS_WEB && navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+
+export const IS_ANDROID = /Android \d/.test(agent);
+
+export const IS_IOS =
+  IS_SAFARI &&
+  (/Mobile\/\w+/.test(agent) || globalThis.navigator?.maxTouchPoints > 2);
+
+export const IS_MAC = /Mac/i.test(platform);
+
+export const IS_WINDOWS = /Win/.test(platform);
