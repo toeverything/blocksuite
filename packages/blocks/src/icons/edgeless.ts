9025e9572361cef8f00bff97890b6f6118cc5900
devlzl <devlzl@outlook.com>
2023-08-16 17:02:44 +0800

    fix: bookmark icon size

--- packages/blocks/src/icons/edgeless.ts
+++ packages/blocks/src/icons/edgeless.ts
@@ -202,8 +202,8 @@ export const EmbedIcon = html`
 `;
 
 export const BookmarkIcon = html`<svg
-  width="24"
-  height="24"
+  width="20"
+  height="20"
   viewBox="0 0 24 24"
   xmlns="http://www.w3.org/2000/svg"
 >
