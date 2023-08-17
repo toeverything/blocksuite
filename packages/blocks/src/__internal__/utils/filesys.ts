91963a1ba1f8b67d861cf8190837c3c4a390be56
lawvs <18554747+lawvs@users.noreply.github.com>
2023-08-18 00:27:56 +0800

    chore: clean

--- packages/blocks/src/__internal__/utils/filesys.ts
+++ packages/blocks/src/__internal__/utils/filesys.ts
@@ -171,6 +171,7 @@ export async function openFileOrFiles({
   return new Promise(resolve => {
     // Append a new `<input type="file" multiple? />` and hide it.
     const input = document.createElement('input');
+    input.classList.add('affine-upload-input');
     input.style.display = 'none';
     input.type = 'file';
     if (multiple) {
