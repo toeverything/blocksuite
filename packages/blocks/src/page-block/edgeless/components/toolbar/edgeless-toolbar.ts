ff63711874e694e73beec7ff3af45098c04d76c6
regischen <regischen@toeverything.info>
2023-08-18 17:35:03 +0800

    fix(edgeless): use correct icon

--- packages/blocks/src/page-block/edgeless/components/toolbar/edgeless-toolbar.ts
+++ packages/blocks/src/page-block/edgeless/components/toolbar/edgeless-toolbar.ts
@@ -24,7 +24,7 @@ import {
   FrameNavigatorNextIcon,
   FrameNavigatorPrevIcon,
   HandIcon,
-  PresentationIcon,
+  PresentationFullScreenIcon,
   SelectIcon,
 } from '../../../../icons/index.js';
 import type { EdgelessPageBlockComponent } from '../../edgeless-page-block.js';
@@ -350,7 +350,7 @@ export class EdgelessToolbar extends WithDisposable(LitElement) {
           this._toggleFullScreen();
         }}
       >
-        ${PresentationIcon}
+        ${PresentationFullScreenIcon}
       </edgeless-tool-icon-button>
       <div class="short-divider"></div>
       <div
