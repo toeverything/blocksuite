10d4142de32cbd759784a9d8264751d133ff0216
Mirone <Saul-Mirone@outlook.com>
2023-08-18 15:48:41 +0800

    chore: fix node import

--- packages/blocks/src/bookmark-block/index.ts
+++ packages/blocks/src/bookmark-block/index.ts
@@ -1,3 +1,3 @@
 export * from './bookmark-block.js';
 export * from './bookmark-model.js';
-export { getBookmarkInitialProps } from './utils.js';
+export { getBookmarkInitialProps } from './components/bookmark-create-modal.js';
