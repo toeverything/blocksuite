6dba0d9dc3b2ece4d5027c2a2c3e618e6e3ece29
Mirone <Saul-Mirone@outlook.com>
2023-08-18 16:45:33 +0800

    fix: node import (#4218)

--- packages/blocks/src/widgets/slash-menu/config.ts
+++ packages/blocks/src/widgets/slash-menu/config.ts
@@ -1,7 +1,7 @@
 import { assertExists } from '@blocksuite/global/utils';
 import { Text } from '@blocksuite/store';
 
-import { REFERENCE_NODE } from '../../__internal__/rich-text/reference-node.js';
+import { REFERENCE_NODE } from '../../__internal__/rich-text/consts.js';
 import { getServiceOrRegister } from '../../__internal__/service/index.js';
 import {
   createPage,
