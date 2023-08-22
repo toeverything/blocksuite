c11723b55f9c64bcaa8ac8ddf4ed29d6b865c54b
Flrande <flrande.ch@gmail.com>
2023-08-22 19:07:37 +0800

    feat(virgo): add keyboard util

---
+++ packages/playground/examples/virgo/markdown.ts
@@ -0,0 +1,335 @@
+import type { VEditor, VRange } from '@blocksuite/virgo';
+import type * as Y from 'yjs';
+
+interface MarkdownMatch {
+  name: string;
+  pattern: RegExp;
+  action: (props: {
+    vEditor: VEditor;
+    prefixText: string;
+    vRange: VRange;
+    pattern: RegExp;
+    undoManager: Y.UndoManager;
+  }) => boolean;
+}
+
+export const markdownMatches: MarkdownMatch[] = [
+  {
+    name: 'bolditalic',
+    pattern: /(?:\*){3}([^\s]+)(?:\*){3}$/g,
+    action: ({ vEditor, prefixText, vRange, pattern, undoManager }) => {
+      const match = pattern.exec(prefixText);
+      if (!match) {
+        return false;
+      }
+
+      const annotatedText = match[0];
+      const startIndex = vRange.index - annotatedText.length;
+
+      vEditor.insertText(
+        {
+          index: startIndex + annotatedText.length,
+          length: 0,
+        },
+        ' '
+      );
+
+      undoManager.stopCapturing();
+
+      vEditor.formatText(
+        {
+          index: startIndex,
+          length: annotatedText.length,
+        },
+        {
+          bold: true,
+          italic: true,
+        }
+      );
+
+      vEditor.deleteText({
+        index: startIndex + annotatedText.length,
+        length: 1,
+      });
+      vEditor.deleteText({
+        index: startIndex + annotatedText.length - 3,
+        length: 3,
+      });
+      vEditor.deleteText({
+        index: startIndex,
+        length: 3,
+      });
+
+      vEditor.setVRange({
+        index: startIndex + annotatedText.length - 6,
+        length: 0,
+      });
+
+      return true;
+    },
+  },
+  {
+    name: 'bold',
+    pattern: /(?:\*){2}([^\s]+)(?:\*){2}$/g,
+    action: ({ vEditor, prefixText, vRange, pattern, undoManager }) => {
+      const match = pattern.exec(prefixText);
+      if (!match) {
+        return false;
+      }
+      const annotatedText = match[0];
+      const startIndex = vRange.index - annotatedText.length;
+
+      vEditor.insertText(
+        {
+          index: startIndex + annotatedText.length,
+          length: 0,
+        },
+        ' '
+      );
+
+      undoManager.stopCapturing();
+
+      vEditor.formatText(
+        {
+          index: startIndex,
+          length: annotatedText.length,
+        },
+        {
+          bold: true,
+        }
+      );
+
+      vEditor.deleteText({
+        index: startIndex + annotatedText.length,
+        length: 1,
+      });
+      vEditor.deleteText({
+        index: startIndex + annotatedText.length - 2,
+        length: 2,
+      });
+      vEditor.deleteText({
+        index: startIndex,
+        length: 2,
+      });
+
+      vEditor.setVRange({
+        index: startIndex + annotatedText.length - 4,
+        length: 0,
+      });
+
+      return true;
+    },
+  },
+  {
+    name: 'italic',
+    pattern: /(?:\*){1}([^\s]+)(?:\*){1}$/g,
+    action: ({ vEditor, prefixText, vRange, pattern, undoManager }) => {
+      const match = pattern.exec(prefixText);
+      if (!match) {
+        return false;
+      }
+      const annotatedText = match[0];
+      const startIndex = vRange.index - annotatedText.length;
+
+      vEditor.insertText(
+        {
+          index: startIndex + annotatedText.length,
+          length: 0,
+        },
+        ' '
+      );
+
+      undoManager.stopCapturing();
+
+      vEditor.formatText(
+        {
+          index: startIndex,
+          length: annotatedText.length,
+        },
+        {
+          italic: true,
+        }
+      );
+
+      vEditor.deleteText({
+        index: startIndex + annotatedText.length,
+        length: 1,
+      });
+      vEditor.deleteText({
+        index: startIndex + annotatedText.length - 1,
+        length: 1,
+      });
+      vEditor.deleteText({
+        index: startIndex,
+        length: 1,
+      });
+
+      vEditor.setVRange({
+        index: startIndex + annotatedText.length - 2,
+        length: 0,
+      });
+
+      return true;
+    },
+  },
+  {
+    name: 'strikethrough',
+    pattern: /(?:~~)([^\s]+)(?:~~)$/g,
+    action: ({ vEditor, prefixText, vRange, pattern, undoManager }) => {
+      const match = pattern.exec(prefixText);
+      if (!match) {
+        return false;
+      }
+      const annotatedText = match[0];
+      const startIndex = vRange.index - annotatedText.length;
+
+      vEditor.insertText(
+        {
+          index: startIndex + annotatedText.length,
+          length: 0,
+        },
+        ' '
+      );
+
+      undoManager.stopCapturing();
+
+      vEditor.formatText(
+        {
+          index: startIndex,
+          length: annotatedText.length,
+        },
+        {
+          strike: true,
+        }
+      );
+
+      vEditor.deleteText({
+        index: startIndex + annotatedText.length,
+        length: 1,
+      });
+      vEditor.deleteText({
+        index: startIndex + annotatedText.length - 2,
+        length: 2,
+      });
+      vEditor.deleteText({
+        index: startIndex,
+        length: 2,
+      });
+
+      vEditor.setVRange({
+        index: startIndex + annotatedText.length - 4,
+        length: 0,
+      });
+
+      return true;
+    },
+  },
+  {
+    name: 'underthrough',
+    pattern: /(?:~)([^\s]+)(?:~)$/g,
+    action: ({ vEditor, prefixText, vRange, pattern, undoManager }) => {
+      const match = pattern.exec(prefixText);
+      if (!match) {
+        return false;
+      }
+      const annotatedText = match[0];
+      const startIndex = vRange.index - annotatedText.length;
+
+      vEditor.insertText(
+        {
+          index: startIndex + annotatedText.length,
+          length: 0,
+        },
+        ' '
+      );
+
+      undoManager.stopCapturing();
+
+      vEditor.formatText(
+        {
+          index: startIndex,
+          length: annotatedText.length,
+        },
+        {
+          underline: true,
+        }
+      );
+
+      vEditor.deleteText({
+        index: startIndex + annotatedText.length,
+        length: 1,
+      });
+      vEditor.deleteText({
+        index: vRange.index - 1,
+        length: 1,
+      });
+      vEditor.deleteText({
+        index: startIndex,
+        length: 1,
+      });
+
+      vEditor.setVRange({
+        index: startIndex + annotatedText.length - 2,
+        length: 0,
+      });
+
+      return true;
+    },
+  },
+  {
+    name: 'code',
+    pattern: /(?:`)([^\s]+)(?:`)$/g,
+    action: ({ vEditor, prefixText, vRange, pattern, undoManager }) => {
+      const match = pattern.exec(prefixText);
+      if (!match) {
+        return false;
+      }
+      const annotatedText = match[0];
+      const startIndex = vRange.index - annotatedText.length;
+
+      if (prefixText.match(/^([* \n]+)$/g)) {
+        return false;
+      }
+
+      vEditor.insertText(
+        {
+          index: startIndex + annotatedText.length,
+          length: 0,
+        },
+        ' '
+      );
+
+      undoManager.stopCapturing();
+
+      vEditor.formatText(
+        {
+          index: startIndex,
+          length: annotatedText.length,
+        },
+        {
+          code: true,
+        }
+      );
+
+      vEditor.deleteText({
+        index: startIndex + annotatedText.length,
+        length: 1,
+      });
+      vEditor.deleteText({
+        index: startIndex + annotatedText.length - 1,
+        length: 1,
+      });
+      vEditor.deleteText({
+        index: startIndex,
+        length: 1,
+      });
+
+      vEditor.setVRange({
+        index: startIndex + annotatedText.length - 2,
+        length: 0,
+      });
+
+      return true;
+    },
+  },
+];
