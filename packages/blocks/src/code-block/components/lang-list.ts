06dc1ab86cb6e78d2b8509178d230dc2f2757330
lawvs <18554747+lawvs@users.noreply.github.com>
2023-08-25 20:16:18 +0800

    fix: update popover styles

--- packages/blocks/src/code-block/components/lang-list.ts
+++ packages/blocks/src/code-block/components/lang-list.ts
@@ -22,8 +22,8 @@ export class LangList extends LitElement {
         display: flex;
         flex-direction: column;
         background: var(--affine-background-overlay-panel-color);
+        color: var(--affine-text-primary-color);
         border-radius: 12px;
-        top: 24px;
         z-index: var(--affine-z-index-popover);
       }
 
@@ -62,6 +62,7 @@ export class LangList extends LitElement {
         border: 1px solid var(--affine-border-color);
         border-radius: 8px;
         padding: 4px 10px;
+        gap: 4px;
       }
 
       #filter-input {
@@ -89,11 +90,9 @@ export class LangList extends LitElement {
       }
 
       .search-icon {
-        height: 100%;
         display: flex;
-        padding-right: 4px;
         align-items: center;
-        fill: var(--affine-icon-color);
+        color: var(--affine-icon-color);
       }
     `;
   }
