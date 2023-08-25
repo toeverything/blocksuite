b8d88450b9f724ed1eae383fcb84d4d60ecb5fc7
Yifeng Wang <doodlewind@toeverything.info>
2023-08-25 10:41:08 +0800

    style: comment

--- packages/blocks/src/__internal__/consts.ts
+++ packages/blocks/src/__internal__/consts.ts
@@ -1,5 +1,6 @@
 export const BLOCK_ID_ATTR = 'data-block-id';
 export const BLOCK_SERVICE_LOADING_ATTR = 'data-service-loading';
+
 export const PREVENT_DEFAULT = false;
 export type PREVENT_DEFAULT = typeof PREVENT_DEFAULT;
 export const ALLOW_DEFAULT = true;
@@ -16,5 +17,5 @@ export const EDGELESS_BLOCK_CHILD_PADDING = 24;
 export const EDGELESS_BLOCK_CHILD_BORDER_WIDTH = 2;
 
 // The height of the header, which is used to calculate the scroll offset
-// In AFFiNE, avoid the option element to be covered by the header, we need to reserve the space for the header
+// In AFFiNE, to avoid the option element to be covered by the header, we need to reserve the space for the header
 export const PAGE_HEADER_HEIGHT = 53;
