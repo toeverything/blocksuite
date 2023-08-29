8a9c2a3b80edc3ba8e5fa1f18484ab3aa2cbc509
lawvs <18554747+lawvs@users.noreply.github.com>
2023-08-29 21:10:09 +0800

    fix: test

--- tests/selection/block.spec.ts
+++ tests/selection/block.spec.ts
@@ -30,7 +30,6 @@ import {
   resetHistory,
   selectAllByKeyboard,
   shamefullyBlurActiveElement,
-  SHORT_KEY,
   type,
   undoByClick,
   undoByKeyboard,
@@ -186,7 +185,7 @@ test('click the list icon can select and delete by forwardDelete', async ({
   await clickListIcon(page, 0);
   await waitNextFrame(page);
   await pressForwardDelete(page);
-  await assertRichTexts(page, ['', '']);
+  await assertRichTexts(page, ['', '', '789']);
 });
 
 test('selection on heavy page', async ({ page }) => {
