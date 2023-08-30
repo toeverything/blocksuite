26d519fb1e343324d529c8bc8d25ac18bc8b366f
regischen <regischen@toeverything.info>
2023-08-30 16:02:03 +0800

    fix: remove unused code

--- packages/blocks/src/page-block/edgeless/utils/rotate-utils.ts
+++
@@ -1,6 +0,0 @@
-import type { EdgelessElement } from '../../../index.js';
-import { isTopLevelBlock } from './query.js';
-
-export function getRotation(element: EdgelessElement) {
-  return isTopLevelBlock(element) ? 0 : element.rotate ?? 0;
-}
