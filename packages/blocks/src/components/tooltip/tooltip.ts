662c6bb9e5002cfd187fa7ce9e87801034621dd0
CatsJuice <wp1298554944@hotmail.com>
2023-08-31 08:52:50 +0800

    style: make tooltip with shortcut conform to design

--- packages/blocks/src/components/tooltip/tooltip.ts
+++ packages/blocks/src/components/tooltip/tooltip.ts
@@ -167,4 +167,29 @@ export const tooltipStyle = css`
     opacity: 1;
     transition-delay: 200ms;
   }
+  /** style for shortcut tooltip */
+  .tooltip-with-shortcut {
+    display: flex;
+    flex-wrap: nowrap;
+    align-items: center;
+    gap: 10px;
+  }
+  .tooltip__shortcut {
+    font-size: 12px;
+    position: relative;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 16px;
+    min-width: 16px;
+  }
+  .tooltip__shortcut::before {
+    content: '';
+    border-radius: 4px;
+    position: absolute;
+    inset: 0;
+    background: currentColor;
+    opacity: 0.2;
+  }
 `;
