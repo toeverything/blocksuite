4f5068e2992fe8bd478a45a95af75bb131a8d56c
Whitewater <me@waterwater.moe>
2023-09-05 17:36:11 +0800

    chore: remove tail whitespace

--- scripts/size-generate.sh
+++ scripts/size-generate.sh
@@ -3,8 +3,8 @@
 set -eu
 
 rm -fr ./temp/size
-pnpm run build:editor 
-pnpm run size:data 
-pnpm -C ./packages/playground run build:size 
-pnpm run --silent size:report >./size-report.md 
+pnpm run build:editor
+pnpm run size:data
+pnpm -C ./packages/playground run build:size
+pnpm run --silent size:report >./size-report.md
 cat ./size-report.md
