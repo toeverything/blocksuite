dd606e2210021369b4812c0c186aa928a2bca9fa
zqran <zqranx@gmail.com>
2023-09-05 17:40:52 +0800

    feat(database): support using arrow keys to select kanban card (#4586)

--- packages/blocks/src/database-block/kanban/hotkeys.ts
+++ packages/blocks/src/database-block/kanban/hotkeys.ts
@@ -13,22 +13,22 @@ export class KanbanHotkeys {
         this.viewEle.selection.focusIn();
       },
       ArrowUp: context => {
-        this.viewEle.selection.focusUp();
+        this.viewEle.selection.focusNext('up');
         context.get('keyboardState').raw.preventDefault();
         return true;
       },
       ArrowDown: context => {
-        this.viewEle.selection.focusDown();
+        this.viewEle.selection.focusNext('down');
         context.get('keyboardState').raw.preventDefault();
         return true;
       },
       ArrowLeft: context => {
-        this.viewEle.selection.focusLeft();
+        this.viewEle.selection.focusNext('left');
         context.get('keyboardState').raw.preventDefault();
         return true;
       },
       ArrowRight: context => {
-        this.viewEle.selection.focusRight();
+        this.viewEle.selection.focusNext('right');
         context.get('keyboardState').raw.preventDefault();
         return true;
       },
