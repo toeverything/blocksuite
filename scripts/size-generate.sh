d57cbb5d2b69b29f77152f9efbc40b788644f287
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-09-05 04:31:48 -0500

    Update scripts/size-generate.sh
    Co-authored-by: Whitewater <me@waterwater.moe>

--- scripts/size-generate.sh
+++ scripts/size-generate.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-rm -fr ./temp/size &&
-  pnpm run build:editor &&
-  pnpm run size:data &&
-  pnpm -C ./packages/playground run build:size &&
-  pnpm run --silent size:report >./size-report.md &&
-  cat ./size-report.md
+set -eu
+
+rm -fr ./temp/size
+pnpm run build:editor 
+pnpm run size:data 
+pnpm -C ./packages/playground run build:size 
+pnpm run --silent size:report >./size-report.md 
+cat ./size-report.md
