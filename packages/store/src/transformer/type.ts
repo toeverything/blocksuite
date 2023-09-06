8126a12f0e7f53a7a0143439df3b2a830a1a0f28
Mirone <Saul-Mirone@outlook.com>
2023-09-06 15:22:34 +0800

    chore: fix

--- packages/store/src/transformer/type.ts
+++ packages/store/src/transformer/type.ts
@@ -24,7 +24,7 @@ export type PageSnapshot = {
       createDate: number;
       tags: string[];
     };
-    versions?: {
+    versions: {
       block: Record<string, number>;
       page: number;
     };
