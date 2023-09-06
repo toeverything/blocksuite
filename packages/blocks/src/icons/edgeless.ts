bc64dc89af7654e102dc42a589634e9bbe4d6d76
Zhilin Liu <zhilin7@qq.com>
2023-09-06 15:36:05 +0800

    feat: toolbar slide button ui update

--- packages/blocks/src/icons/edgeless.ts
+++ packages/blocks/src/icons/edgeless.ts
@@ -2153,8 +2153,8 @@ export const ChangeShapeIcon = html`<svg
 
 export const ArrowRightSmallIcon = svg`<svg 
   xmlns="http://www.w3.org/2000/svg" 
-  width="16" 
-  height="16" 
+  width="32" 
+  height="32" 
   viewBox="0 0 24 24" 
   fill="none"
 >
