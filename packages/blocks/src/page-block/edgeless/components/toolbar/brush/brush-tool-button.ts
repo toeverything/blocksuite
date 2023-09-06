0e7b859fd141dd9ed0f378c3b73b8292a112b236
Zhilin Liu <zhilin7@qq.com>
2023-09-06 23:56:21 +0800

    fix: lint

--- packages/blocks/src/page-block/edgeless/components/toolbar/brush/brush-tool-button.ts
+++ packages/blocks/src/page-block/edgeless/components/toolbar/brush/brush-tool-button.ts
@@ -10,7 +10,7 @@ import {
   type EdgelessTool,
   LineWidth,
 } from '../../../../../__internal__/index.js';
-import { ArrowUpIcon, EdgelessPenIcon } from '../../../../../icons/index.js';
+import { EdgelessPenIcon } from '../../../../../icons/index.js';
 import type { EdgelessPageBlockComponent } from '../../../edgeless-page-block.js';
 import { DEFAULT_BRUSH_COLOR } from '../../panel/color-panel.js';
 import { getTooltipWithShortcut } from '../../utils.js';
