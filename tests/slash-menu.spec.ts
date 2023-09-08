51d1e9d00789bdcbfc7ebcae75d2415ab42e046c
Flrande <flrande.ch@gmail.com>
2023-09-08 22:36:15 +0800

    test: fix

--- tests/slash-menu.spec.ts
+++ tests/slash-menu.spec.ts
@@ -88,6 +88,7 @@ test.describe('slash menu should show and hide correctly', () => {
 
   test('delete the slash symbol should close the slash menu', async () => {
     await page.keyboard.press('Backspace');
+    await waitNextFrame(page);
     await expect(slashMenu).not.toBeVisible();
     await assertStoreMatchJSX(
       page,
