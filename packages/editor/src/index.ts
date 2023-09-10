a892e77079725eacc2074f2bf7237006e9eac566
yunnian <936321732@qq.com>
2023-09-11 03:08:57 +0800

    fix: fix errors in next.js

--- packages/editor/src/index.ts
+++ packages/editor/src/index.ts
@@ -22,7 +22,7 @@ if (env[importIdentifier] === true) {
 }
 
 if (typeof window === 'undefined') {
-  throw new Error(
+  console.error(
     'Seems like you are importing @blocksuite/editor in SSR mode. Which is not supported for now.'
   );
 }
