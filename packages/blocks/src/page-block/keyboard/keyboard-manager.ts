73ac4a736378eaa5d5075e201afedfe205cb051a
fourdim <59462000+fourdim@users.noreply.github.com>
2023-09-11 13:30:43 +0800

    fix: tests

--- packages/blocks/src/page-block/keyboard/keyboard-manager.ts
+++ packages/blocks/src/page-block/keyboard/keyboard-manager.ts
@@ -18,7 +18,7 @@ export class PageKeyboardManager {
             this._page.redo();
           }
         },
-        'Mod-Backspace': () => {},
+        'Mod-Backspace': () => true,
         Backspace: this._handleDelete,
         Delete: this._handleDelete,
         'Control-d': this._handleDelete,
