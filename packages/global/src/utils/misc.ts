b14f4b4d4247dd13beea08f8e9881a20f6538913
lawvs <18554747+lawvs@users.noreply.github.com>
2023-09-12 16:01:36 +0800

    refactor: simpilfy whenHover

--- packages/global/src/utils/misc.ts
+++ packages/global/src/utils/misc.ts
@@ -19,6 +19,21 @@ export function launchIntoFullscreen(element: Element) {
   }
 }
 
+export type WhenHoverOptions = {
+  leaveDelay?: number;
+  /**
+   * When already hovered to the reference element,
+   * but the floating element is not ready,
+   * the callback will still be executed if the `alwayRunWhenNoFloating` is true.
+   *
+   * It is useful when the floating element is removed just before by a user's action,
+   * and the user's mouse is still hovering over the reference element.
+   *
+   * @default true
+   */
+  alwayRunWhenNoFloating?: boolean;
+};
+
 /**
  * Call the `whenHoverChange` callback when the element is hovered.
  *
@@ -55,13 +70,7 @@ export function launchIntoFullscreen(element: Element) {
  */
 export const whenHover = (
   whenHoverChange: (isHover: boolean, event?: Event) => void,
-  {
-    leaveDelay = 300,
-    alwayRunWhenNoFloating = true,
-  }: {
-    leaveDelay?: number;
-    alwayRunWhenNoFloating?: boolean;
-  } = {}
+  { leaveDelay = 300, alwayRunWhenNoFloating = true }: WhenHoverOptions = {}
 ) => {
   /**
    * The event listener will be removed when the signal is aborted.
