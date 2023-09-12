6d12f709d251dc30dd80ed9a32dc368faddf0c18
lawvs <18554747+lawvs@users.noreply.github.com>
2023-09-12 17:24:36 +0800

    chore: update editor layout

--- packages/blocks/src/page-block/doc/doc-page-block.ts
+++ packages/blocks/src/page-block/doc/doc-page-block.ts
@@ -63,6 +63,9 @@ export class DocPageBlockComponent extends BlockElement<
       padding-left: var(--affine-editor-side-padding, ${PAGE_BLOCK_CHILD_PADDING}px);
       /* prettier-ignore */
       padding-right: var(--affine-editor-side-padding, ${PAGE_BLOCK_CHILD_PADDING}px);
+      /* TODO update theme after pass test */
+      padding-left: 96px;
+      padding-right: 96px;
     }
 
     .affine-doc-page-block-title {
