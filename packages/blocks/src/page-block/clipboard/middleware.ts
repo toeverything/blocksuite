d4d2bade474c42d7763989ceab6a472e1b7409e1
Mirone <Saul-Mirone@outlook.com>
2023-09-15 10:47:21 +0800

    chore: middleware

--- packages/blocks/src/page-block/clipboard/middleware.ts
+++ packages/blocks/src/page-block/clipboard/middleware.ts
@@ -1,47 +1,63 @@
 import type { TextRangePoint } from '@blocksuite/block-std';
 import { PathFinder } from '@blocksuite/block-std';
 import type { BlockSuiteRoot } from '@blocksuite/lit';
-import type { JobMiddleware } from '@blocksuite/store';
+import type { BlockSnapshot, JobMiddleware, JobSlots } from '@blocksuite/store';
+import type { BaseBlockModel } from '@blocksuite/store';
+
+const handlePoint = (
+  point: TextRangePoint,
+  snapshot: BlockSnapshot,
+  model: BaseBlockModel
+) => {
+  const { index, length } = point;
+  if (!snapshot.props.text) {
+    return;
+  }
+  (snapshot.props.text as Record<string, unknown>).delta =
+    model.text?.sliceToDelta(index, length + index);
+};
+
+const replaceId = (slots: JobSlots, std: BlockSuiteRoot['std']) => {
+  const idMap = new Map<string, string>();
+  slots.afterExport.on(payload => {
+    if (payload.type === 'block') {
+      const snapshot = payload.snapshot;
+      const original = snapshot.id;
+      let newId: string;
+      if (idMap.has(original)) {
+        newId = idMap.get(original)!;
+      } else {
+        newId = std.page.workspace.idGenerator();
+        idMap.set(original, newId);
+      }
+      snapshot.id = newId;
+    }
+  });
+};
+
+const sliceText = (slots: JobSlots, std: BlockSuiteRoot['std']) => {
+  slots.afterExport.on(payload => {
+    if (payload.type === 'block') {
+      const snapshot = payload.snapshot;
+
+      const model = payload.model;
+      const text = std.selection.find('text');
+      if (text && PathFinder.id(text.from.path) === model.id) {
+        handlePoint(text.from, snapshot, model);
+        return;
+      }
+      if (text && text.to && PathFinder.id(text.to.path) === model.id) {
+        handlePoint(text.to, snapshot, model);
+        return;
+      }
+    }
+  });
+};
 
 export const copyMiddleware = (std: BlockSuiteRoot['std']): JobMiddleware => {
   return ({ slots }) => {
-    const idMap = new Map<string, string>();
-    slots.afterExport.on(payload => {
-      if (payload.type === 'slice') {
-        console.log(payload);
-      }
-      if (payload.type === 'block') {
-        const handlePoint = (point: TextRangePoint) => {
-          const { index, length } = point;
-          if (!snapshot.props.text) {
-            return;
-          }
-          (snapshot.props.text as Record<string, unknown>).delta =
-            model.text?.sliceToDelta(index, length + index);
-        };
-        const snapshot = payload.snapshot;
-        const original = snapshot.id;
-        let newId: string;
-        if (idMap.has(original)) {
-          newId = idMap.get(original)!;
-        } else {
-          newId = std.page.workspace.idGenerator();
-          idMap.set(original, newId);
-        }
-        snapshot.id = newId;
-
-        const model = payload.model;
-        const text = std.selection.find('text');
-        if (text && PathFinder.id(text.from.path) === model.id) {
-          handlePoint(text.from);
-          return;
-        }
-        if (text && text.to && PathFinder.id(text.to.path) === model.id) {
-          handlePoint(text.to);
-          return;
-        }
-      }
-    });
+    replaceId(slots, std);
+    sliceText(slots, std);
   };
 };
 
