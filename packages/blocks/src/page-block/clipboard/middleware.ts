4640a4d333723da8aea99dc73beeda635776ba5b
Mirone <Saul-Mirone@outlook.com>
2023-09-15 17:01:51 +0800

    chore: finish paste middleware

--- packages/blocks/src/page-block/clipboard/middleware.ts
+++ packages/blocks/src/page-block/clipboard/middleware.ts
@@ -1,8 +1,15 @@
 import type { TextRangePoint } from '@blocksuite/block-std';
 import { PathFinder } from '@blocksuite/block-std';
 import type { BlockSuiteRoot } from '@blocksuite/lit';
-import type { BlockSnapshot, JobMiddleware, JobSlots } from '@blocksuite/store';
-import type { BaseBlockModel } from '@blocksuite/store';
+import type {
+  BaseBlockModel,
+  BlockSnapshot,
+  DeltaOperation,
+  JobMiddleware,
+  JobSlots,
+} from '@blocksuite/store';
+
+import type { ParagraphBlockModel } from '../../paragraph-block/index.js';
 
 const handlePoint = (
   point: TextRangePoint,
@@ -63,10 +70,115 @@ export const copyMiddleware = (std: BlockSuiteRoot['std']): JobMiddleware => {
 
 export const pasteMiddleware = (std: BlockSuiteRoot['std']): JobMiddleware => {
   return ({ slots }) => {
+    let from: TextRangePoint | undefined;
+    let to: TextRangePoint | null | undefined;
+    let lastSnapshotId: string | undefined;
+    let lastIndex = 0;
     slots.beforeImport.on(payload => {
-      if (payload.type === 'block') {
+      if (payload.type === 'slice') {
         const text = std.selection.find('text');
-        text;
+        if (!text) {
+          return;
+        }
+        const blockSnapshots = payload.snapshot.content;
+        const firstBlock = blockSnapshots[0];
+        const findLast = (snapshot: BlockSnapshot): BlockSnapshot => {
+          if (snapshot.children && snapshot.children.length > 0) {
+            return findLast(snapshot.children[snapshot.children.length - 1]);
+          }
+          return snapshot;
+        };
+        const lastBlock: BlockSnapshot = findLast(
+          blockSnapshots[blockSnapshots.length - 1]
+        );
+        lastSnapshotId = lastBlock.id;
+
+        ({ from, to } = text);
+        const _to = to ?? from;
+        const fromBlock = std.view.viewFromPath('block', from.path);
+        const toBlock = std.view.viewFromPath('block', _to.path);
+        const firstTextSnapshot = firstBlock.props.text as Record<
+          'delta',
+          DeltaOperation[]
+        >;
+        const lastTextSnapshot = lastBlock.props.text as Record<
+          'delta',
+          DeltaOperation[]
+        >;
+        const firstDelta = firstTextSnapshot.delta;
+        const lastDelta = lastTextSnapshot.delta;
+
+        const fromText = fromBlock?.model.text;
+        const toText = toBlock?.model.text;
+
+        if (!fromText || !toText) {
+          return;
+        }
+
+        const fromDelta = fromText.sliceToDelta(0, from.index);
+        const toDelta = toText.sliceToDelta(
+          _to.index + _to.length,
+          toText.length
+        );
+
+        lastIndex = toText.length - _to.index - _to.length;
+
+        if (!fromDelta || !toDelta) {
+          return;
+        }
+
+        if (firstBlock === lastBlock) {
+          firstBlock.flavour = fromBlock.model.flavour;
+          if (firstBlock.props.type) {
+            firstBlock.props.type = (
+              fromBlock.model as ParagraphBlockModel
+            ).type;
+          }
+          firstTextSnapshot.delta = [...fromDelta, ...firstDelta, ...toDelta];
+          return;
+        }
+
+        firstBlock.flavour = fromBlock.model.flavour;
+        lastBlock.flavour = toBlock.model.flavour;
+        if (firstBlock.props.type) {
+          firstBlock.props.type = (fromBlock.model as ParagraphBlockModel).type;
+        }
+        if (lastBlock.props.type) {
+          lastBlock.props.type = (toBlock.model as ParagraphBlockModel).type;
+        }
+        firstTextSnapshot.delta = [...fromDelta, ...firstDelta];
+        lastTextSnapshot.delta = [...lastDelta, ...toDelta];
+      }
+    });
+    slots.afterImport.on(payload => {
+      if (!from) {
+        return;
+      }
+      if (payload.type === 'slice') {
+        const fromBlock = std.view.viewFromPath('block', from.path);
+        if (fromBlock) {
+          std.page.deleteBlock(fromBlock.model);
+        }
+        if (to) {
+          const toBlock = std.view.viewFromPath('block', to.path);
+          if (toBlock) {
+            std.page.deleteBlock(toBlock.model);
+          }
+        }
+      }
+      if (payload.type === 'block' && payload.snapshot.id === lastSnapshotId) {
+        const point = to ?? from;
+        const parentId = PathFinder.parent(point.path);
+        std.selection.set([
+          std.selection.getInstance('text', {
+            from: {
+              path: parentId.concat(payload.snapshot.id),
+              index: (payload.model.text?.length ?? 0) - lastIndex,
+              length: 0,
+            },
+            to: null,
+          }),
+        ]);
       }
     });
   };
