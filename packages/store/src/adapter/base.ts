b3ce786d2ca479611ad5a0793b4ee2b785775001
fourdim <59462000+fourdim@users.noreply.github.com>
2023-09-18 07:11:31 +0800

    feat(store): add adapter ast walker

--- packages/store/src/adapter/base.ts
+++ packages/store/src/adapter/base.ts
@@ -50,3 +50,150 @@ export abstract class BaseAdapter<AdapterTarget = unknown> {
     payload: ToSliceSnapshotPayload<AdapterTarget>
   ): Promise<SliceSnapshot>;
 }
+
+export type ASTWalkerContext<TNode> = {
+  skip: () => void;
+  addNode: (node: TNode, prop: keyof TNode, index?: number) => void;
+};
+
+type WalkerFn<ONode, TNode> = (
+  node: ONode,
+  parent: ONode | null,
+  context: ASTWalkerContext<TNode>
+) => void;
+
+type NodeProps<Node> = {
+  node: Node;
+  parent: Node | null;
+  prop: keyof Node | null;
+  index: number | null;
+};
+
+// Ported from https://github.com/Rich-Harris/estree-walker MIT License
+export class ASTWalker<ONode extends object, TNode extends object> {
+  private _should_skip = false;
+
+  private _addedTNode: {
+    node: TNode;
+    prop: keyof TNode;
+    index?: number;
+  } | null = null;
+
+  private _enter: WalkerFn<ONode, TNode> | undefined;
+  private _leave: WalkerFn<ONode, TNode> | undefined;
+
+  private context: ASTWalkerContext<TNode>;
+
+  constructor() {
+    this.context = {
+      skip: () => (this._should_skip = true),
+      addNode: (node: TNode, prop: keyof TNode, index?: number) =>
+        (this._addedTNode = { node: node, prop: prop, index: index }),
+    };
+  }
+
+  private _resetContext = () => {
+    this._should_skip = false;
+    this._addedTNode = null;
+  };
+
+  setEnter = (
+    fn: (
+      node: ONode,
+      parent: ONode | null,
+      context: ASTWalkerContext<TNode>
+    ) => void
+  ) => {
+    this._enter = fn;
+  };
+
+  setLeave = (
+    fn: (
+      node: ONode,
+      parent: ONode | null,
+      context: ASTWalkerContext<TNode>
+    ) => void
+  ) => {
+    this._leave = fn;
+  };
+
+  walk = (_oNode: ONode, _tNode: TNode) => {
+    /* empty */
+  };
+
+  private _visit = (o: NodeProps<ONode>, t: NodeProps<TNode>) => {
+    if (!o.node) return;
+
+    if (this._enter) {
+      const should_skip = this._should_skip;
+      const addedTNode = this._addedTNode;
+      this._resetContext();
+      this._enter(o.node as ONode, o.parent, this.context);
+
+      if (addedTNode) {
+        if (addedTNode.index !== undefined) {
+          (t.node[addedTNode.prop] as Array<object>).splice(
+            addedTNode.index,
+            0,
+            addedTNode.node
+          );
+        } else {
+          (t.node[addedTNode.prop] as object) = addedTNode.node;
+        }
+      }
+
+      if (should_skip) {
+        return;
+      }
+    }
+
+    for (const key in o.node) {
+      const value = o.node[key];
+
+      if (value && typeof value === 'object') {
+        if (Array.isArray(value)) {
+          const nodes = value;
+          for (let i = 0; i < nodes.length; i += 1) {
+            const item = nodes[i];
+            if (item !== null && typeof item === 'object') {
+              this._visit(
+                { node: item, parent: o.node, prop: key, index: i },
+                // FIXME: t.node is not correct
+                { node: t.node, parent: null, prop: null, index: null }
+              );
+            }
+          }
+        } else {
+          this._visit(
+            { node: value as ONode, parent: o.node, prop: key, index: null },
+            // FIXME: t.node is not correct
+            { node: t.node, parent: null, prop: null, index: null }
+          );
+        }
+      }
+    }
+
+    if (this._leave) {
+      const should_skip = this._should_skip;
+      const addedTNode = this._addedTNode;
+      this._resetContext();
+      this._leave(o.node as ONode, o.parent, this.context);
+
+      if (addedTNode) {
+        if (addedTNode.index !== undefined) {
+          (t.node[addedTNode.prop] as Array<object>).splice(
+            addedTNode.index,
+            0,
+            addedTNode.node
+          );
+        } else {
+          (t.node[addedTNode.prop] as object) = addedTNode.node;
+        }
+      }
+
+      if (should_skip) {
+        return;
+      }
+    }
+  };
+}
