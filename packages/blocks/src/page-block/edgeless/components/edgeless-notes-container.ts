c84a4e5323297960cee4e3bdbe7a35f163f0a323
Mirone <Saul-Mirone@outlook.com>
2023-09-18 18:04:17 +0800

    fix: note won't sync when collaborating

--- packages/blocks/src/page-block/edgeless/components/edgeless-notes-container.ts
+++ packages/blocks/src/page-block/edgeless/components/edgeless-notes-container.ts
@@ -61,7 +61,7 @@ export class EdgelessNoteMask extends WithDisposable(LitElement) {
 }
 
 @customElement('edgeless-child-note')
-export class EdgelessChildNote extends LitElement {
+export class EdgelessChildNote extends WithDisposable(LitElement) {
   @property({ attribute: false })
   index!: number;
 
@@ -76,6 +76,15 @@ export class EdgelessChildNote extends LitElement {
 
   protected override createRenderRoot() {
     return this;
+  }
+
+  override connectedCallback() {
+    super.connectedCallback();
+    this._disposables.add(
+      this.model.propsUpdated.on(() => {
+        this.requestUpdate();
+      })
+    );
   }
 
   override render() {
