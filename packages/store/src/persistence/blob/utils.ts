824225f0c2fe47d467ba305e536fa1363649d2bd
Alex Yang <himself65@outlook.com>
2023-09-18 20:58:11 -0500

    Revert "fix: code"
    This reverts commit 08279183cece2ba04267dd8abd0edbeb6274d142.

--- packages/store/src/persistence/blob/utils.ts
+++ packages/store/src/persistence/blob/utils.ts
@@ -1,5 +1,5 @@
 export async function sha(input: ArrayBuffer): Promise<string> {
   const hash = await crypto.subtle.digest('SHA-256', input);
   const decoder = new TextDecoder('utf8');
-  return btoa(decoder.decode(hash));
+  return decoder.decode(hash);
 }
