08279183cece2ba04267dd8abd0edbeb6274d142
Alex Yang <himself65@outlook.com>
2023-09-18 20:31:38 -0500

    fix: code

--- packages/store/src/persistence/blob/utils.ts
+++ packages/store/src/persistence/blob/utils.ts
@@ -1,5 +1,5 @@
 export async function sha(input: ArrayBuffer): Promise<string> {
   const hash = await crypto.subtle.digest('SHA-256', input);
   const decoder = new TextDecoder('utf8');
-  return decoder.decode(hash);
+  return btoa(decoder.decode(hash));
 }
