4fb2b58b3a43e0f19fb1ae9a0ac3deb765abf184
Alex Yang <himself65@outlook.com>
2023-09-18 20:58:18 -0500

    Revert "fix: remove usage of `buffer`"
    This reverts commit e6e78f0f719d83207586f9f02d4c8943b0ce5c35.

--- packages/store/src/persistence/blob/utils.ts
+++ packages/store/src/persistence/blob/utils.ts
@@ -1,6 +1,8 @@
+import { Buffer } from 'buffer';
+
 export async function sha(input: ArrayBuffer): Promise<string> {
   const hash = await crypto.subtle.digest('SHA-256', input);
-  return Array.from(new Uint8Array(hash))
-    .map(b => b.toString(16).padStart(2, '0'))
-    .join('');
+  const buffer = Buffer.from(hash);
+
+  return buffer.toString('base64').replace(/\+/g, '-').replace(/\//g, '_');
 }
