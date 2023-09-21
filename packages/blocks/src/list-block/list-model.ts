744fddad677f2ef8cd9a228ba1e484872bb3b26f
lawvs <18554747+lawvs@users.noreply.github.com>
2023-09-21 18:07:05 +0800

    fix: keeping list toggle state

--- packages/blocks/src/list-block/list-model.ts
+++ packages/blocks/src/list-block/list-model.ts
@@ -1,12 +1,21 @@
 import { defineBlockSchema, type SchemaToModel } from '@blocksuite/store';
 
 export type ListType = 'bulleted' | 'numbered' | 'todo' | 'toggle';
+
+declare const BackwardUndefined: unique symbol;
+/**
+ * The `collapsed` property may be `undefined` due to legacy data,
+ * but you should not manually set it to undefined.
+ */
+type ListCollapsed = boolean | typeof BackwardUndefined;
+
 export const ListBlockSchema = defineBlockSchema({
   flavour: 'affine:list',
   props: internal => ({
     type: 'bulleted' as ListType,
     text: internal.Text(),
     checked: false,
+    collapsed: false as ListCollapsed,
   }),
   metadata: {
     version: 1,
