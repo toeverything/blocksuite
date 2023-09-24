72ac411dc4e17aa64ce90132cc95b61553660bb9
hongtao.lye <hongtao.lye@toeverything.info>
2023-09-24 12:56:03 +0800

    fix: disable prettier for ledit template

--- packages/blocks/src/image-block/ledits/utils.ts
+++ packages/blocks/src/image-block/ledits/utils.ts
@@ -1,6 +1,8 @@
+/* eslint-disable prettier/prettier */
 import { html } from 'lit';
 
 export function createInlineIframe() {
+  /* prettier-ignore */
   const htmlContent = html`<!doctype html>
     <html>
       <head>
@@ -60,7 +62,7 @@ export function createInlineIframe() {
 
           if (resultImg === null) return null;
 
-          const pattern = /^data:(image/w+);/;
+          const pattern = /^data:(image\/\w+);/;
           const execResult = pattern.exec(resultImg.src);
 
           if (!execResult) return null;
