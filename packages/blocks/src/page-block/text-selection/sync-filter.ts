d48787ac1d01938626a9ab5bf80c00053792f4ba
Flrande <flrande.ch@gmail.com>
2023-09-26 21:29:31 +0800

    fix: range

---
+++ packages/blocks/src/page-block/text-selection/sync-filter.ts
@@ -0,0 +1,24 @@
+import type { RangeSyncFilter } from '@blocksuite/lit';
+
+import { BLOCK_ID_ATTR } from '../../__internal__/consts.js';
+
+export const pageRangeSyncFilter: RangeSyncFilter = {
+  rangeToTextSelection: range => {
+    if (range) {
+      const { startContainer, endContainer } = range;
+
+      const startBlock = startContainer.parentElement?.closest(
+        `[${BLOCK_ID_ATTR}]`
+      );
+      const endBlock = endContainer.parentElement?.closest(
+        `[${BLOCK_ID_ATTR}]`
+      );
+      console.log(startBlock, endBlock, range);
+      if (startBlock && endBlock) {
+        return startBlock.role === 'content' && endBlock.role === 'content';
+      }
+    }
+
+    return true;
+  },
+};
