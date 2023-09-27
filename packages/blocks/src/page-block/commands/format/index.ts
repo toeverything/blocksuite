1d7ffe47f5b22ef0d3fb784152a2619a9272b531
Flrande <flrande.ch@gmail.com>
2023-09-27 16:34:45 +0800

    fix: build

---
+++ packages/blocks/src/page-block/commands/format/index.ts
@@ -0,0 +1,3 @@
+export * from './format-block.js';
+export * from './format-native.js';
+export * from './format-text.js';
--- packages/blocks/src/page-block/page-service.ts
+++ packages/blocks/src/page-block/page-service.ts
@@ -1,17 +1,19 @@
 import { BlockService } from '@blocksuite/block-std';
 
-import { copySelectedBlockCommand } from './commands/copy-selected-block.js';
-import { deleteSelectedBlockCommand } from './commands/delete-selected-block.js';
-import { deleteSelectedTextCommand } from './commands/delete-selected-text.js';
-import { formatBlockCommand } from './commands/format/format-block.js';
-import { formatNativeCommand } from './commands/format/format-native.js';
-import { formatTextCommand } from './commands/format/format-text.js';
-import { getBlockIndexCommand } from './commands/get-block-index.js';
-import { getBlockSelectionsCommand } from './commands/get-block-selection-by-side.js';
-import { getNextBlockCommand } from './commands/get-next-block.js';
-import { getPrevBlockCommand } from './commands/get-prev-block.js';
-import { getSelectedModelsCommand } from './commands/get-selected-models.js';
-import { getTextSelectionCommand } from './commands/get-text-selection.js';
+import {
+  copySelectedBlockCommand,
+  deleteSelectedBlockCommand,
+  deleteSelectedTextCommand,
+  formatBlockCommand,
+  formatNativeCommand,
+  formatTextCommand,
+  getBlockIndexCommand,
+  getBlockSelectionsCommand,
+  getNextBlockCommand,
+  getPrevBlockCommand,
+  getSelectedModelsCommand,
+  getTextSelectionCommand,
+} from './commands/index.js';
 import type { PageBlockModel } from './page-model.js';
 
 export class PageService extends BlockService<PageBlockModel> {
