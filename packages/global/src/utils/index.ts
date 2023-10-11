c20a8dcd95c8c177ad1a70371829c00558d07b03
lawvs <18554747+lawvs@users.noreply.github.com>
2023-10-11 21:44:48 +0800

    refactor: extract hover to new file

--- packages/global/src/utils/index.ts
+++ packages/global/src/utils/index.ts
@@ -1,6 +1,7 @@
 export * from './assert.js';
 export * from './disposable.js';
 export * from './function.js';
+export * from './hover.js';
 export * from './iterable.js';
 export * from './slot.js';
 export * from './string.js';
