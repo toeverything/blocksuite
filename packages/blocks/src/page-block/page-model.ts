7ed5500427f93cfc115f4c2345dd5530fd483da3
Flrande <flrande.ch@gmail.com>
2023-10-12 16:41:44 +0800

    fix: fix

--- packages/blocks/src/page-block/page-model.ts
+++ packages/blocks/src/page-block/page-model.ts
@@ -5,7 +5,25 @@ type PageProps = {
   title: Text;
 };
 
-export class PageBlockModel extends BaseBlockModel<PageProps> {}
+export class PageBlockModel extends BaseBlockModel<PageProps> {
+  constructor() {
+    super();
+    this.created.on(() => {
+      this.page.slots.rootAdded.on(model => {
+        if (model instanceof PageBlockModel) {
+          const newPageMeta = this.page.workspace.meta.getPageMeta(
+            model.page.id
+          );
+          if (!newPageMeta || newPageMeta.title !== model.title.toString()) {
+            this.page.workspace.setPageMeta(model.page.id, {
+              title: model.title.toString(),
+            });
+          }
+        }
+      });
+    });
+  }
+}
 
 export const PageBlockSchema = defineBlockSchema({
   flavour: 'affine:page',
