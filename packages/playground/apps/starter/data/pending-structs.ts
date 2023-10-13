b021e37bb491a5752d845ce883915ce18957acbd
Mirone <Saul-Mirone@outlook.com>
2023-10-13 09:47:59 +0800

    feat(playground): add entry for pending structs (#5000)

---
+++ packages/playground/apps/starter/data/pending-structs.ts
@@ -0,0 +1,43 @@
+import { Workspace } from '@blocksuite/store';
+import { Text } from '@blocksuite/store';
+
+import type { InitFn } from './utils';
+
+export const pendingStructs: InitFn = async (
+  workspace: Workspace,
+  id: string
+) => {
+  const page = workspace.createPage({ id });
+  const tempPage = workspace.createPage({ id: 'tempPage' });
+  await page.waitForLoaded();
+  await tempPage.waitForLoaded();
+  const pageBlockId = tempPage.addBlock('affine:page', {
+    title: new Text('Pending Structs'),
+  });
+  const vec = Workspace.Y.encodeStateVector(tempPage.spaceDoc);
+
+  // To avoid pending structs, uncomment the following line
+  // const update = Workspace.Y.encodeStateAsUpdate(tempPage.spaceDoc);
+
+  tempPage.addBlock('affine:surface', {}, pageBlockId);
+  // Add note block inside page block
+  const noteId = tempPage.addBlock('affine:note', {}, pageBlockId);
+  tempPage.addBlock(
+    'affine:paragraph',
+    {
+      text: new Text('This is a paragraph block'),
+    },
+    noteId
+  );
+
+  const diff = Workspace.Y.encodeStateAsUpdate(tempPage.spaceDoc, vec);
+
+  // To avoid pending structs, uncomment the following line
+  // Workspace.Y.applyUpdate(page.spaceDoc, update);
+
+  Workspace.Y.applyUpdate(page.spaceDoc, diff);
+};
+
+pendingStructs.id = 'pending-structs';
+pendingStructs.displayName = 'Pending Structs';
+pendingStructs.description = 'Page doc with pending structs';
