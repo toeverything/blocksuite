aea6fe0c2413fb993d3281100cb4aa8ed383191f
luke358 <1494135711@qq.com>
2023-10-18 16:36:32 +0800

    fix(edgeless): fix frame title when inputting Chinese use Enter key

--- packages/blocks/src/page-block/edgeless/components/text/edgeless-frame-title-editor.ts
+++ packages/blocks/src/page-block/edgeless/components/text/edgeless-frame-title-editor.ts
@@ -61,8 +61,8 @@ export class EdgelessFrameTitleEditor extends WithDisposable(
     this.disposables.add(
       dispatcher.add('keyDown', ctx => {
         const state = ctx.get('keyboardState');
-        if (state.raw.key === 'Enter') {
-          setTimeout(() => this._unmount());
+        if (state.raw.key === 'Enter' && !state.raw.isComposing) {
+          this._unmount();
           return true;
         }
         requestAnimationFrame(() => {
