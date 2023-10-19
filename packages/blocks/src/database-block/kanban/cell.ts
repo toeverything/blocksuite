7c1dbe870d01b99b7fcf047b5fd9638e4135cac2
zqran <uuxnet@gmail.com>
2023-10-19 12:24:01 +0800

    fix: lint

--- packages/blocks/src/database-block/kanban/cell.ts
+++ packages/blocks/src/database-block/kanban/cell.ts
@@ -130,6 +130,7 @@ export class KanbanCell extends WithDisposable(ShadowlessElement) {
       rowId: this.cardId,
       isEditing: this.editing,
       selectCurrentCell: this.selectCurrentCell,
+      std: this.view.std,
     };
     const { view, edit } = this.column.renderer;
     this.style.border = this.isFocus
