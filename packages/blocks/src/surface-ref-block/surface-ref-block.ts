14e88211b3013882df429aeb767d858f3179eb8c
Hongtao Lye <hongtao.lye@toeverything.info>
2023-10-23 16:37:23 +0800

    feat: add empty placeholder

--- packages/blocks/src/surface-ref-block/surface-ref-block.ts
+++ packages/blocks/src/surface-ref-block/surface-ref-block.ts
@@ -15,11 +15,8 @@ import {
 } from '../__internal__/theme/css-variables.js';
 import { getThemePropertyValue } from '../__internal__/theme/utils.js';
 import { type AbstractEditor } from '../__internal__/utils/types.js';
-import type {
-  FrameBlockModel,
-  NoteBlockModel,
-  SurfaceBlockModel,
-} from '../models.js';
+import { MoreDeleteIcon } from '../icons/edgeless.js';
+import type { FrameBlockModel, SurfaceBlockModel } from '../models.js';
 import { getNotesInFrame } from '../page-block/edgeless/frame-manager.js';
 import { getBackgroundGrid } from '../page-block/edgeless/utils/query.js';
 import { type PhasorElementType } from '../surface-block/elements/edgeless-element.js';
@@ -29,13 +26,89 @@ import { Renderer } from '../surface-block/renderer.js';
 import { Bound } from '../surface-block/utils/bound.js';
 import { deserializeXYWH } from '../surface-block/utils/xywh.js';
 import type { SurfaceRefBlockModel } from './surface-ref-model.js';
-import { getSurfaceBlock } from './utils.js';
+import { getSurfaceBlock, noContentPlaceholder } from './utils.js';
+
+const NO_CONTENT_TITLE = {
+  'affine:frame': 'Frame',
+  'affine:group': 'Note',
+  DEFAULT: 'Content',
+} as Record<string, string>;
+
+const NO_CONTENT_REASON = {
+  'affine:group': 'This content was ungrouped or deleted on edgeless mode',
+  DEFAULT: 'This content was deleted on edgeless mode',
+} as Record<string, string>;
+
 @customElement('affine-surface-ref')
 export class SurfaceSyncBlockComponent extends BlockElement<SurfaceRefBlockModel> {
   static override styles = css`
     .affine-surface-ref {
       padding: 10px;
       position: relative;
+    }
+
+    .surface-empty-placeholder {
+      padding: 26px 0px 0px;
+    }
+
+    .placeholder-image {
+      margin: 0 auto;
+      text-align: center;
+    }
+
+    .placeholder-text {
+      margin: 12px auto 0;
+      text-align: center;
+      font-size: 28px;
+      font-weight: 600;
+      line-height: 36px;
+      font-family: var(--affine-font-family);
+    }
+
+    .placeholder-action {
+      margin: 32px auto 0;
+      text-align: center;
+    }
+
+    .delete-button {
+      width: 204px;
+      padding: 4px 18px;
+
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+      gap: 4px;
+
+      border-radius: 8px;
+      border: 1px solid var(--affine-border-color);
+
+      font-family: var(--affine-font-family);
+      font-size: 12px;
+      font-weight: 500;
+      line-height: 20px;
+
+      background-color: transparent;
+      cursor: pointer;
+    }
+
+    .delete-button > .icon > svg {
+      color: var(--affine-icon-color);
+      width: 16px;
+      height: 16px;
+      display: block;
+    }
+
+    .placeholder-reason {
+      margin: 72px auto 0;
+      padding: 10px;
+
+      text-align: center;
+      font-size: 12px;
+      font-family: var(--affine-font-family);
+      line-height: 20px;
+
+      color: var(--affine-warning-color);
+      background-color: var(--affine-background-error-color);
     }
 
     .surface-viewport {
@@ -113,12 +186,13 @@ export class SurfaceSyncBlockComponent extends BlockElement<SurfaceRefBlockModel
   initReferenceModel() {
     let referenceWathcer: Disposable | null = null;
     const init = () => {
+      referenceWathcer?.dispose();
+
       const referenceModel = this.getModel(
         this.model.reference
       ) as FrameBlockModel;
-
-      referenceWathcer?.dispose();
-      this._referenceModel = 'xywh' in referenceModel ? referenceModel : null;
+      this._referenceModel =
+        referenceModel && 'xywh' in referenceModel ? referenceModel : null;
 
       if (!this._referenceModel) return;
 
@@ -348,6 +422,10 @@ export class SurfaceSyncBlockComponent extends BlockElement<SurfaceRefBlockModel
     return this.page.getBlockById(id) ?? this._elements.get(id) ?? null;
   }
 
+  deleteThis() {
+    this.page.deleteBlock(this.model);
+  }
+
   viewInEdgeless() {
     if (!this._referenceModel) return;
 
@@ -384,7 +462,13 @@ export class SurfaceSyncBlockComponent extends BlockElement<SurfaceRefBlockModel
     });
   }
 
-  private _renderEdgelessNotes(notes: NoteBlockModel[]) {
+  private _renderTopLevelBlocks() {
+    const notes = getNotesInFrame(
+      this.page,
+      this._referenceModel as FrameBlockModel,
+      false
+    );
+
     return repeat(
       notes,
       model => model.id,
@@ -398,46 +482,72 @@ export class SurfaceSyncBlockComponent extends BlockElement<SurfaceRefBlockModel
     );
   }
 
-  override render() {
-    const { reference } = this.model;
-    const model = this.getModel(reference) as FrameBlockModel;
+  private _renderEmptyPlaceholder(model: SurfaceRefBlockModel) {
+    return html`<div class="surface-empty-placeholder">
+      <div class="placeholder-image">${noContentPlaceholder}</div>
+      <div class="placeholder-text">
+        No Such ${NO_CONTENT_TITLE[model.refFlavour ?? 'DEFAULT']}
+      </div>
+      <div class="placeholder-action">
+        <button class="delete-button" type="button" @click=${this.deleteThis}>
+          <span class="icon">${MoreDeleteIcon}</span
+          ><span>Delete this block</span>
+        </button>
+      </div>
+      <div class="placeholder-reason">
+        ${NO_CONTENT_REASON[model.refFlavour ?? 'DEFAULT']}
+      </div>
+    </div>`;
+  }
 
-    if (!this._surfaceModel || !model || !model.xywh) return nothing;
-
-    const [, , w, h] = deserializeXYWH(model.xywh);
-    const notes = getNotesInFrame(this.page, model, false);
-    const { zoom, translateX, translateY } = this._surfaceRenderer;
+  private _renderSurfaceContent(
+    referenceModel: FrameBlockModel,
+    renderer: Renderer
+  ) {
+    const [, , w, h] = deserializeXYWH(referenceModel.xywh);
+    const { zoom, translateX, translateY } = renderer;
     const { gap } = getBackgroundGrid(zoom, true);
 
-    return html`<div class="affine-surface-ref">
+    return html` <div
+      class="surface-viewport"
+      style=${styleMap({
+        width: `${w}px`,
+        aspectRatio: `${w} / ${h}`,
+        outline: this._focused
+          ? '2px solid var(--affine-primary-color)'
+          : undefined,
+        backgroundSize: `${gap}px ${gap}px`,
+      })}
+    >
+      ${this._referenceModel?.flavour === 'affine:frame'
+        ? html`<div
+            class="surface-block-portal"
+            style=${styleMap({
+              transform: `translate(${translateX}px, ${translateY}px) scale(${zoom})`,
+            })}
+          >
+            ${this._renderTopLevelBlocks()}
+          </div>`
+        : nothing}
       <div
-        class="surface-viewport"
-        style=${styleMap({
-          width: `${w}px`,
-          aspectRatio: `${w} / ${h}`,
-          outline: this._focused
-            ? '2px solid var(--affine-primary-color)'
-            : undefined,
-          backgroundPosition: `${translateX}px ${translateY}px`,
-          backgroundSize: `${gap}px ${gap}px`,
-        })}
+        class="surface-canvas-container"
+        @dblclick=${this.viewInEdgeless}
+        @click=${this.focusBlock}
       >
-        <div
-          class="surface-block-portal"
-          style=${styleMap({
-            transform: `translate(${translateX}px, ${translateY}px) scale(${zoom})`,
-          })}
-        >
-          ${this._renderEdgelessNotes(notes)}
-        </div>
-        <div
-          class="surface-canvas-container"
-          @dblclick=${this.viewInEdgeless}
-          @click=${this.focusBlock}
-        >
-          <!-- attach canvas here -->
-        </div>
+        <!-- attach canvas here -->
       </div>
+    </div>`;
+  }
+
+  override render() {
+    const { _surfaceModel, _referenceModel, _surfaceRenderer, model } = this;
+    const noContent =
+      !_surfaceModel || !_referenceModel || !_referenceModel.xywh;
+
+    return html`<div class="affine-surface-ref">
+      ${noContent
+        ? this._renderEmptyPlaceholder(model)
+        : this._renderSurfaceContent(_referenceModel, _surfaceRenderer)}
     </div>`;
   }
 }
