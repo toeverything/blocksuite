cdd13b8d6f6c9715058e4c47922be65eda06accb
Chen <99816898+donteatfriedrice@users.noreply.github.com>
2023-11-09 19:54:54 +0800

    feat(playground): add more copilot poc actions (#5274)

--- packages/playground/apps/starter/components/copilot/custom-copilot-panel.ts
+++ packages/playground/apps/starter/components/copilot/custom-copilot-panel.ts
@@ -1,23 +1,32 @@
 /* eslint-disable @typescript-eslint/no-restricted-imports */
-
 import '@shoelace-style/shoelace/dist/components/button-group/button-group.js';
 import '@shoelace-style/shoelace/dist/components/button/button.js';
 
 import type { EditorContainer } from '@blocksuite/editor';
 import type { BlockSuiteRoot } from '@blocksuite/lit';
 import { WithDisposable } from '@blocksuite/lit';
+import type { SlPopup } from '@shoelace-style/shoelace';
 import { css, html, LitElement, nothing } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 
 import {
+  sendChangeToneRequest,
+  sendFixSpellingRequest,
+  sendImproveWritingRequest,
+  sendMakeLongerRequest,
+  sendMakeShorterRequest,
   sendRefineRequest,
+  sendSimplifyLanguageRequest,
   sendSummaryRequest,
   sendTranslateRequest,
 } from './api.js';
+import { LANGUAGE, TONE } from './config.js';
+import { insertFromMarkdown } from './utils/markdown-utils.js';
 import {
+  getSelectedBlocks,
   getSelectedTextContent,
   hasSelectedTextContent,
-} from './selection-utils.js';
+} from './utils/selection-utils.js';
 
 @customElement('custom-copilot-panel')
 export class CustomCopilotPanel extends WithDisposable(LitElement) {
@@ -38,6 +47,14 @@ export class CustomCopilotPanel extends WithDisposable(LitElement) {
       z-index: 1;
     }
 
+    .action-button-group {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      flex-wrap: wrap;
+      gap: 4px;
+    }
+
     .result-area {
       box-sizing: border-box;
       margin: 8px auto;
@@ -47,6 +64,7 @@ export class CustomCopilotPanel extends WithDisposable(LitElement) {
       white-space: pre-line;
       font-family: var(--affine-font-family);
       font-size: 12px;
+      overflow-wrap: break-word;
     }
   `;
   @state()
@@ -60,6 +78,10 @@ export class CustomCopilotPanel extends WithDisposable(LitElement) {
 
   @property({ attribute: false })
   editor!: EditorContainer;
+
+  private _lastPayload: { [key: string]: string } | null = null;
+  private _langPopupTimer: ReturnType<typeof setTimeout> | null = null;
+  private _tonePopupTimer: ReturnType<typeof setTimeout> | null = null;
 
   get page() {
     return this.editor.page;
@@ -77,22 +99,137 @@ export class CustomCopilotPanel extends WithDisposable(LitElement) {
     }
   }
 
-  private async _refine() {
-    const selectedLines = getSelectedTextContent(this.root);
-    const result = await sendRefineRequest(selectedLines);
-    this._result = result;
+  private get _selectedTextContent() {
+    return getSelectedTextContent(this.root);
   }
 
-  private async _translate() {
-    const selectedLines = getSelectedTextContent(this.root);
-    const result = await sendTranslateRequest(selectedLines);
-    this._result = result;
+  private async _sendRequest(payload: { [key: string]: string }) {
+    let result = '';
+    switch (payload.action) {
+      case 'refine':
+        result = await sendRefineRequest(payload.input);
+        break;
+      case 'translate':
+        result = await sendTranslateRequest(payload.input, payload.language);
+        break;
+      case 'summary':
+        result = await sendSummaryRequest(payload.input);
+        break;
+      case 'improveWriting':
+        result = await sendImproveWritingRequest(payload.input);
+        break;
+      case 'fixSpelling':
+        result = await sendFixSpellingRequest(payload.input);
+        break;
+      case 'makeShorter':
+        result = await sendMakeShorterRequest(payload.input);
+        break;
+      case 'makeLonger':
+        result = await sendMakeLongerRequest(payload.input);
+        break;
+      case 'changeTone':
+        result = await sendChangeToneRequest(payload.input, payload.tone);
+        break;
+      case 'simplifyLanguage':
+        result = await sendSimplifyLanguageRequest(payload.input);
+        break;
+    }
+
+    return result;
   }
 
-  private async _summary() {
-    const selectedLines = getSelectedTextContent(this.root);
-    const result = await sendSummaryRequest(selectedLines);
-    this._result = result;
+  private async _handleActionClick(
+    action: string,
+    options?: { [key: string]: string }
+  ) {
+    const payload = {
+      action,
+      input: await this._selectedTextContent,
+      ...options,
+    };
+
+    this._lastPayload = payload;
+    this._result = await this._sendRequest(payload);
+  }
+
+  private _clearState() {
+    this._result = '';
+    this._lastPayload = null;
+    this._langPopupTimer = null;
+    this._tonePopupTimer = null;
+  }
+
+  private async _replace() {
+    if (!this._result) return;
+
+    const selectedBlocks = await getSelectedBlocks(this.root);
+    if (!selectedBlocks.length) return;
+
+    const firstBlock = selectedBlocks[0];
+    const parentBlock = firstBlock.parentBlockElement;
+
+    // update selected block
+    const firstIndex = parentBlock.model.children.findIndex(
+      child => child.id === firstBlock.model.id
+    ) as number;
+    selectedBlocks.forEach(block => {
+      this.page.deleteBlock(block.model);
+    });
+
+    const models = await insertFromMarkdown(
+      this.root,
+      this._result,
+      parentBlock.model.id,
+      firstIndex
+    );
+
+    setTimeout(async () => {
+      const parentPath = firstBlock.parentPath;
+      const selections = models
+        .map(model => [...parentPath, model.id])
+        .map(path => this.root.selection.getInstance('block', { path }));
+      this.root.selection.setGroup('note', selections);
+    }, 0);
+
+    this._clearState();
+  }
+
+  private async _insertBelow() {
+    if (!this._result) return;
+
+    const selectedBlocks = await getSelectedBlocks(this.root);
+    const blockLength = selectedBlocks.length;
+    if (!blockLength) return;
+
+    const lastBlock = selectedBlocks[blockLength - 1];
+    const parentBlock = lastBlock.parentBlockElement;
+
+    const lastIndex = parentBlock.model.children.findIndex(
+      child => child.id === lastBlock.model.id
+    ) as number;
+
+    const models = await insertFromMarkdown(
+      this.root,
+      this._result,
+      parentBlock.model.id,
+      lastIndex + 1
+    );
+
+    setTimeout(async () => {
+      const parentPath = lastBlock.parentPath;
+      const selections = models
+        .map(model => [...parentPath, model.id])
+        .map(path => this.root.selection.getInstance('block', { path }));
+      this.root.selection.setGroup('note', selections);
+    }, 0);
+
+    this._clearState();
+  }
+
+  private async _retry() {
+    if (!this._lastPayload) return;
+
+    this._result = await this._sendRequest(this._lastPayload);
   }
 
   private _ResultArea() {
@@ -100,16 +237,135 @@ export class CustomCopilotPanel extends WithDisposable(LitElement) {
 
     return html`
       <div class="result-area">${this._result}</div>
-      <sl-button size="small" disabled> Replace </sl-button>
+      <sl-button size="small" @click=${this._replace}> Replace </sl-button>
+      <sl-button size="small" @click=${this._insertBelow}>
+        Insert below
+      </sl-button>
+      <sl-button size="small" @click=${this._retry}> Retry </sl-button>
     `;
   }
 
+  private _activeLanguagePopup() {
+    const languagePopup = this.shadowRoot?.getElementById('language-popup');
+    if (!languagePopup) return;
+
+    if (this._langPopupTimer) clearTimeout(this._langPopupTimer);
+
+    (languagePopup as SlPopup).active = true;
+  }
+
+  private _disactiveLanguagePopup(delay = 0) {
+    const languagePopup = this.shadowRoot?.getElementById('language-popup');
+    if (!languagePopup) return;
+
+    this._langPopupTimer = setTimeout(() => {
+      if (!languagePopup.matches(':hover')) {
+        (languagePopup as SlPopup).active = false;
+      }
+    }, delay);
+  }
+
+  private _activeTonePopup() {
+    const tonePopup = this.shadowRoot?.getElementById('tone-popup');
+    if (!tonePopup) return;
+
+    if (this._tonePopupTimer) clearTimeout(this._tonePopupTimer);
+    (tonePopup as SlPopup).active = true;
+  }
+
+  private _disactiveTonePopup(delay = 0) {
+    const tonePopup = this.shadowRoot?.getElementById('tone-popup');
+    if (!tonePopup) return;
+
+    this._tonePopupTimer = setTimeout(() => {
+      if (!tonePopup.matches(':hover')) {
+        (tonePopup as SlPopup).active = false;
+      }
+    }, delay);
+  }
+
   private _ActionButtonGroup() {
-    return html` <sl-button-group label="Alignment">
-      <sl-button size="small" @click=${this._refine}>Refine</sl-button>
-      <sl-button size="small" @click=${this._translate}>Translate</sl-button>
-      <sl-button size="small" @click=${this._summary}>Summary</sl-button>
-    </sl-button-group>`;
+    return html` <sl-dropdown
+      id="copilot-actions-dropdown"
+      placement="bottom"
+      hoist
+    >
+      <sl-button size="small" slot="trigger" caret> Ask AI </sl-button>
+      <sl-menu id="copilot-actions-menu">
+        <sl-menu-item @click=${() => this._handleActionClick('refine')}>
+          Refine
+        </sl-menu-item>
+        <sl-menu-item
+          @mouseenter=${this._activeLanguagePopup}
+          @mouseleave=${() => this._disactiveLanguagePopup(200)}
+        >
+          Translate
+        </sl-menu-item>
+        <sl-menu-item @click=${() => this._handleActionClick('summary')}>
+          Summarize
+        </sl-menu-item>
+        <sl-menu-item @click=${() => this._handleActionClick('makeLonger')}>
+          Make Longer
+        </sl-menu-item>
+        <sl-menu-item @click=${() => this._handleActionClick('makeShorter')}>
+          Make Shorter
+        </sl-menu-item>
+        <sl-menu-item
+          @mouseenter=${this._activeTonePopup}
+          @mouseleave=${() => this._disactiveTonePopup(200)}
+        >
+          Change Tone
+        </sl-menu-item>
+        <sl-menu-item @click=${() => this._handleActionClick('improveWriting')}>
+          Improve Writing
+        </sl-menu-item>
+        <sl-menu-item
+          @click=${() => this._handleActionClick('simplifyLanguage')}
+        >
+          Simplify Language
+        </sl-menu-item>
+        <sl-menu-item @click=${() => this._handleActionClick('fixSpelling')}>
+          Fix Spelling and Grammar
+        </sl-menu-item>
+      </sl-menu>
+      <sl-popup
+        id="language-popup"
+        anchor="copilot-actions-menu"
+        placement="right"
+        @mouseleave=${() => this._disactiveLanguagePopup(100)}
+      >
+        <sl-menu>
+          ${LANGUAGE.map(
+            language => html`
+              <sl-menu-item
+                @click=${() =>
+                  this._handleActionClick('translate', { language })}
+              >
+                ${language}
+              </sl-menu-item>
+            `
+          )}
+        </sl-menu>
+      </sl-popup>
+      <sl-popup
+        id="tone-popup"
+        anchor="copilot-actions-menu"
+        placement="right"
+        @mouseleave=${() => this._disactiveTonePopup(100)}
+      >
+        <sl-menu>
+          ${TONE.map(
+            tone => html`
+              <sl-menu-item
+                @click=${() => this._handleActionClick('changeTone', { tone })}
+              >
+                ${tone}
+              </sl-menu-item>
+            `
+          )}
+        </sl-menu>
+      </sl-popup>
+    </sl-dropdown>`;
   }
 
   override render() {
