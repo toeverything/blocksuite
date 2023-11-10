b50a714c7b7d0af1d2c1acb2fca2bffeac7dc620
regischen <regischen@toeverything.info>
2023-11-10 11:44:40 +0800

    fix: remove unused property

--- packages/blocks/src/page-block/edgeless/components/component-toolbar/component-toolbar.ts
+++ packages/blocks/src/page-block/edgeless/components/component-toolbar/component-toolbar.ts
@@ -147,7 +147,6 @@ export class EdgelessComponentToolbar extends WithDisposable(LitElement) {
           .elements=${connectorElements}
           .page=${this.page}
           .surface=${this.surface}
-          .slots=${this.slots}
           .edgeless=${this.edgeless}
         >
         </edgeless-change-connector-button>`
