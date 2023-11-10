d3de2a0798eb711c7deae2cba540d2fa2714dcf2
Flrande <flrande.ch@gmail.com>
2023-11-10 19:35:16 +0800

    fix: failed to format text with color in database

--- packages/blocks/src/_common/widgets/format-bar/components/background/background-button.ts
+++ packages/blocks/src/_common/widgets/format-bar/components/background/background-button.ts
@@ -30,10 +30,9 @@ const updateBackground = (
   root.std.command
     .pipe()
     .withRoot()
-    .try(chain => [chain.getTextSelection(), chain.getBlockSelections()])
     .try(chain => [
-      chain.formatText(payload),
-      chain.formatBlock(payload),
+      chain.getTextSelection().formatText(payload),
+      chain.getBlockSelections().formatBlock(payload),
       chain.formatNative(payload),
     ])
     .run();
