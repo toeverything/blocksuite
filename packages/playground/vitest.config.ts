3326afdce24754f3bff9643dae116426bd5c3929
Hongtao Lye <hongtao.lye@toeverything.info>
2023-11-30 15:57:46 +0800

    fix: remove unused code

--- packages/playground/vitest.config.ts
+++ packages/playground/vitest.config.ts
@@ -4,98 +4,53 @@ import { defineConfig, mergeConfig } from 'vitest/config';
 
 import viteConfig from './vite.config';
 
-export default defineConfig(
-  configEnv =>
-    mergeConfig(
-      viteConfig(configEnv),
-      defineConfig({
-        optimizeDeps: {
-          force: true,
-          include: ['@blocksuite/blocks > buffer'],
+export default defineConfig(configEnv =>
+  mergeConfig(
+    viteConfig(configEnv),
+    defineConfig({
+      optimizeDeps: {
+        force: true,
+        include: ['@blocksuite/blocks > buffer'],
+      },
+      test: {
+        include: ['tests/**/*.spec.ts'],
+        browser: {
+          enabled: true,
+          headless: true,
+          name: 'chromium',
+          provider: 'playwright',
+          providerOptions: {},
         },
-        test: {
-          include: ['tests/**/*.spec.ts'],
-          browser: {
-            enabled: true,
-            headless: true,
-            name: 'chromium',
-            provider: 'playwright',
-            providerOptions: {},
-          },
-          deps: {
-            interopDefault: true,
-          },
-          testTransformMode: {
-            web: ['tests/**/*.spec.ts'],
-          },
-          alias: {
-            '@blocksuite/blocks': path.resolve(
-              fileURLToPath(new URL('../blocks/src', import.meta.url))
-            ),
-            '@blocksuite/blocks/*': path.resolve(
-              fileURLToPath(new URL('../blocks/src/*', import.meta.url))
-            ),
-            '@blocksuite/editor': path.resolve(
-              fileURLToPath(new URL('../editor/src', import.meta.url))
-            ),
-            '@blocksuite/global/*': path.resolve(
-              fileURLToPath(new URL('../global/src/*', import.meta.url))
-            ),
-            '@blocksuite/store': path.resolve(
-              fileURLToPath(new URL('../store/src', import.meta.url))
-            ),
-            '@blocksuite/virgo': path.resolve(
-              fileURLToPath(new URL('../virgo/src', import.meta.url))
-            ),
-            '@blocksuite/virgo/*': path.resolve(
-              fileURLToPath(new URL('../virgo/src/*', import.meta.url))
-            ),
-          },
+        deps: {
+          interopDefault: true,
         },
-      })
-    )
-
-  // defineConfig({
-  //   optimizeDeps: {
-  //     force: true,
-  //     include: ['/node_modules/buffer/'],
-  //   },
-  //   build: {
-  //     commonjsOptions: {
-  //       include: ['/node_modules/buffer/'],
-  //     },
-  //   },
-  //   test: {
-  //     include: ['tests/**/*.spec.ts'],
-  //     browser: {
-  //       enabled: true,
-  //       headless: false,
-  //       name: 'chromium',
-  //       provider: 'playwright',
-  //     },
-  //     testTransformMode: {
-  //       web: ['tests/**/*.spec.ts'],
-  //     },
-  //     alias: {
-  //       '@blocksuite/blocks': path.resolve(
-  //         fileURLToPath(new URL('../blocks/src', import.meta.url))
-  //       ),
-  //       '@blocksuite/editor': path.resolve(
-  //         fileURLToPath(new URL('../editor/src', import.meta.url))
-  //       ),
-  //       '@blocksuite/global/*': path.resolve(
-  //         fileURLToPath(new URL('../global/src/*', import.meta.url))
-  //       ),
-  //       '@blocksuite/store': path.resolve(
-  //         fileURLToPath(new URL('../store/src', import.meta.url))
-  //       ),
-  //       '@blocksuite/virgo': path.resolve(
-  //         fileURLToPath(new URL('../virgo/src', import.meta.url))
-  //       ),
-  //       '@blocksuite/virgo/*': path.resolve(
-  //         fileURLToPath(new URL('../virgo/src/*', import.meta.url))
-  //       ),
-  //     },
-  //   },
-  // })
+        testTransformMode: {
+          web: ['tests/**/*.spec.ts'],
+        },
+        alias: {
+          '@blocksuite/blocks': path.resolve(
+            fileURLToPath(new URL('../blocks/src', import.meta.url))
+          ),
+          '@blocksuite/blocks/*': path.resolve(
+            fileURLToPath(new URL('../blocks/src/*', import.meta.url))
+          ),
+          '@blocksuite/editor': path.resolve(
+            fileURLToPath(new URL('../editor/src', import.meta.url))
+          ),
+          '@blocksuite/global/*': path.resolve(
+            fileURLToPath(new URL('../global/src/*', import.meta.url))
+          ),
+          '@blocksuite/store': path.resolve(
+            fileURLToPath(new URL('../store/src', import.meta.url))
+          ),
+          '@blocksuite/virgo': path.resolve(
+            fileURLToPath(new URL('../virgo/src', import.meta.url))
+          ),
+          '@blocksuite/virgo/*': path.resolve(
+            fileURLToPath(new URL('../virgo/src/*', import.meta.url))
+          ),
+        },
+      },
+    })
+  )
 );
