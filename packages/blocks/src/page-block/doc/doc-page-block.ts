9f70e9e2d86739fd1311e1c38618cdf6df953d40
Mirone <Saul-Mirone@outlook.com>
2023-11-30 11:30:38 +0800

    refactor: remove legacy clipboard

--- packages/blocks/src/page-block/doc/doc-page-block.ts
+++ packages/blocks/src/page-block/doc/doc-page-block.ts
@@ -14,7 +14,6 @@ import {
   asyncFocusRichText,
   matchFlavours,
 } from '../../_common/utils/index.js';
-import { PageClipboard } from '../../_legacy/clipboard/index.js';
 import type { NoteBlockModel } from '../../note-block/index.js';
 import { ClipboardController } from '../clipboard/index.js';
 import type { DocPageBlockWidgetName } from '../index.js';
@@ -142,8 +141,6 @@ export class DocPageBlockComponent extends BlockElement<
   keyboardManager: PageKeyboardManager | null = null;
 
   gesture: Gesture | null = null;
-
-  clipboard = new PageClipboard(this);
 
   clipboardController = new ClipboardController(this);
 
@@ -385,7 +382,6 @@ export class DocPageBlockComponent extends BlockElement<
 
     this.gesture = new Gesture(this);
     this.keyboardManager = new PageKeyboardManager(this);
-    this.clipboard.init(this.page);
     // filter cut event in page title
     this.handleEvent('cut', ctx => {
       const { event } = ctx.get('defaultState');
@@ -589,7 +585,6 @@ export class DocPageBlockComponent extends BlockElement<
 
   override disconnectedCallback() {
     super.disconnectedCallback();
-    this.clipboard.dispose();
     this._disposables.dispose();
     this.gesture = null;
     this.keyboardManager = null;
