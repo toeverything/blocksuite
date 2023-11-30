50629bfd8299b2928ec0c857fcfc7329f18bfd0e
lawvs <18554747+lawvs@users.noreply.github.com>
2023-11-30 17:36:15 +0800

    feat: add new `withDebugDelayStorage`

--- packages/playground/apps/starter/storage.ts
+++ packages/playground/apps/starter/storage.ts
@@ -1,4 +1,5 @@
-import type { PassiveDocProvider, Y } from '@blocksuite/store';
+import { sleep } from '@blocksuite/global/utils';
+import type { BlobStorage, PassiveDocProvider, Y } from '@blocksuite/store';
 import type { IndexedDBProvider } from '@toeverything/y-indexeddb';
 import { createIndexedDBProvider } from '@toeverything/y-indexeddb';
 
@@ -22,3 +23,44 @@ export class IndexedDBProviderWrapper implements PassiveDocProvider {
     return this._connected;
   }
 }
+
+const simulateDelay = async (blob?: Blob | null) => {
+  const bandwidth = 1024 * 1024;
+  const minimal = 500;
+  const maximum = 2500;
+  const size = blob?.size ?? 0;
+  const time = size / bandwidth;
+  // clamp(time, minium, maximum)
+  await sleep(Math.min(maximum, Math.max(minimal, time)));
+};
+
+/**
+ * Wrap the storage with a delay in order to simulate the actual network environment.
+ */
+export const withDebugDelayStorage =
+  (blobStorageCreator: (id: string) => BlobStorage) =>
+  (id: string): BlobStorage => {
+    const storage = blobStorageCreator(id);
+
+    return {
+      crud: {
+        get: async key => {
+          const data = await storage.crud.get(key);
+          await simulateDelay(data);
+          return data;
+        },
+        set: async (key, value) => {
+          await simulateDelay(value);
+          return storage.crud.set(key, value);
+        },
+        delete: async key => {
+          await simulateDelay();
+          return storage.crud.delete(key);
+        },
+        list: async () => {
+          await simulateDelay();
+          return storage.crud.list();
+        },
+      },
+    };
+  };
