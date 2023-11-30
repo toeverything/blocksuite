1873e106c1877ef0bc26ed48dc871315606d144c
Yifeng Wang <doodlewind@toeverything.info>
2023-11-30 09:36:04 +0800

    chore: remove unused feature flags (#5527)

--- packages/global/index.d.ts
+++ packages/global/index.d.ts
@@ -1,14 +1,6 @@
 declare type BlockSuiteFlags = {
-  enable_set_remote_flag: boolean;
-  enable_block_hub: boolean;
-
   enable_transformer_clipboard: boolean;
-
   enable_expand_database_block: boolean;
-
-  enable_toggle_block: boolean;
-  enable_note_index: boolean;
-
   enable_bultin_ledits: boolean;
   readonly: Record<string, boolean>;
 };
