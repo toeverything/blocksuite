a1559b10c8c5d0ead77f56a40de72acf6fbdf5c6
Chen <99816898+donteatfriedrice@users.noreply.github.com>
2023-12-01 15:47:49 +0800

    feat(edgeless): new TOC (#5539)
    Co-authored-by: Yifeng Wang <doodlewind@toeverything.info>

--- packages/playground/apps/starter/components/custom-navigation-panel.ts
+++ packages/playground/apps/starter/components/custom-navigation-panel.ts
@@ -15,10 +15,8 @@ export class CustomNavigationPanel extends WithDisposable(LitElement) {
       border: 1px solid var(--affine-border-color, #e3e2e4);
       background: var(--affine-background-overlay-panel-color);
       height: 100vh;
-      width: 289px;
+      width: 345px;
       box-sizing: border-box;
-      overflow-y: scroll;
-      overflow-x: visible;
       z-index: 1;
     }
   `;
@@ -34,7 +32,7 @@ export class CustomNavigationPanel extends WithDisposable(LitElement) {
   private _renderPanel() {
     return html`<edgeless-toc-notes-panel
       .page=${this.page}
-      .fitPadding=${[50, 300, 50, 50]}
+      .fitPadding=${[50, 360, 50, 50]}
     ></edgeless-toc-notes-panel>`;
   }
 
