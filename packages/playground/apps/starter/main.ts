547ca7d33192d19d0b37b3d25ab20579cd322fd6
Yifeng Wang <doodlewind@toeverything.info>
2023-12-12 14:05:19 +0800

    fix: build

--- packages/playground/apps/starter/main.ts
+++ packages/playground/apps/starter/main.ts
@@ -8,7 +8,7 @@ import '@blocksuite/presets/themes/affine.css';
 import { TestUtils } from '@blocksuite/blocks';
 import { ContentParser } from '@blocksuite/blocks/content-parser';
 import { AffineSchemas } from '@blocksuite/blocks/models';
-import type { BlockSuiteRoot } from '@blocksuite/lit';
+import type { EditorHost } from '@blocksuite/lit';
 import { AiPanel } from '@blocksuite/presets';
 import type { DocProvider, Page } from '@blocksuite/store';
 import { Job, Workspace } from '@blocksuite/store';
@@ -115,7 +115,7 @@ async function main() {
   window.ContentParser = ContentParser;
   Object.defineProperty(globalThis, 'root', {
     get() {
-      return document.querySelector('editor-host') as BlockSuiteRoot;
+      return document.querySelector('editor-host') as EditorHost;
     },
   });
 
