1925958971a87e2515194f9fa88560284ffdb071
Hongtao Lye <hongtao.lye@toeverything.info>
2023-12-14 11:40:25 +0800

    fix: surface title

--- packages/blocks/src/surface-ref-block/surface-ref-block.ts
+++ packages/blocks/src/surface-ref-block/surface-ref-block.ts
@@ -193,10 +193,10 @@ export class SurfaceRefBlockComponent extends BlockElement<SurfaceRefBlockModel>
 
       width: 100%;
       padding: 8px 16px;
-      border: 1px solid #f1f1f1;
+      border: 1px solid var(--affine-border-color);
       gap: 14px;
 
-      background: #fff;
+      background: var(--affine-background-primary-color);
 
       font-size: 12px;
     }
