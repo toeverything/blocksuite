037c0a2b5ac38ea39a89fc0f6813328b58427756
AyushAgrawal-A2 <ayushagl06@gmail.com>
2023-12-19 22:57:35 +0530

    fix: disable page-meta

--- packages/presets/src/editors/doc-editor.ts
+++ packages/presets/src/editors/doc-editor.ts
@@ -1,4 +1,4 @@
-import '../fragments/page-meta/page-meta';
+// import '../fragments/page-meta/page-meta';
 import '../fragments/doc-title/doc-title';
 
 import { DocEditorBlockSpecs } from '@blocksuite/blocks';
@@ -42,7 +42,6 @@ export class DocEditor extends WithDisposable(ShadowlessElement) {
         }
       </style>
       <doc-title .page=${this.page} .editorHostRef=${this.host}></doc-title>
-      <page-meta .page=${this.page} .editorHostRef=${this.host}></page-meta>
       <editor-host
         ${ref(this.host)}
         .page=${this.page}
@@ -57,3 +56,5 @@ declare global {
     'doc-editor': DocEditor;
   }
 }
+
+//<page-meta .page=${this.page} .editorHostRef=${this.host}></page-meta>
