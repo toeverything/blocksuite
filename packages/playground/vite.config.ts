b74289b6bb8bf235d7129fc02aa2f4851cd8a060
Hongtao Lye <hongtao.lye@toeverything.info>
2023-12-20 14:41:01 +0800

    fix: vite config

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -33,7 +33,7 @@ export default ({ mode }) => {
             'node_modules',
             'tests',
             fileURLToPath(new URL('.', import.meta.url)),
-            'templates',
+            'templates/',
             'builtin-templates',
           ],
           forceBuildInstrument: true,
