085832529cd753f5fa4562e74a273e9912a142e8
Ayush Agrawal <54364088+AyushAgrawal-A2@users.noreply.github.com>
2023-12-21 21:05:13 +0530

    fix: setGroup

--- packages/blocks/src/_common/embed-block-helper/embed-block-element.ts
+++ packages/blocks/src/_common/embed-block-helper/embed-block-element.ts
@@ -81,7 +81,7 @@ export class EmbedBlockElement<
       const blockComponent = anchorComponent as this;
       const isInSurface = blockComponent.isInSurface;
       if (!isInSurface) {
-        this.host.selection.set([
+        this.host.selection.setGroup('note', [
           this.host.selection.getInstance('block', {
             path: blockComponent.path,
           }),
