ed54966edb49808d63e6c6334255120ff941dfd6
Mirone <Saul-Mirone@outlook.com>
2023-12-22 16:22:38 +0800

    refactor: remove selected blocks type

--- packages/blocks/src/_legacy/content-parser/types.ts
+++
@@ -1,8 +0,0 @@
-import type { BaseBlockModel } from '@blocksuite/store';
-
-export interface SelectedBlock {
-  model: BaseBlockModel;
-  startPos?: number;
-  endPos?: number;
-  children: SelectedBlock[];
-}
