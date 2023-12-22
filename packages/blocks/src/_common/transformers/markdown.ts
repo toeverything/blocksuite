0d1615aa715e7fef3ec4421a2c5854d663a54344
Mirone <Saul-Mirone@outlook.com>
2023-12-22 11:21:00 +0800

    chore: remove content parser methods

--- packages/blocks/src/_common/transformers/markdown.ts
+++ packages/blocks/src/_common/transformers/markdown.ts
@@ -32,3 +32,44 @@ export async function exportPage(page: Page) {
   }
   download(downloadBlob, name);
 }
+
+type ImportMarkdownOptions = {
+  page: Page;
+  markdown: string;
+  noteId: string;
+  index?: number;
+};
+
+export async function importMarkdown({
+  page,
+  markdown,
+  noteId,
+  index,
+}: ImportMarkdownOptions) {
+  const job = new Job({ workspace: page.workspace });
+  const adapter = new MarkdownAdapter();
+  const snapshot = await adapter.toSliceSnapshot({
+    file: markdown,
+    assets: job.assetsManager,
+    blockVersions: page.workspace.meta.blockVersions!,
+    pageVersion: page.workspace.meta.pageVersion!,
+    workspaceVersion: page.workspace.meta.workspaceVersion!,
+    workspaceId: page.workspace.id,
+    pageId: page.id,
+  });
+
+  await Promise.all(
+    snapshot.content
+      .flatMap(x => x.children)
+      .map(async (block, i) => {
+        await job.snapshotToBlock(
+          block,
+          page,
+          noteId,
+          index != null ? index + i : undefined
+        );
+      })
+  );
+
+  return;
+}
