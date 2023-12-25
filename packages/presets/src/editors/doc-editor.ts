47ed4d042c6df8cb681e2527ad9d96f20c96b4b5
Ayush Agrawal <54364088+AyushAgrawal-A2@users.noreply.github.com>
2023-12-25 22:04:38 +0530

    fix: bug

--- packages/presets/src/editors/doc-editor.ts
+++ packages/presets/src/editors/doc-editor.ts
@@ -25,8 +25,6 @@ export class DocEditor extends WithDisposable(ShadowlessElement) {
     return html`
       <style>
         doc-editor {
-          display: block;
-          height: 100%;
           font-family: var(--affine-font-family);
           background: var(--affine-background-primary-color);
         }
@@ -50,6 +48,8 @@ export class DocEditor extends WithDisposable(ShadowlessElement) {
         }
 
         .doc-editor-container {
+          display: block;
+          height: 100%;
           user-select: none;
         }
       </style>
