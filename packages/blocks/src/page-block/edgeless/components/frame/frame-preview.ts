4091abfa0104003715ef03e45ef742d39f67b75a
Mirone <Saul-Mirone@outlook.com>
2024-01-02 14:17:31 +0800

    refactor: use surface ref service to render frame

--- packages/blocks/src/page-block/edgeless/components/frame/frame-preview.ts
+++ packages/blocks/src/page-block/edgeless/components/frame/frame-preview.ts
@@ -4,39 +4,24 @@ import {
   ShadowlessElement,
   WithDisposable,
 } from '@blocksuite/lit';
-import type { Page, Y } from '@blocksuite/store';
+import { nanoid, type Page } from '@blocksuite/store';
 import { css, html, nothing, type PropertyValues } from 'lit';
 import { customElement, property, query, state } from 'lit/decorators.js';
 import { styleMap } from 'lit/directives/style-map.js';
 
-import {
-  type CssVariableName,
-  isCssVariable,
-} from '../../../../_common/theme/css-variables.js';
-import { getThemePropertyValue } from '../../../../_common/theme/utils.js';
 import type {
   EdgelessElement,
   TopLevelBlockModel,
 } from '../../../../_common/types.js';
 import type { FrameBlockModel } from '../../../../frame-block/frame-model.js';
-import type { SurfaceService } from '../../../../index.js';
+import type { SurfaceRefBlockService } from '../../../../index.js';
+import { type SurfaceService } from '../../../../index.js';
 import type { NoteBlockModel } from '../../../../note-block/note-model.js';
-import { ConnectorElement } from '../../../../surface-block/elements/connector/connector-element.js';
-import type { CanvasElementType } from '../../../../surface-block/elements/edgeless-element.js';
-import { ElementCtors } from '../../../../surface-block/elements/index.js';
-import type { SurfaceElement } from '../../../../surface-block/elements/surface-element.js';
-import {
-  getGroupParent,
-  setGroupParent,
-} from '../../../../surface-block/managers/group-manager.js';
-import { LayerManager } from '../../../../surface-block/managers/layer-manager.js';
-import { Renderer } from '../../../../surface-block/renderer.js';
 import type { SurfaceBlockModel } from '../../../../surface-block/surface-model.js';
 import { Bound } from '../../../../surface-block/utils/bound.js';
 import { deserializeXYWH } from '../../../../surface-block/utils/xywh.js';
 import type { SurfaceRefPortal } from '../../../../surface-ref-block/surface-ref-portal.js';
-import { getSurfaceBlock } from '../../../../surface-ref-block/utils.js';
-import { ConnectorPathGenerator } from '../../connector-manager.js';
+import type { SurfaceRefRenderer } from '../../../../surface-ref-block/surface-ref-renderer.js';
 import type { EdgelessPageBlockComponent } from '../../edgeless-page-block.js';
 
 type RefElement = Exclude<EdgelessElement, NoteBlockModel>;
@@ -107,14 +92,9 @@ export class FramePreview extends WithDisposable(ShadowlessElement) {
   @state()
   private _surfaceModel: SurfaceBlockModel | null = null;
 
-  private _surfaceRenderer = new Renderer({
-    layerManager: new LayerManager(),
-  });
-  private _connectorManager = new ConnectorPathGenerator({
-    pickById: id => this.getModel(id),
-    refresh: () => this._surfaceRenderer.refresh(),
-  });
-  private _elements = new Map<string, SurfaceElement>();
+  private _surfaceRefRendererId: string = nanoid('unknown');
+  private _surfaceRefRenderer!: SurfaceRefRenderer;
+
   private _edgelessDisposables: DisposableGroup | null = null;
   private _pageDisposables: DisposableGroup | null = null;
   private _frameDisposables: DisposableGroup | null = null;
@@ -126,73 +106,50 @@ export class FramePreview extends WithDisposable(ShadowlessElement) {
   blocksPortal!: SurfaceRefPortal;
 
   get surfaceRenderer() {
-    return this._surfaceRenderer;
+    return this._surfaceRefRenderer.surfaceRenderer;
   }
 
   private _attachRenderer() {
-    if (this._surfaceRenderer.canvas.isConnected || !this.container) return;
-
-    this._surfaceRenderer.attach(this.container);
-  }
-
-  private _initSurfaceRenderer(
-    page: Page,
-    surfaceModel: SurfaceBlockModel | null
-  ) {
-    this.surfaceRenderer.layerManager.init([
-      ...this._elements.values(),
-      ...((surfaceModel?.children || []) as EdgelessElement[]),
-      ...(page.getBlockByFlavour('affine:note') as EdgelessElement[]),
-    ]);
-  }
-
-  private _initSurfaceModel(page: Page, disposables: DisposableGroup) {
-    this._surfaceModel = null;
-    this._elements.clear();
-
-    const init = () => {
-      this._surfaceModel = getSurfaceBlock(page);
-
-      if (!this._surfaceModel) return;
-
-      const elementsMap = this._surfaceModel.elements.getValue() as Y.Map<
-        Y.Map<unknown>
-      >;
-      const onElementsChange = (event: Y.YMapEvent<Y.Map<unknown>>) => {
-        this._onElementsChange(event, elementsMap);
-      };
-      elementsMap.observe(onElementsChange);
-      disposables.add(() => elementsMap.unobserve(onElementsChange));
-
-      this._syncFromExistingContainer(elementsMap);
-    };
-
-    init();
-
-    if (!this._surfaceModel) {
-      disposables.add(
-        page.slots.blockUpdated.on(({ type }) => {
-          if (
-            type === 'add' &&
-            !this._surfaceModel &&
-            getSurfaceBlock(this.page)
-          ) {
-            init();
-          }
-        })
-      );
-    }
-  }
-
-  private _onElementUpdatedOrAdded(id: string) {
-    const element = this.getModel(id);
-
     if (
-      element instanceof ConnectorElement &&
-      this._connectorManager.hasRelatedElement(element)
-    ) {
-      this._connectorManager.updatePath(element);
-    }
+      this._surfaceRefRenderer?.surfaceRenderer.canvas.isConnected ||
+      !this.container
+    )
+      return;
+
+    this.surfaceRenderer.attach(this.container);
+  }
+
+  private get _surfaceRefService() {
+    const service = this.host.spec.getService('affine:surface-ref') as
+      | SurfaceRefBlockService
+      | undefined;
+
+    assertExists(service, 'SurfaceRefBlockService not found');
+    return service;
+  }
+
+  private _setupSurfaceRefRenderer() {
+    const surfaceRefService = this._surfaceRefService;
+    const renderer = surfaceRefService.getRenderer(this._surfaceRefRendererId);
+    this._surfaceRefRenderer = renderer;
+
+    this._disposables.add(
+      renderer.slots.surfaceModelChanged.on(model => {
+        this._surfaceModel = model;
+      })
+    );
+    this._disposables.add(
+      renderer.slots.surfaceRendererRefresh.on(() => {
+        this.requestUpdate();
+      })
+    );
+
+    renderer.mount();
+  }
+
+  private _cleanupSurfaceRefRenderer() {
+    const surfaceRefService = this._surfaceRefService;
+    surfaceRefService.removeRenderer(this._surfaceRefRendererId);
   }
 
   private _refreshViewport() {
@@ -207,155 +164,15 @@ export class FramePreview extends WithDisposable(ShadowlessElement) {
     this.requestUpdate();
     this.updateComplete
       .then(() => {
-        this._surfaceRenderer.onResize();
-        this._surfaceRenderer.setViewportByBound(
+        this.surfaceRenderer.onResize();
+        this.surfaceRenderer.setViewportByBound(
           Bound.fromXYWH(deserializeXYWH(referencedModel.xywh))
         );
 
-        this.blocksPortal?.setViewport(this._surfaceRenderer);
+        this.blocksPortal?.setViewport(this.surfaceRenderer);
       })
       .catch(console.error);
   }
-
-  private getModel(id: string): RefElement | null {
-    return (
-      (this.page.getBlockById(id) as Exclude<
-        TopLevelBlockModel,
-        NoteBlockModel
-      >) ??
-      this._elements.get(id) ??
-      null
-    );
-  }
-
-  private _syncFromExistingContainer(elementsMap: Y.Map<Y.Map<unknown>>) {
-    elementsMap.doc?.transact(() => {
-      const yConnectors: Y.Map<unknown>[] = [];
-      const yGroups: Y.Map<unknown>[] = [];
-      elementsMap.forEach(yElement => {
-        const type = yElement.get('type') as CanvasElementType;
-        if (type === 'connector') {
-          yConnectors.push(yElement);
-          return;
-        }
-        if (type === 'group') {
-          yGroups.push(yElement);
-          return;
-        }
-        this._createElementFromYMap(yElement);
-      });
-      yConnectors.forEach(yElement => {
-        this._createElementFromYMap(yElement);
-      });
-      yGroups.forEach(yElement => {
-        this._createElementFromYMap(yElement);
-      });
-    });
-  }
-
-  private _createElementFromYMap(yElement: Y.Map<unknown>) {
-    const type = yElement.get('type') as CanvasElementType;
-    const ElementCtor = ElementCtors[type];
-    assertExists(ElementCtor);
-    const element = new ElementCtor(yElement, {
-      onElementUpdated: ({ id }) => {
-        const element = this.getModel(id);
-
-        if (
-          element instanceof ConnectorElement &&
-          !this._connectorManager.hasRelatedElement(element)
-        ) {
-          this._connectorManager.updatePath(element);
-        }
-      },
-      removeElement: () => {},
-      pickById: id => this.getModel(id),
-      getGroupParent: getGroupParent,
-      setGroupParent: setGroupParent,
-    });
-    element.computedValue = this._getCSSPropertyValue;
-    element.mount(this._surfaceRenderer);
-    this._elements.set(element.id, element);
-    this._onElementUpdatedOrAdded(element.id);
-  }
-
-  private _onElementsChange = (
-    event: Y.YMapEvent<Y.Map<unknown>>,
-    elementsMap: Y.Map<Y.Map<unknown>>
-  ) => {
-    // skip empty event
-    if (event.changes.keys.size === 0) return;
-    const connectors: {
-      change: (typeof event)['changes']['keys'] extends Map<string, infer V>
-        ? V
-        : never;
-      id: string;
-    }[] = [];
-    event.keysChanged.forEach(id => {
-      const change = event.changes.keys.get(id);
-      if (!change) {
-        console.error('invalid event', event);
-        return;
-      }
-
-      if (
-        change.action === 'add' &&
-        elementsMap.get(id)?.get('type') === 'connector'
-      ) {
-        connectors.push({ change, id });
-        return;
-      } else {
-        this._onYEvent(change, id, elementsMap);
-      }
-    });
-    connectors.forEach(({ change, id }) =>
-      this._onYEvent(change, id, elementsMap)
-    );
-  };
-
-  private _onYEvent = (
-    type: Y.YMapEvent<Y.Map<unknown>>['changes']['keys'] extends Map<
-      string,
-      infer V
-    >
-      ? V
-      : never,
-    id: string,
-    elementsMap: Y.Map<Y.Map<unknown>>
-  ) => {
-    if (type.action === 'add') {
-      const yElement = elementsMap.get(id) as Y.Map<unknown>;
-      const type = yElement.get('type') as CanvasElementType;
-
-      const ElementCtor = ElementCtors[type];
-      assertExists(ElementCtor);
-      const element = new ElementCtor(yElement, {
-        onElementUpdated() {},
-        pickById: id => this.getModel(id),
-        getGroupParent: getGroupParent,
-        setGroupParent: setGroupParent,
-        removeElement: () => {},
-      });
-      element.computedValue = this._getCSSPropertyValue;
-      element.mount(this._surfaceRenderer);
-      this.surfaceRenderer.layerManager.add(element);
-      this._elements.set(element.id, element);
-      this._onElementUpdatedOrAdded(element.id);
-    } else if (type.action === 'update') {
-      console.error('update event on yElements is not supported', event);
-    } else if (type.action === 'delete') {
-      const element = this._elements.get(id);
-
-      if (element) {
-        this.surfaceRenderer.layerManager.delete(element);
-        element.unmount();
-        this._elements.delete(id);
-      }
-    }
-
-    this._surfaceRenderer.refresh();
-    this.requestUpdate();
-  };
 
   private _tryLoadFillScreen() {
     const surfaceService = this.host.spec.getService(
@@ -365,23 +182,6 @@ export class FramePreview extends WithDisposable(ShadowlessElement) {
     this.fillScreen =
       surfaceService.editSession.getItem('presentFillScreen') ?? false;
   }
-
-  private _getCSSPropertyValue = (value: string) => {
-    const host = this.host;
-    if (isCssVariable(value)) {
-      const cssValue = getThemePropertyValue(host, value as CssVariableName);
-      if (cssValue === undefined) {
-        console.error(
-          new Error(
-            `All variables should have a value. Please check for any dirty data or variable renaming.Variable: ${value}`
-          )
-        );
-      }
-      return cssValue ?? value;
-    }
-
-    return value;
-  };
 
   private _getViewportWH = (referencedModel: RefElement) => {
     const [, , w, h] = deserializeXYWH(referencedModel.xywh);
@@ -431,12 +231,6 @@ export class FramePreview extends WithDisposable(ShadowlessElement) {
   private _setPageDisposables(page: Page) {
     this._clearPageDisposables();
     this._pageDisposables = new DisposableGroup();
-    this._initSurfaceModel(page, this._pageDisposables);
-    this._initSurfaceRenderer(page, this._surfaceModel);
-    this._connectorManager = new ConnectorPathGenerator({
-      pickById: id => this.getModel(id),
-      refresh: () => this._surfaceRenderer.refresh(),
-    });
 
     this._pageDisposables.add(
       page.slots.blockUpdated.on(event => {
@@ -519,6 +313,7 @@ export class FramePreview extends WithDisposable(ShadowlessElement) {
   override connectedCallback() {
     super.connectedCallback();
     this._tryLoadFillScreen();
+    this._setupSurfaceRefRenderer();
     this._setPageDisposables(this.page);
     this._setEdgelessDisposables(this.edgeless);
     this._setFrameDisposables(this.frame);
@@ -546,6 +341,7 @@ export class FramePreview extends WithDisposable(ShadowlessElement) {
 
   override disconnectedCallback() {
     super.disconnectedCallback();
+    this._cleanupSurfaceRefRenderer();
     this._clearEdgelessDisposables();
     this._clearPageDisposables();
     this._clearFrameDisposables();
