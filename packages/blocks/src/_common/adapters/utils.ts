638d59a6fb8a9ceff5862a1a2c917f49bde3723d
fourdim <59462000+fourdim@users.noreply.github.com>
2024-01-11 21:35:58 -0500

    fix(page): merge delta when exporting code (#5964)

--- packages/blocks/src/_common/adapters/utils.ts
+++ packages/blocks/src/_common/adapters/utils.ts
@@ -1,3 +1,6 @@
+import { isEqual } from '@blocksuite/global/utils';
+import type { DeltaInsert } from '@blocksuite/inline';
+
 export const fetchImage = async (
   url: string,
   init?: RequestInit,
@@ -10,4 +13,29 @@ export const fetchImage = async (
     console.warn('Failed to fetch image from proxy, fallback to originial url');
     return fetch(url, init);
   });
+};
+
+export const mergeDeltas = (
+  acc: DeltaInsert[],
+  cur: DeltaInsert,
+  options: { force?: boolean } = { force: false }
+) => {
+  if (acc.length === 0) {
+    return [cur];
+  }
+  const last = acc[acc.length - 1];
+  if (options?.force) {
+    last.insert = last.insert + cur.insert;
+    last.attributes = Object.create(null);
+    return acc;
+  } else if (
+    typeof last.insert === 'string' &&
+    typeof cur.insert === 'string' &&
+    (isEqual(last.attributes, cur.attributes) ||
+      (last.attributes === undefined && cur.attributes === undefined))
+  ) {
+    last.insert += cur.insert;
+    return acc;
+  }
+  return [...acc, cur];
 };
