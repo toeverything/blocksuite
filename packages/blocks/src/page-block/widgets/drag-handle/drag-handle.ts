cf371969749ef316e1d598a14cec03e0708ca391
Ayush Agrawal <54364088+AyushAgrawal-A2@users.noreply.github.com>
2024-01-18 00:59:09 +0530

    fix: drag handle with note scale

--- packages/blocks/src/page-block/widgets/drag-handle/drag-handle.ts
+++ packages/blocks/src/page-block/widgets/drag-handle/drag-handle.ts
@@ -29,6 +29,7 @@ import {
   Rect,
   type TopLevelBlockModel,
 } from '../../../_common/utils/index.js';
+import type { NoteBlockComponent, NoteBlockModel } from '../../../index.js';
 import { DocPageBlockComponent } from '../../../page-block/doc/doc-page-block.js';
 import { EdgelessPageBlockComponent } from '../../../page-block/edgeless/edgeless-page-block.js';
 import {
@@ -99,6 +100,7 @@ export class AffineDragHandleWidget extends WidgetElement<
   dropIndicator: DropIndicator | null = null;
   lastDragPointerState: PointerEventState | null = null;
   scale = 1;
+  noteScale = 1;
   center: IVec = [0, 0];
   rafID = 0;
 
@@ -367,7 +369,9 @@ export class AffineDragHandleWidget extends WidgetElement<
 
       dragPreview = new DragPreview(offset);
       dragPreview.style.width = `${width / this.scale}px`;
-      dragPreview.style.transform = `translate(${posX}px, ${posY}px) scale(${this.scale})`;
+      dragPreview.style.transform = `translate(${posX}px, ${posY}px) scale(${
+        this.scale * this.noteScale
+      })`;
       dragPreview.appendChild(fragment);
     }
     this.pageBlockElement.appendChild(dragPreview);
@@ -398,7 +402,9 @@ export class AffineDragHandleWidget extends WidgetElement<
       posY = scrollHeight - height;
     }
 
-    this.dragPreview.style.transform = `translate(${posX}px, ${posY}px) scale(${this.scale})`;
+    this.dragPreview.style.transform = `translate(${posX}px, ${posY}px) scale(${
+      this.scale * this.noteScale
+    })`;
   };
 
   private _updateDragPreviewOnViewportUpdate = () => {
@@ -562,25 +568,29 @@ export class AffineDragHandleWidget extends WidgetElement<
 
     const posLeft =
       left -
-      (DRAG_HANDLE_CONTAINER_WIDTH + offsetLeft) * this.scale +
+      (DRAG_HANDLE_CONTAINER_WIDTH + offsetLeft) * this.scale * this.noteScale +
       this._viewportOffset.left;
     const posTop = this._getTopWithBlockElement(blockElement);
 
     const rowPaddingY =
-      ((containerHeight - DRAG_HANDLE_GRABBER_HEIGHT) / 2) * this.scale;
+      ((containerHeight - DRAG_HANDLE_GRABBER_HEIGHT) / 2) *
+      this.scale *
+      this.noteScale;
 
     // use padding to control grabber's height
     const paddingTop = rowPaddingY + posTop - draggingAreaRect.top;
     const paddingBottom =
       draggingAreaRect.height -
       paddingTop -
-      DRAG_HANDLE_GRABBER_HEIGHT * this.scale;
+      DRAG_HANDLE_GRABBER_HEIGHT * this.scale * this.noteScale;
 
     const applyStyle = (transition?: boolean) => {
       container.style.transition = transition ? 'padding 0.25s ease' : 'none';
       container.style.paddingTop = `${paddingTop}px`;
       container.style.paddingBottom = `${paddingBottom}px`;
-      container.style.width = `${DRAG_HANDLE_CONTAINER_WIDTH * this.scale}px`;
+      container.style.width = `${
+        DRAG_HANDLE_CONTAINER_WIDTH * this.scale * this.noteScale
+      }px`;
       container.style.left = `${posLeft}px`;
       container.style.top = `${draggingAreaRect.top}px`;
       container.style.display = 'flex';
@@ -600,9 +610,11 @@ export class AffineDragHandleWidget extends WidgetElement<
       applyStyle(false);
     }
 
-    grabber.style.width = `${DRAG_HANDLE_GRABBER_WIDTH * this.scale}px`;
+    grabber.style.width = `${
+      DRAG_HANDLE_GRABBER_WIDTH * this.scale * this.noteScale
+    }px`;
     grabber.style.borderRadius = `${
-      DRAG_HANDLE_GRABBER_BORDER_RADIUS * this.scale
+      DRAG_HANDLE_GRABBER_BORDER_RADIUS * this.scale * this.noteScale
     }px`;
 
     this._handleAnchorModelDisposables(blockElement.model);
@@ -724,7 +736,8 @@ export class AffineDragHandleWidget extends WidgetElement<
     const offsetLeft = getDragHandleLeftPadding(blockElements);
 
     // Add padding to hover rect
-    left -= (DRAG_HANDLE_CONTAINER_WIDTH + offsetLeft) * this.scale;
+    left -=
+      (DRAG_HANDLE_CONTAINER_WIDTH + offsetLeft) * this.scale * this.noteScale;
     top -= DRAG_HOVER_RECT_PADDING * this.scale;
     right += DRAG_HOVER_RECT_PADDING * this.scale;
     bottom += DRAG_HOVER_RECT_PADDING * this.scale;
@@ -922,16 +935,21 @@ export class AffineDragHandleWidget extends WidgetElement<
       this.host,
       this.pageBlockElement,
       point
-    ) as BlockElement | null;
+    ) as NoteBlockComponent | null;
+    this.noteScale = closestNoteBlock?.model.edgeless.scale ?? 1;
     if (
       closestNoteBlock &&
       this._canEditing(closestNoteBlock) &&
-      !isOutOfNoteBlock(this.host, closestNoteBlock, point, this.scale)
+      !isOutOfNoteBlock(
+        this.host,
+        closestNoteBlock,
+        point,
+        this.scale * this.noteScale
+      )
     ) {
       this._pointerMoveOnBlock(state);
       return true;
     }
-
     this._hide();
     return false;
   };
@@ -1098,10 +1116,20 @@ export class AffineDragHandleWidget extends WidgetElement<
       const { left: viewportLeft, top: viewportTop } = edgelessPage.viewport;
 
       const newNoteId = edgelessPage.addNoteWithPoint(
-        new Point(state.raw.x - viewportLeft, state.raw.y - viewportTop)
+        new Point(state.raw.x - viewportLeft, state.raw.y - viewportTop),
+        {
+          scale: this.noteScale,
+        }
       );
-      const newNoteBlock = this.page.getBlockById(newNoteId);
+      const newNoteBlock = this.page.getBlockById(newNoteId) as NoteBlockModel;
       assertExists(newNoteBlock);
+
+      this.page.updateBlock(newNoteBlock, {
+        edgeless: {
+          ...newNoteBlock.edgeless,
+          scale: this.noteScale,
+        },
+      });
 
       this.page.moveBlocks(selectedBlocks, newNoteBlock);
 
@@ -1297,10 +1325,10 @@ export class AffineDragHandleWidget extends WidgetElement<
       container.style.transition = `padding 0.25s ease`;
 
       grabber.style.width = `${
-        DRAG_HANDLE_GRABBER_WIDTH_HOVERED * this.scale
+        DRAG_HANDLE_GRABBER_WIDTH_HOVERED * this.scale * this.noteScale
       }px`;
       grabber.style.borderRadius = `${
-        DRAG_HANDLE_GRABBER_BORDER_RADIUS * this.scale
+        DRAG_HANDLE_GRABBER_BORDER_RADIUS * this.scale * this.noteScale
       }px`;
 
       this._isDragHandleHovered = true;
