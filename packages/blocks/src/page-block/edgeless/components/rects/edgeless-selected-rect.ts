81cf448fa4f6269e5ac1e1c4c260f6dd0c84a1c7
Ayush Agrawal <54364088+AyushAgrawal-A2@users.noreply.github.com>
2024-01-17 18:43:24 +0530

    fix: update cursor on edge when shift is pressed

--- packages/blocks/src/page-block/edgeless/components/rects/edgeless-selected-rect.ts
+++ packages/blocks/src/page-block/edgeless/components/rects/edgeless-selected-rect.ts
@@ -774,6 +774,7 @@ export class EdgelessSelectedRect extends WithDisposable(LitElement) {
       slots.pressShiftKeyUpdated.on(pressed => {
         this._shiftKey = pressed;
         this._resizeManager.onPressShiftKey(pressed);
+        this._updateCursor(false);
       })
     );
 
