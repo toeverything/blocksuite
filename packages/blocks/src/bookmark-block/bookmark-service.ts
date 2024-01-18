c5f2800e396687de2da388e89ca7070f5013c2d6
DarkSky <25152247+darkskygit@users.noreply.github.com>
2024-01-18 16:45:19 +0800

    feat: export set link preview / image proxy api & update docs (#5969)

--- packages/blocks/src/bookmark-block/bookmark-service.ts
+++ packages/blocks/src/bookmark-block/bookmark-service.ts
@@ -1,15 +1,14 @@
 import { BlockService } from '@blocksuite/block-std';
 
+import { LinkPreviewer } from '../_common/embed-block-helper/index.js';
 import type { BookmarkBlockModel } from './bookmark-model.js';
-import {
-  queryUrlDataFromAffineWorker,
-  setLinkPreviewEndpoint,
-} from './utils.js';
 
 export class BookmarkService extends BlockService<BookmarkBlockModel> {
+  private static readonly linkPreviewer = new LinkPreviewer();
+
   queryUrlData = (url: string) => {
-    return queryUrlDataFromAffineWorker(url);
+    return BookmarkService.linkPreviewer.query(url);
   };
 
-  setLinkPreviewEndpoint = setLinkPreviewEndpoint;
+  static setLinkPreviewEndpoint = BookmarkService.linkPreviewer.setEndpoint;
 }
