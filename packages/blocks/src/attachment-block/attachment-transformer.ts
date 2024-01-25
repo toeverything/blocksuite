100e15b579f66c61ddb5fa4bf2927f01f612d031
fourdim <59462000+fourdim@users.noreply.github.com>
2024-01-24 23:32:01 -0500

    fix(page): add attachment transformer (#6109)

---
+++ packages/blocks/src/attachment-block/attachment-transformer.ts
@@ -0,0 +1,28 @@
+import type {
+  FromSnapshotPayload,
+  SnapshotReturn,
+  ToSnapshotPayload,
+} from '@blocksuite/store';
+import { BaseBlockTransformer } from '@blocksuite/store';
+
+import type { AttachmentBlockProps } from './attachment-model.js';
+
+export class AttachmentBlockTransformer extends BaseBlockTransformer<AttachmentBlockProps> {
+  override async toSnapshot(payload: ToSnapshotPayload<AttachmentBlockProps>) {
+    const snapshot = await super.toSnapshot(payload);
+    const sourceId = payload.model.sourceId;
+    if (sourceId) await payload.assets.readFromBlob(sourceId);
+
+    return snapshot;
+  }
+
+  override async fromSnapshot(
+    payload: FromSnapshotPayload
+  ): Promise<SnapshotReturn<AttachmentBlockProps>> {
+    const snapshotRet = await super.fromSnapshot(payload);
+    const sourceId = snapshotRet.props.sourceId;
+    if (sourceId) await payload.assets.writeToBlob(sourceId);
+
+    return snapshotRet;
+  }
+}
