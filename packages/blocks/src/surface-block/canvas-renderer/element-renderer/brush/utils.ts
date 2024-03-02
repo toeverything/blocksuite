5995462b091de51abf5bc9016089adf0c7d1ae24
L. <zover.v@gmail.com>
2024-03-03 04:47:13 +0800

    fix: points may be empty

--- packages/blocks/src/surface-block/canvas-renderer/element-renderer/brush/utils.ts
+++ packages/blocks/src/surface-block/canvas-renderer/element-renderer/brush/utils.ts
@@ -1,14 +1,12 @@
 import { getStroke } from '../../../perfect-freehand/getStroke.js';
 
 export function getSolidStrokePoints(points: number[][], lineWidth: number) {
-  const simulatePressure = points[0].length == 2;
-
   return getStroke(points, {
     size: lineWidth,
     thinning: 0.6,
     streamline: 0.5,
     smoothing: 0.5,
     easing: t => Math.sin((t * Math.PI) / 2),
-    simulatePressure,
+    simulatePressure: points[0]?.length === 2,
   });
 }
