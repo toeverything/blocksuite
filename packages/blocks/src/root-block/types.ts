44f59d9dfe5f568342186761308270c637c4f731
wumo <47590453+wumo1016@users.noreply.github.com>
2024-03-04 17:00:01 +0800

    chore: update import path of `AFFINE_BLOCK_HUB_WIDGET` (#6361)

--- packages/blocks/src/root-block/types.ts
+++ packages/blocks/src/root-block/types.ts
@@ -1,4 +1,4 @@
-// import type { AFFINE_BLOCK_HUB_WIDGET } from '../_common/widgets/block-hub/index.js';
+// import type { AFFINE_BLOCK_HUB_WIDGET } from '../root-block/widgets/block-hub/block-hub.js';
 
 import type { EdgelessRootBlockComponent } from './edgeless/edgeless-root-block.js';
 import type { PageRootBlockComponent } from './page/page-root-block.js';
