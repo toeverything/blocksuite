6d6537b205956d46585e0f7c1913bab5b5753ce9
Mirone <Saul-Mirone@outlook.com>
2024-03-06 13:37:25 +0800

    feat: add text style commands (#6406)

---
+++ packages/blocks/src/note-block/commands/utils.ts
@@ -0,0 +1,25 @@
+import type { Command } from '@blocksuite/block-std';
+import { assertInstanceOf } from '@blocksuite/global/utils';
+import { EditorHost } from '@blocksuite/lit';
+
+import { handleCommonStyle } from '../../_common/configs/text-format/utils.js';
+import type { AffineTextAttributes } from '../../_common/inline/presets/affine-inline-specs.js';
+
+export function getTextStyleCommand(
+  key: Extract<
+    keyof AffineTextAttributes,
+    'bold' | 'italic' | 'underline' | 'strike' | 'code'
+  >
+): Command {
+  return (ctx, next) => {
+    const host = ctx.std.host;
+    assertInstanceOf(host, EditorHost);
+    const [result] = handleCommonStyle(host, key);
+
+    if (result) {
+      return next();
+    }
+
+    return false;
+  };
+}
