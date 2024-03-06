741f82141a63e6e2a78e862a80da893bee89ac14
Flrande <flrande.ch@gmail.com>
2024-03-06 16:09:53 +0800

    refactor(sync): replace `MemoryDocSource` with `NoopDocSource`

---
+++ packages/framework/sync/src/doc/impl/noop.ts
@@ -0,0 +1,16 @@
+import type { DocSource } from '../source.js';
+
+export class NoopDocSource implements DocSource {
+  name = 'noop';
+
+  pull(_docId: string, _data: Uint8Array) {
+    return null;
+  }
+  push(_docId: string, _data: Uint8Array) {}
+  subscribe(
+    _cb: (docId: string, data: Uint8Array) => void,
+    _disconnect: (reason: string) => void
+  ) {
+    return () => {};
+  }
+}
