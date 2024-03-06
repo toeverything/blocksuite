7e3f0a79baeadcbfc783b1155a3fe190583e644d
Yifeng Wang <doodlewind@toeverything.info>
2024-03-06 11:46:46 +0800

    feat(examples): add svelte basic example

---
+++ examples/svelte-basic/src/components/Sidebar.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import type { Doc } from '@blocksuite/store';
+  import { getContext } from 'svelte';
+  import { onMount } from 'svelte';
+  import type { AppState } from '../editor/editor';
+  import type { Writable } from 'svelte/store';
+
+  const appState = getContext<Writable<AppState>>('appState');
+
+  let docs: Doc[] = [];
+
+  const selectDoc = (doc: Doc) => {
+    appState.update(state => {
+      state.editor.doc = doc;
+      return state;
+    });
+  };
+
+  onMount(() => {
+    appState.subscribe(({ workspace, editor }) => {
+      docs = [...workspace.docs.values()];
+      workspace.slots.docUpdated.on(() => {
+        docs = [...workspace.docs.values()];
+      });
+      editor.slots.docLinkClicked.on(() => {
+        docs = [...workspace.docs.values()];
+      });
+    });
+  });
+</script>
+
+<div class="sidebar">
+  <div class="header">All Docs</div>
+  <div class="doc-list">
+    {#each docs as doc}
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <!-- svelte-ignore a11y-no-static-element-interactions -->
+      <div
+        class:doc-item={true}
+        class:active={$appState.editor.doc === doc}
+        on:click={() => selectDoc(doc)}
+      >
+        {doc.meta?.title || 'Untitled'}
+      </div>
+    {/each}
+  </div>
+</div>
