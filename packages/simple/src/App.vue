2e861258ba84850f069354b16141dfd37b90d33c
alighasami <alighassami@yahoo.com>
2024-03-10 17:21:40 +0330

    add vue porject for simple

---
+++ packages/simple/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ packages/simple/src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about"></div>
+</template>
+
+<style></style>
