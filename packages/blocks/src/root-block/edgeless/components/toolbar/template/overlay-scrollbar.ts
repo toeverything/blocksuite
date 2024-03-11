6c6178851d5f4a3aa1428b88c6f58ebb55432a7c
Hongtao Lye <hongtao.lye@toeverything.info>
2024-03-11 14:18:09 +0800

    chore: add comment

--- packages/blocks/src/root-block/edgeless/components/toolbar/template/overlay-scrollbar.ts
+++ packages/blocks/src/root-block/edgeless/components/toolbar/template/overlay-scrollbar.ts
@@ -6,6 +6,20 @@ import { requestConnectedFrame } from '../../../../../_common/utils/event.js';
 
 /**
  * A scrollbar that is only visible when the user is interacting with it.
+ * Append this element to the a container that has a scrollable element. Which means
+ * the scrollable element should lay on the same level as the overlay-scrollbar.
+ *
+ * And the scrollable element should have a `data-scrollable` attribute.
+ *
+ * Example:
+ * ```
+ * <div class="container">
+ *    <div class="scrollable-element-with-fixed-height" data-scrollable>
+ *       <!--.... very long content ....-->
+ *    </div>
+ *    <overlay-scrollbar></overlay-scrollbar>
+ * </div>
+ * ```
  *
  * Note:
  * - It only works with vertical scrollbars.
