340c62e76d1a4398c093d03824633b8964204133
Flrande <50035259+Flrande@users.noreply.github.com>
2024-03-13 13:52:48 +0800

    fix(inline): v-line is not fully adapted for horizontal scrolling (#6467)

--- packages/framework/inline/src/components/v-line.ts
+++ packages/framework/inline/src/components/v-line.ts
@@ -91,6 +91,7 @@ export class VLine extends LitElement {
       // this padding is used to make cursor can be placed at the
       // start and end of the line when the first and last element is embed element
       padding: '0 0.5px',
+      display: 'inline-block',
     })}>${renderElements}</div>`;
   }
 
