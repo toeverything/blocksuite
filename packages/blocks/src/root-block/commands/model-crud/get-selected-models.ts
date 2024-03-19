14f8401f9115daba00e404f4b032f5cdcdae6a23
Chen <99816898+donteatfriedrice@users.noreply.github.com>
2024-03-19 16:30:51 +0800

    chore: update function docs

--- packages/blocks/src/root-block/commands/model-crud/get-selected-models.ts
+++ packages/blocks/src/root-block/commands/model-crud/get-selected-models.ts
@@ -5,14 +5,29 @@ import type { BlockModel } from '@blocksuite/store';
 /**
  * Retrieves the selected models based on the provided selection types and mode.
  *
- * @param ctx - The command context.
+ * @param ctx - The command context, which includes the types of selections to be retrieved and the mode of the selection.
  * @param ctx.types - The selection types to be retrieved. Can be an array of 'block', 'text', or 'image'.
  * @param ctx.mode - The mode of the selection. Can be 'all', 'flat', or 'highest'.
- * ‘all’: Returns all selected models.
- * ‘flat’: Returns all selected models in a flat array.
- * ‘highest’: Returns the highest selected models.
+ * @example
+ * // Assuming `commandContext` is an instance of the command context
+ * getSelectedModelsCommand(commandContext, (result) => {
+ *   console.log(result.selectedModels);
+ * });
+ *
+ * // Example selection:
+ * // aaa
+ * //   b[bb
+ * //     ccc
+ * // ddd
+ * //   ee]e
+ *
+ * // all mode: [aaa, bbb, ccc, ddd, eee]
+ * // flat mode: [bbb, ccc, ddd, eee]
+ * // highest mode: [bbb, ddd]
+ *
+ * // The match function will be evaluated before filtering using mode
  * @param next - The next function to be called.
- * @returns The selected models.
+ * @returns An object containing the selected models as an array of BlockModel instances.
  */
 export const getSelectedModelsCommand: Command<
   never,
