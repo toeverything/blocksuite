3020e7ba67ac20e523546cb5e556c88c39b5dbd5
Yifeng Wang <doodlewind@toeverything.info>
2024-03-20 17:27:45 +0800

    refactor(store): support streaming `editor.doc` from empty state

--- packages/presets/src/editors/edgeless-editor.ts
+++ packages/presets/src/editors/edgeless-editor.ts
@@ -3,7 +3,7 @@ import { EdgelessEditorBlockSpecs } from '@blocksuite/blocks';
 import { noop } from '@blocksuite/global/utils';
 import { EditorHost, ShadowlessElement, WithDisposable } from '@blocksuite/lit';
 import type { Doc } from '@blocksuite/store';
-import { css, html } from 'lit';
+import { css, html, nothing } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { createRef, type Ref, ref } from 'lit/directives/ref.js';
 
@@ -49,6 +49,13 @@ export class EdgelessEditor extends WithDisposable(ShadowlessElement) {
     return this._host.value as EditorHost;
   }
 
+  override connectedCallback() {
+    super.connectedCallback();
+    this._disposables.add(
+      this.doc.slots.rootAdded.on(() => this.requestUpdate())
+    );
+  }
+
   override async getUpdateComplete(): Promise<boolean> {
     const result = await super.getUpdateComplete();
     await this.host.updateComplete;
@@ -56,6 +63,8 @@ export class EdgelessEditor extends WithDisposable(ShadowlessElement) {
   }
 
   override render() {
+    if (!this.doc.root) return nothing;
+
     return html`
       <div class="affine-edgeless-viewport">
         <editor-host
