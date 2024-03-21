f28f78fb7c18edb0be3499355928f0068d270065
regischen <regischen@toeverything.info>
2024-03-21 15:47:00 +0800

    feat: add image upload functionality

--- packages/blocks/src/index.ts
+++ packages/blocks/src/index.ts
@@ -61,7 +61,7 @@ export {
 } from './_common/theme/theme-observer.js';
 export * from './_common/transformers/index.js';
 export { type AbstractEditor, NoteDisplayMode } from './_common/types.js';
-export { on, once } from './_common/utils/event.js';
+export { on, once, openFileOrFiles } from './_common/utils/index.js';
 export { createDefaultDoc } from './_common/utils/init.js';
 export { getThemeMode } from './_common/utils/query.js';
 export * from './_specs/_specs.js';
--- packages/presets/src/fragments/chat-panel/index.ts
+++ packages/presets/src/fragments/chat-panel/index.ts
@@ -1,5 +1,6 @@
 import { WithDisposable } from '@blocksuite/block-std';
-import { css, html, LitElement } from 'lit';
+import { openFileOrFiles } from '@blocksuite/blocks';
+import { css, html, LitElement, nothing } from 'lit';
 import { property, query, state } from 'lit/decorators.js';
 import { repeat } from 'lit/directives/repeat.js';
 
@@ -11,17 +12,7 @@ import {
   SmallHintIcon,
 } from '../_common/icons.js';
 
-type ChatMessage =
-  | {
-      type: 'user';
-      content: string;
-    }
-  | {
-      type: 'ai';
-      content: string;
-    };
-
-type ChatStatus = 'idle' | 'loading' | 'error' | 'success';
+const MaximumImageCount = 8;
 
 export class ChatPanel extends WithDisposable(LitElement) {
   static override styles = css`
@@ -86,14 +77,13 @@ export class ChatPanel extends WithDisposable(LitElement) {
 
     .chat-panel-input {
       margin-top: 12px;
-      height: 100px;
       position: relative;
     }
 
     .chat-panel-input-actions {
       position: absolute;
       right: 16px;
-      bottom: -6px;
+      bottom: 6px;
       display: flex;
       gap: 8px;
       align-items: center;
@@ -108,7 +98,7 @@ export class ChatPanel extends WithDisposable(LitElement) {
       border-radius: 4px;
       padding: 8px 12px;
       width: calc(100% - 32px);
-      height: calc(100% - 12px);
+      min-height: 100px;
       border: 1px solid var(--affine-border-color);
       font-size: 14px;
       font-weight: 400;
@@ -135,12 +125,24 @@ export class ChatPanel extends WithDisposable(LitElement) {
       color: var(--affine-text-secondary-color);
       font-size: 12px;
     }
+
+    .chat-panel-images {
+      display: flex;
+      gap: 6px;
+      flex-wrap: wrap;
+      margin: 8px 0;
+    }
+    .chat-panel-images img {
+      border-radius: 4px;
+      border: 1px solid var(--affine-border-color);
+    }
   `;
-  @state()
-  messages: ChatMessage[] = [];
 
   @state()
-  status: ChatStatus = 'idle';
+  images: File[] = [];
+
+  @state()
+  currentMessage: string = '';
 
   @property({ attribute: false })
   editor!: AffineEditorContainer;
@@ -168,40 +170,82 @@ export class ChatPanel extends WithDisposable(LitElement) {
     if (!text) {
       return;
     }
+    this.textarea.value = '';
+    this.currentMessage = '';
     this.copilot.askAI(this.copilot.actions.createCommonTextAction(text), text);
   };
 
   override render() {
     const messages = this.copilot.history.history;
-    return html`<div class="chat-panel-container">
-      <div class="chat-panel-title">AFFINE AI</div>
-      <div class="chat-panel-messages">
+    return html`<style>
+        .chat-panel-send svg rect {
+          fill: ${this.currentMessage
+            ? 'var(--affine-primary-color)'
+            : 'var(--affine-text-disable-color)'};
+        }
+      </style>
+      <div class="chat-panel-container">
+        <div class="chat-panel-title">AFFINE AI</div>
+        <div class="chat-panel-messages">
+          ${messages.length === 0
+            ? html`<div class="chat-panel-messages-placeholder">
+                ${AffineIcon}
+                <div>What can I help you with?</div>
+              </div>`
+            : repeat(messages, message => message.render(this.editor.host))}
+        </div>
         ${messages.length === 0
-          ? html`<div class="chat-panel-messages-placeholder">
-              ${AffineIcon}
-              <div>What can I help you with?</div>
+          ? html`<div class="chat-panel-hints">
+              <div>Start with current selection</div>
+              <div>you've chosen within the doc</div>
             </div>`
-          : repeat(messages, message => message.render(this.editor.host))}
-      </div>
-      ${messages.length === 0 &&
-      html`<div class="chat-panel-hints">
-        <div>Start with current selection</div>
-        <div>you've chosen within the doc</div>
-      </div>`}
-      <div class="chat-panel-input">
-        <textarea placeholder="What are your thoughts? "> </textarea>
-        <div class="chat-panel-input-actions">
-          <div class="image-upload">${ImageIcon}</div>
-          <div @click="${this.send}" class="chat-panel-send">
-            ${ChatSendIcon}
+          : nothing}
+        <div class="chat-panel-input">
+          ${this.images.length > 0
+            ? html`<div class="chat-panel-images">
+                ${repeat(
+                  this.images,
+                  image => image.name,
+                  image =>
+                    html`<img
+                      width="58"
+                      height="58"
+                      src="${URL.createObjectURL(image)}"
+                      alt="${image.name}"
+                    />`
+                )}
+              </div>`
+            : nothing}
+          <textarea
+            placeholder="What are your thoughts?"
+            @input=${() => {
+              this.currentMessage = this.textarea.value;
+            }}
+          ></textarea>
+          <div class="chat-panel-input-actions">
+            <div
+              class="image-upload"
+              @click=${async () => {
+                const images = await openFileOrFiles({
+                  acceptType: 'Images',
+                  multiple: true,
+                });
+                if (!images) return;
+                this.images = images.slice(0, MaximumImageCount);
+              }}
+            >
+              ${ImageIcon}
+            </div>
+            <div @click="${this.send}" class="chat-panel-send">
+              ${ChatSendIcon}
+            </div>
           </div>
         </div>
-      </div>
-      <div class="chat-panel-footer">
-        ${SmallHintIcon}
-        <div>AI outputs can be misleading or wrong</div>
-      </div>
-    </div>`;
+        <div class="chat-panel-footer">
+          ${SmallHintIcon}
+          <div>AI outputs can be misleading or wrong</div>
+        </div>
+      </div>`;
   }
 }
 
