5cc9d680f5698f715acf905c2cb13bffdcd080c4
golok727 <goooolok727@gmail.com>
2024-03-26 12:19:44 +0530

    feat: sanitize

--- packages/blocks/src/root-block/edgeless/controllers/clipboard.ts
+++ packages/blocks/src/root-block/edgeless/controllers/clipboard.ts
@@ -11,6 +11,7 @@ import {
   fromJSON,
   Job,
 } from '@blocksuite/store';
+import DOMPurify from 'dompurify';
 
 import {
   DEFAULT_IMAGE_PROXY_ENDPOINT,
@@ -1383,13 +1384,14 @@ function tryGetSvgFromClipboard(clipboardData: DataTransfer) {
     clipboardData.getData('text/plain'),
     'image/svg+xml'
   );
+  const svg = svgDoc.documentElement;
 
-  if (svgDoc.documentElement.tagName !== 'svg') {
+  if (svg.tagName !== 'svg' || !svg.hasAttribute('xmlns')) {
     return null;
   }
-  const svgContent = new XMLSerializer().serializeToString(
-    svgDoc.documentElement
-  );
+  const svgContent = DOMPurify.sanitize(svgDoc.documentElement, {
+    USE_PROFILES: { svg: true },
+  });
   const blob = new Blob([svgContent], { type: 'image/svg+xml' });
   const file = new File([blob], 'pasted-image.svg', { type: 'image/svg+xml' });
   return file;
