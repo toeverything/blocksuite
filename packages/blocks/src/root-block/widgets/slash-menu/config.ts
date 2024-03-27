2f7b1e74d33c01baea325deaf7179ea93019a462
Whitewater <me@waterwater.moe>
2024-03-27 14:58:25 +0800

    fix(page): should not remove empty line when canceling insertion (#6584)
    Co-authored-by: Mirone <Saul-Mirone@outlook.com>

--- packages/blocks/src/root-block/widgets/slash-menu/config.ts
+++ packages/blocks/src/root-block/widgets/slash-menu/config.ts
@@ -238,6 +238,7 @@ export const menuGroups: SlashMenuOptions['menus'] = [
           }
 
           const imageFiles = await getImageFilesFromLocal();
+          if (!imageFiles.length) return;
 
           const imageService = rootElement.host.spec.getService('affine:image');
           const maxFileSize = imageService.maxFileSize;
