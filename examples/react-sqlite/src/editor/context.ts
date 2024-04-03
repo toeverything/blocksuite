31e94713f93b1fc81b3865b5fdda134d65b1aab6
Yifeng Wang <doodlewind@toeverything.info>
2024-04-03 12:22:25 +0800

    refactor(examples): sync collection lifecycle with provider

--- examples/react-sqlite/src/editor/context.ts
+++ examples/react-sqlite/src/editor/context.ts
@@ -1,13 +1,11 @@
 import { AffineEditorContainer } from '@blocksuite/presets';
-import { DocCollection } from '@blocksuite/store';
 import { createContext, useContext } from 'react';
-import { Provider } from './provider/provider';
+import { CollectionProvider } from './provider/provider';
 
 export interface EditorContextType {
   editor: AffineEditorContainer | null;
-  collection: DocCollection | null;
-  provider: Provider | null;
-  updateCollection: (newCollection: DocCollection) => void;
+  provider: CollectionProvider | null;
+  updateProvider: (newProvider: CollectionProvider) => void;
 }
 
 export const EditorContext = createContext<EditorContextType | null>(null);
