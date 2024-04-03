bdc56fef97d8e1e53d3852f8253a33c91a56ea6e
Yifeng Wang <doodlewind@toeverything.info>
2024-04-03 12:23:18 +0800

    refactor(examples): sync collection lifecycle with provider (#6683)

--- examples/react-sqlite/src/editor/editor.ts
+++ examples/react-sqlite/src/editor/editor.ts
@@ -1,39 +1,23 @@
 import { AffineEditorContainer } from '@blocksuite/presets';
-import { Provider } from './provider/provider';
+import { CollectionProvider } from './provider/provider';
 import { Doc } from '@blocksuite/store';
 import { DocCollection } from '@blocksuite/store';
-import { createContext, useContext } from 'react';
 import '@blocksuite/presets/themes/affine.css';
 
 export interface EditorContextType {
   editor: AffineEditorContainer | null;
   collection: DocCollection | null;
-  provider: Provider | null;
+  provider: CollectionProvider | null;
   updateCollection: (newCollection: DocCollection) => void;
 }
 
-export const EditorContext = createContext<EditorContextType | null>(null);
-
-export function useEditor() {
-  return useContext(EditorContext);
-}
-
 export async function initEditor() {
-  const provider = await Provider.init();
-  await provider.connect();
+  const provider = await CollectionProvider.init();
 
-  const { collection: collection } = provider;
-  let doc: Doc | null = null;
-
-  collection.docs.forEach(d => {
-    doc = doc ?? d;
-  });
-  if (!doc) {
-    throw Error('doc not found');
-  }
-
+  const { collection } = provider;
   const editor = new AffineEditorContainer();
-  editor.doc = doc;
+
+  editor.doc = [...collection.docs.values()][0];
   editor.slots.docLinkClicked.on(({ docId }) => {
     const target = <Doc>collection.getDoc(docId);
     editor.doc = target;
