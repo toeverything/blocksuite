1a4a7e510f332418e0368cbcb743a089c37d381d
Лебедев Константин <k.lebedev@corp.mail.ru>
2024-04-04 14:39:22 +0300

    feat(edgeless): font family panel refactoring

--- packages/blocks/src/root-block/edgeless/components/panel/font-family-panel.ts
+++ packages/blocks/src/root-block/edgeless/components/panel/font-family-panel.ts
@@ -1,8 +1,21 @@
-import { css, html, LitElement, unsafeCSS } from 'lit';
+import { css, html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
 import { CanvasTextFontFamily } from '../../../../surface-block/consts.js';
 import { wrapFontFamily } from '../../../../surface-block/utils/font.js';
+
+export const CanvasTextFontFamiliesNames: Record<
+  keyof typeof CanvasTextFontFamily,
+  string
+> = {
+  Inter: 'Inter',
+  Kalam: 'Kalam',
+  Satoshi: 'Satoshi',
+  Poppins: 'Poppins',
+  Lora: 'Lora',
+  BebasNeue: 'Bebas Neue',
+  OrelegaOne: 'Orelega One',
+};
 
 @customElement('edgeless-font-family-panel')
 export class EdgelessFontFamilyPanel extends LitElement {
@@ -25,45 +38,15 @@ export class EdgelessFontFamilyPanel extends LitElement {
     .font-family-container edgeless-tool-icon-button {
       width: 100%;
     }
-
-    .inter {
-      font-family: ${unsafeCSS(wrapFontFamily(CanvasTextFontFamily.Inter))},
-        sans-serif;
-    }
-
-    .kalam {
-      font-family: ${unsafeCSS(wrapFontFamily(CanvasTextFontFamily.Kalam))},
-        sans-serif;
-    }
-
-    .satoshi {
-      font-family: ${unsafeCSS(wrapFontFamily(CanvasTextFontFamily.Satoshi))},
-        sans-serif;
-    }
-
-    .poppins {
-      font-family: ${unsafeCSS(wrapFontFamily(CanvasTextFontFamily.Poppins))},
-        sans-serif;
-    }
-
-    .lora {
-      font-family: ${unsafeCSS(wrapFontFamily(CanvasTextFontFamily.Lora))},
-        sans-serif;
-    }
-
-    .bebas-neue {
-      font-family: ${unsafeCSS(wrapFontFamily(CanvasTextFontFamily.BebasNeue))},
-        sans-serif;
-    }
-
-    .orelega-one {
-      font-family: ${unsafeCSS(wrapFontFamily(CanvasTextFontFamily.OrelegaOne))},
-        sans-serif;
-    }
   `;
 
   @property({ attribute: false })
   value = CanvasTextFontFamily.Inter;
+
+  @property({ attribute: false })
+  options = Object.keys(
+    CanvasTextFontFamily
+  ) as (keyof typeof CanvasTextFontFamily)[];
 
   @property({ attribute: false })
   onSelect?: (value: EdgelessFontFamilyPanel['value']) => void;
@@ -78,89 +61,25 @@ export class EdgelessFontFamilyPanel extends LitElement {
   override render() {
     return html`
       <div class="font-family-container">
-        <edgeless-tool-icon-button
-          class="inter"
-          .active=${this.value === CanvasTextFontFamily.Inter}
-          .activeMode=${'background'}
-          .iconContainerPadding=${2}
-          @click=${() => {
-            this._onSelect(CanvasTextFontFamily.Inter);
-          }}
-        >
-          <div class="font-family-button">Inter</div>
-        </edgeless-tool-icon-button>
+        ${this.options.map(key => {
+          const font = CanvasTextFontFamily[key];
 
-        <edgeless-tool-icon-button
-          class="kalam"
-          .active=${this.value === CanvasTextFontFamily.Kalam}
-          .activeMode=${'background'}
-          .iconContainerPadding=${2}
-          @click=${() => {
-            this._onSelect(CanvasTextFontFamily.Kalam);
-          }}
-        >
-          <div class="font-family-button">Kalam</div>
-        </edgeless-tool-icon-button>
-
-        <edgeless-tool-icon-button
-          class="satoshi"
-          .active=${this.value === CanvasTextFontFamily.Satoshi}
-          .activeMode=${'background'}
-          .iconContainerPadding=${2}
-          @click=${() => {
-            this._onSelect(CanvasTextFontFamily.Satoshi);
-          }}
-        >
-          <div class="font-family-button">Satoshi</div>
-        </edgeless-tool-icon-button>
-
-        <edgeless-tool-icon-button
-          class="poppins"
-          .active=${this.value === CanvasTextFontFamily.Poppins}
-          .activeMode=${'background'}
-          .iconContainerPadding=${2}
-          @click=${() => {
-            this._onSelect(CanvasTextFontFamily.Poppins);
-          }}
-        >
-          <div class="font-family-button">Poppins</div>
-        </edgeless-tool-icon-button>
-
-        <edgeless-tool-icon-button
-          class="lora"
-          .active=${this.value === CanvasTextFontFamily.Lora}
-          .activeMode=${'background'}
-          .iconContainerPadding=${2}
-          @click=${() => {
-            this._onSelect(CanvasTextFontFamily.Lora);
-          }}
-        >
-          <div class="font-family-button">Lora</div>
-        </edgeless-tool-icon-button>
-
-        <edgeless-tool-icon-button
-          class="bebas-neue"
-          .active=${this.value === CanvasTextFontFamily.BebasNeue}
-          .activeMode=${'background'}
-          .iconContainerPadding=${2}
-          @click=${() => {
-            this._onSelect(CanvasTextFontFamily.BebasNeue);
-          }}
-        >
-          <div class="font-family-button">Bebas Neue</div>
-        </edgeless-tool-icon-button>
-
-        <edgeless-tool-icon-button
-          class="orelega-one"
-          .active=${this.value === CanvasTextFontFamily.OrelegaOne}
-          .activeMode=${'background'}
-          .iconContainerPadding=${2}
-          @click=${() => {
-            this._onSelect(CanvasTextFontFamily.OrelegaOne);
-          }}
-        >
-          <div class="font-family-button">Orelega One</div>
-        </edgeless-tool-icon-button>
+          return html`
+            <edgeless-tool-icon-button
+              style="font-family: ${wrapFontFamily(font)}"
+              .active=${this.value === font}
+              .activeMode=${'background'}
+              .iconContainerPadding=${2}
+              @click=${() => {
+                this._onSelect(font);
+              }}
+            >
+              <div class="font-family-button">
+                ${CanvasTextFontFamiliesNames[key]}
+              </div>
+            </edgeless-tool-icon-button>
+          `;
+        })}
       </div>
     `;
   }
