0515ca851b181b5805c0c9fbc2422cbc3a214977
Mirone <Saul-Mirone@outlook.com>
2024-04-18 13:59:42 +0800

    chore: ignore sourcemap in node modules

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -3,6 +3,7 @@ import { createRequire } from 'node:module';
 import path, { resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
+import type { Plugin } from 'vite';
 import { defineConfig, loadEnv } from 'vite';
 import istanbul from 'vite-plugin-istanbul';
 import wasm from 'vite-plugin-wasm';
@@ -14,6 +15,21 @@ const enableIstanbul = !!process.env.CI || !!process.env.COVERAGE;
 const chunkSizeReport = !!process.env.CHUNK_SIZE_REPORT;
 
 const cache = new Map();
+
+export function sourcemapExclude(): Plugin {
+  return {
+    name: 'sourcemap-exclude',
+    transform(code: string, id: string) {
+      if (id.includes('node_modules') && !id.includes('@blocksuite')) {
+        return {
+          code,
+          // https://github.com/rollup/rollup/blob/master/docs/plugin-development/index.md#source-code-transformations
+          map: { mappings: '' },
+        };
+      }
+    },
+  };
+}
 
 function isDepInclude(
   id: string,
@@ -107,6 +123,7 @@ export default ({ mode }) => {
     },
     plugins: [
       hmrPlugin,
+      sourcemapExclude(),
       enableIstanbul &&
         istanbul({
           cwd: fileURLToPath(new URL('../..', import.meta.url)),
