69871fd44fd8952a357aa0d091cfe6a751caaa67
golok727 <goooolok727@gmail.com>
2024-04-23 13:16:00 +0530

    fix(database): can't scroll in side layout

--- packages/blocks/src/database-block/data-view/common/detail/layout.ts
+++ packages/blocks/src/database-block/data-view/common/detail/layout.ts
@@ -18,7 +18,8 @@ import { RecordDetail } from './detail.js';
 class SideLayoutModal extends ShadowlessElement {
   static override styles = css`
     side-layout-modal {
-      display: block;
+      display: flex;
+      flex-direction: column;
       position: absolute;
       right: 0;
       top: 0;
@@ -29,12 +30,18 @@ class SideLayoutModal extends ShadowlessElement {
       box-shadow: -5px 0px 10px 0px rgba(0, 0, 0, 0.05);
     }
 
+    .side-modal-content {
+      flex: 1;
+      overflow-y: auto;
+    }
+
     .side-modal-header {
-      margin: 12px 0;
-      padding: 0 12px;
+      padding: 12px;
       display: flex;
       align-items: center;
-      justify-content: end;
+      position: sticky;
+      top: 0;
+      width: 100%;
     }
 
     .header-ops {
@@ -60,10 +67,6 @@ class SideLayoutModal extends ShadowlessElement {
     .header-op:hover {
       background-color: var(--affine-hover-color);
     }
-
-    .close-modal {
-      margin-left: 20px;
-    }
   `;
   @property({ attribute: false })
   content?: HTMLElement;
@@ -82,12 +85,12 @@ class SideLayoutModal extends ShadowlessElement {
   override render() {
     return html`
       <div class="side-modal-header">
-        <div class="header-ops">${this.renderOps()}</div>
         <div @click="${this.close}" class="close-modal header-op">
           ${CrossIcon}
         </div>
+        <div class="header-ops">${this.renderOps()}</div>
       </div>
-      ${this.content}
+      <div class="side-modal-content">${this.content}</div>
     `;
   }
 }
