2ba4f0889194ff59bb4225e32e8671dd72ed0a5f
zzj3720 <zuozijian1994@gmail.com>
2024-04-29 16:56:40 +0800

    fix(database): rich-text cell level copy and paste

--- packages/blocks/src/database-block/columns/title/define.ts
+++ packages/blocks/src/database-block/columns/title/define.ts
@@ -1,4 +1,4 @@
-import type { Y } from '@blocksuite/store';
+import type { Text } from '@blocksuite/store';
 
 import { columnType } from '../../data-view/column/column-config.js';
 import { tRichText } from '../../data-view/logical/data-type.js';
@@ -10,7 +10,8 @@ declare global {
     [titleColumnType.type]: typeof titlePureColumnConfig.model;
   }
 }
-export const titlePureColumnConfig = titleColumnType.modelConfig<Y.Text>({
+
+export const titlePureColumnConfig = titleColumnType.modelConfig<Text>({
   name: 'Title',
   type: () => tRichText.create(),
   defaultData: () => ({}),
@@ -21,4 +22,21 @@ export const titlePureColumnConfig = titleColumnType.modelConfig<Y.Text>({
     };
   },
   cellToJson: data => data?.toString() ?? null,
+  onUpdate: (value, _data, callback) => {
+    value.yText.observe(callback);
+    callback();
+    return {
+      dispose: () => {
+        value.yText.unobserve(callback);
+      },
+    };
+  },
+  valueUpdate: (value, _data, newValue) => {
+    const v = newValue as unknown;
+    if (typeof v === 'string') {
+      value.replace(0, value.length, v);
+      return value;
+    }
+    return newValue;
+  },
 });
