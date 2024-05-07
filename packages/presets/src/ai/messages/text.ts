2e463b1c47415e4b511651867825de2f5bf29671
Chen <99816898+donteatfriedrice@users.noreply.github.com>
2024-05-07 13:16:24 +0800

    fix: text renderer scroll behivior

--- packages/presets/src/ai/messages/text.ts
+++ packages/presets/src/ai/messages/text.ts
@@ -2,15 +2,15 @@ import { type EditorHost, WithDisposable } from '@blocksuite/block-std';
 import type { AffineAIPanelState } from '@blocksuite/blocks';
 import { type AffineAIPanelWidgetConfig } from '@blocksuite/blocks';
 import { type Doc } from '@blocksuite/store';
-import { css, html, LitElement, nothing, type PropertyValues } from 'lit';
+import { css, html, LitElement, type PropertyValues } from 'lit';
 import { customElement, property, query } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
 
 import { CustomPageEditorBlockSpecs } from '../utils/custom-specs.js';
 import { markDownToDoc } from '../utils/markdown-utils.js';
 
-@customElement('ai-answer-text-preview')
-export class AIAnswerTextPreview extends WithDisposable(LitElement) {
+@customElement('ai-answer-text')
+export class AIAnswerText extends WithDisposable(LitElement) {
   static override styles = css`
     .ai-answer-text-editor.affine-page-viewport {
       background: transparent;
@@ -44,6 +44,26 @@ export class AIAnswerTextPreview extends WithDisposable(LitElement) {
         }
       }
     }
+
+    .ai-answer-text-container {
+      overflow-y: auto;
+      overflow-x: hidden;
+      padding: 0;
+      overscroll-behavior-y: none;
+    }
+    .ai-answer-text-container.show-scrollbar::-webkit-scrollbar {
+      width: 5px;
+      height: 100px;
+    }
+    .ai-answer-text-container.show-scrollbar::-webkit-scrollbar-thumb {
+      border-radius: 20px;
+    }
+    .ai-answer-text-container.show-scrollbar:hover::-webkit-scrollbar-thumb {
+      background-color: var(--affine-black-30);
+    }
+    .ai-answer-text-container.show-scrollbar::-webkit-scrollbar-corner {
+      display: none;
+    }
   `;
 
   @property({ attribute: false })
@@ -51,6 +71,22 @@ export class AIAnswerTextPreview extends WithDisposable(LitElement) {
 
   @property({ attribute: false })
   answer!: string;
+
+  @property({ attribute: false })
+  state?: AffineAIPanelState;
+
+  @property({ attribute: false })
+  maxHeight?: number;
+
+  @query('.ai-answer-text-container')
+  private _container!: HTMLDivElement;
+
+  private _onWheel(e: MouseEvent) {
+    e.stopPropagation();
+    if (this.state === 'generating') {
+      e.preventDefault();
+    }
+  }
 
   private _doc!: Doc;
   private _answers: string[] = [];
@@ -100,74 +136,15 @@ export class AIAnswerTextPreview extends WithDisposable(LitElement) {
     }
   }
 
-  override render() {
-    return html`
-      <div class="ai-answer-text-editor affine-page-viewport">
-        ${this.host.renderSpecPortal(this._doc, CustomPageEditorBlockSpecs)}
-      </div>
-    `;
-  }
-}
-
-@customElement('ai-answer-text')
-export class AIAnswerText extends LitElement {
-  static override styles = css`
-    .ai-answer-text-container {
-      overflow-y: auto;
-      overflow-x: hidden;
-      padding: 0;
-      overscroll-behavior-y: none;
-    }
-    .ai-answer-text-container.show-scrollbar::-webkit-scrollbar {
-      width: 5px;
-      height: 100px;
-    }
-    .ai-answer-text-container.show-scrollbar::-webkit-scrollbar-thumb {
-      border-radius: 20px;
-    }
-    .ai-answer-text-container.show-scrollbar:hover::-webkit-scrollbar-thumb {
-      background-color: var(--affine-black-30);
-    }
-    .ai-answer-text-container.show-scrollbar::-webkit-scrollbar-corner {
-      display: none;
-    }
-  `;
-
-  @property({ attribute: false })
-  host!: EditorHost;
-
-  @property({ attribute: false })
-  answer!: string;
-
-  @property({ attribute: false })
-  state?: AffineAIPanelState;
-
-  @property({ attribute: false })
-  maxHeight?: number;
-
-  @query('.ai-answer-text-container')
-  private _container!: HTMLDivElement;
-
-  private _onWheel(e: MouseEvent) {
-    e.stopPropagation();
-    if (this.state === 'generating') {
-      e.preventDefault();
-    }
-  }
-
   override updated(changedProperties: PropertyValues) {
     super.updated(changedProperties);
-
-    if (changedProperties.has('answer')) {
-      requestAnimationFrame(() => {
-        if (!this._container) return;
-        this._container.scrollTop = this._container.scrollHeight;
-      });
-    }
+    requestAnimationFrame(() => {
+      if (!this._container) return;
+      this._container.scrollTop = this._container.scrollHeight;
+    });
   }
 
   override render() {
-    if (!this.answer) return nothing;
     const classes = classMap({
       'ai-answer-text-container': true,
       'show-scrollbar': !!this.maxHeight,
@@ -181,10 +158,9 @@ export class AIAnswerText extends LitElement {
         }
       </style>
       <div class=${classes} @wheel=${this._onWheel}>
-        <ai-answer-text-preview
-          .host=${this.host}
-          .answer=${this.answer}
-        ></ai-answer-text-preview>
+        <div class="ai-answer-text-editor affine-page-viewport">
+          ${this.host.renderSpecPortal(this._doc, CustomPageEditorBlockSpecs)}
+        </div>
       </div>
     `;
   }
@@ -193,7 +169,6 @@ export class AIAnswerText extends LitElement {
 declare global {
   interface HTMLElementTagNameMap {
     'ai-answer-text': AIAnswerText;
-    'ai-answer-text-preview': AIAnswerTextPreview;
   }
 }
 
