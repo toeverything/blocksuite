a3008e5231ff3e7d52fc12c76899274f5d58e957
doouding <github@douding.io>
2024-05-16 06:54:09 +0000

    refactor(edgeless): better rendering control using lit scheduleUpdate (#7068)

--- packages/blocks/src/root-block/edgeless/components/block-portal/edgeless-portal-base.ts
+++ packages/blocks/src/root-block/edgeless/components/block-portal/edgeless-portal-base.ts
@@ -2,6 +2,7 @@ import { ShadowlessElement, WithDisposable } from '@blocksuite/block-std';
 import type { BlockModel } from '@blocksuite/store';
 import { property } from 'lit/decorators.js';
 
+import { requestConnectedFrame } from '../../../../_common/utils/event.js';
 import type { SurfaceBlockComponent } from '../../../../surface-block/surface-block.js';
 import type { EdgelessRootBlockComponent } from '../../edgeless-root-block.js';
 
@@ -20,8 +21,36 @@ export class EdgelessPortalBase<T extends BlockModel> extends WithDisposable(
   @property({ attribute: false })
   edgeless!: EdgelessRootBlockComponent;
 
+  @property({ attribute: false })
+  updatingSet!: Set<string>;
+
+  @property({ attribute: false })
+  concurrentUpdatingCount!: number;
+
   protected renderModel(model: T) {
     return this.surface.host.renderModel(model);
+  }
+
+  protected override scheduleUpdate(): void | Promise<unknown> {
+    const { promise, resolve } = Promise.withResolvers<void>();
+    const detect = () => {
+      if (this.updatingSet.size < this.concurrentUpdatingCount) {
+        this.updatingSet.add(this.model.id);
+        // eslint-disable-next-line @typescript-eslint/no-floating-promises
+        super.scheduleUpdate();
+
+        requestConnectedFrame(() => {
+          this.updatingSet.delete(this.model.id);
+        }, this);
+        resolve();
+      } else {
+        requestConnectedFrame(detect, this);
+      }
+    };
+
+    detect();
+
+    return promise;
   }
 
   override connectedCallback(): void {
