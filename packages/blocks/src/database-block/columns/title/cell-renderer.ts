35c0bdaaa34f0d4946cbe35423e11247e4943d37
zzj3720 <zuozijian1994@gmail.com>
2024-05-21 13:59:03 +0800

    fix(database): duplicate icon of title cell in kanban

--- packages/blocks/src/database-block/columns/title/cell-renderer.ts
+++ packages/blocks/src/database-block/columns/title/cell-renderer.ts
@@ -2,6 +2,7 @@ import type { CellRenderProps } from '../../data-view/column/index.js';
 import { createFromBaseCellRenderer } from '../../data-view/column/renderer.js';
 import { map } from '../../data-view/utils/uni-component/operation.js';
 import { createIcon } from '../../data-view/utils/uni-icon.js';
+import { DataViewTableManager } from '../../data-view/view/presets/table/table-view-manager.js';
 import { titlePureColumnConfig } from './define.js';
 import { HeaderAreaTextCell, HeaderAreaTextCellEditing } from './text.js';
 
@@ -10,11 +11,17 @@ export const titleColumnConfig = titlePureColumnConfig.renderConfig({
   cellRenderer: {
     view: map(
       createFromBaseCellRenderer(HeaderAreaTextCell),
-      (props: CellRenderProps) => ({ ...props, showIcon: true })
+      (props: CellRenderProps) => ({
+        ...props,
+        showIcon: props.view instanceof DataViewTableManager,
+      })
     ),
     edit: map(
       createFromBaseCellRenderer(HeaderAreaTextCellEditing),
-      (props: CellRenderProps) => ({ ...props, showIcon: true })
+      (props: CellRenderProps) => ({
+        ...props,
+        showIcon: props.view instanceof DataViewTableManager,
+      })
     ),
   },
 });
