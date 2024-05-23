74fde29098a9a5ae47587a38c181431f4895db23
golok727 <123532141+golok727@users.noreply.github.com>
2024-05-23 08:26:06 +0000

    fix(database): number cell alignment (#7121)
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/cHyh9ImFn5gccR3uNC1w/c94bec92-7351-4838-aabb-4d80f759524b.png)

--- packages/blocks/src/database-block/data-view/common/detail/field.ts
+++ packages/blocks/src/database-block/data-view/common/detail/field.ts
@@ -31,11 +31,6 @@ export class RecordField extends WithDisposable(ShadowlessElement) {
       gap: 12px;
     }
 
-    .affine-database-number {
-      text-align: left;
-      justify-content: start;
-    }
-
     .field-left {
       padding: 4px 6px 4px 4px;
       display: flex;
@@ -82,6 +77,11 @@ export class RecordField extends WithDisposable(ShadowlessElement) {
       display: flex;
       align-items: center;
       border: 1px solid transparent;
+    }
+
+    .field-content .affine-database-number {
+      text-align: left;
+      justify-content: start;
     }
 
     .field-content:hover {
