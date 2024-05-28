be60b716d5c0ec4975a048bdf58365e07fd741ac
Saul-Mirone <Saul-Mirone@outlook.com>
2024-05-28 06:21:22 +0000

    feat: add quick search service to insert link universally (#7154)

--- packages/blocks/src/root-block/root-service.ts
+++ packages/blocks/src/root-block/root-service.ts
@@ -1,13 +1,18 @@
 import type { BlockElement, EditorHost } from '@blocksuite/block-std';
 import { BlockService } from '@blocksuite/block-std';
 import { assertExists } from '@blocksuite/global/utils';
+import type { BlockModel } from '@blocksuite/store';
 
 import {
   FileDropManager,
   type FileDropOptions,
 } from '../_common/components/file-drop-manager.js';
 import type { NotificationService } from '../_common/components/notification-service.js';
-import { DEFAULT_IMAGE_PROXY_ENDPOINT } from '../_common/consts.js';
+import {
+  DEFAULT_IMAGE_PROXY_ENDPOINT,
+  EMBED_CARD_HEIGHT,
+  EMBED_CARD_WIDTH,
+} from '../_common/consts.js';
 import { ExportManager } from '../_common/export-manager/export-manager.js';
 import {
   HtmlTransformer,
@@ -15,10 +20,12 @@ import {
   ZipTransformer,
 } from '../_common/transformers/index.js';
 import { type EmbedCardStyle, NoteDisplayMode } from '../_common/types.js';
+import { getRootByEditorHost } from '../_common/utils/index.js';
 import { matchFlavours } from '../_common/utils/model.js';
 import { asyncFocusRichText } from '../_common/utils/selection.js';
 import type { NoteBlockModel } from '../note-block/note-model.js';
 import { CommunityCanvasTextFonts } from '../surface-block/consts.js';
+import { Bound, Vec } from '../surface-block/index.js';
 import { EditPropsStore } from '../surface-block/managers/edit-session.js';
 import {
   copySelectedModelsCommand,
@@ -36,6 +43,8 @@ import {
   getSelectedModelsCommand,
   getTextSelectionCommand,
 } from './commands/index.js';
+import type { EdgelessRootBlockComponent } from './edgeless/edgeless-root-block.js';
+import type { EdgelessElementType } from './edgeless/edgeless-types.js';
 import { FontLoader } from './font-loader/font-loader.js';
 import type { RootBlockModel } from './root-model.js';
 import type { RootBlockComponent } from './types.js';
@@ -46,6 +55,15 @@ export type EmbedOptions = {
   styles: EmbedCardStyle[];
   viewType: 'card' | 'embed';
 };
+
+export type QuickSearchResult =
+  | { docId: string }
+  | { userInput: string }
+  | null;
+
+export interface QuickSearchService {
+  searchDoc: (options: { action: 'insert' }) => Promise<QuickSearchResult>;
+}
 
 export class RootService extends BlockService<RootBlockModel> {
   readonly fontLoader = new FontLoader();
@@ -81,16 +99,10 @@ export class RootService extends BlockService<RootBlockModel> {
     return viewportElement;
   }
 
-  private _getEditorMode: (docId: string) => 'page' | 'edgeless' = docId =>
+  accessor quickSearchService: QuickSearchService | null = null;
+
+  accessor getEditorMode: (docId: string) => 'page' | 'edgeless' = docId =>
     docId.endsWith('edgeless') ? 'edgeless' : 'page';
-
-  get getEditorMode() {
-    return this._getEditorMode;
-  }
-
-  set getEditorMode(value) {
-    this._getEditorMode = value;
-  }
 
   private _getDocUpdatedAt: (docId: string) => Date = () => new Date();
 
@@ -228,5 +240,147 @@ export class RootService extends BlockService<RootBlockModel> {
       index: text.length,
       length: 0,
     })?.catch(console.error);
+  };
+
+  private _getMode = () => {
+    const rootId = this.doc.root?.id;
+    if (!rootId) return 'page';
+
+    const root = this.std.view.getBlock(rootId);
+    if (!root) return 'page';
+
+    return root.tagName === 'AFFINE-EDGELESS-ROOT' ? 'edgeless' : 'page';
+  };
+
+  private _getParentModelBySelection = (): {
+    index: number | undefined;
+    model: BlockModel | null;
+  } => {
+    const currentMode = this._getMode();
+    const root = this.doc.root;
+    if (!root)
+      return {
+        index: undefined,
+        model: null,
+      };
+
+    if (currentMode === 'edgeless') {
+      const surface =
+        root.children.find(child => child.flavour === 'affine:surface') ?? null;
+      return { index: undefined, model: surface };
+    }
+
+    if (currentMode === 'page') {
+      let selectedBlock: BlockModel | null = this.selectedBlocks[0]?.model;
+      let index: undefined | number = undefined;
+
+      if (!selectedBlock) {
+        // if no block is selected, append to the last note block
+        selectedBlock = this._getLastNoteBlock();
+      }
+
+      while (selectedBlock && selectedBlock.flavour !== 'affine:note') {
+        // selectedBlock = this.doc.getParent(selectedBlock.id);
+        const parent = this.doc.getParent(selectedBlock.id);
+        index = parent?.children.indexOf(selectedBlock);
+        selectedBlock = parent;
+      }
+
+      return { index, model: selectedBlock };
+    }
+
+    return {
+      index: undefined,
+      model: null,
+    };
+  };
+
+  private _insertCard = (
+    flavour: string,
+    targetStyle: EmbedCardStyle,
+    props: Record<string, unknown>
+  ) => {
+    const host = this.host as EditorHost;
+
+    const mode = this._getMode();
+    const { model, index } = this._getParentModelBySelection();
+
+    if (mode === 'page') {
+      host.doc.addBlock(flavour as never, props, model, index);
+      return;
+    }
+    if (mode === 'edgeless') {
+      const edgelessRoot = getRootByEditorHost(
+        host
+      ) as EdgelessRootBlockComponent | null;
+      if (!edgelessRoot) return;
+
+      const surface = edgelessRoot.surface;
+      const center = Vec.toVec(surface.renderer.center);
+      edgelessRoot.service.addBlock(
+        flavour as EdgelessElementType,
+        {
+          ...props,
+          xywh: Bound.fromCenter(
+            center,
+            EMBED_CARD_WIDTH[targetStyle],
+            EMBED_CARD_HEIGHT[targetStyle]
+          ).serialize(),
+          style: targetStyle,
+        },
+        surface.model
+      );
+
+      edgelessRoot.tools.setEdgelessTool({
+        type: 'default',
+      });
+      return;
+    }
+  };
+
+  private _insertLink = (url: string) => {
+    const host = this.host as EditorHost;
+    const rootService = host.spec.getService('affine:page');
+
+    const embedOptions = rootService.getEmbedBlockOptions(url);
+
+    let flavour = 'affine:bookmark';
+    let targetStyle: EmbedCardStyle = 'vertical';
+    const props: Record<string, unknown> = { url };
+    if (embedOptions) {
+      flavour = embedOptions.flavour;
+      targetStyle = embedOptions.styles[0];
+    }
+
+    this._insertCard(flavour, targetStyle, props);
+  };
+
+  private _insertDoc = (docId: string) => {
+    const flavour = 'affine:embed-synced-doc';
+    const targetStyle: EmbedCardStyle = 'vertical';
+    const props: Record<string, unknown> = { pageId: docId };
+
+    this._insertCard(flavour, targetStyle, props);
+  };
+
+  insertLinkByQuickSearch = async () => {
+    if (!this.quickSearchService) return;
+
+    const result = await this.quickSearchService.searchDoc({
+      action: 'insert',
+    });
+    if (!result) return;
+
+    // add linked doc
+    if ('docId' in result) {
+      this._insertDoc(result.docId);
+      return;
+    }
+
+    // add normal link;
+    if ('userInput' in result) {
+      this._insertLink(result.userInput);
+      return;
+    }
   };
 }
