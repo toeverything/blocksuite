687d000d4e793a9a356ab60b2a69363748792e3a
3720 <zuozijian1994@gmail.com>
2024-06-17 12:32:51 +0800

    feat(database): link column supports displaying the title of internal doc link (#7328)

--- packages/blocks/src/database-block/data-view/column/presets/link/cell-renderer.ts
+++ packages/blocks/src/database-block/data-view/column/presets/link/cell-renderer.ts
@@ -1,12 +1,15 @@
 import './components/link-node.js';
 
+import { assertExists } from '@blocksuite/global/utils';
 import { baseTheme } from '@toeverything/theme';
 import { css, unsafeCSS } from 'lit';
-import { customElement, query } from 'lit/decorators.js';
+import { customElement, query, state } from 'lit/decorators.js';
 import { html } from 'lit/static-html.js';
 
 import { PenIcon } from '../../../../../_common/icons/index.js';
 import { isValidUrl, normalizeUrl } from '../../../../../_common/utils/url.js';
+import type { RootBlockComponent } from '../../../../../root-block/index.js';
+import { HostContextKey } from '../../../../context/host-context.js';
 import { stopPropagation } from '../../../utils/event.js';
 import { createIcon } from '../../../utils/uni-icon.js';
 import { BaseCellRenderer } from '../../base-cell.js';
@@ -19,7 +22,6 @@ export class LinkCell extends BaseCellRenderer<string> {
     affine-database-link-cell {
       width: 100%;
       user-select: none;
-      cursor: pointer;
     }
 
     affine-database-link-cell:hover .affine-database-link-icon {
@@ -63,7 +65,18 @@ export class LinkCell extends BaseCellRenderer<string> {
       height: 16px;
       fill: var(--affine-icon-color);
     }
+    .data-view-link-column-linked-doc {
+      text-decoration: underline;
+      text-decoration-color: var(--affine-divider-color);
+      transition: text-decoration-color 0.2s ease-out;
+      cursor: pointer;
+    }
+    .data-view-link-column-linked-doc:hover {
+      text-decoration-color: var(--affine-icon-color);
+    }
   `;
+
+  private preValue?: string;
 
   private _onClick = (event: Event) => {
     event.stopPropagation();
@@ -90,14 +103,74 @@ export class LinkCell extends BaseCellRenderer<string> {
     this.selectCurrentCell(true);
   };
 
+  get std() {
+    const host = this.view.getContext(HostContextKey);
+    return host?.std;
+  }
+
+  override updated() {
+    if (this.value !== this.preValue) {
+      const std = this.std;
+      this.preValue = this.value;
+      if (!this.value) {
+        this.docId = undefined;
+        return;
+      }
+      const result = std?.spec
+        .getService('affine:page')
+        .quickSearchService?.searchDoc({
+          userInput: this.value,
+        });
+      result
+        ?.then(res => {
+          if (res && 'docId' in res) {
+            this.docId = res.docId;
+            return;
+          }
+          this.docId = undefined;
+        })
+        .catch(() => {
+          this.docId = undefined;
+        });
+    }
+  }
+
+  openDoc = (e: MouseEvent) => {
+    e.stopPropagation();
+    if (!this.docId) {
+      return;
+    }
+    const std = this.std;
+    const rootId = std?.doc.root?.id;
+    if (!rootId) {
+      return;
+    }
+    const rootElement = std?.view.viewFromPath('block', [
+      rootId,
+    ]) as RootBlockComponent | null;
+    assertExists(rootElement);
+
+    rootElement.slots.docLinkClicked.emit({ docId: this.docId });
+  };
+
+  @state()
+  accessor docId: string | undefined = undefined;
+
   override render() {
     const linkText = this.value ?? '';
-
+    const docName =
+      this.docId && this.std?.collection.getDoc(this.docId)?.meta?.title;
     return html`
       <div class="affine-database-link" @click="${this._onClick}">
-        <affine-database-link-node
-          .link="${linkText}"
-        ></affine-database-link-node>
+        ${docName
+          ? html`<span
+              class="data-view-link-column-linked-doc"
+              @click="${this.openDoc}"
+              >${docName}</span
+            >`
+          : html`<affine-database-link-node
+              .link="${linkText}"
+            ></affine-database-link-node>`}
         <div class="affine-database-link-icon" @click="${this._onEdit}">
           ${PenIcon}
         </div>
