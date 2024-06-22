884933c6e4221082724897b5ace869a0a29c1a95
EYHN <cneyhn@gmail.com>
2024-06-22 23:11:40 +0800

    fix(blocks): fix web worker compatibility

--- packages/playground/apps/starter/utils/test-worker.ts
+++ packages/playground/apps/starter/utils/test-worker.ts
@@ -6,10 +6,7 @@ import '@blocksuite/store';
 // import '@blocksuite/block-std'; // seems not working
 import '@blocksuite/blocks/schemas';
 
-console.log('Worker is running');
-
 globalThis.onmessage = event => {
-  console.log('Worker received message:', event.data);
   const { data } = event;
   if (data === 'ping') {
     postMessage('pong');
