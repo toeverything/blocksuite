0016382526e952a7a0470e44f53f7b6936ce6218
Flrande <50035259+Flrande@users.noreply.github.com>
2024-06-24 13:43:18 +0800

    fix: add meta init function (#7395)

--- packages/blocks/src/__tests__/utils/create-job.ts
+++ packages/blocks/src/__tests__/utils/create-job.ts
@@ -2,6 +2,7 @@ import { DocCollection, Job, Schema } from '@blocksuite/store';
 
 export function createJob() {
   const schema = new Schema();
-  const docCollection = new DocCollection({ schema });
-  return new Job({ collection: docCollection });
+  const collection = new DocCollection({ schema });
+  collection.meta.initialize();
+  return new Job({ collection });
 }
