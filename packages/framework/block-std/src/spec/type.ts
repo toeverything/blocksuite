8c80cf5054a217c0bbac9b1b2216f5a7fee9cb84
Mirone <Saul-Mirone@outlook.com>
2024-07-19 11:48:48 +0800

    fix: type

--- packages/framework/block-std/src/spec/type.ts
+++ packages/framework/block-std/src/spec/type.ts
@@ -1,13 +1,14 @@
 import type { DisposableGroup } from '@blocksuite/global/utils';
 import type { BlockSchemaType } from '@blocksuite/store';
+import type { StaticValue } from 'lit/static-html.js';
 
 import type { BlockService } from '../service/index.js';
 import type { BlockServiceConstructor } from '../service/index.js';
 import type { BlockSpecSlots } from './slots.js';
 
 export interface BlockView<WidgetNames extends string = string> {
-  component: BlockSuite.Component;
-  widgets?: Record<WidgetNames, BlockSuite.Component>;
+  component: StaticValue;
+  widgets?: Record<WidgetNames, StaticValue>;
 }
 
 export interface BlockSpec<
