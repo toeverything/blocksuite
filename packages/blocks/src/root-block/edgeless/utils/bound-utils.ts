b7c9e04d1c03eea798e725df9d314f13edb587d7
fourdim <59462000+fourdim@users.noreply.github.com>
2024-07-25 11:33:18 +0000

    fix(edgeless): discard group like model from calculating bound (#7718)
    This closes [BS-824](https://linear.app/affine-design/issue/BS-824).

--- packages/blocks/src/root-block/edgeless/utils/bound-utils.ts
+++ packages/blocks/src/root-block/edgeless/utils/bound-utils.ts
@@ -1,8 +1,13 @@
 import { Bound } from '@blocksuite/global/utils';
 
+import { SurfaceGroupLikeModel } from '../../../surface-block/element-model/base.js';
+
 export function edgelessElementsBound(elements: BlockSuite.EdgelessModel[]) {
   if (elements.length === 0) return new Bound();
   return elements.reduce((prev, element) => {
+    if (element instanceof SurfaceGroupLikeModel) {
+      return prev;
+    }
     return prev.unite(element.elementBound);
   }, elements[0].elementBound);
 }
