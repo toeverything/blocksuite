e715089029ec07694d33fd184a74762b340c5c6a
Mirone <Saul-Mirone@outlook.com>
2024-08-01 12:24:34 +0800

    fix: playground drag area

--- tests/image.spec.ts
+++ tests/image.spec.ts
@@ -222,7 +222,7 @@ test('popup menu should follow position of image when scrolling', async ({
   const menuRect = await menu.boundingBox();
   if (!imageRect) throw new Error('image not found');
   if (!menuRect) throw new Error('menu not found');
-  expect(imageRect.y).toBeCloseTo(-188, 172);
+  expect(imageRect.y).toBeCloseTo(-202, 172);
   expect(menuRect.y).toBeCloseTo(65, -0.325);
 });
 
