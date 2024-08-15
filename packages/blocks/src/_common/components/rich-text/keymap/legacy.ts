26cc76ad5171252a3698afb34fe3245bba987342
Mirone <Saul-Mirone@outlook.com>
2024-08-15 16:00:04 +0800

    refactor: remove legacy backspace

--- packages/blocks/src/_common/components/rich-text/keymap/legacy.ts
+++ packages/blocks/src/_common/components/rich-text/keymap/legacy.ts
@@ -13,13 +13,7 @@ import {
   handleBlockEndEnter,
   handleBlockSplit,
   handleLineEndForwardDelete,
-  handleLineStartBackspace,
 } from '../rich-text-operations.js';
-
-function isCollapsedAtBlockStart(inlineEditor: AffineInlineEditor) {
-  const inlineRange = inlineEditor.getInlineRange();
-  return inlineRange?.index === 0 && inlineRange?.length === 0;
-}
 
 function isCollapsedAtBlockEnd(inlineEditor: AffineInlineEditor) {
   const inlineRange = inlineEditor.getInlineRange();
@@ -45,21 +39,6 @@ export function hardEnter(
   }
   handleBlockSplit(editorHost, model, range.index, range.length);
   return KEYBOARD_PREVENT_DEFAULT;
-}
-
-export function onBackspace(
-  editorHost: EditorHost,
-  model: BlockModel,
-  e: KeyboardEvent,
-  inlineEditor: AffineInlineEditor
-) {
-  if (isCollapsedAtBlockStart(inlineEditor)) {
-    e.stopPropagation();
-    handleLineStartBackspace(editorHost, model);
-    return KEYBOARD_PREVENT_DEFAULT;
-  }
-  e.stopPropagation();
-  return KEYBOARD_ALLOW_DEFAULT;
 }
 
 export function onForwardDelete(
