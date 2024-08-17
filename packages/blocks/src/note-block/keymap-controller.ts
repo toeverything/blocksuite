796678c476d159466b6c5ba727ed56ebc9e3df64
Mirone <Saul-Mirone@outlook.com>
2024-08-17 20:38:47 +0800

    chore: fix

--- packages/blocks/src/note-block/keymap-controller.ts
+++ packages/blocks/src/note-block/keymap-controller.ts
@@ -447,7 +447,6 @@ export class KeymapController implements ReactiveController {
         .filter<BaseSelection>(sel => !sel.is('block'))
         .concat(blocks);
     });
-    return true;
   };
 
   private _onShiftArrowDown = () => {
