7cc5389dff1956d681b9e94c9d4b113f56dcff46
akumatus <akumatus@gmail.com>
2024-08-28 08:37:04 +0000

    fix: make spec config optional (#8064)

--- packages/blocks/src/code-block/code-block-spec.ts
+++ packages/blocks/src/code-block/code-block-spec.ts
@@ -2,15 +2,10 @@ import type { BlockSpec } from '@blocksuite/block-std';
 
 import { CodeBlockSchema } from '@blocksuite/affine-model';
 import { literal } from 'lit/static-html.js';
-import { bundledLanguagesInfo } from 'shiki';
 
 import type { CodeBlockConfig } from './code-block-config.js';
 
 import { CodeBlockService } from './code-block-service.js';
-import {
-  CODE_BLOCK_DEFAULT_DARK_THEME,
-  CODE_BLOCK_DEFAULT_LIGHT_THEME,
-} from './highlight/const.js';
 
 export const CodeBlockSpec: BlockSpec<
   'codeToolbar' | 'codeLangList',
@@ -26,11 +21,4 @@ export const CodeBlockSpec: BlockSpec<
     },
   },
   service: CodeBlockService,
-  config: {
-    theme: {
-      dark: CODE_BLOCK_DEFAULT_DARK_THEME,
-      light: CODE_BLOCK_DEFAULT_LIGHT_THEME,
-    },
-    langs: bundledLanguagesInfo,
-  },
 };
