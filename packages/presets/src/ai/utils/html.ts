5e5ba838e01db40115356903071659eeaaa7d776
Yifeng Wang <doodlewind@toeverything.info>
2024-08-29 19:41:36 +0800

    chore(presets): remove legacy components

--- packages/presets/src/ai/utils/html.ts
+++
@@ -1,5 +0,0 @@
-export function preprocessHtml(answer: string) {
-  const start = answer.indexOf('<!DOCTYPE html>');
-  const end = answer.indexOf('</html>');
-  return answer.slice(start, end + '</html>'.length);
-}
