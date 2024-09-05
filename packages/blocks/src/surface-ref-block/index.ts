f209c086beb0bba2063e7fe2f2cae432fcc945bb
donteatfriedrice <chs@bupt.cn>
2024-09-05 04:38:23 +0000

    feat: export surface ref spec (#8176)

--- packages/blocks/src/surface-ref-block/index.ts
+++ packages/blocks/src/surface-ref-block/index.ts
@@ -3,6 +3,7 @@ import type { SurfaceRefBlockService } from './surface-ref-service.js';
 export * from './surface-ref-block.js';
 export * from './surface-ref-block-edgeless.js';
 export * from './surface-ref-service.js';
+export * from './surface-ref-spec.js';
 
 declare global {
   namespace BlockSuite {
