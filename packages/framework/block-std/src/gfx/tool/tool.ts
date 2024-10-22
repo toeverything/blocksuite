27561d0d6c9ad3a91aa06289f7f8a22359146b9a
doouding <github@douding.io>
2024-10-22 03:24:31 +0000

    refactor: edgeless tool manager (#8438)
    The `EdgelessToolManager` has been renamed to `GfxToolController`. All the legacy tools will be rewrited using the new infra.
    
    #### How to add a new tool
    Let's take an example of pan tool.
    
    ```typescript
    import { BaseTool } from '@blocksuite/block-std/gfx';
    
    // extend the BaseTool
    export class PanTool extends BaseTool {
      // every tool class should have static toolName
      static override toolName = 'pan';
    
      dragStart(e) {
        this._lastPoint = [e.x, e.y];
      }
    
      dragEnd(e) {
        this._lastPoint = null;
      }
    
      dragMove(e) {
        if (!this._lastPoint) return;
    
        const { viewport } = this._service;
        const { zoom } = viewport;
    
        const [lastX, lastY] = this._lastPoint;
        const deltaX = lastX - e.x;
        const deltaY = lastY - e.y;
    
        this._lastPoint = [e.x, e.y];
    
        this.gfx.viewport.applyDeltaCenter(deltaX / zoom, deltaY / zoom);
      }
    }
    ```
    
    Once you finish defining the tool, just append it to the spec array.
    
    ```typescript
    // edgeless-spec.ts file
    import { PanTool } from '../tools/pan-tool.ts';
    
    export const EdgelessSpec = [
       // .... omit the other part of spec
    
       // append it to the array
       PanTool
    ]
    ```
    
    Continue in https://github.com/toeverything/blocksuite/pull/8471

---
+++ packages/framework/block-std/src/gfx/tool/tool.ts
@@ -0,0 +1,98 @@
+import { type Container, createIdentifier } from '@blocksuite/global/di';
+import { BlockSuiteError, ErrorCode } from '@blocksuite/global/exceptions';
+
+import type { UIEventStateContext } from '../../event/base.js';
+import type { ExtensionType } from '../../extension/extension.js';
+import type { GfxController } from '../controller.js';
+
+import { GfxControllerIdentifier } from '../identifiers.js';
+import { eventTarget, type SupportedEvents } from './tool-controller.js';
+
+export abstract class BaseTool {
+  static toolName: string = '';
+
+  get active() {
+    return this.gfx.tool.currentTool$.peek() === this;
+  }
+
+  get toolName() {
+    return (this.constructor as typeof BaseTool).toolName;
+  }
+
+  constructor(readonly gfx: GfxController) {}
+
+  /**
+   * Called when the tool is activated.
+   * @param option - The data passed as second argument when calling `ToolController.use`.
+   */
+  activate(_: Record<string, unknown>): void {}
+
+  addHook(
+    evtName: SupportedEvents,
+    handler: (evtState: UIEventStateContext) => undefined | boolean
+  ): void {
+    this.gfx.tool[eventTarget].addHook(evtName, handler);
+  }
+
+  click(_: UIEventStateContext): void {}
+
+  contextMenu(_: UIEventStateContext): void {}
+
+  /**
+   * Called when the tool is deactivated.
+   */
+  deactivate(): void {}
+
+  doubleClick(_: UIEventStateContext): void {}
+
+  dragEnd(_: UIEventStateContext): void {}
+
+  dragMove(_: UIEventStateContext): void {}
+
+  dragStart(_: UIEventStateContext): void {}
+
+  /**
+   * Called when the tool is registered.
+   */
+  mounted(): void {}
+
+  pointerDown(_: UIEventStateContext): void {}
+
+  pointerMove(_: UIEventStateContext): void {}
+
+  pointerOut(_: UIEventStateContext): void {}
+
+  pointerUp(_: UIEventStateContext): void {}
+
+  tripleClick(_: UIEventStateContext): void {}
+
+  /**
+   * Called when the tool is unloaded, usually when the whole `ToolController` is destroyed.
+   */
+  unmounted(): void {}
+}
+
+export const ToolIdentifier = createIdentifier<BaseTool>('GfxTool');
+
+export function GfxToolExtension(
+  toolCtors: (typeof BaseTool)[]
+): ExtensionType {
+  return {
+    setup: (di: Container) => {
+      toolCtors.forEach(Ctor => {
+        if (!Ctor.toolName) {
+          throw new BlockSuiteError(
+            ErrorCode.ValueNotExists,
+            'The tool must have a static property `toolName`'
+          );
+        }
+
+        di.addImpl(ToolIdentifier(Ctor.toolName), Ctor, [
+          GfxControllerIdentifier,
+        ]);
+      });
+    },
+  };
+}
+
+export interface GfxToolsMap {}
