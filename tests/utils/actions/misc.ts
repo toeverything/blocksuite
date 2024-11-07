15d991a5bf3b869b815516594411848a1d3d05e4
Flrande <flrande.ch@gmail.com>
2024-11-07 16:34:31 +0800

    fix: test

--- tests/utils/actions/misc.ts
+++ tests/utils/actions/misc.ts
@@ -1253,14 +1253,6 @@ export function inlineEditorInnerTextToString(innerText: string): string {
 }
 
 export async function focusTitle(page: Page) {
-  // click to ensure editor is active
-  await page.mouse.move(0, 0);
-  const editor = getEditorHostLocator(page);
-  const locator = editor.locator('affine-page-root').first();
-  // need to set `force` to true when clicking on `affine-selected-blocks`
-  await locator.click({ force: true });
-  // avoid trigger double click
-  await page.waitForTimeout(500);
   await page.evaluate(i => {
     const docTitle = document.querySelectorAll('doc-title')[i];
     if (!docTitle) {
