91a9175519d4b95cb134ead82e7342991f0b5995
caojiafu@cvte.com <caojiafu@cvte.com>
2024-11-15 18:13:33 +0800

    fix(blocks): cell-block remove the selectBlock

--- packages/blocks/src/cell-block/cell-service.ts
+++ packages/blocks/src/cell-block/cell-service.ts
@@ -1,5 +1,5 @@
 import { CellBlockSchema } from '@blocksuite/affine-model';
-import { BlockService, type Command } from '@blocksuite/block-std';
+import { BlockService } from '@blocksuite/block-std';
 
 export class CellBlockService extends BlockService {
   static override readonly flavour = CellBlockSchema.model.flavour;
@@ -7,18 +7,18 @@ export class CellBlockService extends BlockService {
   override mounted(): void {
     super.mounted();
 
-    this.std.command.add('selectBlock', selectBlock);
+    // this.std.command.add('selectBlock', selectBlock);
   }
 }
 
-export const selectBlock: Command<'focusBlock'> = (ctx, next) => {
-  const { focusBlock } = ctx;
-  if (!focusBlock) {
-    return;
-  }
+// export const selectBlock: Command<'focusBlock'> = (ctx, next) => {
+//   const { focusBlock } = ctx;
+//   if (!focusBlock) {
+//     return;
+//   }
 
-  return next();
-};
+//   return next();
+// };
 
 declare global {
   namespace BlockSuite {
