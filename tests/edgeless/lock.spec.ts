1c6b1d488a0721ad045fa59add59b1af9bf0e1ea
L-Sun <zover.v@gmail.com>
2024-12-05 16:24:36 +0800

    test(edgeless): init edgeless element lock feature tests

---
+++ tests/edgeless/lock.spec.ts
@@ -0,0 +1,94 @@
+import { edgelessCommonSetup } from 'utils/actions/edgeless.js';
+
+import { test } from '../utils/playwright.js';
+
+test.describe('lock', () => {
+  test('edgeless element can be locked and unlocked', async ({ page }) => {
+    await edgelessCommonSetup(page);
+  });
+
+  test('locked element should be selectable', async ({ page }) => {
+    await edgelessCommonSetup(page);
+  });
+
+  test('descendant of locked element should not be selectable. unlocking will recover', async ({
+    page,
+  }) => {
+    await edgelessCommonSetup(page);
+  });
+
+  test('the selected rect of locked element should contains descendant. unlocking will recover', async ({
+    page,
+  }) => {
+    await edgelessCommonSetup(page);
+    // frame
+    // mindmap
+  });
+
+  test('locked element should be copyable.', async ({ page }) => {
+    await edgelessCommonSetup(page);
+  });
+
+  test('locked element and descendant should not be draggable and moved by arrow key. unlocking will recover', async ({
+    page,
+  }) => {
+    await edgelessCommonSetup(page);
+  });
+
+  test('locked element should be moved if parent is moved', async ({
+    page,
+  }) => {
+    await edgelessCommonSetup(page);
+  });
+
+  test('locked element should not be scalable and rotatable. unlocking will recover', async ({
+    page,
+  }) => {
+    await edgelessCommonSetup(page);
+  });
+
+  test('locked element should not be editable. unlocking will recover', async ({
+    page,
+  }) => {
+    await edgelessCommonSetup(page);
+
+    // using `Tab` navigate into note to edit
+  });
+
+  test('locked element should not be deletable. unlocking will recover', async ({
+    page,
+  }) => {
+    await edgelessCommonSetup(page);
+
+    // short cut
+    // erase
+  });
+
+  test('locked frame should not add new child element. unlocking will recover', async ({
+    page,
+  }) => {
+    await edgelessCommonSetup(page);
+  });
+
+  test('endpoint of locked connector should not be changeable. unlocking will recover', async ({
+    page,
+  }) => {
+    await edgelessCommonSetup(page);
+  });
+
+  test('locking multiple elements will create locked group', async ({
+    page,
+  }) => {
+    await edgelessCommonSetup(page);
+  });
+
+  test('locking a group should not create a new group', async ({ page }) => {
+    await edgelessCommonSetup(page);
+  });
+
+  test('unlocking a element should not unlock its locked descendant', async ({
+    page,
+  }) => {
+    await edgelessCommonSetup(page);
+  });
+});
