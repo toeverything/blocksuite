b9a9b9d5d087a830af9347afd7c26ac3d318d25d
L-Sun <zover.v@gmail.com>
2024-12-16 10:14:07 +0000

    fix(blocks): the width of the card view is not fully filled (#8988)
    Close [BS-1777](https://linear.app/affine-design/issue/BS-1777/card在切换为full-width后没有responsively适配)

--- packages/affine/block-embed/src/embed-linked-doc-block/styles.ts
+++ packages/affine/block-embed/src/embed-linked-doc-block/styles.ts
@@ -185,7 +185,7 @@ export const styles = css`
   }
 
   .affine-embed-linked-doc-block:not(.in-canvas) {
-    max-width: 100%;
+    width: 100%;
     min-width: calc(min(${EMBED_CARD_MIN_WIDTH}px, 100%));
   }
 
