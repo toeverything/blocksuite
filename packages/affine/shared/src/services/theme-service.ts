4d1534cc123857b8a9dd6bb551962aab8366fd1b
L-Sun <zover.v@gmail.com>
2024-12-19 08:19:44 +0000

    fix(blocks): remove lit dependency from theme service (#9025)
    continue #9023

--- packages/affine/shared/src/services/theme-service.ts
+++ packages/affine/shared/src/services/theme-service.ts
@@ -12,7 +12,6 @@ import {
   combinedDarkCssVariables,
   combinedLightCssVariables,
 } from '@toeverything/theme';
-import { unsafeCSS } from 'lit';
 
 import { isInsideEdgelessEditor } from '../utils/index.js';
 
@@ -206,26 +205,3 @@ export const getThemeObserver = (function () {
     return observer;
   };
 })();
-
-const toolbarColorKeys: Array<keyof AffineCssVariables> = [
-  '--affine-background-overlay-panel-color',
-  '--affine-v2-layer-background-overlayPanel' as never,
-  '--affine-background-error-color',
-  '--affine-background-primary-color',
-  '--affine-background-tertiary-color',
-  '--affine-icon-color',
-  '--affine-icon-secondary',
-  '--affine-border-color',
-  '--affine-divider-color',
-  '--affine-text-primary-color',
-  '--affine-hover-color',
-  '--affine-hover-color-filled',
-];
-
-export const lightToolbarStyles = toolbarColorKeys.map(
-  key => `${key}: ${unsafeCSS(combinedLightCssVariables[key])};`
-);
-
-export const darkToolbarStyles = toolbarColorKeys.map(
-  key => `${key}: ${unsafeCSS(combinedDarkCssVariables[key])};`
-);
